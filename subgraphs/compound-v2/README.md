f665c1ee3b4134d430e06167d1764f3ce84d0d2f
Dylan Melotik <dmelotik@wisc.edu>
2022-03-21 22:34:50 -0500

    working on deposit entity to start

---
+++ subgraphs/compound-v2/README.md
@@ -0,0 +1,23 @@
+# Compound Subgraph
+
+
+## Notes
+
++ Each CToken is a different "Market" > need to import all of those
++ Figure out how to spin up locally
++ 
+
+
+## Links
+
+Protocol: https://compound.finance/
+
+Docs: https://compound.finance/docs
+
+Smart contracts: https://github.com/compound-finance/compound-protocol
+
+Deployed addresses: https://compound.finance/docs#networks
+
+Existing subgraphs: https://github.com/compound-finance/compound-v2-subgraph
+
+Subgraph in the studio: 
---
+++ subgraphs/compound-v2/src/mappings/cToken.ts
@@ -0,0 +1,62 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  AccrueInterest,
+  Mint,
+  Redeem,
+  Borrow,
+  RepayBorrow,
+  LiquidateBorrow,
+  NewPendingAdmin,
+  NewAdmin,
+  NewComptroller,
+  NewMarketInterestRateModel,
+  NewReserveFactor,
+  ReservesReduced,
+  Failure,
+  Transfer,
+  Approval
+} from "../types/cAAVE/cToken"
+import { Deposit, Token } from "../types/schema"
+
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+
+export function handleMint(event: Mint): void {}
+
+export function handleRedeem(event: Redeem): void {}
+
+export function handleBorrow(event: Borrow): void {}
+
+export function handleRepayBorrow(event: RepayBorrow): void {}
+
+export function handleLiquidateBorrow(event: LiquidateBorrow): void {}
+
+export function handleNewPendingAdmin(event: NewPendingAdmin): void {}
+
+export function handleNewAdmin(event: NewAdmin): void {}
+
+export function handleNewComptroller(event: NewComptroller): void {}
+
+export function handleNewMarketInterestRateModel(
+  event: NewMarketInterestRateModel
+): void {}
+
+export function handleNewReserveFactor(event: NewReserveFactor): void {}
+
+export function handleReservesReduced(event: ReservesReduced): void {}
+
+export function handleFailure(event: Failure): void {}
+
+export function handleTransfer(event: Transfer): void {
+  let deposit = new Deposit((event.transaction.hash.toI32() - event.logIndex.toI32()).toString())
+  deposit.from = event.params.from.toHex()
+
+  deposit.save()
+}
+
+export function handleApproval(event: Approval): void {}
