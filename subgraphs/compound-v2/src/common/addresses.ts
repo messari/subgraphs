4ef20473754f79e37b3024f7556f73012612ece8
Dylan Melotik <dmelotik@wisc.edu>
2022-03-22 16:37:36 -0500

    added to address consts and update manifest

---
+++ subgraphs/compound-v2/src/common/addresses.ts
@@ -0,0 +1,34 @@
+// store common addresses
+
+import { Address } from "@graphprotocol/graph-ts";
+
+// null address
+export const ADDRESS_ZERO = Address.fromString("0x0000000000000000000000000000000000000000")
+
+// "factory" address (Comptroller.sol)
+export const COMPTROLLER_ADDRESS = Address.fromString("0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b")
+
+// COMP address
+// TODO: might not be necessary
+export const COMP_ADDRESS = Address.fromString("0xc00e94cb662c3520282e6f5717214004a7f26888")
+
+// cToken addresses
+export const CAAVE_ADDRESS  =   Address.fromString("0xe65cdb6479bac1e22340e4e755fae7e509ecd06c")
+export const CBAT_ADDRESS   =   Address.fromString("0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e")
+export const CCOMP_ADDRESS  =   Address.fromString("0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4")
+export const CDAI_ADDRESS   =   Address.fromString("0x5d3a536e4d6dbd6114cc1ead35777bab948e3643")
+export const CETH_ADDRESS   =   Address.fromString("0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5")
+export const CFEI_ADDRESS   =   Address.fromString("0x7713dd9ca933848f6819f38b8352d9a15ea73f67")
+export const CLINK_ADDRESS  =   Address.fromString("0xface851a4921ce59e912d19329929ce6da6eb0c7")
+export const CMKR_ADDRESS   =   Address.fromString("0x95b4ef2869ebd94beb4eee400a99824bf5dc325b")
+export const CREP_ADDRESS   =   Address.fromString("0x158079ee67fce2f58472a96584a73c7ab9ac95c1")
+export const CSAI_ADDRESS   =   Address.fromString("0xf5dce57282a584d2746faf1593d3121fcac444dc")
+export const CSUSHI_ADDRESS =   Address.fromString("0x4b0181102a0112a2ef11abee5563bb4a3176c9d7")
+export const CTUSD_ADDRESS  =   Address.fromString("0x12392f67bdf24fae0af363c24ac620a2f67dad86")
+export const CUNI_ADDRESS   =   Address.fromString("0x35a18000230da775cac24873d00ff85bccded550")
+export const CUSDC_ADDRESS  =   Address.fromString("0x39aa39c021dfbae8fac545936693ac917d5e7563")
+export const CUSDP_ADDRESS  =   Address.fromString("0x041171993284df560249b57358f931d9eb7b925d")
+export const CUSDT_ADDRESS  =   Address.fromString("0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9")
+export const CWBTC2_ADDRESS =   Address.fromString("0xccf4429db6322d5c611ee964527d42e5d685dd6a")
+export const CYFI_ADDRESS   =   Address.fromString("0x80a2ae356fc9ef4305676f7a3e2ed04e12c33946")
+export const CZRX_ADDRESS   =   Address.fromString("0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407")
---
+++ subgraphs/compound-v2/src/common/constants.ts
@@ -0,0 +1,29 @@
+// store common variables
+
+import { 
+    BigInt, 
+    BigDecimal, 
+} from "@graphprotocol/graph-ts";
+
+// BigInt 0 and 1
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+
+// BigDecimal 0 and 1
+export let ZERO_BD = BigDecimal.fromString("0");
+export let ONE_BD = BigDecimal.fromString("1");
+
+// protocol constants
+export const PROTOCOL_ID = "0x4CF8E50A5ac16731FA2D8D9591E195A285eCaA82";
+export const NETWORK_FANTOM = "FANTOM";
+export const PROTOCOL_TYPE_LENDING = "LENDING";
+
+export const DEFAULT_DECIMALS = 18;
+export const SECONDS_PER_DAY = 86400;
+
+// Interaction types for snapshot calculations
+export const DEPOSIT_INTERACTION = "DEPOSIT";
+export const WITHDRAW_INTERACTION = "WITHDRAW";
+export const BORROW_INTERACTION = "BORROW";
+export const REWARD_INTERACTION = "REWARD";
+export const REPAY_INTERACTION = "REPAY";
