15b1190a74168f1d4be34a8ffcbcaa9b769d66a6
Stan36 <stan36@pop-os.localdomain>
2022-03-30 11:14:51 -0700

    trying to fix git issue

---
+++ subgraphs/dodo-v2/messari-dodo-v2/src/mapping.ts
@@ -0,0 +1,154 @@
+import { BigInt, BigDecimal, Address } from "@graphprotocol/graph-ts";
+import {
+  DODOLpToken_ADDRESS,
+  vDODOToken_ADDRESS,
+  ZERO_BI,
+  ONE_BI,
+  ZERO_BD,
+  ADDRESS_ZERO
+} from "./utils/constants";
+
+import {
+  DVMFactory,
+  NewDVM,
+  OwnershipTransferPrepared,
+  OwnershipTransferred,
+  RemoveDVM
+} from "../generated/DVMFactory/DVMFactory";
+
+import {
+  DODOLpToken,
+  Approval,
+  Burn,
+  Mint,
+  OwnershipTransferPrepared,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/DODOLpToken/DODOLpToken";
+
+import {
+  vDODOToken,
+  Approval,
+  ChangePerReward,
+  DonateDODO,
+  MintVDODO,
+  PreDeposit,
+  OwnershipTransferPrepared,
+  OwnershipTransferred,
+  RedeemVDODO,
+  SetCantransfer,
+  Transfer,
+  UpdateDODOFeeBurnRatio
+} from "../generated/vDODOToken/vDODOToken";
+
+import {
+  DVM,
+  BuyShares,
+  SellShares,
+  DODOSwap,
+  DODOFlashLoan,
+  Transfer,
+  Approval,
+  Mint,
+  Burn
+} from "../generated/templates/DVM/DVM";
+
+import { ERC20, Transfer, Approval } from "../generated/templates/ERC20/ERC20";
+
+import {
+  DexAmmProtocol,
+  LiquidityPool,
+  Token,
+  RewardToken
+} from "../generated/schema";
+
+export function handleNewDVM(event: NewDVM): void {
+  let dodo = DexAmmProtocol.load(event.address.toHex());
+  let pool = LiquidityPool.load(event.params.dvm.toHex());
+  let it = Token.load(event.params.baseToken.toHex());
+  let ot = Token.load(event.params.quoteToken.toHex());
+  let dodoLp = RewardToken.load(DODOLpToken_ADDRESS);
+  let vdodo = RewardToken.load(vDODOToken_ADDRESS);
+
+  if (!dodoLp) {
+    dodoLp = new RewardToken(DODOLpToken_ADDRESS);
+    let tokenC = getToken(Address.fromString(DODOLpToken_ADDRESS));
+    dodoLp.name = tokenC.name();
+    dodoLp.symbol = tokenC.symbol();
+    dodoLp.decimals = tokenC.decimals();
+    dodoLp.type = "DEPOSIT";
+    dodoLp.save();
+  }
+
+  if (!vdodo) {
+    vdodo = new RewardToken(vDODOToken_ADDRESS);
+    let tokenC = getToken(Address.fromString(vDODOToken_ADDRESS));
+    vdodo.name = tokenC.name();
+    vdodo.symbol = tokenC.symbol();
+    vdodo.decimals = tokenC.decimals();
+    vdodo.type = "DEPOSIT";
+    vdodo.save();
+  }
+
+  if (!it) {
+    it = new Token(event.params.baseToken.toHex());
+    let tokenC = getToken(event.params.baseToken);
+    it.name = tokenC.name();
+    it.symbol = tokenC.symbol();
+    it.decimals = tokenC.decimals();
+  }
+  it.save();
+
+  if (!ot) {
+    ot = new Token(event.params.quoteToken.toHex());
+    let tokenC = getToken(event.params.baseToken);
+    ot.name = tokenC.name();
+    ot.symbol = tokenC.symbol();
+    ot.decimals = tokenC.decimals();
+  }
+  ot.save();
+
+  if (!dodo) {
+    dodo = new DexAmmProtocol(event.address.toHex());
+    dodo.name = "DODO V2";
+    dodo.slug = "messari-dodo";
+    dodo.schemaVersion = "schema-dex-amm";
+    dodo.subgraphVersion = "0.0.2";
+    dodo.network = "ETHEREUM";
+    dodo.type = "EXCHANGE";
+    dodo.totalUniqueUsers = 0;
+    dodo.totalValueLockedUSD = ZERO_BD;
+  }
+
+  if (!pool) {
+    pool = new LiquidityPool(event.params.dvm.toHex());
+    pool.protocol = dodo.id;
+    pool.inputTokens = [it.id];
+    pool.outputToken = ot.id;
+    pool.rewardTokens = [dodoLp.id, vdodo.id];
+    pool.totalValueLockedUSD = ZERO_BD;
+    pool.totalVolumeUSD = ZERO_BD;
+    pool.inputTokenBalances = [ZERO_BI];
+    pool.outputTokenSupply = ZERO_BI;
+    pool.outputTokenPriceUSD = ZERO_BD;
+    pool.rewardTokenEmissionsAmount = [ZERO_BI];
+    pool.rewardTokenEmissionsUSD = [ZERO_BD];
+    pool.createdTimestamp = event.block.timestamp;
+    pool.createdBlockNumber = event.block.number;
+  }
+
+  dodo.save();
+  pool.save();
+}
+
+export function handleOwnershipTransferPrepared(
+  event: OwnershipTransferPrepared
+): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handleRemoveDVM(event: RemoveDVM): void {}
+
+function getToken(address: Address): ERC20 {
+  return ERC20.bind(address);
+}
---
+++ subgraphs/dodo-v2/messari-dodo-v2/src/utils/constants.ts
@@ -0,0 +1,11 @@
+import { BigInt, BigDecimal, Address } from "@graphprotocol/graph-ts";
+
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export const DODOLpToken_ADDRESS = "0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd";
+export const vDODOToken_ADDRESS = "0xc4436fbae6eba5d95bf7d53ae515f8a707bd402a";
+
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let ZERO_BD = BigDecimal.fromString("0");
+export let ONE_BD = BigDecimal.fromString("1");
+export let BI_18 = BigInt.fromI32(18);
