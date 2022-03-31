337ab821b0414415da9c590478f332ccbe8890a6
Jaideep Das <jaideep@hypha.cc>
2022-03-31 23:39:55 +0530

    pool handlers

--- subgraphs/vesper/config_yaml_gen.js
+++ subgraphs/vesper/config_yaml_gen.js
@@ -6,8 +6,24 @@ const _ = require("lodash");
 const genFunc = () => {
   console.info("Config generation started");
   const context = {
-    controllers: _.filter(controllers, { chainId: 1 }).map(e => ({name: e.name, address: e.address})),
-    tokens: _.filter(tokens, { chainId: 1 }).map(e => ({name: e.symbol, address: e.address})),
+    controllers: _.filter(controllers, { chainId: 1 }).map((e) => ({
+      name: e.name,
+      address: e.address,
+    })),
+    tokens: _.filter(tokens, { chainId: 1 }).map((e) => ({
+      name: e.symbol,
+      address: e.address,
+    })),
+    pools: _.filter(pools, (e) => {
+      return (
+        e.chainId === 1 && e.stage === "prod" && e.version === 3
+      );
+    }).map((e) => ({
+      name: e.name,
+      address: e.address,
+      version: e.version,
+      birthblock: e.birthblock,
+    })),
   };
 
   console.log(context);
