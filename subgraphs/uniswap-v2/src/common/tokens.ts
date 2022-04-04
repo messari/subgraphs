84d078168c1d16b82150d0329e8c0357095357c4
Chris Steege <chris@Chriss-MacBook-Pro.local>
2022-04-03 23:45:34 -0400

    Added token save

--- subgraphs/uniswap-v2/src/common/tokens.ts
+++ subgraphs/uniswap-v2/src/common/tokens.ts
@@ -32,6 +32,7 @@ export function getOrCreateLPToken(tokenAddress: Address, token0: Token, token1:
       token.symbol = token0.name + '/' + token1.name
       token.name = token0.name + '/' + token1.name + " LP"
       token.decimals = DEFAULT_DECIMALS
+      token.save()
   }
   return token
 }
