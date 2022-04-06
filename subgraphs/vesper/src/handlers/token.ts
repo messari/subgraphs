03e6465467f3131a61d6c9b41a64b02dead59f15
Jaideep Das <jaideep@hypha.cc>
2022-04-06 19:00:04 +0530

    removed unused handlers

--- subgraphs/vesper/src/handlers/token.ts
+++
@@ -1,6 +0,0 @@
-import { Token, RewardToken } from "../../generated/schema";
-import { Approval, Transfer } from "../../generated/poolV3_vaUSDC/Erc20Token";
-
-export function handleApproval(event: Approval): void {}
-
-export function handleTransfer(event: Transfer): void {}
