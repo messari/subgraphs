19c04eee2d0e2fd8a6ab44bddffca3a2758cda43
Jaideep Das <jaideep@hypha.cc>
2022-04-06 19:00:04 +0530

    removed unused handlers

--- subgraphs/vesper/src/handlers/controller.ts
+++
@@ -1,4 +0,0 @@
-import { Deposit } from "../../generated/schema";
-import { OwnershipTransferred } from "../../generated/poolV3_vaUSDC/Controller";
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
