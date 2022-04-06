73243a037c055d8e40f43df6ba7d24f3b3f93c48
Jaideep Das <jaideep@hypha.cc>
2022-04-06 16:03:02 +0530

    fixed issue with genesis start block

--- subgraphs/vesper/src/handlers/controller.ts
+++ subgraphs/vesper/src/handlers/controller.ts
@@ -1,4 +1,4 @@
 import { Deposit } from "../../generated/schema";
-import { OwnershipTransferred } from "../../generated/controller/Controller";
+import { OwnershipTransferred } from "../../generated/poolV3_vaUSDC/Controller";
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
