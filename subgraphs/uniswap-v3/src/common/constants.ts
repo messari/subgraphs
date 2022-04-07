7fac5d8edb58c3bd47b667cf8d77e0a77f414617
0xbe1 <101405096+0xbe1@users.noreply.github.com>
2022-04-07 12:53:40 +0800

    add FIXED_PROTOCOL_FEE and DYNAMIC_PROTOCOL_FEE (#44)
    refactor uniswap v3 at the same time

--- subgraphs/uniswap-v3/src/common/constants.ts
+++ subgraphs/uniswap-v3/src/common/constants.ts
@@ -29,10 +29,11 @@ export namespace ProtocolType {
 }
 
 export namespace LiquidityPoolFeeType {
-  export const TRADING_FEE = "TRADING_FEE"
-  export const PROTOCOL_FEE = "PROTOCOL_FEE"
-  export const TIERED_FEE = "TIERED_FEE"
-  export const DYNAMIC_FEE = "DYNAMIC_FEE"
+  export const FIXED_TRADING_FEE = "FIXED_TRADING_FEE";
+  export const TIERED_TRADING_FEE = "TIERED_TRADING_FEE";
+  export const DYNAMIC_TRADING_FEE = "DYNAMIC_TRADING_FEE";
+  export const FIXED_PROTOCOL_FEE = "FIXED_PROTOCOL_FEE";
+  export const DYNAMIC_PROTOCOL_FEE = "DYNAMIC_PROTOCOL_FEE";
 }
 
 export namespace RewardTokenType {
