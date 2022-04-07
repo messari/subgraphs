831039eacd12165bc034adf2fc60dbbe764a096b
cbrzn <cesarbrazon10@gmail.com>
2022-04-07 22:45:47 +0200

    balancer-v2: update token input balance on swap

--- subgraphs/balancer-v2/src/mappings/handlers.ts
+++ subgraphs/balancer-v2/src/mappings/handlers.ts
@@ -1,8 +1,9 @@
-import {PoolBalanceChanged, PoolRegistered, TokensRegistered} from "../../generated/Vault/Vault";
+import {PoolBalanceChanged, PoolRegistered, Swap, TokensRegistered} from "../../generated/Vault/Vault";
 import {createPool, getOrCreateToken} from "../common/getters";
 import {LiquidityPool} from "../../generated/schema";
 import {BigInt} from "@graphprotocol/graph-ts";
 import {BIGINT_ZERO} from "../common/constants";
+import {updateFinancials, updateUsageMetrics} from "../common/metrics";
 
 export function handlePoolRegister(event: PoolRegistered): void {
   createPool(event.params.poolId.toHexString(), event.params.poolAddress, event.block)
@@ -26,15 +27,45 @@ export function handleTokensRegister(event: TokensRegistered): void {
 }
 
 export function handlePoolBalanceChanged(event: PoolBalanceChanged): void {
+  updateFinancials(event)
+  updateUsageMetrics(event, event.transaction.from)
+
   let pool = LiquidityPool.load(event.params.poolId.toHexString())
-  if (pool == null) {
-    return
-  }
+  if (pool == null) return
   let amounts: BigInt[] = []
   for (let i=0; i<event.params.deltas.length; i++) {
     let currentAmount = pool.inputTokenBalances[i]
     amounts.push(currentAmount.plus(event.params.deltas[i]))
   }
   pool.inputTokenBalances = amounts;
+  pool.save()
+}
+
+export function handleSwap(event: Swap): void {
+  updateFinancials(event)
+  updateUsageMetrics(event, event.transaction.from)
+
+  let pool = LiquidityPool.load(event.params.poolId.toHexString())
+  if (pool == null) return
+
+  let tokenInIndex: u32 = -1;
+  let tokenOutIndex: u32 = -1;
+
+  for (let i=0; i<pool.inputTokens.length; i++) {
+    if (event.params.tokenIn.toHexString() === pool.inputTokens[i]) {
+      tokenInIndex = i
+    }
+
+    if (event.params.tokenIn.toHexString() === pool.inputTokens[i]) {
+      tokenOutIndex = i
+    }
+  }
+
+  if (tokenInIndex < 0) return
+  if (tokenOutIndex < 0) return
+
+  pool.inputTokenBalances[tokenInIndex] = pool.inputTokenBalances[tokenInIndex].plus(event.params.amountIn)
+  pool.inputTokenBalances[tokenOutIndex] = pool.inputTokenBalances[tokenOutIndex].minus(event.params.amountIn)
+
   pool.save()
 }
