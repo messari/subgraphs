b20790f4afc9196fb1cbacfd1a34d4cf8353e137
Chris Steege <chris@Chriss-MacBook-Pro.local>
2022-04-13 14:11:32 -0400

    Just getting started with refactoring

--- subgraphs/apeswap/src/mappings/factory.ts
+++ subgraphs/apeswap/src/mappings/factory.ts
@@ -5,22 +5,25 @@ import { getOrCreatePool } from "../helpers/pool";
 import { BIGDECIMAL_ZERO, HELPER_STORE_ID } from "../utils/constant";
 import { getOrCreateToken } from "../utils/token";
 
-export function handlePairCreated(event: PairCreated): void {
-  let pairAddress = event.params.pair;
-  let token0Address = event.params.token0;
-  let token1Address = event.params.token1;
-  // create new bundle, if it doesn't already exist
-  let helperStore = _HelperStore.load(HELPER_STORE_ID);
-  if (helperStore == null) {
-    let helperStore = new _HelperStore(HELPER_STORE_ID);
-    helperStore._value = BIGDECIMAL_ZERO;
-    helperStore.save();
-  }
-  // Create Tokens
-  let token0 = getOrCreateToken(token0Address);
-  let token1 = getOrCreateToken(token1Address);
-  // Create pair
-  getOrCreatePool(event, pairAddress, token0, token1);
-  // create the tracked contract based on the template
-  Pool.create(pairAddress);
+export function handleNewPair(event: PairCreated): void {
+
+  let protocol = getOrCreateDex()
+
+  // create the tokens and tokentracker
+  let token0 = getOrCreateToken(event.params.token0)
+  let token1 = getOrCreateToken(event.params.token1)
+  let LPtoken = getOrCreateLPToken(event.params.pair, token0, token1)
+
+  let tokenTracker0 = getOrCreateTokenTracker(event.params.token0)
+  let tokenTracker1 = getOrCreateTokenTracker(event.params.token1)
+
+  tokenTracker0.derivedETH = findEthPerToken(tokenTracker0)
+  tokenTracker1.derivedETH = findEthPerToken(tokenTracker1)
+
+  UpdateTokenWhitelists(tokenTracker0, tokenTracker1, event.params.pair)
+
+  CreateLiquidityPool(event, protocol, event.params.pair, token0, token1, LPtoken)
+
+  token0.save()
+  token1.save()
 }
