f3584c5ad9b7f5cf8d0b7502484449a1e66d2112
Jaideep Das <jaideep.jdof@gmail.com>
2022-04-22 01:49:01 +0530

    added zero check for withdraw

--- subgraphs/vesper-finance-v3/src/handlers/pool.ts
+++ subgraphs/vesper-finance-v3/src/handlers/pool.ts
@@ -262,6 +262,20 @@ export function handleWithdrawV3(event: Withdraw): void {
     event.block.number,
     event.block.timestamp
   );
+  let poolAddress = dataSource.address();
+  let poolAddressHex = poolAddress.toHexString();
+  let poolV3 = PoolV3.bind(poolAddress);
+  if (
+    event.params.owner === ZERO_ADDRESS ||
+    hasStrategy(poolV3.getStrategies(), event.params.owner)
+  ) {
+    let toHex = event.params.owner.toHexString();
+    log.info(
+      "Withdraw Event for pool V3 {} was made by {} - it is not interest fees.",
+      [poolAddressHex, toHex]
+    );
+    return;
+  }
   getOrCreateWithdraw(event, dataSource.address());
   updateAllSnapshots(event, dataSource.address());
   // let poolAddress = dataSource.address();
@@ -292,7 +306,7 @@ export function handleTransferV3(event: Transfer): void {
   ]);
   let poolV3 = PoolV3.bind(poolAddress);
   if (
-    event.params.from.toHexString() === ZERO_ADDRESS.toHexString() ||
+    event.params.from === ZERO_ADDRESS ||
     hasStrategy(poolV3.getStrategies(), event.params.to)
   ) {
     let toHex = event.params.to.toHexString();
@@ -339,7 +353,7 @@ export function handleDepositV3(event: Deposit): void {
   ]);
   let poolV3 = PoolV3.bind(poolAddress);
   if (
-    event.params.owner.toHexString() === ZERO_ADDRESS.toHexString() ||
+    event.params.owner === ZERO_ADDRESS ||
     hasStrategy(poolV3.getStrategies(), event.params.owner)
   ) {
     let toHex = event.params.owner.toHexString();
