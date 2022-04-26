e6ec8f3296587b172c792295b0c43abdb712b02e
Nandy Bâ <nandyba@yahoo.fr>
2022-04-26 16:44:18 +0200

    Init Harvest Finance subgraph

---
+++ subgraphs/harvest-finance/src/mapping.ts
@@ -0,0 +1,10 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  fWETHContract,
+  Deposit as DepositEvent
+} from "../generated/fWETHContract/fWETHContract"
+import { Token } from "../generated/schema"
+
+export function handleDeposit(event: DepositEvent): void {
+  
+}
