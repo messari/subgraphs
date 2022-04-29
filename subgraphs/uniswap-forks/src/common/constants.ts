a4d15cc19223fb3f08b26bf5b4611ec68c62deb6
Chris Steege <chris@Chriss-MacBook-Pro.local>
2022-04-28 20:48:54 -0400

    Increased efficiency of rewards calculations

--- subgraphs/uniswap-forks/src/common/constants.ts
+++ subgraphs/uniswap-forks/src/common/constants.ts
@@ -94,6 +94,7 @@ export const USDC_DENOMINATOR = BigDecimal.fromString("1000000");
 export const BIGINT_ZERO = BigInt.fromI32(0);
 export const BIGINT_ONE = BigInt.fromI32(1);
 export const BIGINT_TWO = BigInt.fromI32(2);
+export const BIGINT_FIVE = BigInt.fromI32(5);
 export const BIGINT_TEN = BigInt.fromI32(10);
 export const BIGINT_HUNDRED = BigInt.fromI32(100);
 export const BIGINT_THOUSAND = BigInt.fromI32(1000);
@@ -104,6 +105,7 @@ export const INT_ZERO = 0 as i32;
 export const INT_ONE = 1 as i32;
 export const INT_TWO = 2 as i32;
 export const INT_FOUR = 4 as i32;
+export const INT_FIVE = 5 as i32;
 
 export const BIGDECIMAL_ZERO = new BigDecimal(BIGINT_ZERO);
 export const BIGDECIMAL_ONE = new BigDecimal(BIGINT_ONE);
