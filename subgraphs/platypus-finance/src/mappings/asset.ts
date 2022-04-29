2f412fdfa3dd7a5557b86bb31804e0a8a5d677c2
Arjun Nemani <nemaniarjun@gmail.com>
2022-04-30 01:03:08 +0530

    Add comments to fill pool.ts

--- subgraphs/platypus-finance/src/mappings/asset.ts
+++ subgraphs/platypus-finance/src/mappings/asset.ts
@@ -2,6 +2,17 @@
 
 import { Transfer, MaxSupplyUpdated, PoolUpdated } from "../../generated/templates/Asset/Asset";
 
-export function handleTransfer(event: Transfer): void {}
-export function handleMaxSupplyUpdated(event: MaxSupplyUpdated): void {}
-export function handlePoolUpdated(event: PoolUpdated): void {}
+export function handleTransfer(event: Transfer): void {
+    // noop
+}
+export function handleMaxSupplyUpdated(event: MaxSupplyUpdated): void {
+    // noop
+}
+export function handlePoolUpdated(event: PoolUpdated): void {
+    // Fetch old liq pool
+    // Remove from old liq pool
+    // Fetch new liq pool
+    // Add to new pool
+    // UpdateTVL
+
+}
