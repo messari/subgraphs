fb64b66a48146bfa6d94a7fdabc2337d37acc5d5
Dylan Melotik <dmelotik@wisc.edu>
2022-04-30 10:16:14 -0500

    figuring out how to get input token

--- subgraphs/rari-yield-ag/src/mappings/mappings.ts
+++ subgraphs/rari-yield-ag/src/mappings/mappings.ts
@@ -1,6 +1,6 @@
 // map blockchain data to entities outlined in schema.graphql
 
-import { Address } from "@graphprotocol/graph-ts";
+import { Address, log } from "@graphprotocol/graph-ts";
 import { YIELD_MANAGER_ADDRESS, YIELD_VAULT_ADDRESS, ZERO_ADDRESS } from "../common/utils/constants";
 import { Deposit, RariYieldFundManager } from "../../generated/RariYieldFundManager/RariYieldFundManager";
 import { createDeposit } from "./helpers";
@@ -8,7 +8,9 @@ import { Withdraw } from "../../generated/schema";
 
 export function handleYieldDeposit(event: Deposit): void {
   // get address of asset
-  //    let contract = new RariYieldFundManager(Address.fromString(YIELD_MANAGER_ADDRESS));
+  let contract = RariYieldFundManager.bind(Address.fromString(YIELD_MANAGER_ADDRESS));
+  let tryToken = contract.try_getAcceptedCurrencies();
+  log.warning("currency code: {} transaction hash {}", [event.params.currencyCode.toHexString(), event.transaction.hash.toHexString()]);
 
   createDeposit(
     event,
@@ -20,6 +22,4 @@ export function handleYieldDeposit(event: Deposit): void {
   // TODO: add snapshot functions
 }
 
-export function handleYieldWithdrawal(event: Withdraw): void {
-    
-}
+export function handleYieldWithdrawal(event: Withdraw): void {}
