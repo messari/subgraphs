50428ddfdb31355d46006a835741a9ec8426475e
Dylan Melotik <dmelotik@wisc.edu>
2022-05-01 12:14:05 -0500

    add in withdraw

--- subgraphs/rari-yield-ag/src/mappings/helpers.ts
+++ subgraphs/rari-yield-ag/src/mappings/helpers.ts
@@ -2,7 +2,14 @@
 
 import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { RARI_DEPLOYER } from "../common/utils/constants";
-import { Deposit } from "../../generated/schema";
+import { Deposit, Withdraw } from "../../generated/schema";
+
+let codes = [
+  "0xa5e92f3efb6826155f1f728e162af9d7cda33a574a1153b58f03ea01cc37e568",
+  "0x8b1a1d9c2b109e527c9134b25b1a1833b16b6594f92daa9f6d9b7a6024bce9d0",
+  "0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa",
+  "0x33d80a03b5585b94e68b56bdea4f57fd2e459401902cb2f61772e1b630afb4b2",
+];
 
 //////////////////////////////
 //// Transaction Entities ////
@@ -14,6 +21,7 @@ export function createDeposit(
   amountUSD: BigInt,
   asset: Address,
   vault: string,
+  code: string,
 ): void {
   // create id
   let hash = event.transaction.hash;
@@ -24,6 +32,9 @@ export function createDeposit(
   let deposit = new Deposit(id);
 
   // TODO: getOrCreate asset (deposited token)
+  if (!codes.includes(code)) {
+    deposit.currencyCode = code;
+  }
 
   // fill in vars
   deposit.hash = hash.toHexString();
@@ -39,6 +50,43 @@ export function createDeposit(
   deposit.vault = vault;
 
   deposit.save();
+}
+
+export function createWithdraw(
+  event: ethereum.Event,
+  amount: BigInt,
+  amountUSD: BigInt,
+  asset: Address,
+  vault: string,
+  code: string,
+): void {
+  // create id
+  let hash = event.transaction.hash;
+  let logIndex = event.transaction.index;
+  let id = hash.toHexString() + "-" + logIndex.toString();
+
+  // create Deposit
+  let withdraw = new Withdraw(id);
+
+  // TODO: getOrCreate asset (deposited token)
+  if (!codes.includes(code)) {
+    withdraw.currencyCode = code;
+  }
+
+  // populate vars,
+  withdraw.hash = hash.toHexString();
+  withdraw.logIndex = logIndex.toI32();
+  withdraw.protocol = RARI_DEPLOYER;
+  withdraw.to = event.address.toHexString();
+  withdraw.from = event.transaction.from.toHexString();
+  withdraw.blockNumber = event.block.number;
+  withdraw.timestamp = event.block.timestamp;
+  withdraw.asset = asset.toHexString(); // TODO: fix
+  withdraw.amount = amount;
+  withdraw.amountUSD = amountUSD.toBigDecimal(); // TODO: fix
+  withdraw.vault = vault;
+
+  withdraw.save();
 }
 
 /////////////////////////
--- subgraphs/rari-yield-ag/src/mappings/mappings.ts
+++ subgraphs/rari-yield-ag/src/mappings/mappings.ts
@@ -2,15 +2,15 @@
 
 import { Address, log } from "@graphprotocol/graph-ts";
 import { YIELD_MANAGER_ADDRESS, YIELD_VAULT_ADDRESS, ZERO_ADDRESS } from "../common/utils/constants";
-import { Deposit, RariYieldFundManager } from "../../generated/RariYieldFundManager/RariYieldFundManager";
-import { createDeposit } from "./helpers";
-import { Withdraw } from "../../generated/schema";
+import { Deposit, RariYieldFundManager, Withdrawal } from "../../generated/RariYieldFundManager/RariYieldFundManager";
+import { createDeposit, createWithdraw } from "./helpers";
 
 export function handleYieldDeposit(event: Deposit): void {
   // get address of asset
-  let contract = RariYieldFundManager.bind(Address.fromString(YIELD_MANAGER_ADDRESS));
-  let tryToken = contract.try_getAcceptedCurrencies();
-  log.warning("currency code: {} transaction hash {}", [event.params.currencyCode.toHexString(), event.transaction.hash.toHexString()]);
+  log.warning("currency code: {} transaction hash {}", [
+    event.params.currencyCode.toHexString(),
+    event.transaction.hash.toHexString(),
+  ]);
 
   createDeposit(
     event,
@@ -18,8 +18,18 @@ export function handleYieldDeposit(event: Deposit): void {
     event.params.amountUsd,
     Address.fromString(ZERO_ADDRESS), // TODO: change
     YIELD_VAULT_ADDRESS,
+    event.params.currencyCode.toHexString(),
   );
   // TODO: add snapshot functions
 }
 
-export function handleYieldWithdrawal(event: Withdraw): void {}
+export function handleYieldWithdrawal(event: Withdrawal): void {
+  createWithdraw(
+    event,
+    event.params.amount,
+    event.params.amountUsd,
+    Address.fromString(ZERO_ADDRESS),
+    YIELD_VAULT_ADDRESS,
+    event.params.currencyCode.toHexString(),
+  );
+}
