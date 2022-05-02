98f18fb37b8d85e2e184a5b58521763d678395a1
Chris Steege <chris@Chriss-MacBook-Pro.local>
2022-05-02 17:03:57 -0400

    Reformatted file structures

--- subgraphs/uniswap-forks/config/_networkConfig.ts
+++ subgraphs/uniswap-forks/config/_networkConfig.ts
@@ -11,7 +11,7 @@ export namespace Protocol {
 }
 
 // Choose which protocol you are indexing. The deployed network will already be determined
-let PROTOCOL_NAME_TEMP = Protocol.UNISWAPV2;
+let PROTOCOL_NAME_TEMP = Protocol.SUSHISWAP;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,9 +38,12 @@ let STABLE_COINS_TEMP: string[]; // A list of stable coins
 let STABLE_ORACLE_POOLS_TEMP: string[]; // A list of [stable coin / native token] oracle pools
 let UNTRACKED_PAIRS_TEMP: string[]; // rebass tokens, dont count in tracked volume
 
+log.warning("HELLO", []);
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if (PROTOCOL_NAME_TEMP == Protocol.UNISWAPV2 && dataSource.network() == Network.MAINNET.toLowerCase()) {
+  log.warning("HELLO1", []);
   PROTOCOL_SLUG_TEMP = "uniswap-v2";
   NETWORK_TEMP = Network.MAINNET;
 
@@ -94,6 +97,8 @@ if (PROTOCOL_NAME_TEMP == Protocol.UNISWAPV2 && dataSource.network() == Network.
   ];
   UNTRACKED_PAIRS_TEMP = ["0x9ea3b5b4ec044b70375236a281986106457b20ef"];
 } else if (PROTOCOL_NAME_TEMP == Protocol.APESWAP && dataSource.network() == Network.MATIC.toLowerCase()) {
+  log.warning("HELLO2", []);
+
   PROTOCOL_SLUG_TEMP = "apeswap";
   NETWORK_TEMP = Network.MATIC;
 
@@ -132,6 +137,8 @@ if (PROTOCOL_NAME_TEMP == Protocol.UNISWAPV2 && dataSource.network() == Network.
     "0x019011032a7ac3a87ee885b6c08467ac46ad11cd", // WMATIC/USDC
   ];
 } else if (PROTOCOL_NAME_TEMP == Protocol.APESWAP && dataSource.network() == Network.BSC.toLowerCase()) {
+  log.warning("HELLO3", []);
+
   PROTOCOL_SLUG_TEMP = "apeswap";
   NETWORK_TEMP = Network.BSC;
 
@@ -175,9 +182,256 @@ if (PROTOCOL_NAME_TEMP == Protocol.UNISWAPV2 && dataSource.network() == Network.
     "0xf3010261b58b2874639ca2e860e9005e3be5de0b", // DAI/WBNB created block 481116
     "0x20bcc3b8a0091ddac2d0bc30f68e6cbb97de59cd", // USDT/WBNB created block 648115
   ];
+} else if (PROTOCOL_NAME_TEMP == Protocol.APESWAP && dataSource.network() == Network.BSC.toLowerCase()) {
+  log.warning("HELLO4", []);
+
+  PROTOCOL_SLUG_TEMP = "apeswap";
+  NETWORK_TEMP = Network.BSC;
+
+  FACTORY_ADDRESS_TEMP = "0xCf083Be4164828f00cAE704EC15a36D711491284";
+  FACTORY_CONTRACT_TEMP = Factory.bind(Address.fromString(FACTORY_ADDRESS_TEMP));
+
+  TRADING_FEE_TEMP = BigDecimal.fromString("2");
+  PROTOCOL_FEE_TO_ON_TEMP = BigDecimal.fromString("0.5");
+  LP_FEE_TO_ON_TEMP = BigDecimal.fromString("1.5");
+  PROTOCOL_FEE_TO_OFF_TEMP = BigDecimal.fromString("0.0");
+  LP_FEE_TO_OFF_TEMP = BigDecimal.fromString("2");
+  FEE_ON_OFF_TEMP = FeeSwitch.ON;
+
+  REWARD_INTERVAL_TYPE_TEMP = RewardIntervalType.TIMESTAMP;
+
+  NATIVE_TOKEN_TEMP = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
+  REWARD_TOKENS_TEMP = ["0x5d47bAbA0d66083C52009271faF3F50DCc01023C"];
+  WHITELIST_TOKENS_TEMP = [
+    "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", // WBNB
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+    "0x55d398326f99059ff775485246999027b3197955", // USDT
+    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+    "0x4bd17003473389a42daf6a0a729f6fdb328bbbd7", // VAI
+    "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
+    "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // WETH
+    "0x250632378e573c6be1ac2f97fcdf00515d0aa91b", // BETH
+    "0x603c7f932ed1fc6575303d8fb018fdcbb0f39a95", // BANANA
+    "0xdDb3Bd8645775F59496c821E4F55A7eA6A6dc299", // GNANA
+  ];
+  STABLE_COINS_TEMP = [
+    "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+    "0x0000000000085d4780b73119b644ae5ecd22b376", // TUSD
+    "0x956f47f50a910163d8bf957cf5846d573e7f87ca", // FEI
+    "0x4dd28568d05f09b02220b09c2cb307bfd837cb95",
+  ];
+  STABLE_ORACLE_POOLS_TEMP = [
+    "0x51e6d27fa57373d8d4c256231241053a70cb1d93", // BUSD/WBNB created block 4857769
+    "0xf3010261b58b2874639ca2e860e9005e3be5de0b", // DAI/WBNB created block 481116
+    "0x20bcc3b8a0091ddac2d0bc30f68e6cbb97de59cd", // USDT/WBNB created block 648115
+  ];
+} else if (PROTOCOL_NAME_TEMP == Protocol.SUSHISWAP && dataSource.network() == Network.BSC.toLowerCase()) {
+  log.warning("HELLO5", []);
+
+  PROTOCOL_SLUG_TEMP = "Sushiswap";
+  NETWORK_TEMP = Network.BSC;
+
+  FACTORY_ADDRESS_TEMP = "0xc35dadb65012ec5796536bd9864ed8773abc74c4";
+  FACTORY_CONTRACT_TEMP = Factory.bind(Address.fromString(FACTORY_ADDRESS_TEMP));
+
+  TRADING_FEE_TEMP = BigDecimal.fromString("3");
+  PROTOCOL_FEE_TO_ON_TEMP = BigDecimal.fromString("0.5");
+  LP_FEE_TO_ON_TEMP = BigDecimal.fromString("2.5");
+  PROTOCOL_FEE_TO_OFF_TEMP = BigDecimal.fromString("0.0");
+  LP_FEE_TO_OFF_TEMP = BigDecimal.fromString("3");
+  FEE_ON_OFF_TEMP = FeeSwitch.ON;
+
+  // REWARD_INTERVAL_TYPE_TEMP = RewardIntervalType.TIMESTAMP;
+
+  NATIVE_TOKEN_TEMP = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
+  REWARD_TOKENS_TEMP = ["0x947950BcC74888a40Ffa2593C5798F11Fc9124C4"];
+  WHITELIST_TOKENS_TEMP = [
+    "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", // wBNB
+    "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // wETH
+    "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
+    "0x55d398326f99059ff775485246999027b3197955", // USDT
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+    "0xf16e81dce15b08f326220742020379b855b87df9", // ICE
+  ];
+  STABLE_COINS_TEMP = [
+    "0x55d398326f99059ff775485246999027b3197955", // USDT
+    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+  ];
+  STABLE_ORACLE_POOLS_TEMP = [
+    "0xc7632b7b2d768bbb30a404e13e1de48d1439ec21", // USDC/wETH
+    "0x2905817b020fd35d9d09672946362b62766f0d69", // USDT/wETH
+    "0xe6cf29055e747e95c058f64423d984546540ede5", // DAI/wETH
+  ];
+} else if (PROTOCOL_NAME_TEMP == Protocol.SUSHISWAP && dataSource.network() == Network.FANTOM.toLowerCase()) {
+  log.warning("HELLO6", []);
+
+  PROTOCOL_SLUG_TEMP = "Sushiswap";
+  NETWORK_TEMP = Network.FANTOM;
+
+  FACTORY_ADDRESS_TEMP = "0xc35dadb65012ec5796536bd9864ed8773abc74c4";
+  FACTORY_CONTRACT_TEMP = Factory.bind(Address.fromString(FACTORY_ADDRESS_TEMP.toLowerCase()));
+
+  TRADING_FEE_TEMP = BigDecimal.fromString("3");
+  PROTOCOL_FEE_TO_ON_TEMP = BigDecimal.fromString("0.5");
+  LP_FEE_TO_ON_TEMP = BigDecimal.fromString("2.5");
+  PROTOCOL_FEE_TO_OFF_TEMP = BigDecimal.fromString("0.0");
+  LP_FEE_TO_OFF_TEMP = BigDecimal.fromString("3");
+  FEE_ON_OFF_TEMP = FeeSwitch.ON;
+
+  REWARD_INTERVAL_TYPE_TEMP = RewardIntervalType.TIMESTAMP;
+
+  NATIVE_TOKEN_TEMP = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+  REWARD_TOKENS_TEMP = ["0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"];
+  WHITELIST_TOKENS_TEMP = [
+    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
+    "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+    "0x0000000000085d4780b73119b644ae5ecd22b376", // TUSD
+    "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643", // cDAI
+    "0x39aa39c021dfbae8fac545936693ac917d5e7563", // cUSDC
+    "0x86fadb80d8d2cff3c3680819e4da99c10232ba0f", // EBASE
+    "0x57ab1ec28d129707052df4df418d58a2d46d5f51", // sUSD
+    "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", // MKR
+    "0xc00e94cb662c3520282e6f5717214004a7f26888", // COMP
+    "0x514910771af9ca656af840dff83e8264ecf986ca", //LINK
+    "0x960b236a07cf122663c4303350609a66a7b288c0", //ANT
+    "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f", //SNX
+    "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e", //YFI
+    "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8", // yCurv
+    "0x853d955acef822db058eb8505911ed77f175b99e", // FRAX
+    "0xa47c8bf37f92abed4a126bda807a7b7498661acd", // WUST
+    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+  ];
+  STABLE_COINS_TEMP = [
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+    "0x55d398326f99059ff775485246999027b3197955", // USDT
+    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+  ];
+  STABLE_ORACLE_POOLS_TEMP = [
+    "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0", // USDC/wETH
+    "0x06da0fd433C1A5d7a4faa01111c044910A184553", // USDT/wETH
+    "0xC3D03e4F041Fd4cD388c549Ee2A29a9E5075882f", // DAI/wETH
+  ];
 } else if (PROTOCOL_NAME_TEMP == Protocol.SUSHISWAP && dataSource.network() == Network.MAINNET.toLowerCase()) {
+  log.warning("HELLO7", []);
+
   PROTOCOL_SLUG_TEMP = "Sushiswap";
   NETWORK_TEMP = Network.MAINNET;
+
+  FACTORY_ADDRESS_TEMP = "0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac";
+  FACTORY_CONTRACT_TEMP = Factory.bind(Address.fromString(FACTORY_ADDRESS_TEMP));
+
+  TRADING_FEE_TEMP = BigDecimal.fromString("3");
+  PROTOCOL_FEE_TO_ON_TEMP = BigDecimal.fromString("0.5");
+  LP_FEE_TO_ON_TEMP = BigDecimal.fromString("2.5");
+  PROTOCOL_FEE_TO_OFF_TEMP = BigDecimal.fromString("0.0");
+  LP_FEE_TO_OFF_TEMP = BigDecimal.fromString("3");
+  FEE_ON_OFF_TEMP = FeeSwitch.ON;
+
+  REWARD_INTERVAL_TYPE_TEMP = RewardIntervalType.TIMESTAMP;
+
+  NATIVE_TOKEN_TEMP = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+  REWARD_TOKENS_TEMP = ["0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"];
+  WHITELIST_TOKENS_TEMP = [
+    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
+    "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+    "0x0000000000085d4780b73119b644ae5ecd22b376", // TUSD
+    "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643", // cDAI
+    "0x39aa39c021dfbae8fac545936693ac917d5e7563", // cUSDC
+    "0x86fadb80d8d2cff3c3680819e4da99c10232ba0f", // EBASE
+    "0x57ab1ec28d129707052df4df418d58a2d46d5f51", // sUSD
+    "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", // MKR
+    "0xc00e94cb662c3520282e6f5717214004a7f26888", // COMP
+    "0x514910771af9ca656af840dff83e8264ecf986ca", //LINK
+    "0x960b236a07cf122663c4303350609a66a7b288c0", //ANT
+    "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f", //SNX
+    "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e", //YFI
+    "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8", // yCurv
+    "0x853d955acef822db058eb8505911ed77f175b99e", // FRAX
+    "0xa47c8bf37f92abed4a126bda807a7b7498661acd", // WUST
+    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+  ];
+  STABLE_COINS_TEMP = [
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+    "0x55d398326f99059ff775485246999027b3197955", // USDT
+    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+  ];
+  STABLE_ORACLE_POOLS_TEMP = [
+    "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0", // USDC/wETH
+    "0x06da0fd433C1A5d7a4faa01111c044910A184553", // USDT/wETH
+    "0xC3D03e4F041Fd4cD388c549Ee2A29a9E5075882f", // DAI/wETH
+  ];
+} else if (PROTOCOL_NAME_TEMP == Protocol.SUSHISWAP && dataSource.network() == Network.MATIC.toLowerCase()) {
+  log.warning("HELLO8", []);
+
+  PROTOCOL_SLUG_TEMP = "Sushiswap";
+  NETWORK_TEMP = Network.MATIC;
+
+  FACTORY_ADDRESS_TEMP = "0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac";
+  FACTORY_CONTRACT_TEMP = Factory.bind(Address.fromString(FACTORY_ADDRESS_TEMP));
+
+  TRADING_FEE_TEMP = BigDecimal.fromString("3");
+  PROTOCOL_FEE_TO_ON_TEMP = BigDecimal.fromString("0.5");
+  LP_FEE_TO_ON_TEMP = BigDecimal.fromString("2.5");
+  PROTOCOL_FEE_TO_OFF_TEMP = BigDecimal.fromString("0.0");
+  LP_FEE_TO_OFF_TEMP = BigDecimal.fromString("3");
+  FEE_ON_OFF_TEMP = FeeSwitch.ON;
+
+  REWARD_INTERVAL_TYPE_TEMP = RewardIntervalType.TIMESTAMP;
+
+  NATIVE_TOKEN_TEMP = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+  REWARD_TOKENS_TEMP = ["0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"];
+  WHITELIST_TOKENS_TEMP = [
+    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
+    "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+    "0x0000000000085d4780b73119b644ae5ecd22b376", // TUSD
+    "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643", // cDAI
+    "0x39aa39c021dfbae8fac545936693ac917d5e7563", // cUSDC
+    "0x86fadb80d8d2cff3c3680819e4da99c10232ba0f", // EBASE
+    "0x57ab1ec28d129707052df4df418d58a2d46d5f51", // sUSD
+    "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", // MKR
+    "0xc00e94cb662c3520282e6f5717214004a7f26888", // COMP
+    "0x514910771af9ca656af840dff83e8264ecf986ca", //LINK
+    "0x960b236a07cf122663c4303350609a66a7b288c0", //ANT
+    "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f", //SNX
+    "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e", //YFI
+    "0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8", // yCurv
+    "0x853d955acef822db058eb8505911ed77f175b99e", // FRAX
+    "0xa47c8bf37f92abed4a126bda807a7b7498661acd", // WUST
+    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+  ];
+  STABLE_COINS_TEMP = [
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+    "0x55d398326f99059ff775485246999027b3197955", // USDT
+    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+  ];
+  STABLE_ORACLE_POOLS_TEMP = [
+    "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0", // USDC/wETH
+    "0x06da0fd433C1A5d7a4faa01111c044910A184553", // USDT/wETH
+    "0xC3D03e4F041Fd4cD388c549Ee2A29a9E5075882f", // DAI/wETH
+  ];
+} else if (PROTOCOL_NAME_TEMP == Protocol.SUSHISWAP && dataSource.network() == Network.XDAI.toLowerCase()) {
+  log.warning("HELLO9", []);
+
+  PROTOCOL_SLUG_TEMP = "Sushiswap";
+  NETWORK_TEMP = Network.XDAI;
 
   FACTORY_ADDRESS_TEMP = "0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac";
   FACTORY_CONTRACT_TEMP = Factory.bind(Address.fromString(FACTORY_ADDRESS_TEMP));
