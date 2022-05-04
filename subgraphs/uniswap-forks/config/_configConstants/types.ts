acd0dd51d1417f9b136d844d4f5e1119b42328ba
Chris Steege <chris@Chriss-MacBook-Pro.local>
2022-05-04 18:01:37 -0400

    Updated config

--- subgraphs/uniswap-forks/config/_configConstants/types.ts
+++ subgraphs/uniswap-forks/config/_configConstants/types.ts
@@ -6,3 +6,5 @@ export type Network_StringListMap = { [Network: string]: string[] }
 export type Protocol_Network_StringMap = { [Protocol: string]: Network_StringMap }
 export type Protocol_Network_BigDecimalMap = { [Protocol: string]: Network_BigDecimalMap }
 export type Protocol_Network_StringListMap = { [Protocol: string]: Network_StringListMap }
+
+export type Network_StringMaps = { [Network: string]: any }
--- subgraphs/uniswap-forks/config/_networkConfig.ts
+++ subgraphs/uniswap-forks/config/_networkConfig.ts
@@ -1,4 +1,4 @@
-import { Address, dataSource, log } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, dataSource, log } from "@graphprotocol/graph-ts";
 import { _PROTOCOL_SLUG } from "./_configConstants/slug";
 import { _FACTORY_ADDRESS } from "./_configConstants/factories";
 import { _TRADING_FEE, _PROTOCOL_FEE_TO_ON, _PROTOCOL_FEE_TO_OFF, _LP_FEE_TO_ON, _LP_FEE_TO_OFF, _FEE_SWITCH } from "./_configConstants/fees";
@@ -9,35 +9,101 @@ import { _STABLE_COINS } from "./_configConstants/tokens/stables";
 import { _REFERENCE_TOKEN } from "./_configConstants/tokens/reference";
 import { _UNTRACKED_PAIRS } from "./_configConstants/tokens/untracked";
 import { _WHITELISTS } from "./_configConstants/tokens/whitelists";
-import { Protocol } from "../src/common/constants";
+import { Network, Protocol } from "../src/common/constants";
 import { toBytesArray } from "../src/common/utils/utils";
 import { Factory } from "../generated/Factory/Factory";
 
-// Choose which protocol you are indexing. The deployed network will already be determined
-let PROTOCOL_NAME_TEMP = Protocol.UNISWAP_V2;
+let PROTOCOL = Protocol.APESWAP;
+let NETWORK = dataSource.network().toLowerCase();
 
-export namespace NetworkConfigs {
-  export const NETWORK = dataSource.network().toUpperCase();
+class NetworkConfigurations {
+  PROTOCOL_NAME: string;
+  PROTOCOL_SLUG: string;
+  NETWORK: string;
 
-  export const PROTOCOL_NAME = PROTOCOL_NAME_TEMP;
-  export const PROTOCOL_SLUG = _PROTOCOL_SLUG[PROTOCOL_NAME];
-  
-  export const FACTORY_ADDRESS = _FACTORY_ADDRESS[PROTOCOL_NAME][NETWORK];
-  export const FACTORY_CONTRACT = Factory.bind(Address.fromString(FACTORY_ADDRESS));
+  FACTORY_ADDRESS: string
+  FACTORY_CONTRACT: Factory
 
-  export const TRADING_FEE = _TRADING_FEE[PROTOCOL_NAME][NETWORK];
-  export const PROTOCOL_FEE_TO_ON = _PROTOCOL_FEE_TO_ON[PROTOCOL_NAME][NETWORK];
-  export const LP_FEE_TO_ON = _LP_FEE_TO_ON[PROTOCOL_NAME][NETWORK];
-  export const PROTOCOL_FEE_TO_OFF = _PROTOCOL_FEE_TO_OFF[PROTOCOL_NAME][NETWORK];
-  export const LP_FEE_TO_OFF = _LP_FEE_TO_OFF[PROTOCOL_NAME][NETWORK];
-  export const FEE_ON_OFF = _FEE_SWITCH[PROTOCOL_NAME][NETWORK];
+  TRADING_FEE: BigDecimal;
+  PROTOCOL_FEE_TO_ON: BigDecimal;
+  LP_FEE_TO_ON: BigDecimal;
+  PROTOCOL_FEE_TO_OFF: BigDecimal;
+  LP_FEE_TO_OFF: BigDecimal;
+  FEE_ON_OFF: string;
 
-  export const REWARD_INTERVAL_TYPE = _REWARD_INTERVAL[PROTOCOL_NAME][NETWORK];
+  REWARD_INTERVAL_TYPE: string;
 
-  export const REFERENCE_TOKEN = _REFERENCE_TOKEN[PROTOCOL_NAME][NETWORK];
-  export const REWARD_TOKEN = _REWARD_TOKEN[PROTOCOL_NAME][NETWORK];
-  export const WHITELIST_TOKENS = _WHITELISTS[PROTOCOL_NAME][NETWORK];
-  export const STABLE_COINS = _STABLE_COINS[PROTOCOL_NAME];
-  export const STABLE_ORACLE_POOLS = _STABLE_POOLS[PROTOCOL_NAME][NETWORK];
-  export const UNTRACKED_PAIRS = _UNTRACKED_PAIRS[PROTOCOL_NAME][NETWORK];
+  NATIVE_TOKEN: string;
+  REWARD_TOKENS: string;
+  WHITELIST_TOKENS: string[];
+  STABLE_COINS: string[];
+  STABLE_ORACLE_POOLS: string[];
+  UNTRACKED_PAIRS: string[];
+
+  constructor() {
+  }
+
+  // constructor(Protocol: string, Network: string) {
+  //   this.NETWORK = Network.toUpperCase();
+
+  //   this.PROTOCOL_NAME = Protocol;
+  //   this.PROTOCOL_SLUG = _PROTOCOL_SLUG[Protocol];
+
+  //   this.FACTORY_ADDRESS = _FACTORY_ADDRESS[Protocol][Network];
+  //   this.FACTORY_CONTRACT = Factory.bind(Address.fromString(this.FACTORY_ADDRESS));
+
+  //   this.TRADING_FEE = _TRADING_FEE[Protocol][Network];
+  //   this.PROTOCOL_FEE_TO_ON = _PROTOCOL_FEE_TO_ON[Protocol][Network];
+  //   this.LP_FEE_TO_ON = _LP_FEE_TO_ON[Protocol][Network];
+  //   this.PROTOCOL_FEE_TO_OFF = _PROTOCOL_FEE_TO_OFF[Protocol][Network];
+  //   this.LP_FEE_TO_OFF = _LP_FEE_TO_OFF[Protocol][Network];
+  //   this.FEE_ON_OFF = _FEE_SWITCH[Protocol][Network];
+
+  //   this.REWARD_INTERVAL_TYPE = _REWARD_INTERVAL[Protocol][Network];
+  //   this.NATIVE_TOKEN = _REFERENCE_TOKEN[Protocol][Network];
+  //   this.REWARD_TOKENS = _REWARD_TOKEN[Protocol][Network];
+  //   this.WHITELIST_TOKENS = _WHITELISTS[Protocol][Network];
+  //   this.STABLE_COINS = _STABLE_COINS[Protocol];
+  //   this.STABLE_ORACLE_POOLS = _STABLE_POOLS[Protocol][Network];
+  //   this.UNTRACKED_PAIRS = _UNTRACKED_PAIRS[Protocol][Network];
+  // }
+
+
+  }
 }
+
+let NewtworkConfigurations = new NetworkConfigurations()
+
+if (PROTOCOL == Protocol.APESWAP && dataSource.network().toUpperCase() == Network.BSC) {
+  
+} else if (PROTOCOL == Protocol.APESWAP && dataSource.network().toUpperCase() == Network.MATIC) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.ARBITRUM_ONE) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.AVALANCHE) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.BSC) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.CELO) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.FANTOM) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.FUSE) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.MAINNET) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.MATIC) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.MOONBEAM) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.MOONRIVER) {
+  
+} else if (PROTOCOL == Protocol.SUSHISWAP && dataSource.network().toUpperCase() == Network.XDAI) {
+  
+} else if (PROTOCOL == Protocol.UNISWAP_V2 && dataSource.network().toUpperCase() == Network.MAINNET) {
+  
+}
+
+
+
+let Configurations = new NetworkConfigurations(Protocol.SUSHISWAP, dataSource.network());
