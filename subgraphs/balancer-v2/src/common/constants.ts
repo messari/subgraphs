4eff815828e24b55d93a4af913b2704814f99647
Vincent Wen <wensi.isnew@gmail.com>
2022-05-07 09:18:09 -0700

    Renamed folders to stay consistent with deployments

--- subgraphs/balancer-v2/src/common/constants.ts
+++ subgraphs/balancer-v2/src/common/constants.ts
@@ -9,48 +9,48 @@ import { BigDecimal, BigInt, Address, dataSource } from "@graphprotocol/graph-ts
 // See below for a complete list:
 // https://thegraph.com/docs/en/hosted-service/what-is-hosted-service/#supported-networks-on-the-hosted-service
 export namespace Network {
-  export const ARBITRUM_ONE = "ARBITRUM_ONE";
-  export const AVALANCHE = "AVALANCHE";
-  export const AURORA = "AURORA";
-  export const BSC = "BSC"; // aka BNB Chain
-  export const CELO = "CELO";
-  export const MAINNET = "MAINNET"; // Ethereum mainnet
-  export const FANTOM = "FANTOM";
-  export const FUSE = "FUSE";
-  export const MOONBEAM = "MOONBEAM";
-  export const MOONRIVER = "MOONRIVER";
-  export const NEAR_MAINNET = "NEAR_MAINNET";
-  export const OPTIMISM = "OPTIMISM";
-  export const MATIC = "MATIC"; // aka Polygon
-  export const XDAI = "XDAI"; // aka Gnosis Chain
+    export const ARBITRUM_ONE = "ARBITRUM_ONE";
+    export const AVALANCHE = "AVALANCHE";
+    export const AURORA = "AURORA";
+    export const BSC = "BSC"; // aka BNB Chain
+    export const CELO = "CELO";
+    export const MAINNET = "MAINNET"; // Ethereum mainnet
+    export const FANTOM = "FANTOM";
+    export const FUSE = "FUSE";
+    export const MOONBEAM = "MOONBEAM";
+    export const MOONRIVER = "MOONRIVER";
+    export const NEAR_MAINNET = "NEAR_MAINNET";
+    export const OPTIMISM = "OPTIMISM";
+    export const MATIC = "MATIC"; // aka Polygon
+    export const XDAI = "XDAI"; // aka Gnosis Chain
 }
 
 export namespace ProtocolType {
-  export const EXCHANGE = "EXCHANGE";
-  export const LENDING = "LENDING";
-  export const YIELD = "YIELD";
-  export const BRIDGE = "BRIDGE";
-  export const GENERIC = "GENERIC";
+    export const EXCHANGE = "EXCHANGE";
+    export const LENDING = "LENDING";
+    export const YIELD = "YIELD";
+    export const BRIDGE = "BRIDGE";
+    export const GENERIC = "GENERIC";
 }
 
 export namespace VaultFeeType {
-  export const MANAGEMENT_FEE = "MANAGEMENT_FEE";
-  export const PERFORMANCE_FEE = "PERFORMANCE_FEE";
-  export const DEPOSIT_FEE = "DEPOSIT_FEE";
-  export const WITHDRAWAL_FEE = "WITHDRAWAL_FEE";
+    export const MANAGEMENT_FEE = "MANAGEMENT_FEE";
+    export const PERFORMANCE_FEE = "PERFORMANCE_FEE";
+    export const DEPOSIT_FEE = "DEPOSIT_FEE";
+    export const WITHDRAWAL_FEE = "WITHDRAWAL_FEE";
 }
 
 export namespace LiquidityPoolFeeType {
-  export const FIXED_TRADING_FEE = "FIXED_TRADING_FEE";
-  export const TIERED_TRADING_FEE = "TIERED_TRADING_FEE";
-  export const DYNAMIC_TRADING_FEE = "DYNAMIC_TRADING_FEE";
-  export const FIXED_PROTOCOL_FEE = "FIXED_PROTOCOL_FEE";
-  export const DYNAMIC_PROTOCOL_FEE = "DYNAMIC_PROTOCOL_FEE";
+    export const FIXED_TRADING_FEE = "FIXED_TRADING_FEE";
+    export const TIERED_TRADING_FEE = "TIERED_TRADING_FEE";
+    export const DYNAMIC_TRADING_FEE = "DYNAMIC_TRADING_FEE";
+    export const FIXED_PROTOCOL_FEE = "FIXED_PROTOCOL_FEE";
+    export const DYNAMIC_PROTOCOL_FEE = "DYNAMIC_PROTOCOL_FEE";
 }
 
 export namespace RewardTokenType {
-  export const DEPOSIT = "DEPOSIT";
-  export const BORROW = "BORROW";
+    export const DEPOSIT = "DEPOSIT";
+    export const BORROW = "BORROW";
 }
 //////////////////////////////
 ///// Ethereum Addresses /////
@@ -76,6 +76,7 @@ export const BIGDECIMAL_ZERO = new BigDecimal(BIGINT_ZERO);
 export const BIGDECIMAL_ONE = new BigDecimal(BIGINT_ONE);
 export const BIGDECIMAL_TWO = new BigDecimal(BIGINT_TWO);
 
+
 /////////////////////
 ///// Date/Time /////
 /////////////////////
@@ -87,15 +88,15 @@ export const SECONDS_PER_HOUR = 60 * 60;
 ///// Protocol Specific /////
 /////////////////////////////
 
-export let VAULT_ADDRESS = Address.fromString("0xBA12222222228d8Ba445958a75a0704d566BF2C8");
-export let FEE_COLLECTOR_ADDRESS = Address.fromString("0xce88686553686da562ce7cea497ce749da109f9f");
-export let WETH: Address = Address.fromString("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
-export let WMATIC: Address = Address.fromString("0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0");
-export let WBTC: Address = Address.fromString("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599");
-export let USDC: Address = Address.fromString("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
-export let USDT: Address = Address.fromString("0xdAC17F958D2ee523a2206206994597C13D831ec7");
-export let BAL: Address = Address.fromString("0xba100000625a3754423978a60c9317c58a424e3D");
-export let DAI: Address = Address.fromString("0x6B175474E89094C44Da98b954EedeAC495271d0F");
+export let VAULT_ADDRESS = Address.fromString('0xBA12222222228d8Ba445958a75a0704d566BF2C8')
+export let FEE_COLLECTOR_ADDRESS = Address.fromString('0xce88686553686da562ce7cea497ce749da109f9f')
+export let WETH: Address = Address.fromString('0x82aF49447D8a07e3bd95BD0d56f35241523fBab1');
+export let WMATIC: Address = Address.fromString('0x0000000000000000000000000000000000000000');
+export let WBTC: Address = Address.fromString('0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f');
+export let USDC: Address = Address.fromString('0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8');
+export let USDT: Address = Address.fromString('0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9');
+export let BAL: Address = Address.fromString('0x040d1EdC9569d4Bab2D15287Dc5A4F10F56a56B8');
+export let DAI: Address = Address.fromString('0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1');
 
 export let USD_STABLE_ASSETS: Address[] = [USDC, DAI, USDT];
 export let BASE_ASSETS: Address[] = [WETH, WMATIC, WBTC, BAL];
