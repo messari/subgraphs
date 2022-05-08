cdd473a30795ed031f5a9ffd0a32d0b52c9e612c
Nandy Bâ <nandyba@yahoo.fr>
2022-05-09 00:35:47 +0200

    Harvest Finance - Add vault totalValueLockedUSD stats

--- subgraphs/harvest-finance/src/VaultListener.ts
+++ subgraphs/harvest-finance/src/VaultListener.ts
@@ -8,12 +8,14 @@ import {
   Withdraw as WithdrawEvent
 } from "../generated/ControllerListener/VaultContract"
 import { getOrCreateToken } from './entities/Token'
-import { Vault } from "../generated/schema";
+import { Vault, Token } from "../generated/schema";
 import { WETH_ADDRESS } from './constant'
 import { getOrCreateVault } from './entities/Vault'
 import { getOrCreateDeposit } from './entities/Transaction'
 import { getOrCreateToken } from './entities/Token'
 import { depositUpdateMetrics } from './entities/Metrics'
+import * as constants from "./constant";
+import { getUsdPricePerToken } from "./Prices";
 
 export function handleDeposit(event: DepositEvent): void {
 
@@ -29,7 +31,6 @@ export function handleDeposit(event: DepositEvent): void {
   let block = event.block;
   let deposit = getOrCreateDeposit(hash, index, block);
 
-
   deposit.from = event.params.beneficiary.toHex();
   deposit.to = vault.inputToken;
   deposit.asset = vault.inputToken;
@@ -37,26 +38,27 @@ export function handleDeposit(event: DepositEvent): void {
   deposit.vault = vault.id;
   deposit.save();
 
+  vault.inputTokenBalance += amount;
+  vault.save();
+
+
+  let inputTokenAddress = Address.fromString(vault.inputToken)
+  let inputTokenPrice = getUsdPricePerToken(inputTokenAddress);
+
+  // exist because vault create it
+  let inputToken = Token.load(inputTokenAddress.toHex());
+
+  let inputTokenDecimals = constants.BIGINT_TEN.pow(inputToken!.decimals as u8);
+
+  vault.totalValueLockedUSD = inputTokenPrice.usdPrice
+    .times(vault.inputTokenBalance.toBigDecimal())
+    .div(inputTokenDecimals.toBigDecimal())
+    .div(inputTokenPrice.decimals.toBigDecimal());
+
+
+  vault.save();
+
   depositUpdateMetrics(event, vault);
   
 }
 
-export function handleInvest(event: InvestEvent): void {
-
-}
-
-export function handleStrategyAnnounced(event: StrategyAnnouncedEvent): void {
-  
-}
-
-export function handleStrategyChanged(event: StrategyChangedEvent): void {
-  
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  
-}
-
-export function handleWithdraw(event: WithdrawEvent): void {
-  
-}
