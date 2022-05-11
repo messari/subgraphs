ea3e9a9cd82b295688f2bc4352426c4bb3da1674
Dylan Melotik <dmelotik@wisc.edu>
2022-05-11 13:46:08 -0500

    add fuse v1 constants

---
+++ subgraphs/compound-forks/fuse-v1/src/constants.ts
@@ -0,0 +1,29 @@
+// rari fuse v1 constants
+import { LendingType, Network, ProtocolType, RiskType } from "../../src/constants";
+
+////////////////////////////
+//// Ethereum Addresses ////
+////////////////////////////
+
+// TODO: move some to overall constants
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+export const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+export const ETH_NAME = "Ether";
+export const ETH_SYMBOL = "ETH";
+
+export const FACTORY_CONTRACT = "0x835482FE0532f169024d5E9410199369aAD5C77E"; // FusePoolDirectory.sol
+
+///////////////////////////
+//// Protocol Specific ////
+///////////////////////////
+
+export const NETWORK_ETHEREUM = Network.MAINNET;
+export const PROTOCOL_TYPE = ProtocolType.LENDING;
+export const LENDING_TYPE = LendingType.POOLED;
+export const PROTOCOL_RISK_TYPE = RiskType.ISOLATED;
+export const PROTOCOL_NAME = "Fuse v1";
+export const PROTOCOL_SLUG = "fuse-v2";
+export const SUBGRAPH_VERSION = "0.0.3";
+export const SCHEMA_VERSION = "1.2.1";
+export const METHODOLOGY_VERSION = "1.0.0";
--- subgraphs/compound-forks/fuse-v1/src/mappings.ts
+++ subgraphs/compound-forks/fuse-v1/src/mappings.ts
@@ -1,5 +1,5 @@
 // fuse v1 handlers
-import { log } from "@graphprotocol/graph-ts";
+import { Address, log } from "@graphprotocol/graph-ts";
 import {
   ProtocolData,
   _getOrCreateProtocol,
@@ -16,18 +16,18 @@ import {
   _handleRepayBorrow,
   _handleLiquidateBorrow,
   UpdateMarketData,
-  AccruePer,
   _handleAccrueInterest,
   getOrElse,
 } from "../../src/mapping";
 
 import { PoolRegistered } from "../generated/FusePoolDirectory/FusePoolDirectory";
-import { Comptroller } from "../generated/templates";
+import { Comptroller } from "../generated/FusePoolDirectory/Comptroller";
 import { _Comptroller } from "../generated/schema";
 
 export function handlePoolRegistered(event: PoolRegistered): void {
-  log.warning("creating Pool aka new comptroller", []);
-  let troller = Comptroller.create(event.params.pool.comptroller);
+  log.warning("creating Pool aka new comptroller {}", [event.params.pool.comptroller.toHexString()]);
+  let troller = Comptroller.bind(event.params.pool.comptroller);
+
 
   // create comptroller entity
   let comptroller = new _Comptroller(
@@ -35,5 +35,10 @@ export function handlePoolRegistered(event: PoolRegistered): void {
   );
 
   comptroller.name = event.params.pool.name.toString();
+  // TODO: get price oracle
+  let tryOracle = troller.try_oracle();
+  comptroller.priceOracle = tryOracle.reverted ? "" : tryOracle.value.toHexString(); 
   comptroller.save();
+
+  
 }
