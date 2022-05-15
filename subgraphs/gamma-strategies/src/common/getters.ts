c3b9c033da729faba403cf55af44f79bc27beba6
loc <l0c4t0r@gmail.com>
2022-05-15 07:27:02 +0000

    Track basic protocol/vault entities

---
+++ subgraphs/gamma-strategies/src/common/getters.ts
@@ -0,0 +1,178 @@
+// import { log } from "@graphprotocol/graph-ts"
+import {
+  Address,
+  BigDecimal,
+  dataSource,
+  ethereum,
+} from "@graphprotocol/graph-ts";
+import {
+  Token,
+  UsageMetricsDailySnapshot,
+  FinancialsDailySnapshot,
+  UsageMetricsHourlySnapshot,
+  RewardToken,
+  YieldAggregator,
+} from "../../generated/schema";
+import { fetchTokenSymbol, fetchTokenName, fetchTokenDecimals } from "./tokens";
+import {
+  BIGDECIMAL_ZERO,
+  Network,
+  INT_ZERO,
+  ProtocolType,
+  SECONDS_PER_DAY,
+  BIGINT_ZERO,
+  SECONDS_PER_HOUR,
+  RewardTokenType,
+  PROTOCOL_NAME,
+  PROTOCOL_SLUG,
+  PROTOCOL_SCHEMA_VERSION,
+  PROTOCOL_SUBGRAPH_VERSION,
+  PROTOCOL_METHODOLOGY_VERSION,
+} from "../common/constants";
+
+export function getOrCreateToken(tokenAddress: Address): Token {
+  let token = Token.load(tokenAddress.toHexString());
+  // fetch info if null
+  if (!token) {
+    token = new Token(tokenAddress.toHexString());
+    token.symbol = fetchTokenSymbol(tokenAddress);
+    token.name = fetchTokenName(tokenAddress);
+    token.decimals = fetchTokenDecimals(tokenAddress);
+    token.lastPriceUSD = BIGDECIMAL_ZERO;
+    token.lastPriceBlockNumber = BIGINT_ZERO;
+    token.save();
+  }
+  return token;
+}
+
+// export function getOrCreateRewardToken(address: Address): RewardToken {
+//   let rewardToken = RewardToken.load(address.toHexString());
+//   if (rewardToken == null) {
+//     let token = getOrCreateToken(address);
+//     rewardToken = new RewardToken(address.toHexString());
+//     rewardToken.token = token.id;
+//     rewardToken.type = RewardTokenType.DEPOSIT;
+//     rewardToken.save();
+
+//     return rewardToken as RewardToken;
+//   }
+//   return rewardToken as RewardToken;
+// }
+
+
+export function getOrCreateUsageMetricDailySnapshot(
+  event: ethereum.Event
+): UsageMetricsDailySnapshot {
+  // Number of days since Unix epoch
+  let id = event.block.timestamp.toI32() / SECONDS_PER_DAY;
+  let dayId = id.toString();
+  // Create unique id for the day
+  let usageMetrics = UsageMetricsDailySnapshot.load(dayId);
+
+  if (!usageMetrics) {
+    usageMetrics = new UsageMetricsDailySnapshot(dayId);
+    // usageMetrics.protocol = FACTORY_ADDRESS;
+
+    usageMetrics.dailyActiveUsers = INT_ZERO;
+    usageMetrics.cumulativeUniqueUsers = INT_ZERO;
+    usageMetrics.dailyTransactionCount = INT_ZERO;
+    usageMetrics.dailyDepositCount = INT_ZERO;
+    usageMetrics.dailyWithdrawCount = INT_ZERO;
+
+    usageMetrics.blockNumber = event.block.number;
+    usageMetrics.timestamp = event.block.timestamp;
+
+    usageMetrics.save();
+  }
+
+  return usageMetrics;
+}
+
+export function getOrCreateUsageMetricHourlySnapshot(
+  event: ethereum.Event
+): UsageMetricsHourlySnapshot {
+  // Number of days since Unix epoch
+  let hour = event.block.timestamp.toI32() / SECONDS_PER_HOUR;
+  let hourId = hour.toString();
+
+  // Create unique id for the day
+  let usageMetrics = UsageMetricsHourlySnapshot.load(hourId);
+
+  if (!usageMetrics) {
+    usageMetrics = new UsageMetricsHourlySnapshot(hourId);
+    // usageMetrics.protocol = FACTORY_ADDRESS;
+
+    usageMetrics.hourlyActiveUsers = INT_ZERO;
+    usageMetrics.cumulativeUniqueUsers = INT_ZERO;
+    usageMetrics.hourlyTransactionCount = INT_ZERO;
+    usageMetrics.hourlyDepositCount = INT_ZERO;
+    usageMetrics.hourlyWithdrawCount = INT_ZERO;
+
+    usageMetrics.blockNumber = event.block.number;
+    usageMetrics.timestamp = event.block.timestamp;
+
+    usageMetrics.save();
+  }
+
+  return usageMetrics;
+}
+
+export function getOrCreateFinancialsDailySnapshot(
+  event: ethereum.Event
+): FinancialsDailySnapshot {
+  // Number of days since Unix epoch
+  let dayID = event.block.timestamp.toI32() / SECONDS_PER_DAY;
+  let id = dayID.toString();
+
+  let financialMetrics = FinancialsDailySnapshot.load(id);
+
+  if (!financialMetrics) {
+    financialMetrics = new FinancialsDailySnapshot(id);
+    // financialMetrics.protocol = FACTORY_ADDRESS;
+
+    financialMetrics.totalValueLockedUSD = BIGDECIMAL_ZERO;
+
+    financialMetrics.cumulativeSupplySideRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.dailySupplySideRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.cumulativeProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.dailyProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.dailyTotalRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.cumulativeTotalRevenueUSD = BIGDECIMAL_ZERO;
+
+    financialMetrics.blockNumber = event.block.number;
+    financialMetrics.timestamp = event.block.timestamp;
+
+    financialMetrics.save();
+  }
+  return financialMetrics;
+}
+
+///////////////////////////
+///// Yield Specific /////
+///////////////////////////
+
+export function getOrCreateYieldAggregator(
+  registryAddress: Address
+): YieldAggregator {
+  let registryId = registryAddress.toHexString();
+  let protocol = YieldAggregator.load(registryId);
+  
+  if (!protocol) {
+    protocol = new YieldAggregator(registryId);
+    protocol.name = PROTOCOL_NAME;
+    protocol.slug = PROTOCOL_SLUG;
+    protocol.schemaVersion = PROTOCOL_SCHEMA_VERSION;
+    protocol.subgraphVersion = PROTOCOL_SUBGRAPH_VERSION;
+    protocol.methodologyVersion = PROTOCOL_METHODOLOGY_VERSION;
+    protocol.network = dataSource.network().toUpperCase();
+    protocol.type = ProtocolType.YIELD;
+    protocol.totalValueLockedUSD = BigDecimal.zero();
+    protocol.protocolControlledValueUSD = BigDecimal.zero();
+    protocol.cumulativeSupplySideRevenueUSD = BigDecimal.zero();
+    protocol.cumulativeProtocolSideRevenueUSD = BigDecimal.zero();
+    protocol.cumulativeTotalRevenueUSD = BigDecimal.zero();
+    protocol.cumulativeUniqueUsers = INT_ZERO;
+    protocol.save();
+  }
+  return protocol;
+}
---
+++ subgraphs/gamma-strategies/src/mappings/handlers.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prefer-const */
+import {  log } from "@graphprotocol/graph-ts"
+import { HypeAdded } from "../../generated/HypeRegistry/HypeRegistry"
+import { Deposit, Withdraw } from "../../generated/templates/Hypervisor/Hypervisor";
+import { Hypervisor as HypervisorContract } from "../../generated/templates/Hypervisor/Hypervisor"
+import { Hypervisor as HypervisorTemplate } from "../../generated/templates"
+import { getOrCreateYieldAggregator } from "../common/getters"
+import { getOrCreateVault } from "./helpers"
+
+
+export function handleHypeAdded(event: HypeAdded): void {
+
+    let hypervisorContract = HypervisorContract.bind(event.params.hype)
+    let test_amount = hypervisorContract.try_getTotalAmounts()
+    // Prevents subgraph crashing from bad address added to registry
+    if (test_amount.reverted) {
+        log.warning("Could not add {}, does not appear to be a hypervisor", [event.params.hype.toHex()])
+        return
+    }
+
+    getOrCreateYieldAggregator(event.address)
+    getOrCreateVault(event.params.hype, event.block)
+    
+    HypervisorTemplate.create(event.params.hype)
+
+}
+
+
+export function handleDeposit(event: Deposit): void {
+    let vault = getOrCreateVault(event.address, event.block)
+}
+
+export function handleWithdraw(event: Withdraw): void {
+    let vault = getOrCreateVault(event.address, event.block)
+}
