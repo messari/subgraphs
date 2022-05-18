1ea09be0876d18ad5a41ea740b849b2ca6dbe53c
loc <l0c4t0r@gmail.com>
2022-05-18 08:50:54 +0000

    Track rebalance events, add config for other networks

--- subgraphs/gamma-strategies/src/mappings/handlers.ts
+++ subgraphs/gamma-strategies/src/mappings/handlers.ts
@@ -1,69 +1,69 @@
 /* eslint-disable prefer-const */
-import { log } from "@graphprotocol/graph-ts"
-import { HypeAdded } from "../../generated/HypeRegistry/HypeRegistry"
-import { Deposit, Withdraw, Rebalance } from "../../generated/templates/Hypervisor/Hypervisor";
-import { Hypervisor as HypervisorContract } from "../../generated/templates/Hypervisor/Hypervisor"
-import { Hypervisor as HypervisorTemplate } from "../../generated/templates"
-import { getOrCreateYieldAggregator } from "../common/getters"
-import { createDeposit, createWithdraw } from "./helpers/events";
-import { updateUsageMetrics } from "./helpers/usageMetrics"
-import { getOrCreateVault } from "./helpers/vaults"
+import { log } from "@graphprotocol/graph-ts";
+import { HypeAdded } from "../../generated/HypeRegistry/HypeRegistry";
+import {
+  Deposit,
+  Withdraw,
+  Rebalance,
+} from "../../generated/templates/Hypervisor/Hypervisor";
+import { Hypervisor as HypervisorContract } from "../../generated/templates/Hypervisor/Hypervisor";
+import { Hypervisor as HypervisorTemplate } from "../../generated/templates";
+import { getOrCreateYieldAggregator } from "../common/getters";
+import {
+  createDeposit,
+  createRebalance,
+  createWithdraw,
+} from "./helpers/events";
+import { updateUsageMetrics } from "./helpers/usageMetrics";
+import { getOrCreateVault } from "./helpers/vaults";
 import { UsageType } from "../common/constants";
 import { updateRevenue, updateTvl } from "./helpers/financials";
 
-
 export function handleHypeAdded(event: HypeAdded): void {
+  let hypervisorContract = HypervisorContract.bind(event.params.hype);
+  let test_amount = hypervisorContract.try_getTotalAmounts();
+  // Prevents subgraph crashing from bad address added to registry
+  if (test_amount.reverted) {
+    log.warning("Could not add {}, does not appear to be a hypervisor", [
+      event.params.hype.toHex(),
+    ]);
+    return;
+  }
 
-    let hypervisorContract = HypervisorContract.bind(event.params.hype)
-    let test_amount = hypervisorContract.try_getTotalAmounts()
-    // Prevents subgraph crashing from bad address added to registry
-    if (test_amount.reverted) {
-        log.warning("Could not add {}, does not appear to be a hypervisor", [event.params.hype.toHex()])
-        return
-    }
+  getOrCreateYieldAggregator(event.address);
+  getOrCreateVault(event.params.hype, event.block);
 
-    getOrCreateYieldAggregator(event.address)
-    getOrCreateVault(event.params.hype, event.block)
-    
-    HypervisorTemplate.create(event.params.hype)
-
+  HypervisorTemplate.create(event.params.hype);
 }
 
 export function handleDeposit(event: Deposit): void {
-    // Create deposit event
-    createDeposit(event)
+  // Create deposit event
+  createDeposit(event);
 
-    // Update vault: TVL, token balances
-    let vault = getOrCreateVault(event.address, event.block)
-    vault.outputTokenSupply += event.params.shares
-    vault.save()
+  // Update vault token supply
+  let vault = getOrCreateVault(event.address, event.block);
+  vault.outputTokenSupply += event.params.shares;
+  vault.save();
 
-    // Update usage metrics
-    updateUsageMetrics(event.params.to, UsageType.DEPOSIT, event)
-
-    // Update TVL
-    updateTvl(event)
+  updateUsageMetrics(event.params.to, UsageType.DEPOSIT, event);
+  updateTvl(event);
 }
 
 export function handleWithdraw(event: Withdraw): void {
-    // Create withdraw event
-    createWithdraw(event)
+  // Create withdraw event
+  createWithdraw(event);
 
-    // Update vault: TVL, token balances
-    let vault = getOrCreateVault(event.address, event.block)
-    vault.outputTokenSupply -= event.params.shares
-    vault.save()
+  // Update vault token supply
+  let vault = getOrCreateVault(event.address, event.block);
+  vault.outputTokenSupply -= event.params.shares;
+  vault.save();
 
-    // Update usage metrics
-    updateUsageMetrics(event.params.to, UsageType.WITHDRAW, event)
-
-    // Update TVL
-    updateTvl(event)
+  updateUsageMetrics(event.params.to, UsageType.WITHDRAW, event);
+  updateTvl(event);
 }
 
 export function handleRebalance(event: Rebalance): void {
-    // Update revenue
-    updateRevenue(event)
-    // Update TVL
-    updateTvl(event)
+  createRebalance(event);
+  updateRevenue(event);
+  updateTvl(event);
 }
--- subgraphs/gamma-strategies/src/mappings/helpers/financials.ts
+++ subgraphs/gamma-strategies/src/mappings/helpers/financials.ts
@@ -1,20 +1,16 @@
 import {
   Address,
-  BigInt,
-  BigDecimal,
   dataSource,
   ethereum,
 } from "@graphprotocol/graph-ts";
-import { getUsdPrice, getUsdPricePerToken } from "../../prices";
 import { Rebalance } from "../../../generated/templates/Hypervisor/Hypervisor";
 import { Hypervisor as HypervisorContract } from "../../../generated/templates/Hypervisor/Hypervisor";
 import { getOrCreateUnderlyingToken, getOrCreateVault } from "./vaults";
 import {
   BIGDECIMAL_HUNDRED,
-  BIGDECIMAL_ONE,
-  ProtocolType,
+  BIGINT_ZERO,
   PROTOCOL_PERFORMANCE_FEE,
-  REGISTRY_ADDRESS,
+  REGISTRY_ADDRESS_MAP,
 } from "../../common/constants";
 import {
   getOrCreateFinancialsDailySnapshot,
@@ -23,9 +19,10 @@ import {
   getOrCreateVaultHourlySnapshot,
   getOrCreateYieldAggregator,
 } from "../../common/getters";
-import { VaultDailySnapshot } from "../../../generated/schema";
-import { BIGINT_TEN } from "../../prices/common/constants";
+import { getDualTokenUSD } from "./pricing";
+import { bigIntToBigDecimal } from "../../common/utils/numbers";
 
+/* Update TVL related fields in all entities */
 export function updateTvl(event: ethereum.Event): void {
   let underlyingToken = getOrCreateUnderlyingToken(event.address);
 
@@ -33,9 +30,9 @@ export function updateTvl(event: ethereum.Event): void {
   if (event.block.number > underlyingToken.lastAmountBlockNumber) {
     let hypeContract = HypervisorContract.bind(event.address);
     let totalAmounts = hypeContract.getTotalAmounts();
-    underlyingToken.lastAmount0 = totalAmounts.value0
-    underlyingToken.lastAmount1 = totalAmounts.value1
-    underlyingToken.save()
+    underlyingToken.lastAmount0 = totalAmounts.value0;
+    underlyingToken.lastAmount1 = totalAmounts.value1;
+    underlyingToken.save();
   }
 
   let vault = getOrCreateVault(event.address, event.block);
@@ -49,36 +46,54 @@ export function updateTvl(event: ethereum.Event): void {
     event.block.number
   );
 
+  // Calculate price per share
+  let outputToken = getOrCreateToken(Address.fromString(vault.outputToken!));
+  let vaultTokenSupply = vault.outputTokenSupply!;
+  let outputTokenPriceUSD = vault.outputTokenPriceUSD;
+  if (vaultTokenSupply > BIGINT_ZERO) {
+    let outputTokenSupplyDecimals = bigIntToBigDecimal(vaultTokenSupply, outputToken.decimals)
+    outputTokenPriceUSD = newTvl.div(outputTokenSupplyDecimals);
+  }
+
   // Update entities
   let vaultDailySnapshot = getOrCreateVaultDailySnapshot(event);
   let vaultHourlySnapshot = getOrCreateVaultHourlySnapshot(event);
   let protocol = getOrCreateYieldAggregator(
-    Address.fromString(REGISTRY_ADDRESS.mustGet(dataSource.network()))
+    REGISTRY_ADDRESS_MAP.get(dataSource.network())!
   );
+  let financialsDailySnapshot = getOrCreateFinancialsDailySnapshot(event);
 
   vault.totalValueLockedUSD = newTvl;
+  vault.outputTokenPriceUSD = outputTokenPriceUSD;
 
   vaultDailySnapshot.totalValueLockedUSD = newTvl;
+  vaultDailySnapshot.outputTokenPriceUSD = outputTokenPriceUSD;
   vaultDailySnapshot.blockNumber = event.block.number;
   vaultDailySnapshot.timestamp = event.block.timestamp;
 
   vaultHourlySnapshot.totalValueLockedUSD = newTvl;
+  vaultHourlySnapshot.outputTokenPriceUSD = outputTokenPriceUSD;
   vaultHourlySnapshot.blockNumber = event.block.number;
   vaultHourlySnapshot.timestamp = event.block.timestamp;
 
   protocol.totalValueLockedUSD += newTvl.minus(oldTvl);
 
+  financialsDailySnapshot.totalValueLockedUSD = protocol.totalValueLockedUSD;
+  financialsDailySnapshot.blockNumber = event.block.number;
+  financialsDailySnapshot.timestamp = event.block.timestamp;
+
   vault.save();
   vaultDailySnapshot.save();
   vaultHourlySnapshot.save();
   protocol.save();
+  financialsDailySnapshot.save();
 }
 
 export function updateRevenue(event: Rebalance): void {
   let underlyingToken = getOrCreateUnderlyingToken(event.address);
 
   // Calculate revenue in USD
-  let eventProtocolSideRevenueUSD = getDualTokenUSD(
+  let eventTotalRevenueUSD = getDualTokenUSD(
     Address.fromString(underlyingToken.token0),
     Address.fromString(underlyingToken.token1),
     event.params.feeAmount0,
@@ -86,20 +101,18 @@ export function updateRevenue(event: Rebalance): void {
     event.block.number
   );
 
-  const protocolToSupplyFeeRatio = BIGDECIMAL_HUNDRED.div(
-    PROTOCOL_PERFORMANCE_FEE
-  ).minus(BIGDECIMAL_ONE);
-  let eventSupplySideRevenueUSD = eventProtocolSideRevenueUSD.times(
-    protocolToSupplyFeeRatio
-  );
+  const SupplySideShare = BIGDECIMAL_HUNDRED.minus(PROTOCOL_PERFORMANCE_FEE);
+  let eventSupplySideRevenueUSD = eventTotalRevenueUSD
+    .times(SupplySideShare)
+    .div(BIGDECIMAL_HUNDRED);
 
-  let eventTotalRevenueUSD = eventProtocolSideRevenueUSD.plus(
-    eventSupplySideRevenueUSD
-  );
+  let eventProtocolSideRevenueUSD = eventTotalRevenueUSD
+    .times(PROTOCOL_PERFORMANCE_FEE)
+    .div(BIGDECIMAL_HUNDRED);
 
   // Update entities
   let protocol = getOrCreateYieldAggregator(
-    Address.fromString(REGISTRY_ADDRESS.mustGet(dataSource.network()))
+    REGISTRY_ADDRESS_MAP.get(dataSource.network())!
   );
   let financialsDailySnapshot = getOrCreateFinancialsDailySnapshot(event);
 
@@ -128,36 +141,3 @@ export function updateRevenue(event: Rebalance): void {
   financialsDailySnapshot.save();
 }
 
-export function getDualTokenUSD(
-  token0Address: Address,
-  token1Address: Address,
-  amount0: BigInt,
-  amount1: BigInt,
-  blockNumber: BigInt
-): BigDecimal {
-  // Update token prices
-  let token0 = getOrCreateToken(token0Address);
-  if (blockNumber > token0.lastPriceBlockNumber!) {
-    let fetchPrice = getUsdPricePerToken(token0Address);
-    token0.lastPriceUSD = fetchPrice.usdPrice;
-    token0.lastPriceBlockNumber = blockNumber;
-    token0.save();
-  }
-
-  let token1 = getOrCreateToken(token0Address);
-  if (blockNumber > token1.lastPriceBlockNumber!) {
-    let fetchPrice = getUsdPricePerToken(token0Address);
-    token1.lastPriceUSD = fetchPrice.usdPrice;
-    token1.lastPriceBlockNumber = blockNumber;
-    token1.save();
-  }
-
-  let amount0Usd = token0.lastPriceUSD!
-    .times(amount0.toBigDecimal())
-    .div(BIGINT_TEN.pow(token0.decimals as u8).toBigDecimal());
-  let amount1Usd = token1.lastPriceUSD!
-    .times(amount1.toBigDecimal())
-    .div(BIGINT_TEN.pow(token1.decimals as u8).toBigDecimal());
-
-  return amount0Usd.plus(amount1Usd);
-}
