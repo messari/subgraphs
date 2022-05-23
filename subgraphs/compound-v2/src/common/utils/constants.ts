773eb4ec5faf6b394a2b8f9070877b2f068be12f
Dylan Melotik <dmelotik@wisc.edu>
2022-05-23 10:14:39 -0500

    undo changes to compound-v2

--- subgraphs/compound-v2/src/common/utils/constants.ts
+++ subgraphs/compound-v2/src/common/utils/constants.ts
@@ -55,18 +55,19 @@ export namespace SubgraphNetwork {
   export const XDAI = "xdai";
 }
 
-export namespace LiquidityPoolFeeType {
-  export const FIXED_TRADING_FEE = "FIXED_TRADING_FEE";
-  export const TIERED_TRADING_FEE = "TIERED_TRADING_FEE";
-  export const DYNAMIC_TRADING_FEE = "DYNAMIC_TRADING_FEE";
-  export const FIXED_LP_FEE = "FIXED_LP_FEE";
-  export const DYNAMIC_LP_FEE = "DYNAMIC_LP_FEE";
-  export const FIXED_PROTOCOL_FEE = "FIXED_PROTOCOL_FEE";
-  export const DYNAMIC_PROTOCOL_FEE = "DYNAMIC_PROTOCOL_FEE";
-}
 export namespace RewardTokenType {
   export const DEPOSIT = "DEPOSIT";
   export const BORROW = "BORROW";
+}
+
+export namespace LendingType {
+  export const CDP = "CDP";
+  export const POOLED = "POOLED";
+}
+
+export namespace RiskType {
+  export const GLOBAL = "GLOBAL";
+  export const ISOLATED = "ISOLATED";
 }
 
 export namespace InterestRateType {
