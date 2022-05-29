96226d23d85b5d799ac84adb0f7c9902c09d4a17
tannishmango <tannishmango@protonmail.com>
2022-05-29 12:37:59 -0400

    Update subgraph version

--- subgraphs/makerdao/src/common/constants.ts
+++ subgraphs/makerdao/src/common/constants.ts
@@ -7,7 +7,7 @@ import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 export const PROTOCOL_NAME = "MakerDao";
 export const PROTOCOL_SLUG = "makerdao";
 export const PROTOCOL_SCHEMA_VERSION = "1.2.1";
-export const PROTOCOL_SUBGRAPH_VERSION = "1.0.0";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.0.1";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 
 ////////////////////////
