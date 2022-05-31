420571de9108751d959151681d565b92acc31ba8
DhruvJain1122 <amritkumarjain321@gmail.com>
2022-05-31 08:39:32 +0000

    fix

--- subgraphs/uniswap-forks/protocols/spookyswap/config/networks/fantom/fantom.ts
+++ subgraphs/uniswap-forks/protocols/spookyswap/config/networks/fantom/fantom.ts
@@ -1,5 +1,5 @@
 import { Address, BigDecimal } from '@graphprotocol/graph-ts';
-import { Factory } from '../../../generated/Factory/Factory';
+import { Factory } from '../../../../../generated/Factory/Factory';
 import { FeeSwitch, Network, PROTOCOL_SCHEMA_VERSION, RewardIntervalType } from '../../../../../src/common/constants';
 import { Configurations } from '../../../../../configurations/configurations/interface';
 import { PROTOCOL_SUBGRAPH_VERSION, PROTOCOL_METHODOLOGY_VERSION, PROTOCOL_NAME, PROTOCOL_SLUG } from '../../../src/common/constants';
