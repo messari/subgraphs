9bd08100d6073ec4152eb158bc3b8830e7cd64f3
Hugo Boisselle-Morin <hugo.boisselle@gmail.com>
2022-05-30 21:50:01 -0400

    Scale amountToUsd result by 1e18

--- subgraphs/euler/src/common/conversions.ts
+++ subgraphs/euler/src/common/conversions.ts
@@ -1,5 +1,5 @@
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
-import { BIGDECIMAL_ZERO } from "./constants";
+import { BIGDECIMAL_ZERO, DECIMAL_PRECISION } from "./constants";
 
 export function isNullEthValue(value: string): boolean {
   return value == '0x0000000000000000000000000000000000000000000000000000000000000001';
@@ -15,5 +15,5 @@ export function amountToUsd(amount: BigInt, twap: BigInt, twapPrice: BigDecimal)
   const wethPrice = twapD.div(twapPrice);
   const wethPerAsset = twapD.div(divisor);
 
-  return assetAmount.times(wethPrice).times(wethPerAsset);
+  return assetAmount.times(wethPrice).times(wethPerAsset).div(DECIMAL_PRECISION);
 }
