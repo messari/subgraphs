4435bd2f921cf21132f92f74ec4e8305bff8aede
tannishmango <tannishmango@protonmail.com>
2022-05-31 16:21:55 -0400

    update subgraph version

--- subgraphs/abracadabra/src/common/constants.ts
+++ subgraphs/abracadabra/src/common/constants.ts
@@ -184,6 +184,6 @@ export const BSC_NETWORK = "bsc";
 export const AVALANCHE_NETWORK = "avalanche";
 
 export const schemaVersion = "1.2.1";
-export const subgraphVersion = "1.0.2";
+export const subgraphVersion = "1.0.3";
 export const methodologyVersion = "1.0.0";
 
