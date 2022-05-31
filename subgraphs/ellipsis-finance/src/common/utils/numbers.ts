6315d9f99725b8f59ac7974a9004c4b2c9cca842
tannishmango <tannishmango@protonmail.com>
2022-05-31 16:35:41 -0400

    Update Deposit/Withdraw entities
    Track pool balances by events instead of contract queries

--- subgraphs/ellipsis-finance/src/common/utils/numbers.ts
+++ subgraphs/ellipsis-finance/src/common/utils/numbers.ts
@@ -1,4 +1,5 @@
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { BIGINT_NEG_ONE, BIGINT_ZERO } from "../constants";
 
 export function bigIntToBigDecimal(quantity: BigInt, decimals: i32 = 18): BigDecimal {
   return quantity.divDecimal(
@@ -60,4 +61,11 @@ export function rayToWad(a: BigInt): BigInt {
 export function wadToRay(a: BigInt): BigInt {
   const result = a.times(BigInt.fromI32(10).pow(9));
   return result;
+}
+
+export function absValBigInt(value: BigInt): BigInt {
+  if (value.lt(BIGINT_ZERO)) {
+    return value.times(BIGINT_NEG_ONE);
+  }
+  return value;
 }
