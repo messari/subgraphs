1c4e5ac8d2937efd2b985066245357dc729d8ce9
Dylan Melotik <dmelotik@wisc.edu>
2022-05-31 18:08:41 -0500

    update versions

--- subgraphs/compound-forks/cream-finance/src/mapping.ts
+++ subgraphs/compound-forks/cream-finance/src/mapping.ts
@@ -183,7 +183,7 @@ function getOrCreateProtocol(): LendingProtocol {
     "CREAM Finance",
     "cream-finance",
     "1.2.1",
-    "1.0.1",
+    "1.0.2",
     "1.0.0",
     network,
     comptroller.try_liquidationIncentiveMantissa(),
