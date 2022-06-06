e41f8b8b2454fafb6f75d99b6795ed1f202804ca
Samuel Pitko <samuel.pitko@gmail.com>
2022-06-06 17:09:10 +0300

    Add deployment configurations

--- subgraphs/qidao/prepare.sh
+++ subgraphs/qidao/prepare.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./node_modules/.bin/mustache config/$1.json subgraph.template.yaml > subgraph.yaml
+./node_modules/.bin/mustache protocols/qidao/config/networks/$1/$1.json protocols/qidao/config/templates/qidao.template.yaml > subgraph.yaml
