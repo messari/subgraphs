978fc59a2f51db5d42df00d758a18dd4cb98369a
dmelotik <dmelotik@wisc.edu>
2022-06-08 14:20:36 -0500

    update schema

--- subgraphs/layer-one/src/constants.ts
+++ subgraphs/layer-one/src/constants.ts
@@ -1,3 +1,47 @@
-import { BigDecimal } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal } from "@graphprotocol/graph-ts";
+
+
+
+/////////////////////////
+//// Enums / Classes ////
+/////////////////////////
+
+// The network names corresponding to the Network enum in the schema.
+// They also correspond to the ones in `dataSource.network()` after converting to lower case.
+// See below for a complete list:
+// https://thegraph.com/docs/en/hosted-service/what-is-hosted-service/#supported-networks-on-the-hosted-service
+export namespace Network {
+    export const ARBITRUM_ONE = "ARBITRUM_ONE";
+    export const AVALANCHE = "AVALANCHE";
+    export const AURORA = "AURORA";
+    export const BSC = "BSC"; // aka BNB Chain
+    export const CELO = "CELO";
+    export const MAINNET = "MAINNET"; // Ethereum mainnet
+    export const FANTOM = "FANTOM";
+    export const FUSE = "FUSE";
+    export const MOONBEAM = "MOONBEAM";
+    export const MOONRIVER = "MOONRIVER";
+    export const NEAR_MAINNET = "NEAR_MAINNET";
+    export const OPTIMISM = "OPTIMISM";
+    export const MATIC = "MATIC"; // aka Polygon
+    export const XDAI = "XDAI"; // aka Gnosis Chain
+  }
+
+/////////////////
+//// Numbers ////
+/////////////////
 
 export const BIGDECIMAL_ZERO = BigDecimal.fromString("0");
+
+///////////////////
+//// Addresses ////
+///////////////////
+
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+
+//////////////////////////////////
+//// Chain-specific constants ////
+//////////////////////////////////
+
+if 
