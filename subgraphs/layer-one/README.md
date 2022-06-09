efe2c5ff09fa03471a2b01763cd2fcc13ebf9cb9
dmelotik <dmelotik@wisc.edu>
2022-06-09 00:37:01 -0500

    update schema

--- subgraphs/layer-one/README.md
+++ subgraphs/layer-one/README.md
@@ -4,10 +4,37 @@ This subgraph and schema will gather a standard set of L1 data metrics that matt
 
 ## About the Schema
 
-The timeseries data is important. There is daily, hourly, and minutely data. The following data is collected:
+The timeseries data is important, and it follows our `Blockchain` data in addition to hourly and daily metrics.
 
-- TODO
+The following data is collected:
+
+- `totalBlocksMined` is the current block height
+- Blocks mined during this day/hour
+- `blocksPerDay` is a rolling target at the blocks/day using an algorithm
+- Difficulty, is the amount of effort required to mine a block
+  - `totalDifficulty` captures the difficulty it took to reach a given block
+  - daily/hourly difficulty is the new difficulty added during this day/hour
+- Average difficulty = ( difficulty during that timeseries entity ) / ( blocks mined in that timeseries entity )
+- `daily/hourlyGasUsed` represents the gas used during the day
+- `daily/hourlyGasLimit` is the gasLimit at last block in the timeseries
+- The total amount of fees burnt up until this day/hour
+  - Burnt Fees = `gasUsed` \* `baseFeePerGas`
+- The total rewards emitted up until day/hour
+- The reward emissions for a given timeseries entity
+- The supply of the native asset at a given timeseries entity
+
+### Calculating Reward Emissions
+
+Reward emissions refers to the amount of rewards in the native asset rewarded to the miner for mining a block.
+
+This is network specific:
+
+- Mainnet:
+  - Prior to London upgrade: Not possible b/c we do not have gas price
+  - Post London upgrade: Not possible b/c we do not have tip amount
 
 ### Reference Subgraphs
 
 - https://github.com/stakewise/subgraphs/tree/main/subgraphs/ethereum
+- https://github.com/graphprotocol/example-subgraph
+- https://ethereum.org/en/developers/docs/gas/
--- subgraphs/layer-one/src/mapping.ts
+++ subgraphs/layer-one/src/mapping.ts
@@ -1,12 +1,85 @@
 import { ethereum } from "@graphprotocol/graph-ts";
-import { Block } from "../generated/schema";
-import { BIGDECIMAL_ZERO } from "./constants";
+import { Block, Blockchain, Miner } from "../generated/schema";
+import {
+  BIGDECIMAL_ZERO,
+  BIGINT_ZERO,
+  BLOCKCHAIN_NAME,
+  INT_ZERO,
+  MAX_SUPPLY,
+  METHODOLOGY_VERSION,
+  NATIVE_TOKEN,
+  NATIVE_TOKEN_DECIMALS,
+  SCHEMA_VERSION,
+  SUBGRAPH_VERSION,
+} from "./constants";
 
 export function handleBlock(block: ethereum.Block): void {
   let blockEntity = new Block(block.hash.toHexString());
 
   blockEntity.blockNumber = block.number;
   blockEntity.timestamp = block.timestamp;
-  blockEntity.price = BIGDECIMAL_ZERO; // TODO add pricing logic
+  blockEntity.size = block.size;
+  blockEntity.baseFeePerGas = block.baseFeePerGas;
+  blockEntity.difficulty = block.difficulty;
+  blockEntity.totalDifficulty = block.totalDifficulty;
+  blockEntity.gasLimit = block.gasLimit;
+  blockEntity.gasUsed = block.gasUsed;
+  blockEntity.parentHash = block.parentHash.toHexString();
+
+  // get or create miner entity
+  let miner = getOrCreateMiner(block.author.toHexString());
+  miner.blocksMined++;
+  miner.totalDifficulty = miner.totalDifficulty.plus(block.difficulty);
+  miner.save();
+
+  blockEntity.author = miner.id;
   blockEntity.save();
+
+  // update blockchain entity
+  let blockchain = getOrCreateBlockchain(NATIVE_TOKEN);
+  blockchain.totalBlocksMined++;
+  blockchain.totalDifficulty = block.totalDifficulty;
+  // TODO: update blocksperday
+  blockchain.save();
+}
+
+/////////////////
+//// Getters ////
+/////////////////
+
+function getOrCreateBlockchain(id: string): Blockchain {
+  let blockchain = Blockchain.load(id);
+  if (!blockchain) {
+    blockchain = new Blockchain(id);
+    blockchain.schemaVersion = SCHEMA_VERSION;
+    blockchain.subgraphVersion = SUBGRAPH_VERSION;
+    blockchain.methodologyVersion = METHODOLOGY_VERSION;
+    blockchain.decimals = NATIVE_TOKEN_DECIMALS;
+    blockchain.name = BLOCKCHAIN_NAME;
+    blockchain.maxSupply = MAX_SUPPLY;
+    blockchain.currentSupply = BIGINT_ZERO;
+    blockchain.totalBlocksMined = INT_ZERO;
+    blockchain.totalDifficulty = BIGINT_ZERO;
+    blockchain.blocksPerDay = BIGDECIMAL_ZERO;
+
+    if (MAX_SUPPLY != BIGINT_ZERO) {
+      blockchain.maxSupply = MAX_SUPPLY;
+    }
+
+    blockchain.save();
+  }
+  return blockchain;
+}
+
+// grab a miner entity or create a new one if it does not exist
+function getOrCreateMiner(address: string): Miner {
+  let miner = Miner.load(address);
+  if (!miner) {
+    miner = new Miner(address);
+    miner.blocksMined = INT_ZERO;
+    miner.totalDifficulty = BIGINT_ZERO;
+    miner.totalRewards = BIGINT_ZERO;
+    miner.save();
+  }
+  return miner;
 }
