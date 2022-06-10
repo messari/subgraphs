c2a78191bfef9a6d9f68f4115a9b5126a490eaee
dmelotik <dmelotik@wisc.edu>
2022-06-09 20:22:23 -0500

    mappings

--- subgraphs/network/src/mapping.ts
+++ subgraphs/network/src/mapping.ts
@@ -1,51 +1,320 @@
-import { ethereum } from "@graphprotocol/graph-ts";
-import { Block, Blockchain, Miner } from "../generated/schema";
+import { ethereum, BigInt } from "@graphprotocol/graph-ts";
+import {
+  ActiveAuthor,
+  Author,
+  Block,
+  Blockchain,
+  DailySnapshot,
+  HourlySnapshot,
+} from "../generated/schema";
 import {
   BIGDECIMAL_ZERO,
   BIGINT_ZERO,
   BLOCKCHAIN_NAME,
+  INITIAL_SUPPLY,
+  IntervalType,
+  INT_NEGATIVE_ONE,
+  INT_TWO,
   INT_ZERO,
   MAX_SUPPLY,
   METHODOLOGY_VERSION,
   NATIVE_TOKEN,
   NATIVE_TOKEN_DECIMALS,
   SCHEMA_VERSION,
+  SECONDS_PER_DAY,
+  SECONDS_PER_HOUR,
   SUBGRAPH_VERSION,
 } from "./constants";
+import { exponentToBigDecimal, getBlocksPerDay } from "./utils";
 
 export function handleBlock(block: ethereum.Block): void {
-  let blockEntity = new Block(block.hash.toHexString());
-
-  blockEntity.blockNumber = block.number;
-  blockEntity.timestamp = block.timestamp;
-  blockEntity.size = block.size;
-  blockEntity.baseFeePerGas = block.baseFeePerGas;
-  blockEntity.difficulty = block.difficulty;
-  blockEntity.totalDifficulty = block.totalDifficulty;
-  blockEntity.gasLimit = block.gasLimit;
-  blockEntity.gasUsed = block.gasUsed;
-  blockEntity.parentHash = block.parentHash.toHexString();
-
-  // get or create miner entity
-  let miner = getOrCreateMiner(block.author.toHexString());
-  miner.blocksMined++;
-  miner.totalDifficulty = miner.totalDifficulty.plus(block.difficulty);
-  miner.save();
-
-  blockEntity.author = miner.id;
-  blockEntity.save();
+  let blockEntity = createBlock(block);
 
   // update blockchain entity
+  let blockchain = updateBlockchain(block);
+
+  // update author entity
+  let authorId = block.author.toHexString();
+  let author = Author.load(authorId);
+  if (!author) {
+    author = new Author(authorId);
+    author.cumulativeBlocksCreated = INT_ZERO;
+    author.cumulativeDifficulty = BIGINT_ZERO;
+    author.cumulativeRewards = BIGINT_ZERO;
+    author.save();
+
+    // update unique authors
+    blockchain.cumulativeUniqueAuthors++;
+    blockchain.save();
+  }
+  author.cumulativeBlocksCreated++;
+  author.cumulativeDifficulty = author.cumulativeDifficulty.plus(
+    block.difficulty
+  );
+  author.save();
+
+  // create/update daily/hourly metrics
+  updateMetrics(block, blockchain);
+}
+
+//////////////////
+//// Updaters ////
+//////////////////
+
+function updateBlockchain(block: ethereum.Block): Blockchain {
   let blockchain = getOrCreateBlockchain(NATIVE_TOKEN);
-  blockchain.totalBlocksMined++;
-  blockchain.totalDifficulty = block.totalDifficulty;
-  // TODO: update blocksperday
+  blockchain.blockHeight = block.number.toI32();
+  blockchain.cumulativeDifficulty = block.totalDifficulty;
+  blockchain.cumulativeGasUsed = blockchain.cumulativeGasUsed.plus(
+    block.gasUsed
+  );
+  blockchain.gasLimit = block.gasLimit;
+  if (block.baseFeePerGas) {
+    blockchain.cumulativeBurntFees = blockchain.cumulativeBurntFees.plus(
+      block.baseFeePerGas.times(block.gasUsed)
+    );
+  }
+  blockchain.blocksPerDay = getBlocksPerDay(block.timestamp, block.number);
   blockchain.save();
+
+  return blockchain;
+}
+
+function updateMetrics(block: ethereum.Block, blockchain: Blockchain): void {
+  let dailySnapshot = getOrCreateDailySnapshot(block.timestamp);
+  let hourlySnapshot = getOrCreateHourlySnapshot(block.timestamp);
+
+  // update snapshots
+  updateDailySnapshot(dailySnapshot, block, blockchain);
+  updateHourlySnapshot(hourlySnapshot, block, blockchain);
+}
+
+function updateDailySnapshot(
+  snapshot: DailySnapshot,
+  block: ethereum.Block,
+  blockchain: Blockchain
+): void {
+
+  // update overlapping fields for snapshot
+  snapshot.cumulativeUniqueAuthors = blockchain.cumulativeUniqueAuthors;
+  snapshot.blockHeight = blockchain.blockHeight;
+  snapshot.timestamp = block.timestamp;
+  snapshot.blocksPerDay = blockchain.blocksPerDay;
+  snapshot.cumulativeDifficulty = blockchain.cumulativeDifficulty;
+  snapshot.cumulativeBurntFees = blockchain.cumulativeBurntFees;
+  snapshot.cumulativeRewards = blockchain.cumulativeRewards;
+  snapshot.supply = blockchain.supply;
+
+  // check for new author
+  let id =
+    IntervalType.DAILY + "-" + block.author.toHexString() + "-" + snapshot.id;
+  let activeAuthor = ActiveAuthor.load(id);
+  if (!activeAuthor) {
+    activeAuthor = new ActiveAuthor(id);
+    activeAuthor.save();
+    snapshot.dailyActiveAuthors++;
+  }
+
+  // update daily metrics
+  snapshot.dailyBlocks++;
+  snapshot.dailyDifficulty = snapshot.dailyDifficulty.plus(block.difficulty);
+  snapshot.dailyMeanDifficulty = snapshot.dailyDifficulty
+    .toBigDecimal()
+    .div(snapshot.dailyBlocks.toBigDecimal());
+  snapshot.dailyCumulativeGasUsed = snapshot.dailyCumulativeGasUsed.plus(
+    block.gasUsed
+  );
+  snapshot.dailyCumulativeGasLimit = snapshot.dailyCumulativeGasLimit.plus(
+    block.gasLimit
+  );
+  snapshot.dailyMeanGasUsed = snapshot.dailyCumulativeGasUsed
+    .toBigDecimal()
+    .div(snapshot.dailyBlocks.toBigDecimal());
+  snapshot.dailyMeanGasLimit = snapshot.dailyCumulativeGasLimit
+    .toBigDecimal()
+    .div(snapshot.dailyBlocks.toBigDecimal());
+  if (block.baseFeePerGas) {
+    snapshot.dailyBurntFees = snapshot.dailyBurntFees.plus(
+      block.baseFeePerGas.times(block.gasUsed)
+    );
+  }
+  if (block.size) {
+    snapshot.dailyCumulativeSize = snapshot.dailyCumulativeSize.plus(
+      block.size
+    );
+  }
+  snapshot.dailyMeanBlockSize = snapshot.dailyCumulativeSize
+    .toBigDecimal()
+    .div(snapshot.dailyBlocks.toBigDecimal());
+
+  // calc mean block interval
+  // mean = (firstTimestamp - timestamp) / dailyBlocks
+  let timestampDiff = snapshot.firstTimestamp.minus(block.timestamp);
+  snapshot.dailyMeanBlockInterval = timestampDiff
+    .toBigDecimal()
+    .div(snapshot.dailyBlocks.toBigDecimal());
+
+  snapshot.save();
+}
+
+function updateHourlySnapshot(
+  snapshot: HourlySnapshot,
+  block: ethereum.Block,
+  blockchain: Blockchain
+): void {
+
+  // update overlapping fields for snapshot
+  snapshot.cumulativeUniqueAuthors = blockchain.cumulativeUniqueAuthors;
+  snapshot.blockHeight = blockchain.blockHeight;
+  snapshot.timestamp = block.timestamp;
+  snapshot.blocksPerDay = blockchain.blocksPerDay;
+  snapshot.cumulativeDifficulty = blockchain.cumulativeDifficulty;
+  snapshot.cumulativeBurntFees = blockchain.cumulativeBurntFees;
+  snapshot.cumulativeRewards = blockchain.cumulativeRewards;
+  snapshot.supply = blockchain.supply;
+
+  // check for new author
+  let id =
+    IntervalType.HOURLY + "-" + block.author.toHexString() + "-" + snapshot.id;
+  let activeAuthor = ActiveAuthor.load(id);
+  if (!activeAuthor) {
+    activeAuthor = new ActiveAuthor(id);
+    activeAuthor.save();
+    snapshot.hourlyActiveAuthors++;
+  }
+
+  // update hourly metrics
+  snapshot.hourlyBlocks++;
+  snapshot.hourlyDifficulty = snapshot.hourlyDifficulty.plus(block.difficulty);
+  snapshot.hourlyMeanDifficulty = snapshot.hourlyDifficulty
+    .toBigDecimal()
+    .div(snapshot.hourlyBlocks);
+  snapshot.hourlyCumulativeGasUsed = snapshot.hourlyCumulativeGasUsed.plus(
+    block.gasUsed
+  );
+  snapshot.hourlyCumulativeGasLimit = snapshot.hourlyCumulativeGasLimit.plus(
+    block.gasLimit
+  );
+  snapshot.hourlyMeanGasUsed = snapshot.hourlyCumulativeGasUsed
+    .toBigDecimal()
+    .div(snapshot.hourlyBlocks);
+  snapshot.hourlyMeanGasLimit = snapshot.hourlyCumulativeGasLimit
+    .toBigDecimal()
+    .div(snapshot.hourlyBlocks);
+  if (block.baseFeePerGas) {
+    snapshot.hourlyBurntFees = snapshot.hourlyBurntFees.plus(
+      block.baseFeePerGas.times(block.gasUsed)
+    );
+  }
+  if (block.size) {
+    snapshot.hourlyCumulativeSize = snapshot.hourlyCumulativeSize.plus(
+      block.size
+    );
+  }
+  snapshot.hourlyMeanBlockSize = snapshot.hourlyCumulativeSize
+    .toBigDecimal()
+    .div(snapshot.hourlyBlocks);
+
+  // calc mean block interval
+  // mean = (firstTimestamp - timestamp) / hourlyBlocks
+  let timestampDiff = snapshot.firstTimestamp.minus(block.timestamp);
+  snapshot.hourlyMeanBlockInterval = timestampDiff
+    .toBigDecimal()
+    .div(snapshot.hourlyBlocks);
+
+  snapshot.save();
 }
 
 /////////////////
 //// Getters ////
 /////////////////
+
+function getOrCreateDailySnapshot(timestamp: BigInt): DailySnapshot {
+  let id = (timestamp.toI64() / SECONDS_PER_DAY).toString();
+  let dailySnapshot = DailySnapshot.load(id);
+  if (!dailySnapshot) {
+    dailySnapshot = new DailySnapshot(id);
+    dailySnapshot.blockchain = NATIVE_TOKEN;
+    dailySnapshot.cumulativeUniqueAuthors = INT_ZERO;
+    dailySnapshot.dailyActiveAuthors = INT_ZERO;
+    dailySnapshot.blockHeight = INT_ZERO;
+    dailySnapshot.timestamp = timestamp;
+    dailySnapshot.dailyBlocks = INT_ZERO;
+    dailySnapshot.blocksPerDay = BIGDECIMAL_ZERO;
+    dailySnapshot.cumulativeDifficulty = BIGINT_ZERO;
+    dailySnapshot.dailyDifficulty = BIGINT_ZERO;
+    dailySnapshot.dailyMeanDifficulty = BIGDECIMAL_ZERO;
+    dailySnapshot.dailyCumulativeGasUsed = BIGINT_ZERO;
+    dailySnapshot.dailyCumulativeGasLimit = BIGINT_ZERO;
+    dailySnapshot.dailyMeanGasUsed = BIGDECIMAL_ZERO;
+    dailySnapshot.dailyMeanGasLimit = BIGDECIMAL_ZERO;
+    dailySnapshot.firstTimestamp = timestamp;
+    dailySnapshot.dailyMeanBlockInterval = BIGDECIMAL_ZERO;
+    dailySnapshot.dailyCumulativeSize = BIGINT_ZERO;
+    dailySnapshot.dailyMeanBlockSize = BIGDECIMAL_ZERO;
+
+    dailySnapshot.save();
+  }
+  return dailySnapshot;
+}
+
+function getOrCreateHourlySnapshot(timestamp: BigInt): HourlySnapshot {
+  let id = (timestamp.toI64() / SECONDS_PER_HOUR).toString();
+  let hourlySnapshot = HourlySnapshot.load(id);
+  if (!hourlySnapshot) {
+    hourlySnapshot = new HourlySnapshot(id);
+    hourlySnapshot.blockchain = NATIVE_TOKEN;
+    hourlySnapshot.cumulativeUniqueAuthors = INT_ZERO;
+    hourlySnapshot.hourlyActiveAuthors = INT_ZERO;
+    hourlySnapshot.blockHeight = INT_ZERO;
+    hourlySnapshot.timestamp = timestamp;
+    hourlySnapshot.hourlyBlocks = INT_ZERO;
+    hourlySnapshot.blocksPerDay = BIGDECIMAL_ZERO;
+    hourlySnapshot.cumulativeDifficulty = BIGINT_ZERO;
+    hourlySnapshot.hourlyDifficulty = BIGINT_ZERO;
+    hourlySnapshot.hourlyMeanDifficulty = BIGDECIMAL_ZERO;
+    hourlySnapshot.hourlyCumulativeGasUsed = BIGINT_ZERO;
+    hourlySnapshot.hourlyCumulativeGasLimit = BIGINT_ZERO;
+    hourlySnapshot.hourlyMeanGasUsed = BIGDECIMAL_ZERO;
+    hourlySnapshot.hourlyMeanGasLimit = BIGDECIMAL_ZERO;
+    hourlySnapshot.firstTimestamp = timestamp;
+    hourlySnapshot.hourlyMeanBlockInterval = BIGDECIMAL_ZERO;
+    hourlySnapshot.hourlyCumulativeSize = BIGINT_ZERO;
+    hourlySnapshot.hourlyMeanBlockSize = BIGDECIMAL_ZERO;
+
+    hourlySnapshot.save();
+  }
+  return hourlySnapshot;
+}
+
+function createBlock(block: ethereum.Block): Block {
+  let blockEntity = new Block(block.number.toString());
+
+  blockEntity.hash = block.hash.toHexString();
+  blockEntity.timestamp = block.timestamp;
+  blockEntity.author = block.author.toHexString();
+  blockEntity.size = block.size;
+  blockEntity.baseFeePerGas = block.baseFeePerGas;
+  blockEntity.difficulty = block.difficulty;
+  blockEntity.cumulativeDifficulty = block.totalDifficulty;
+  blockEntity.gasLimit = block.gasLimit;
+  blockEntity.gasUsed = block.gasUsed;
+  blockEntity.gasFilledPercentage = block.gasUsed
+    .toBigDecimal()
+    .div(block.gasLimit.toBigDecimal())
+    .times(exponentToBigDecimal(INT_TWO));
+  blockEntity.parentHash = block.parentHash.toHexString();
+  if (block.baseFeePerGas) {
+    blockEntity.burntFees = block.baseFeePerGas.times(block.gasUsed);
+  } else {
+    blockEntity.burntFees = BIGINT_ZERO;
+  }
+  blockEntity.day = (block.timestamp.toI64() / SECONDS_PER_DAY).toString();
+  blockEntity.hour = (block.timestamp.toI64() / SECONDS_PER_HOUR).toString();
+  blockEntity.save();
+
+  return blockEntity;
+}
 
 function getOrCreateBlockchain(id: string): Blockchain {
   let blockchain = Blockchain.load(id);
@@ -56,30 +325,24 @@ function getOrCreateBlockchain(id: string): Blockchain {
     blockchain.methodologyVersion = METHODOLOGY_VERSION;
     blockchain.decimals = NATIVE_TOKEN_DECIMALS;
     blockchain.name = BLOCKCHAIN_NAME;
-    blockchain.maxSupply = MAX_SUPPLY;
-    blockchain.currentSupply = BIGINT_ZERO;
-    blockchain.totalBlocksMined = INT_ZERO;
-    blockchain.totalDifficulty = BIGINT_ZERO;
-    blockchain.blocksPerDay = BIGDECIMAL_ZERO;
-
     if (MAX_SUPPLY != BIGINT_ZERO) {
       blockchain.maxSupply = MAX_SUPPLY;
     }
+    if (INITIAL_SUPPLY != INT_NEGATIVE_ONE) {
+      blockchain.supply = BigInt.fromI32(INITIAL_SUPPLY);
+    }
+    blockchain.blockHeight = INT_ZERO;
+    blockchain.cumulativeDifficulty = BIGINT_ZERO;
+    blockchain.cumulativeGasUsed = BIGINT_ZERO;
+    blockchain.gasLimit = BIGINT_ZERO;
+    blockchain.cumulativeBurntFees = BIGINT_ZERO;
+    blockchain.blocksPerDay = BIGDECIMAL_ZERO;
 
     blockchain.save();
   }
   return blockchain;
 }
 
-// grab a miner entity or create a new one if it does not exist
-function getOrCreateMiner(address: string): Miner {
-  let miner = Miner.load(address);
-  if (!miner) {
-    miner = new Miner(address);
-    miner.blocksMined = INT_ZERO;
-    miner.totalDifficulty = BIGINT_ZERO;
-    miner.totalRewards = BIGINT_ZERO;
-    miner.save();
-  }
-  return miner;
-}
+/////////////////
+//// Helpers ////
+/////////////////
---
+++ subgraphs/network/src/utils.ts
@@ -0,0 +1,221 @@
+import { BigDecimal, BigInt, dataSource, log } from "@graphprotocol/graph-ts";
+import { _CircularBuffer } from "../generated/schema";
+import {
+  BIGDECIMAL_ZERO,
+  INT_FOUR,
+  INT_NEGATIVE_ONE,
+  INT_ONE,
+  INT_TWO,
+  INT_ZERO,
+  SECONDS_PER_DAY,
+  SubgraphNetwork,
+} from "./constants";
+
+// turn exponent into a BigDecimal number
+export function exponentToBigDecimal(decimals: i32): BigDecimal {
+  let bigDecimal = BigDecimal.fromString("1");
+  for (let i = 0; i < decimals; i++) {
+    bigDecimal = bigDecimal.times(BigDecimal.fromString("10"));
+  }
+  return bigDecimal;
+}
+
+/////////////////////////
+//// Block Rate Calc ////
+/////////////////////////
+
+/* Constants needed for block rate calculations */
+
+export const CIRCULAR_BUFFER = "CIRCULAR_BUFFER";
+
+// only store blocks that are seperated by TIME_STORAGE_INTERVAL seconds
+export const TIMESTAMP_STORAGE_INTERVAL = 600;
+
+// BUFFER_SIZE is the max number of blocks stored at a time
+export const BUFFER_SIZE = 144;
+
+// the time interval we want to calculate rewards for (ie, forecast period)
+// here it is 86400 s = 1 day
+export const RATE_IN_SECONDS_BD = BigDecimal.fromString(
+  SECONDS_PER_DAY.toString()
+);
+
+export const STARTING_BLOCKS_PER_DAY = RATE_IN_SECONDS_BD.div(
+  getStartingBlockRate()
+);
+
+// The window size determines the range of blocks that you track from the current block minus the window size.
+// Window of block time used to calculate the moving average.
+export const WINDOW_SIZE_SECONDS = SECONDS_PER_DAY;
+export const WINDOW_SIZE_SECONDS_BD = BigDecimal.fromString(
+  WINDOW_SIZE_SECONDS.toString()
+);
+
+// calculate block rate
+export function getBlocksPerDay(
+  currentTimestamp: BigInt,
+  currentBlockNumber: BigInt
+): BigDecimal {
+  let circularBuffer = getOrCreateCircularBuffer();
+
+  // Create entity for the current block
+  let currentTimestampI32 = currentTimestamp.toI32();
+  let currentBlockNumberI32 = currentBlockNumber.toI32();
+
+  let blocks = circularBuffer.blocks;
+
+  // Interval between index and the index of the start of the window block
+  let windowWidth = abs(
+    circularBuffer.windowStartIndex - circularBuffer.nextIndex
+  );
+  if (windowWidth == INT_ZERO) {
+    if (circularBuffer.nextIndex >= circularBuffer.bufferSize) {
+      blocks[INT_ZERO] = currentTimestampI32;
+      blocks[INT_ONE] = currentBlockNumberI32;
+      circularBuffer.nextIndex = INT_TWO;
+    } else {
+      blocks[circularBuffer.nextIndex] = currentTimestampI32;
+      blocks[circularBuffer.nextIndex + INT_ONE] = currentBlockNumberI32;
+      circularBuffer.nextIndex += INT_TWO;
+    }
+
+    circularBuffer.save();
+
+    // return because there is only 1 reference point.
+    return circularBuffer.blocksPerDay;
+  }
+
+  // Add current timestamp and block numnber to array if new block is at least X blocks later than previously stored.
+  // Used to save memory and efficiency on array resizing.
+  let recentSavedTimestamp: i32;
+  if (circularBuffer.nextIndex == INT_ZERO) {
+    recentSavedTimestamp = blocks[circularBuffer.bufferSize - INT_TWO];
+  } else {
+    recentSavedTimestamp = blocks[circularBuffer.nextIndex - INT_TWO];
+  }
+
+  if (
+    currentTimestampI32 - recentSavedTimestamp <=
+    TIMESTAMP_STORAGE_INTERVAL
+  ) {
+    return circularBuffer.blocksPerDay;
+  }
+
+  blocks[circularBuffer.nextIndex] = currentTimestampI32;
+  blocks[circularBuffer.nextIndex + INT_ONE] = currentBlockNumberI32;
+  if (circularBuffer.nextIndex >= BUFFER_SIZE - INT_TWO) {
+    circularBuffer.nextIndex = INT_ZERO;
+  } else {
+    circularBuffer.nextIndex += INT_TWO;
+  }
+  // The timestamp at the start of the window (default 24 hours in seconds).
+  let startTimestamp = currentTimestampI32 - WINDOW_SIZE_SECONDS;
+
+  // Make sure to still have 2 blocks to calculate rate (This shouldn't happen past the beginning).
+  while (
+    abs(circularBuffer.nextIndex - circularBuffer.windowStartIndex) > INT_FOUR
+  ) {
+    let windowIndexBlockTimestamp = blocks[circularBuffer.windowStartIndex];
+
+    // Shift the start of the window if the current timestamp moves out of desired rate window
+    if (windowIndexBlockTimestamp < startTimestamp) {
+      circularBuffer.windowStartIndex =
+        circularBuffer.windowStartIndex + INT_TWO;
+      if (circularBuffer.windowStartIndex >= circularBuffer.bufferSize) {
+        circularBuffer.windowStartIndex = INT_ZERO;
+      }
+    } else {
+      break;
+    }
+  }
+
+  // Wideness of the window in seconds.
+  let windowSecondsCount = BigDecimal.fromString(
+    (currentTimestampI32 - blocks[circularBuffer.windowStartIndex]).toString()
+  );
+
+  // Wideness of the window in blocks.
+  let windowBlocksCount = BigDecimal.fromString(
+    (
+      currentBlockNumberI32 - blocks[circularBuffer.windowStartIndex + INT_ONE]
+    ).toString()
+  );
+
+  // Estimate block speed for the window in seconds.
+  let unnormalizedBlockSpeed =
+    WINDOW_SIZE_SECONDS_BD.div(windowSecondsCount).times(windowBlocksCount);
+
+  // block speed converted to specified rate.
+  let normalizedBlockSpeed = RATE_IN_SECONDS_BD.div(
+    WINDOW_SIZE_SECONDS_BD
+  ).times(unnormalizedBlockSpeed);
+
+  // Update BlockTracker with new values.
+  circularBuffer.blocksPerDay = normalizedBlockSpeed;
+  circularBuffer.blocks = blocks;
+
+  circularBuffer.save();
+
+  return circularBuffer.blocksPerDay;
+}
+
+// get or create the circular buffer
+function getOrCreateCircularBuffer(): _CircularBuffer {
+  let circularBuffer = _CircularBuffer.load(CIRCULAR_BUFFER);
+
+  if (!circularBuffer) {
+    circularBuffer = new _CircularBuffer(CIRCULAR_BUFFER);
+
+    let blocks = new Array<i32>(BUFFER_SIZE);
+    for (let i = INT_ZERO; i < BUFFER_SIZE; i += INT_TWO) {
+      blocks[i] = INT_NEGATIVE_ONE;
+      blocks[i + INT_ONE] = INT_NEGATIVE_ONE;
+    }
+
+    circularBuffer.blocks = blocks;
+    circularBuffer.windowStartIndex = INT_ZERO;
+    circularBuffer.nextIndex = INT_ZERO;
+    circularBuffer.bufferSize = BUFFER_SIZE;
+    circularBuffer.blocksPerDay = STARTING_BLOCKS_PER_DAY;
+
+    circularBuffer.save();
+  }
+
+  return circularBuffer;
+}
+
+// grabs the block rate initially when subgraph is deployed
+function getStartingBlockRate(): BigDecimal {
+  // Block rates pulled from google searches - rough estimates
+
+  let network = dataSource.network();
+  if (network == SubgraphNetwork.ETHEREUM) {
+    return BigDecimal.fromString("13.39");
+  } else if (network == SubgraphNetwork.ARBITRUM) {
+    return BigDecimal.fromString("15");
+  } else if (network == SubgraphNetwork.AURORA) {
+    return BigDecimal.fromString("1.03");
+  } else if (network == SubgraphNetwork.BSC) {
+    return BigDecimal.fromString("5");
+  } else if (network == SubgraphNetwork.CELO) {
+    return BigDecimal.fromString("5");
+  } else if (network == SubgraphNetwork.FANTOM) {
+    return BigDecimal.fromString("1");
+  } else if (network == SubgraphNetwork.OPTIMISM) {
+    return BigDecimal.fromString("12.5");
+  } else if (network == SubgraphNetwork.POLYGON) {
+    return BigDecimal.fromString("2");
+  } else if (network == SubgraphNetwork.XDAI) {
+    return BigDecimal.fromString("5");
+  }
+  // Blocks are mined as needed
+  // else if (network == SubgraphNetwork.AVALANCHE) return BigDecimal.fromString("2.5")
+  // else if (dataSource.network() == "cronos") return BigDecimal.fromString("13.39")
+  // else if (dataSource.network() == "harmony") return BigDecimal.fromString("13.39")
+  // else if (dataSource.network() == SubgraphNetwork.MOONBEAM) return BigDecimal.fromString("13.39")
+  // else if (dataSource.network() == SubgraphNetwork.MOONRIVER) return BigDecimal.fromString("13.39")
+  else {
+    log.warning("getStartingBlockRate(): Network not found", []);
+    return BIGDECIMAL_ZERO;
+  }
+}
