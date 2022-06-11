05cf0bbb1b8fc167b478d2de31a104e56474b3ec
loc <82075699+l0c4t0r@users.noreply.github.com>
2022-06-11 14:12:06 +0800

    Add Gamma Strategies subgraph (#231)
    * Track basic protocol/vault entities
    
    * Track usage metrics
    
    * WIP: add financials and USD pricing
    
    * Track rebalance events, add config for other networks
    
    * Add ability to skip certain price sources
    
    * Various bug fixes
    
    * minor fixes

---
+++ subgraphs/gamma-strategies/src/mappings/handlers.ts
@@ -0,0 +1,73 @@
+/* eslint-disable prefer-const */
+import { log } from "@graphprotocol/graph-ts";
+import { HypeAdded } from "../../generated/HypeRegistry/HypeRegistry";
+import {
+  Deposit,
+  Withdraw,
+  Rebalance,
+} from "../../generated/templates/Hypervisor/Hypervisor";
+import { Hypervisor as HypervisorContract } from "../../generated/templates/Hypervisor/Hypervisor";
+import { Hypervisor as HypervisorTemplate } from "../../generated/templates";
+import { getOrCreateYieldAggregator } from "../common/getters";
+import {
+  createDeposit,
+  createRebalance,
+  createWithdraw,
+} from "./helpers/events";
+import { updateUsageMetrics } from "./helpers/usageMetrics";
+import { getOrCreateVault, updateVaultSnapshots } from "./helpers/vaults";
+import { UsageType } from "../common/constants";
+import { updateRevenue, updateTvl } from "./helpers/financials";
+
+export function handleHypeAdded(event: HypeAdded): void {
+  let hypervisorContract = HypervisorContract.bind(event.params.hype);
+  let test_amount = hypervisorContract.try_getTotalAmounts();
+  // Prevents subgraph crashing from bad address added to registry
+  if (test_amount.reverted) {
+    log.warning("Could not add {}, does not appear to be a hypervisor", [
+      event.params.hype.toHex(),
+    ]);
+    return;
+  }
+
+  getOrCreateYieldAggregator(event.address);
+  getOrCreateVault(event.params.hype, event.block);
+
+  HypervisorTemplate.create(event.params.hype);
+}
+
+export function handleDeposit(event: Deposit): void {
+  // Create deposit event
+  createDeposit(event);
+
+  // Update vault token supply
+  let vault = getOrCreateVault(event.address, event.block);
+  vault.inputTokenBalance += event.params.shares;
+  vault.outputTokenSupply += event.params.shares;
+  vault.save();
+
+  updateUsageMetrics(event.params.to, UsageType.DEPOSIT, event);
+  updateTvl(event);
+  updateVaultSnapshots(event);
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  // Create withdraw event
+  createWithdraw(event);
+
+  // Update vault token supply
+  let vault = getOrCreateVault(event.address, event.block);
+  vault.inputTokenBalance -= event.params.shares;
+  vault.outputTokenSupply -= event.params.shares;
+  vault.save();
+
+  updateUsageMetrics(event.params.to, UsageType.WITHDRAW, event);
+  updateTvl(event);
+  updateVaultSnapshots(event);
+}
+
+export function handleRebalance(event: Rebalance): void {
+  createRebalance(event);
+  updateRevenue(event);
+  updateTvl(event);
+}
