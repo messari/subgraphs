990f73006ddea56ba3d74eab056b4ffd884ce438
Seven Shi <seven_shi@qq.com>
2022-06-15 00:14:33 +0800

    Balancer (#307)
    * Refactoring balancer
    
    * fix some issue
    
    * fix tvl
    
    * RollBACK
    
    * add beets
    
    * no message
    
    * no message
    
    * fix  pool level data issue
    
    * fix  issues
    
    * fix isBeets
    
    Co-authored-by: seven <seven.shi88@gmail.com>

--- subgraphs/balancer-v2/src/common/metrics.ts
+++ subgraphs/balancer-v2/src/common/metrics.ts
@@ -1,203 +1,187 @@
-import { Address, BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
+// import { log } from '@graphprotocol/graph-ts'
+import { BigDecimal, ethereum, BigInt, Address, log } from "@graphprotocol/graph-ts";
+import { Account, Token, ActiveAccount, _HelperStore, DexAmmProtocol, LiquidityPool } from "../../generated/schema";
+import { getUsdPrice } from "../prices";
+import {
+  SECONDS_PER_DAY,
+  INT_ZERO,
+  INT_ONE,
+  BIGDECIMAL_ONE,
+  UsageType,
+  SECONDS_PER_HOUR,
+  INT_TWO,
+  BIGINT_ZERO,
+  BIGINT_NEG_ONE,
+} from "./constants";
 import {
   getOrCreateDex,
-  getOrCreateFinancials,
-  getOrCreateDailyUsageMetricSnapshot,
-  updatePoolDailySnapshot,
-  updatePoolHourlySnapshot,
-  getOrCreateHourlyUsageMetricSnapshot,
+  getLiquidityPool,
+  getLiquidityPoolFee,
+  getLiquidityPoolAmounts,
   getOrCreateToken,
+  getOrCreateTokenWhitelist,
+  getOrCreateLiquidityPoolDailySnapshot,
+  getOrCreateLiquidityPoolHourlySnapshot,
+  getOrCreateFinancialsDailySnapshot,
+  getOrCreateUsageMetricDailySnapshot,
+  getOrCreateUsageMetricHourlySnapshot,
 } from "./getters";
-import { BIGDECIMAL_ONE, BIGDECIMAL_ZERO, FEE_COLLECTOR_ADDRESS, SECONDS_PER_DAY, SECONDS_PER_HOUR } from "./constants";
-import {
-  Account,
-  DailyActiveAccount,
-  LiquidityPool,
-  Swap,
-  LiquidityPoolFee,
-  Token,
-  LiquidityPoolDailySnapshot,
-  _HourlyActiveAccount,
-} from "../../generated/schema";
-import { calculatePrice, isUSDStable, TokenInfo, valueInUSD } from "./pricing";
-import { hasVirtualSupply, scaleDown } from "./tokens";
-import { ProtocolFeesCollector } from "../../generated/Vault/ProtocolFeesCollector";
-import { getUsdPrice } from "../prices";
+import { calculatePrice, isUSDStable, TokenInfo } from "./pricing";
+import { scaleDown } from "./tokens";
+import { percToDec, convertTokenToDecimal } from "./utils/utils";
 
+// Update FinancialsDailySnapshots entity
 export function updateFinancials(event: ethereum.Event): void {
-  let financialMetrics = getOrCreateFinancials(event);
-  let dex = getOrCreateDex();
-  let totalValueLocked = BIGDECIMAL_ZERO;
-  let totalVolumeUsd = BIGDECIMAL_ZERO;
-  let totalFeesUsd = BIGDECIMAL_ZERO;
-  let totalProtocolGeneratedFee = BIGDECIMAL_ZERO;
-  let totalRevenueGeneratedFee = BIGDECIMAL_ZERO;
+  let financialMetricsDaily = getOrCreateFinancialsDailySnapshot(event);
 
-  let dailyVolumeUsd = BIGDECIMAL_ZERO;
-
-  for (let i = 0; i < dex._poolIds.length; i++) {
-    let pool = LiquidityPool.load(dex._poolIds[i]);
-    if (pool) {
-      totalValueLocked = totalValueLocked.plus(pool.totalValueLockedUSD);
-      totalVolumeUsd = totalVolumeUsd.plus(pool.cumulativeVolumeUSD);
-      totalRevenueGeneratedFee = totalRevenueGeneratedFee.plus(pool._sideRevenueGeneratedFee);
-      totalProtocolGeneratedFee = totalProtocolGeneratedFee.plus(pool._protocolGeneratedFee);
-      totalFeesUsd = totalFeesUsd.plus(pool._totalSwapFee);
-      // Number of days since Unix epoch
-      const daysSinceEpoch: i64 = event.block.timestamp.toI64() / SECONDS_PER_DAY;
-      let dailySnapshot = LiquidityPoolDailySnapshot.load(pool.id.concat("-").concat(daysSinceEpoch.toString()));
-      if (dailySnapshot) {
-        dailyVolumeUsd = dailyVolumeUsd.plus(dailySnapshot.dailyVolumeUSD);
-      }
-    }
-  }
-
-  financialMetrics.totalValueLockedUSD = totalValueLocked;
-  financialMetrics.cumulativeVolumeUSD = totalVolumeUsd;
-  financialMetrics.cumulativeTotalRevenueUSD = totalFeesUsd;
-  financialMetrics.dailyVolumeUSD = dailyVolumeUsd;
-  financialMetrics.cumulativeSupplySideRevenueUSD = totalRevenueGeneratedFee;
-  financialMetrics.cumulativeProtocolSideRevenueUSD = totalProtocolGeneratedFee;
-  financialMetrics.blockNumber = event.block.number;
-  financialMetrics.timestamp = event.block.timestamp;
-
-  financialMetrics.save();
-}
-
-export function updateUsageMetrics(event: ethereum.Event, from: Address): void {
-  // Number of days since Unix epoch
-  let dailyId: i64 = event.block.timestamp.toI64() / SECONDS_PER_DAY;
-  // Number of hours since Unix epoch
-  let hourlyId: i64 = event.block.timestamp.toI64() / SECONDS_PER_HOUR;
-
-  let dailyUsageMetrics = getOrCreateDailyUsageMetricSnapshot(event);
-  let hourlyUsageMetrics = getOrCreateHourlyUsageMetricSnapshot(event);
+  let protocol = getOrCreateDex();
 
   // Update the block number and timestamp to that of the last transaction of that day
-  dailyUsageMetrics.blockNumber = event.block.number;
-  dailyUsageMetrics.timestamp = event.block.timestamp;
-  dailyUsageMetrics.dailyTransactionCount += 1;
+  financialMetricsDaily.blockNumber = event.block.number;
+  financialMetricsDaily.timestamp = event.block.timestamp;
+  financialMetricsDaily.totalValueLockedUSD = protocol.totalValueLockedUSD;
+  financialMetricsDaily.cumulativeVolumeUSD = protocol.cumulativeVolumeUSD;
 
-  // Update the block number and timestamp to that of the last transaction of that hour
-  hourlyUsageMetrics.blockNumber = event.block.number;
-  hourlyUsageMetrics.timestamp = event.block.timestamp;
-  hourlyUsageMetrics.hourlyTransactionCount += 1;
-
-  let accountId = from.toHexString();
-  let account = Account.load(accountId);
-  let protocol = getOrCreateDex();
-  if (!account) {
-    account = new Account(accountId);
-    account.save();
-
-    protocol.cumulativeUniqueUsers += 1;
-    protocol.save();
-  }
-  dailyUsageMetrics.cumulativeUniqueUsers = protocol.cumulativeUniqueUsers;
-  hourlyUsageMetrics.cumulativeUniqueUsers = protocol.cumulativeUniqueUsers;
-
-  // Combine the id and the user address to generate a unique user id for the day
-  let dailyActiveAccountId = dailyId.toString() + "-" + from.toHexString();
-  let dailyActiveAccount = DailyActiveAccount.load(dailyActiveAccountId);
-  if (!dailyActiveAccount) {
-    dailyActiveAccount = new DailyActiveAccount(dailyActiveAccountId);
-    dailyActiveAccount.save();
-    dailyUsageMetrics.dailyActiveUsers += 1;
-  }
-  dailyUsageMetrics.save();
-
-  // Combine the id and the user address to generate a unique user id for the hour
-  let hourlyActiveAccountId = hourlyId.toString() + "-" + from.toHexString();
-  let hourlyActiveAccount = _HourlyActiveAccount.load(hourlyActiveAccountId);
-  if (!hourlyActiveAccount) {
-    hourlyActiveAccount = new _HourlyActiveAccount(hourlyActiveAccountId);
-    hourlyActiveAccount.save();
-    hourlyUsageMetrics.hourlyActiveUsers += 1;
-  }
-
-  hourlyUsageMetrics.save();
+  financialMetricsDaily.save();
 }
 
-export function updatePoolMetrics(event: ethereum.Event, pool: LiquidityPool): void {
-  let newPoolLiquidity = BigDecimal.zero();
-  let tokenWithoutPrice = false;
-  for (let i = 0; i < pool.inputTokens.length; i++) {
-    let currentToken = Address.fromString(pool.inputTokens[i]);
-    let currentTokenBalance = scaleDown(pool.inputTokenBalances[i], Address.fromString(pool.inputTokens[i]));
+// Update usage metrics entities
+export function updateUsageMetrics(event: ethereum.Event, fromAddress: Address, usageType: string): void {
+  let from = fromAddress.toHexString();
 
-    if (isUSDStable(currentToken)) {
-      newPoolLiquidity = newPoolLiquidity.plus(currentTokenBalance);
-      continue;
-    }
-
-    if (pool.outputToken == pool.inputTokens[i] && pool._hasVirtualSupply) continue;
-
-    const token = Token.load(currentToken.toHexString());
-    let tokenPrice: BigDecimal | null = token!.lastPriceUSD;
-    if (!token || !tokenPrice) {
-      tokenWithoutPrice = true;
-      continue;
-    }
-
-    let currentTokenValueInUsd = valueInUSD(currentTokenBalance, Address.fromString(token.id));
-    newPoolLiquidity = newPoolLiquidity.plus(currentTokenValueInUsd);
-  }
-
-  if (tokenWithoutPrice) return;
-
-  let oldPoolLiquidity = pool.totalValueLockedUSD;
-  let liquidityChange = newPoolLiquidity.minus(oldPoolLiquidity);
+  let usageMetricsDaily = getOrCreateUsageMetricDailySnapshot(event);
+  let usageMetricsHourly = getOrCreateUsageMetricHourlySnapshot(event);
 
   let protocol = getOrCreateDex();
-  let financials = getOrCreateFinancials(event);
-  protocol.totalValueLockedUSD = protocol.totalValueLockedUSD.plus(liquidityChange);
-  const swap = Swap.load(event.transaction.hash.toHexString().concat("-").concat(event.logIndex.toHexString()));
-  if (swap) {
-    let divisor =
-      swap.amountInUSD.gt(BIGDECIMAL_ZERO) && swap.amountOutUSD.gt(BIGDECIMAL_ZERO)
-        ? BigDecimal.fromString("2")
-        : BIGDECIMAL_ONE;
-    let swapValue = swap.amountInUSD.plus(swap.amountOutUSD).div(divisor);
-    let fee = LiquidityPoolFee.load(pool.fees[0]);
-    if (fee && swap.tokenIn != pool.outputToken && swap.tokenOut != pool.outputToken) {
-      let feesCollector = ProtocolFeesCollector.bind(FEE_COLLECTOR_ADDRESS);
-      let protocolSwapPercentage = scaleDown(feesCollector.getSwapFeePercentage(), null);
-      let supplySidePercentage = BigDecimal.fromString("1").minus(protocolSwapPercentage);
-      let swapFee = swapValue.times(fee.feePercentage);
 
-      pool._totalSwapFee = pool._totalSwapFee.plus(swapFee);
-      pool._protocolGeneratedFee = pool._protocolGeneratedFee.plus(swapFee.times(protocolSwapPercentage));
-      pool._sideRevenueGeneratedFee = pool._sideRevenueGeneratedFee.plus(swapFee.times(supplySidePercentage));
-      protocol.cumulativeSupplySideRevenueUSD = protocol.cumulativeSupplySideRevenueUSD.plus(
-        swapFee.times(supplySidePercentage),
-      );
-      protocol.cumulativeProtocolSideRevenueUSD = protocol.cumulativeProtocolSideRevenueUSD.plus(
-        swapFee.times(protocolSwapPercentage),
-      );
-      protocol.cumulativeTotalRevenueUSD = protocol.cumulativeVolumeUSD.plus(swapFee);
-      financials.dailyProtocolSideRevenueUSD = financials.dailyProtocolSideRevenueUSD.plus(
-        swapFee.times(protocolSwapPercentage),
-      );
-      financials.dailySupplySideRevenueUSD = financials.dailySupplySideRevenueUSD.plus(
-        swapFee.times(supplySidePercentage),
-      );
-      financials.dailyTotalRevenueUSD = financials.dailyTotalRevenueUSD.plus(swapFee);
-      financials.dailyVolumeUSD = financials.dailyVolumeUSD.plus(swapValue);
-    }
-    pool.cumulativeVolumeUSD = pool.cumulativeVolumeUSD.plus(swapValue);
-    protocol.cumulativeVolumeUSD = protocol.cumulativeVolumeUSD.plus(swapValue);
-  }
-  pool.totalValueLockedUSD = newPoolLiquidity;
-  const outputTokenSupply = scaleDown(pool.outputTokenSupply, Address.fromString(pool.outputToken));
-  if (outputTokenSupply.gt(BIGDECIMAL_ZERO)) {
-    pool.outputTokenPriceUSD = newPoolLiquidity.div(outputTokenSupply);
+  // Update the block number and timestamp to that of the last transaction of that day
+  usageMetricsDaily.blockNumber = event.block.number;
+  usageMetricsDaily.timestamp = event.block.timestamp;
+  usageMetricsDaily.dailyTransactionCount += INT_ONE;
+
+  usageMetricsHourly.blockNumber = event.block.number;
+  usageMetricsHourly.timestamp = event.block.timestamp;
+  usageMetricsHourly.hourlyTransactionCount += INT_ONE;
+
+  if (usageType == UsageType.DEPOSIT) {
+    usageMetricsDaily.dailyDepositCount += INT_ONE;
+    usageMetricsHourly.hourlyDepositCount += INT_ONE;
+  } else if (usageType == UsageType.WITHDRAW) {
+    usageMetricsDaily.dailyWithdrawCount += INT_ONE;
+    usageMetricsHourly.hourlyWithdrawCount += INT_ONE;
+  } else if (usageType == UsageType.SWAP) {
+    usageMetricsDaily.dailySwapCount += INT_ONE;
+    usageMetricsHourly.hourlySwapCount += INT_ONE;
   }
 
-  financials.save();
+  // Number of days since Unix epoch
+  let day = event.block.timestamp.toI32() / SECONDS_PER_DAY;
+  let hour = event.block.timestamp.toI32() / SECONDS_PER_HOUR;
+
+  let dayId = day.toString();
+  let hourId = hour.toString();
+
+  // Combine the id and the user address to generate a unique user id for the day
+  let dailyActiveAccountId = from.concat("-").concat(dayId);
+  let dailyActiveAccount = ActiveAccount.load(dailyActiveAccountId);
+  if (!dailyActiveAccount) {
+    dailyActiveAccount = new ActiveAccount(dailyActiveAccountId);
+    usageMetricsDaily.dailyActiveUsers += INT_ONE;
+    dailyActiveAccount.save();
+  }
+
+  let hourlyActiveAccountId = from.concat("-").concat(hourId);
+  let hourlyActiveAccount = ActiveAccount.load(hourlyActiveAccountId);
+  if (!hourlyActiveAccount) {
+    hourlyActiveAccount = new ActiveAccount(hourlyActiveAccountId);
+    usageMetricsHourly.hourlyActiveUsers += INT_ONE;
+    hourlyActiveAccount.save();
+  }
+
+  let account = Account.load(from);
+  if (!account) {
+    account = new Account(from);
+    protocol.cumulativeUniqueUsers += INT_ONE;
+    account.save();
+  }
+  usageMetricsDaily.cumulativeUniqueUsers = protocol.cumulativeUniqueUsers;
+  usageMetricsHourly.cumulativeUniqueUsers = protocol.cumulativeUniqueUsers;
+
+  usageMetricsDaily.save();
+  usageMetricsHourly.save();
+  protocol.save();
+}
+
+// Update UsagePoolDailySnapshot entity
+export function updatePoolMetrics(event: ethereum.Event, poolAddress: string): void {
+  // get or create pool metrics
+  let pool = getLiquidityPool(poolAddress);
+  let poolMetricsDaily = getOrCreateLiquidityPoolDailySnapshot(event,poolAddress);
+  let poolMetricsHourly = getOrCreateLiquidityPoolHourlySnapshot(event,poolAddress);
+
+
+
+  // Update the block number and timestamp to that of the last transaction of that day
+  poolMetricsDaily.totalValueLockedUSD = pool.totalValueLockedUSD;
+  poolMetricsDaily.cumulativeVolumeUSD = pool.cumulativeVolumeUSD;
+  poolMetricsDaily.inputTokenBalances = pool.inputTokenBalances;
+  poolMetricsDaily.inputTokenWeights = pool.inputTokenWeights;
+  poolMetricsDaily.outputTokenSupply = pool.outputTokenSupply;
+  poolMetricsDaily.outputTokenPriceUSD = pool.outputTokenPriceUSD;
+  poolMetricsDaily.blockNumber = event.block.number;
+  poolMetricsDaily.timestamp = event.block.timestamp;
+
+  poolMetricsHourly.totalValueLockedUSD = pool.totalValueLockedUSD;
+  poolMetricsHourly.cumulativeVolumeUSD = pool.cumulativeVolumeUSD;
+  poolMetricsHourly.inputTokenBalances = pool.inputTokenBalances;
+  poolMetricsHourly.inputTokenWeights = pool.inputTokenWeights;
+  poolMetricsHourly.outputTokenSupply = pool.outputTokenSupply;
+  poolMetricsHourly.outputTokenPriceUSD = pool.outputTokenPriceUSD;
+  poolMetricsHourly.blockNumber = event.block.number;
+  poolMetricsHourly.timestamp = event.block.timestamp;
+
+  poolMetricsDaily.save();
+  poolMetricsHourly.save();
+}
+
+// Update the volume for all relavant entities
+export function updateVolumeAndFee(
+  event: ethereum.Event,
+  protocol: DexAmmProtocol,
+  pool: LiquidityPool,
+  trackedAmountUSD: BigDecimal,
+): void {
+  let financialMetrics = getOrCreateFinancialsDailySnapshot(event);
+  let supplyFee = getLiquidityPoolFee(pool.fees[INT_ZERO]);
+  let protocolFee = getLiquidityPoolFee(pool.fees[INT_ONE]);
+  let tradingFee = getLiquidityPoolFee(pool.fees[INT_TWO]);
+
+
+  financialMetrics.dailyVolumeUSD = financialMetrics.dailyVolumeUSD.plus(trackedAmountUSD);
+  pool.cumulativeVolumeUSD = pool.cumulativeVolumeUSD.plus(trackedAmountUSD);
+  protocol.cumulativeVolumeUSD = protocol.cumulativeVolumeUSD.plus(trackedAmountUSD);
+
+  let tradingFeeAmountUSD = trackedAmountUSD.times(tradingFee.feePercentage!);
+  let supplyFeeAmountUSD = tradingFeeAmountUSD.times(supplyFee.feePercentage!);
+  let protocolFeeAmountUSD = tradingFeeAmountUSD.times(protocolFee.feePercentage!);
+
+  protocol.cumulativeTotalRevenueUSD = protocol.cumulativeTotalRevenueUSD.plus(tradingFeeAmountUSD);
+  protocol.cumulativeSupplySideRevenueUSD = protocol.cumulativeSupplySideRevenueUSD.plus(supplyFeeAmountUSD);
+  protocol.cumulativeProtocolSideRevenueUSD = protocol.cumulativeProtocolSideRevenueUSD.plus(protocolFeeAmountUSD);
+
+  financialMetrics.dailyTotalRevenueUSD = financialMetrics.dailyTotalRevenueUSD.plus(tradingFeeAmountUSD);
+  financialMetrics.dailySupplySideRevenueUSD = financialMetrics.dailySupplySideRevenueUSD.plus(supplyFeeAmountUSD);
+  financialMetrics.dailyProtocolSideRevenueUSD =
+    financialMetrics.dailyProtocolSideRevenueUSD.plus(protocolFeeAmountUSD);
+
+  financialMetrics.cumulativeTotalRevenueUSD = protocol.cumulativeTotalRevenueUSD;
+  financialMetrics.cumulativeSupplySideRevenueUSD = protocol.cumulativeSupplySideRevenueUSD;
+  financialMetrics.cumulativeProtocolSideRevenueUSD = protocol.cumulativeProtocolSideRevenueUSD;
+
+  financialMetrics.save();
+
   protocol.save();
   pool.save();
-  updatePoolDailySnapshot(event, pool);
-  updatePoolHourlySnapshot(event, pool);
 }
 
 export function updateTokenPrice(
@@ -234,7 +218,7 @@ export function updateTokenPrice(
   );
 
   if (tokenInfo) {
-    const token = getOrCreateToken(tokenInfo.address);
+    const token = getOrCreateToken(tokenInfo.address.toHexString());
     const index = tokenInfo.address == tokenOut ? tokenOutIndex : tokenInIndex;
     const currentBalance = scaleDown(pool.inputTokenBalances[index], Address.fromString(pool.inputTokens[index]));
     // We check if current balance multiplied by the price is over 10k USD, if not,
@@ -248,17 +232,17 @@ export function updateTokenPrice(
     }
   }
 
-  if (getOrCreateDex().network == "MATIC" || getOrCreateDex().network == "ARBITRUM_ONE") return;
+  if (getOrCreateDex().network == "MATIC") return;
 
   if (!isUSDStable(tokenIn)) {
-    const token = getOrCreateToken(tokenIn);
+    const token = getOrCreateToken(tokenIn.toHexString());
     token.lastPriceUSD = getUsdPrice(tokenIn, BIGDECIMAL_ONE);
     token.lastPriceBlockNumber = blockNumber;
     token.save();
   }
 
   if (!isUSDStable(tokenOut)) {
-    const token = getOrCreateToken(tokenOut);
+    const token = getOrCreateToken(tokenOut.toHexString());
     token.lastPriceUSD = getUsdPrice(tokenOut, BIGDECIMAL_ONE);
     token.lastPriceBlockNumber = blockNumber;
     token.save();
