411d54dfa2d0076f901ca27e006a375504ee3bd1
tnkrxyz <tnkrxyz@gmail.com>
2022-06-14 05:57:28 +0000

    Refactored to make compound forks handler generic

--- subgraphs/compound-forks/benqi/src/mapping.ts
+++ subgraphs/compound-forks/benqi/src/mapping.ts
@@ -89,7 +89,8 @@ class RewardTokenEmission {
 
 export function handleNewPriceOracle(event: NewPriceOracle): void {
   let protocol = getOrCreateProtocol();
-  _handleNewPriceOracle(protocol, event);
+  let newPriceOracle = event.params.newPriceOracle;
+  _handleNewPriceOracle(protocol, newPriceOracle);
 }
 
 export function handleMarketListed(event: MarketListed): void {
@@ -153,42 +154,69 @@ export function handleMarketListed(event: MarketListed): void {
 }
 
 export function handleNewCollateralFactor(event: NewCollateralFactor): void {
-  _handleNewCollateralFactor(event);
+  let marketID = event.params.cToken.toHexString();
+  let collateralFactorMantissa = event.params.newCollateralFactorMantissa;
+  _handleNewCollateralFactor(marketID, collateralFactorMantissa);
 }
 
 export function handleNewLiquidationIncentive(
   event: NewLiquidationIncentive
 ): void {
   let protocol = getOrCreateProtocol();
-  _handleNewLiquidationIncentive(protocol, event);
+  let newLiquidationIncentive = event.params.newLiquidationIncentiveMantissa;
+  _handleNewLiquidationIncentive(protocol, newLiquidationIncentive);
 }
 
 export function handleActionPaused(event: ActionPaused1): void {
-  _handleActionPaused(event);
+  let marketID = event.params.cToken.toHexString();
+  let action = event.params.action;
+  let pauseState = event.params.pauseState;
+  _handleActionPaused(marketID, action, pauseState);
 }
 
 export function handleNewReserveFactor(event: NewReserveFactor): void {
-  _handleNewReserveFactor(event);
+  let marketID = event.address.toHexString();
+  let newReserveFactorMantissa = event.params.newReserveFactorMantissa;
+  _handleNewReserveFactor(marketID, newReserveFactorMantissa);
 }
 
 export function handleMint(event: Mint): void {
-  _handleMint(comptrollerAddr, event);
+  let minter = event.params.minter;
+  let mintAmount = event.params.mintAmount;
+  _handleMint(comptrollerAddr, minter, mintAmount, event);
 }
 
 export function handleRedeem(event: Redeem): void {
-  _handleRedeem(comptrollerAddr, event);
+  let redeemer = event.params.redeemer;
+  let redeemAmount = event.params.redeemAmount;
+  _handleRedeem(comptrollerAddr, redeemer, redeemAmount, event);
 }
 
 export function handleBorrow(event: BorrowEvent): void {
-  _handleBorrow(comptrollerAddr, event);
+  let borrower = event.params.borrower;
+  let borrowAmount = event.params.borrowAmount;
+  _handleBorrow(comptrollerAddr, borrower, borrowAmount, event);
 }
 
 export function handleRepayBorrow(event: RepayBorrow): void {
-  _handleRepayBorrow(comptrollerAddr, event);
+  let payer = event.params.payer;
+  let repayAmount = event.params.repayAmount;
+  _handleRepayBorrow(comptrollerAddr, payer, repayAmount, event);
 }
 
 export function handleLiquidateBorrow(event: LiquidateBorrow): void {
-  _handleLiquidateBorrow(comptrollerAddr, event);
+  let cTokenCollateral = event.params.cTokenCollateral;
+  let liquidator = event.params.liquidator;
+  let seizeTokens = event.params.seizeTokens;
+  let repayAmount = event.params.repayAmount;
+  _handleLiquidateBorrow(
+    comptrollerAddr,
+    cTokenCollateral,
+    liquidator,
+    seizeTokens,
+    repayAmount,
+    event
+  );
 }
 
 export function handleAccrueInterest(event: AccrueInterest): void {
@@ -209,7 +237,15 @@ export function handleAccrueInterest(event: AccrueInterest): void {
     SECONDS_PER_YEAR
   );
 
-  _handleAccrueInterest(updateMarketData, comptrollerAddr, event);
+  let interestAccumulated = event.params.interestAccumulated;
+  let totalBorrows = event.params.totalBorrows;
+  _handleAccrueInterest(
+    updateMarketData,
+    comptrollerAddr,
+    interestAccumulated,
+    totalBorrows,
+    event
+  );
 }
 
 function getOrCreateProtocol(): LendingProtocol {
@@ -219,7 +255,7 @@ function getOrCreateProtocol(): LendingProtocol {
     "BENQI",
     "benqi",
     "1.2.1",
-    "1.0.4",
+    "1.0.5",
     "1.0.0",
     Network.AVALANCHE,
     comptroller.try_liquidationIncentiveMantissa(),
