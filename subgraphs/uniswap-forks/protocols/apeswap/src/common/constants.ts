94021c2b529f62a892fde5e60d8274271ce8a26d
Chris Steege <steegecas@gmail.com>
2022-06-14 22:10:57 -0400

    Removed warnings and added spaces at end of files

--- subgraphs/uniswap-forks/protocols/apeswap/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/apeswap/src/common/constants.ts
@@ -8,5 +8,3 @@ export const PROTOCOL_SUBGRAPH_VERSION = "1.1.1";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 export const PROTOCOL_NAME = "ApeSwap";
 export const PROTOCOL_SLUG = "apeswap";
-
-log.warning("HELLO", [])
