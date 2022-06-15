0bd5070d2da3139e4a439fcdf93675c324dedcaf
Andrés Fernández <andresavena19@gmail.com>
2022-06-15 18:30:47 -0500

    feature: financial metrics

--- subgraphs/harvest-finance/src/modules/timeSeries.ts
+++ subgraphs/harvest-finance/src/modules/timeSeries.ts
@@ -1,9 +1,32 @@
 import { decimal, integer } from "@protofire/subgraph-toolkit";
-import { UsageMetricsDailySnapshot, UsageMetricsHourlySnapshot } from "../../generated/schema"
+import { FinancialsDailySnapshot, UsageMetricsDailySnapshot, UsageMetricsHourlySnapshot } from "../../generated/schema"
 import { protocol } from "./protocol";
 import { shared } from "./shared";
 
 export namespace timeSeries {
+
+	export namespace financials {
+
+		export function loadOrCreateFinancialDailySnapshot(id: string): FinancialsDailySnapshot {
+			let entity = FinancialsDailySnapshot.load(id)
+			if (entity == null) {
+				entity = new FinancialsDailySnapshot(id)
+				entity.protocol = protocol.getProtocolId(shared.constants.PROTOCOL_ID);
+				entity.totalValueLockedUSD = decimal.ZERO
+				entity.protocolControlledValueUSD = decimal.ZERO
+				entity.dailySupplySideRevenueUSD = decimal.ZERO
+				entity.cumulativeSupplySideRevenueUSD = decimal.ZERO
+				entity.dailyProtocolSideRevenueUSD = decimal.ZERO
+				entity.cumulativeProtocolSideRevenueUSD = decimal.ZERO
+				entity.dailyTotalRevenueUSD = decimal.ZERO
+				entity.cumulativeTotalRevenueUSD = decimal.ZERO
+				entity.blockNumber = integer.ZERO
+				entity.timestamp = integer.ZERO
+			}
+			return entity as FinancialsDailySnapshot
+		}
+
+	}
 	export namespace usageMetrics {
 		export function loadOrCreateHourlySnapshot(id: string): UsageMetricsHourlySnapshot {
 			let entity = UsageMetricsHourlySnapshot.load(id)
