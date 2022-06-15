f37b16521d50e6997442e4b9fed564642e5d3e1c
Harsh Agarwal <hagarwal9200@gmail.com>
2022-06-15 22:15:38 +0530

    Stake DAO - Schema update 1.3.0 (#328)

--- subgraphs/stakedao/src/common/constants.ts
+++ subgraphs/stakedao/src/common/constants.ts
@@ -6,20 +6,28 @@ import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 // https://thegraph.com/docs/en/hosted-service/what-is-hosted-service/#supported-networks-on-the-hosted-service
 export namespace Network {
   export const ARBITRUM_ONE = "ARBITRUM_ONE";
+  export const ARWEAVE_MAINNET = "ARWEAVE_MAINNET";
   export const AVALANCHE = "AVALANCHE";
+  export const BOBA = "BOBA";
   export const AURORA = "AURORA";
   export const BSC = "BSC"; // aka BNB Chain
   export const CELO = "CELO";
+  export const COSMOS = "COSMOS";
+  export const CRONOS = "CRONOS";
   export const MAINNET = "MAINNET"; // Ethereum mainnet
   export const FANTOM = "FANTOM";
   export const FUSE = "FUSE";
+  export const HARMONY = "HARMONY";
+  export const JUNO = "JUNO";
   export const MOONBEAM = "MOONBEAM";
   export const MOONRIVER = "MOONRIVER";
   export const NEAR_MAINNET = "NEAR_MAINNET";
   export const OPTIMISM = "OPTIMISM";
+  export const OSMOSIS = "OSMOSIS";
   export const MATIC = "MATIC"; // aka Polygon
   export const XDAI = "XDAI"; // aka Gnosis Chain
 }
+
 export namespace ProtocolType {
   export const EXCHANGE = "EXCHANGE";
   export const LENDING = "LENDING";
@@ -43,8 +51,8 @@ export namespace RewardTokenType {
 export namespace Protocol {
   export const NAME = "Stake DAO";
   export const SLUG = "stake-dao";
-  export const SCHEMA_VERSION = "1.2.1";
-  export const SUBGRAPH_VERSION = "1.2.0";
+  export const SCHEMA_VERSION = "1.3.0";
+  export const SUBGRAPH_VERSION = "1.3.0";
   export const METHODOLOGY_VERSION = "1.0.0";
   export const NETWORK = Network.MAINNET;
   export const TYPE = ProtocolType.YIELD;
