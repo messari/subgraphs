33e50fb7bfc57d95e5a49d7935492cc287e3ac51
Harsh <hagarwal9200@gmail.com>
2022-06-20 18:13:09 +0530

    Update Schema version

--- subgraphs/makerdao/src/common/constants.ts
+++ subgraphs/makerdao/src/common/constants.ts
@@ -6,8 +6,8 @@ import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 export const PROTOCOL_NAME = "MakerDao";
 export const PROTOCOL_SLUG = "makerdao";
-export const PROTOCOL_SCHEMA_VERSION = "1.2.1";
-export const PROTOCOL_SUBGRAPH_VERSION = "1.0.1";
+export const PROTOCOL_SCHEMA_VERSION = "1.3.0";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.1.0";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 
 ////////////////////////
