79b8ba89193886c749b5bc92301f07676e3c8dec
Vincent Wen <wensi.isnew@gmail.com>
2022-06-19 22:58:13 -0700

    Update Balancer/Beethoven version

--- subgraphs/balancer-v2/src/common/constants.ts
+++ subgraphs/balancer-v2/src/common/constants.ts
@@ -9,7 +9,7 @@ import { BigDecimal, BigInt, Address, dataSource } from "@graphprotocol/graph-ts
 ////////////////////
 
 export const PROTOCOL_SCHEMA_VERSION = "1.3.0";
-export const PROTOCOL_SUBGRAPH_VERSION = "1.1.1";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.1.2";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 
 export const PROTOCOL_NAME = "Beethoven X";
