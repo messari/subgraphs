1ef017592924b0429fdb5c12c060139e63427447
Andrés Fernández <andresavena19@gmail.com>
2022-06-21 13:42:53 -0500

    wip: vault listener

--- subgraphs/harvest-finance/src/mappings/VaultListener.ts
+++ subgraphs/harvest-finance/src/mappings/VaultListener.ts
@@ -1,4 +1,16 @@
-export function handleDeposit(): void { }
+import { Deposit } from "../../generated/ControllerListener/VaultContract";
+import { accounts, vaults } from "../modules";
+
+export function handleDeposit(event: Deposit): void {
+
+	let account = accounts.loadOrCreateActiveAccount(event.params.beneficiary)
+	account.save()
+	// Increase protocl users
+
+	let vault = vaults.loadOrCreateVault(event.address.toHexString())
+	vault.save()
+}
+
 export function handleStrategyAnnounced(): void { }
 export function handleStrategyChanged(): void { }
 export function handleDoHardWorkCall(): void { }
