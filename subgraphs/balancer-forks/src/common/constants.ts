bbc36a0c8d64b44d57fbe42646d8411d3ffe5b63
steegecs <56660047+steegecs@users.noreply.github.com>
2022-06-21 14:10:57 -0700

    Balancer V2 - Update dir structure and package.json (#367)
    * Updated dir structure and package.json
    
    * Updated package.json script
    
    * Updated balancer name in deployment configurations
    
    * Update deploy script in package.json
    
    * Added Beethoven X
    
    * Modify deployment configurations and remove old directory

--- subgraphs/balancer-forks/src/common/constants.ts
+++ subgraphs/balancer-forks/src/common/constants.ts
@@ -12,36 +12,39 @@ export const PROTOCOL_SCHEMA_VERSION = "1.3.0";
 export const PROTOCOL_SUBGRAPH_VERSION = "1.1.2";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 
-export const PROTOCOL_NAME = "Beethoven X";
-export const PROTOCOL_SLUG = "beethoven-x";
+export const PROTOCOL_NAME = "Beethoven X"
+export const PROTOCOL_SLUG = "beethoven-x"
+
+
+
 
 // The network names corresponding to the Network enum in the schema.
 // They also correspond to the ones in `dataSource.network()` after converting to lower case.
 // See below for a complete list:
 // https://thegraph.com/docs/en/hosted-service/what-is-hosted-service/#supported-networks-on-the-hosted-service
 export namespace Network {
-  export const ARBITRUM_ONE = "ARBITRUM_ONE";
-  export const AVALANCHE = "AVALANCHE";
-  export const AURORA = "AURORA";
-  export const BSC = "BSC"; // aka BNB Chain
-  export const CELO = "CELO";
-  export const MAINNET = "MAINNET"; // Ethereum mainnet
-  export const FANTOM = "FANTOM";
-  export const FUSE = "FUSE";
-  export const MOONBEAM = "MOONBEAM";
-  export const MOONRIVER = "MOONRIVER";
-  export const NEAR_MAINNET = "NEAR_MAINNET";
-  export const OPTIMISM = "OPTIMISM";
-  export const MATIC = "MATIC"; // aka Polygon
-  export const XDAI = "XDAI"; // aka Gnosis Chain
+    export const ARBITRUM_ONE = "ARBITRUM_ONE";
+    export const AVALANCHE = "AVALANCHE";
+    export const AURORA = "AURORA";
+    export const BSC = "BSC"; // aka BNB Chain
+    export const CELO = "CELO";
+    export const MAINNET = "MAINNET"; // Ethereum mainnet
+    export const FANTOM = "FANTOM";
+    export const FUSE = "FUSE";
+    export const MOONBEAM = "MOONBEAM";
+    export const MOONRIVER = "MOONRIVER";
+    export const NEAR_MAINNET = "NEAR_MAINNET";
+    export const OPTIMISM = "OPTIMISM";
+    export const MATIC = "MATIC"; // aka Polygon
+    export const XDAI = "XDAI"; // aka Gnosis Chain
 }
 
 export namespace ProtocolType {
-  export const EXCHANGE = "EXCHANGE";
-  export const LENDING = "LENDING";
-  export const YIELD = "YIELD";
-  export const BRIDGE = "BRIDGE";
-  export const GENERIC = "GENERIC";
+    export const EXCHANGE = "EXCHANGE";
+    export const LENDING = "LENDING";
+    export const YIELD = "YIELD";
+    export const BRIDGE = "BRIDGE";
+    export const GENERIC = "GENERIC";
 }
 
 export namespace VaultFeeType {
@@ -94,9 +97,7 @@ export const BIGINT_HUNDRED = BigInt.fromI32(100);
 export const BIGINT_192 = BigInt.fromI32(192);
 export const BIGINT_TEN_THOUSAND = BigInt.fromI32(10000);
 export const BIGINT_MILLION = BigInt.fromI32(1000000);
-export const BIGINT_MAX = BigInt.fromString(
-  "115792089237316195423570985008687907853269984665640564039457584007913129639935",
-);
+export const BIGINT_MAX = BigInt.fromString("115792089237316195423570985008687907853269984665640564039457584007913129639935");
 export const BIGDECIMAL_NEG_ONE = new BigDecimal(BIGINT_NEG_ONE);
 export const BIGDECIMAL_ZERO = new BigDecimal(BIGINT_ZERO);
 export const BIGDECIMAL_ONE = new BigDecimal(BIGINT_ONE);
@@ -124,26 +125,31 @@ export const MS_PER_DAY = new BigDecimal(BigInt.fromI32(24 * 60 * 60 * 1000));
 export const MS_PER_YEAR = DAYS_PER_YEAR.times(new BigDecimal(BigInt.fromI32(24 * 60 * 60 * 1000)));
 export const PROTOCOL_FEE_TO_OFF = BigDecimal.fromString("0");
 
+
 /////////////////////////////
 ///// Protocol Specific /////
 /////////////////////////////
 
-export let DEFAULTNETWORK = "OPTIMISM";
+export let DEFAULT_NETWORK =  "OPTIMISM" 
 
-export let VAULT_ADDRESS = Address.fromString("0xBA12222222228d8Ba445958a75a0704d566BF2C8");
+export let VAULT_ADDRESS = Address.fromString('0xBA12222222228d8Ba445958a75a0704d566BF2C8')
 
-export let REWARDTOKEN = "";
+export let REWARD_TOKEN = "" 
 
-export let DAI: Address = Address.fromString("0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1");
-export let USDC: Address = Address.fromString("0x7F5c764cBc14f9669B88837ca1490cCa17c31607");
-export let USDT: Address = Address.fromString("0x94b008aA00579c1307B0EF2c499aD98a8ce58e58");
-export let FRAX: Address = Address.fromString("0x2e3d870790dc77a83dd1d18184acc7439a53f475");
-export let OP: Address = Address.fromString("0x4200000000000000000000000000000000000042");
-export let WBTC: Address = Address.fromString("0x68f180fcCe6836688e9084f035309E29Bf0A2095");
-export let WETH: Address = Address.fromString("0x4200000000000000000000000000000000000006");
-export let BEETS: Address = Address.fromString("0x97513e975a7fA9072c72C92d8000B0dB90b163c5");
-export let SNX: Address = Address.fromString("0x8700daec35af8ff88c16bdf0418774cb3d7599b4");
-export let LINK: Address = Address.fromString("0x350a791bfc2c21f9ed5d10980dad2e2638ffa7f6");
 
-export let USD_STABLE_ASSETS: Address[] = [USDC, DAI, USDT, FRAX];
-export let BASE_ASSETS: Address[] = [WETH, OP, WBTC, BEETS, SNX, LINK];
+
+export let DAI: Address = Address.fromString('0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1');
+export let USDC: Address = Address.fromString('0x7F5c764cBc14f9669B88837ca1490cCa17c31607');
+export let USDT: Address = Address.fromString('0x94b008aA00579c1307B0EF2c499aD98a8ce58e58');
+export let FRAX: Address = Address.fromString('0x2e3d870790dc77a83dd1d18184acc7439a53f475');
+export let OP: Address = Address.fromString('0x4200000000000000000000000000000000000042');
+export let WBTC: Address = Address.fromString('0x68f180fcCe6836688e9084f035309E29Bf0A2095');
+export let WETH: Address = Address.fromString('0x4200000000000000000000000000000000000006');
+export let BEETS: Address = Address.fromString('0x97513e975a7fA9072c72C92d8000B0dB90b163c5');
+export let SNX: Address = Address.fromString('0x8700daec35af8ff88c16bdf0418774cb3d7599b4');
+export let LINK: Address = Address.fromString('0x350a791bfc2c21f9ed5d10980dad2e2638ffa7f6');
+
+export let USD_STABLE_ASSETS: Address[] = [USDC, DAI, USDT,FRAX];
+export let BASE_ASSETS: Address[] = [WETH, OP, WBTC, BEETS,SNX,LINK];
+
+
