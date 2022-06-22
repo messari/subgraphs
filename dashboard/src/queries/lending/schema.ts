d4aadc395d5007b3da3f652fb9cc95a26a9d3add
jihok <1700785+jihok@users.noreply.github.com>
2022-06-22 10:51:59 -0400

    finish supporting schema 1.3.0 (#387)
    * update lending to 1.3.0
    
    * update yield to 1.3.0
    
    * update dashboard version

--- dashboard/src/queries/lending/schema.ts
+++ dashboard/src/queries/lending/schema.ts
@@ -12,8 +12,9 @@ export const schema = (version: string): Schema => {
       return schema110();
     case Versions.Schema120:
       return schema120();
+    case Versions.Schema130:
     default:
-      return schema120();
+      return schema130();
   }
 };
 
@@ -778,6 +779,397 @@ export const schema120 = (): Schema => {
       liquidationThreshold
       liquidationPenalty
       totalValueLockedUSD
+      totalDepositBalanceUSD
+      cumulativeDepositUSD
+      totalBorrowBalanceUSD
+      cumulativeBorrowUSD
+      cumulativeLiquidateUSD
+      inputTokenBalance
+      inputTokenPriceUSD
+      outputTokenSupply
+      outputTokenPriceUSD
+      exchangeRate
+      rewardTokenEmissionsAmount
+      rewardTokenEmissionsUSD
+    }
+  }`;
+
+  const protocolFields = {
+    id: "ID!",
+    name: "String!",
+    slug: "String!",
+    schemaVersion: "String!",
+    subgraphVersion: "String!",
+    methodologyVersion: "String!",
+    network: "Network!",
+    type: "ProtocolType!",
+    lendingType: "LendingType",
+    riskType: "RiskType",
+    mintedTokens: "[Token!]",
+    cumulativeUniqueUsers: "Int!",
+    totalValueLockedUSD: "BigDecimal!",
+    protocolControlledValueUSD: "BigDecimal",
+    cumulativeSupplySideRevenueUSD: "BigDecimal!",
+    cumulativeProtocolSideRevenueUSD: "BigDecimal!",
+    cumulativeTotalRevenueUSD: "BigDecimal!",
+    totalDepositBalanceUSD: "BigDecimal!",
+    cumulativeDepositUSD: "BigDecimal!",
+    totalBorrowBalanceUSD: "BigDecimal!",
+    cumulativeBorrowUSD: "BigDecimal!",
+    cumulativeLiquidateUSD: "BigDecimal!",
+    mintedTokenSupplies: "[BigInt!]",
+  };
+
+  return {
+    entities,
+    entitiesData,
+    query,
+    poolData,
+    events,
+    protocolFields,
+    poolTimeseriesQuery,
+    financialsQuery,
+    hourlyUsageQuery,
+    dailyUsageQuery,
+    protocolTableQuery,
+    poolsQuery,
+  };
+};
+
+export const schema130 = (): Schema => {
+  const entities = [
+    "financialsDailySnapshots",
+    "usageMetricsDailySnapshots",
+    "marketDailySnapshots",
+    "usageMetricsHourlySnapshots",
+    "marketHourlySnapshots",
+  ];
+  const entitiesData = {
+    // Each Array within this array contains strings of the fields to pull for the entity type of the same index above
+    financialsDailySnapshots: {
+      id: "ID!",
+      totalValueLockedUSD: "BigDecimal!",
+      dailySupplySideRevenueUSD: "BigDecimal!",
+      cumulativeSupplySideRevenueUSD: "BigDecimal!",
+      dailyProtocolSideRevenueUSD: "BigDecimal!",
+      cumulativeProtocolSideRevenueUSD: "BigDecimal!",
+      dailyTotalRevenueUSD: "BigDecimal!",
+      cumulativeTotalRevenueUSD: "BigDecimal!",
+      totalBorrowBalanceUSD: "BigDecimal!",
+      dailyBorrowUSD: "BigDecimal!",
+      cumulativeBorrowUSD: "BigDecimal!",
+      totalDepositBalanceUSD: "BigDecimal!",
+      dailyDepositUSD: "BigDecimal!",
+      cumulativeDepositUSD: "BigDecimal!",
+      dailyLiquidateUSD: "BigDecimal!",
+      cumulativeLiquidateUSD: "BigDecimal!",
+      mintedTokenSupplies: "[BigInt!]",
+      protocolControlledValueUSD: "BigDecimal",
+      timestamp: "BigInt!",
+    },
+    usageMetricsDailySnapshots: {
+      id: "ID!",
+      cumulativeUniqueUsers: "Int!",
+      dailyActiveUsers: "Int!",
+      dailyTransactionCount: "Int!",
+      dailyDepositCount: "Int!",
+      dailyWithdrawCount: "Int!",
+      dailyBorrowCount: "Int!",
+      dailyRepayCount: "Int!",
+      dailyLiquidateCount: "Int!",
+      timestamp: "BigInt!",
+    },
+    marketDailySnapshots: {
+      id: "ID!",
+      totalValueLockedUSD: "BigDecimal!",
+      cumulativeSupplySideRevenueUSD: "BigDecimal!",
+      dailySupplySideRevenueUSD: "BigDecimal!",
+      cumulativeProtocolSideRevenueUSD: "BigDecimal!",
+      dailyProtocolSideRevenueUSD: "BigDecimal!",
+      cumulativeTotalRevenueUSD: "BigDecimal!",
+      dailyTotalRevenueUSD: "BigDecimal!",
+      totalDepositBalanceUSD: "BigDecimal!",
+      dailyDepositUSD: "BigDecimal!",
+      cumulativeDepositUSD: "BigDecimal!",
+      totalBorrowBalanceUSD: "BigDecimal!",
+      dailyBorrowUSD: "BigDecimal!",
+      cumulativeBorrowUSD: "BigDecimal!",
+      dailyLiquidateUSD: "BigDecimal!",
+      cumulativeLiquidateUSD: "BigDecimal!",
+      inputTokenBalance: "BigInt!",
+      inputTokenPriceUSD: "BigDecimal!",
+      outputTokenSupply: "BigInt!",
+      outputTokenPriceUSD: "BigDecimal!",
+      rates: "[InterestRate!]!",
+      exchangeRate: "BigDecimal",
+      rewardTokenEmissionsAmount: "[BigInt!]",
+      rewardTokenEmissionsUSD: "[BigDecimal!]",
+      timestamp: "BigInt!",
+    },
+    usageMetricsHourlySnapshots: {
+      id: "ID!",
+      cumulativeUniqueUsers: "Int!",
+      hourlyActiveUsers: "Int!",
+      hourlyTransactionCount: "Int!",
+      hourlyDepositCount: "Int!",
+      hourlyWithdrawCount: "Int!",
+      hourlyBorrowCount: "Int!",
+      hourlyRepayCount: "Int!",
+      hourlyLiquidateCount: "Int!",
+      timestamp: "BigInt!",
+    },
+    marketHourlySnapshots: {
+      id: "ID!",
+      totalValueLockedUSD: "BigDecimal!",
+      cumulativeSupplySideRevenueUSD: "BigDecimal!",
+      hourlySupplySideRevenueUSD: "BigDecimal!",
+      cumulativeProtocolSideRevenueUSD: "BigDecimal!",
+      hourlyProtocolSideRevenueUSD: "BigDecimal!",
+      cumulativeTotalRevenueUSD: "BigDecimal!",
+      hourlyTotalRevenueUSD: "BigDecimal!",
+      totalDepositBalanceUSD: "BigDecimal!",
+      hourlyDepositUSD: "BigDecimal!",
+      cumulativeDepositUSD: "BigDecimal!",
+      totalBorrowBalanceUSD: "BigDecimal!",
+      hourlyBorrowUSD: "BigDecimal!",
+      cumulativeBorrowUSD: "BigDecimal!",
+      hourlyLiquidateUSD: "BigDecimal!",
+      cumulativeLiquidateUSD: "BigDecimal!",
+      inputTokenBalance: "BigInt!",
+      inputTokenPriceUSD: "BigDecimal!",
+      outputTokenSupply: "BigInt!",
+      outputTokenPriceUSD: "BigDecimal!",
+      exchangeRate: "BigDecimal",
+      rates: "[InterestRate!]!",
+      rewardTokenEmissionsAmount: "[BigInt!]",
+      rewardTokenEmissionsUSD: "[BigDecimal!]",
+      timestamp: "BigInt!",
+    },
+  };
+
+  const adjustedMarketDailyFields = Object.keys(entitiesData.marketDailySnapshots);
+  const adjustedMarketHourlyFields = Object.keys(entitiesData.marketHourlySnapshots);
+  adjustedMarketDailyFields[adjustedMarketDailyFields.indexOf("rates")] = "rates{id,side,rate,type}";
+  adjustedMarketHourlyFields[adjustedMarketHourlyFields.indexOf("rates")] = "rates{id,side,rate,type}";
+
+  const finanQuery =
+    "financialsDailySnapshots(first: 1000, orderBy: timestamp, orderDirection: desc) {" +
+    Object.keys(entitiesData.financialsDailySnapshots).join(",") +
+    "}";
+  const usageDailyQuery =
+    "usageMetricsDailySnapshots(first: 1000, orderBy: timestamp, orderDirection: desc) {" +
+    Object.keys(entitiesData.usageMetricsDailySnapshots).join(",") +
+    "}";
+  const usageHourlyQuery =
+    "usageMetricsHourlySnapshots(first: 1000, orderBy: timestamp, orderDirection: desc) {" +
+    Object.keys(entitiesData.usageMetricsHourlySnapshots).join(",") +
+    "}";
+
+  const marketDailyQuery =
+    "marketDailySnapshots(first: 1000, orderBy: timestamp, orderDirection: desc, where: {market: $poolId}) {" +
+    adjustedMarketDailyFields.join(",") +
+    "}";
+  const marketHourlyQuery =
+    "marketHourlySnapshots(first: 1000, orderBy: timestamp, orderDirection: desc, where: {market: $poolId}) {" +
+    adjustedMarketHourlyFields.join(",") +
+    "}";
+
+  const eventsFields = ["hash", "to", "from", "timestamp", "amount", "amountUSD"];
+
+  const events: string[] = ["deposits", "withdraws", "borrows", "repays", "liquidates"];
+  const eventsQuery: any[] = events.map((event) => {
+    let options = "";
+    const baseStr =
+      event + "(first: 1000, orderBy: timestamp, orderDirection: desc, where: {market: $poolId}" + options + ") { ";
+    let fields = eventsFields.join(", ");
+    if (event === "liquidates") {
+      fields += ", profitUSD";
+    }
+    return baseStr + fields + " }";
+  });
+
+  const poolData: { [x: string]: string } = {
+    id: "ID!",
+    name: "String",
+    inputToken: "Token!",
+    outputToken: "Token",
+    rewardTokens: "[RewardToken!]",
+    isActive: "Boolean!",
+    canUseAsCollateral: "Boolean!",
+    canBorrowFrom: "Boolean!",
+    maximumLTV: "BigDecimal!",
+    liquidationThreshold: "BigDecimal!",
+    liquidationPenalty: "BigDecimal!",
+    totalValueLockedUSD: "BigDecimal!",
+    cumulativeSupplySideRevenueUSD: "BigDecimal!",
+    cumulativeProtocolSideRevenueUSD: "BigDecimal!",
+    cumulativeTotalRevenueUSD: "BigDecimal!",
+    totalDepositBalanceUSD: "BigDecimal!",
+    cumulativeDepositUSD: "BigDecimal!",
+    totalBorrowBalanceUSD: "BigDecimal!",
+    cumulativeBorrowUSD: "BigDecimal!",
+    cumulativeLiquidateUSD: "BigDecimal!",
+    inputTokenBalance: "BigInt!",
+    inputTokenPriceUSD: "BigDecimal!",
+    outputTokenSupply: "BigInt!",
+    outputTokenPriceUSD: "BigDecimal!",
+    exchangeRate: "BigDecimal",
+    rates: "[InterestRate!]!",
+    rewardTokenEmissionsAmount: "[BigInt!]",
+    rewardTokenEmissionsUSD: "[BigDecimal!]",
+  };
+
+  const financialsQuery = `
+    query Data {
+      ${finanQuery}
+    }`;
+  const hourlyUsageQuery = `
+    query Data {
+      ${usageHourlyQuery}
+    }`;
+  const dailyUsageQuery = `
+    query Data {
+      ${usageDailyQuery}
+    }`;
+
+  const protocolTableQuery = `
+    query Data($protocolId: String) {
+      lendingProtocol(id:$protocolId) {
+        id      
+        name
+        slug
+        schemaVersion
+        subgraphVersion
+        methodologyVersion
+        network
+        type
+        lendingType
+        riskType
+        mintedTokens {
+          id
+          decimals
+        }
+        cumulativeUniqueUsers
+        totalValueLockedUSD
+        cumulativeSupplySideRevenueUSD
+        cumulativeProtocolSideRevenueUSD
+        cumulativeTotalRevenueUSD
+        protocolControlledValueUSD
+        cumulativeSupplySideRevenueUSD
+        cumulativeProtocolSideRevenueUSD
+        cumulativeTotalRevenueUSD
+        totalDepositBalanceUSD
+        cumulativeDepositUSD
+        totalBorrowBalanceUSD
+        cumulativeBorrowUSD
+        cumulativeLiquidateUSD
+        mintedTokenSupplies
+      }
+    }`;
+
+  const poolsQuery = `
+      query Data {
+        markets {
+          id
+          name
+        }
+      }
+    `;
+
+  const poolTimeseriesQuery = `
+  query Data($poolId: String) {
+    ${marketDailyQuery}
+    ${marketHourlyQuery}
+  }
+  `;
+
+  const query = `
+  query Data($poolId: String, $protocolId: String){
+    _meta {
+      block {
+        number
+      }
+      deployment
+    }
+    protocols {
+      id
+      name
+      type
+      schemaVersion
+      subgraphVersion
+      methodologyVersion
+    }
+    
+    lendingProtocols {
+      id      
+      name
+      slug
+      schemaVersion
+      subgraphVersion
+      methodologyVersion
+      network
+      type
+      lendingType
+      riskType
+      mintedTokens {
+        id
+        decimals
+      }
+      cumulativeUniqueUsers
+      totalValueLockedUSD
+      protocolControlledValueUSD
+      cumulativeSupplySideRevenueUSD
+      cumulativeProtocolSideRevenueUSD
+      cumulativeTotalRevenueUSD
+      totalDepositBalanceUSD
+      cumulativeDepositUSD
+      totalBorrowBalanceUSD
+      cumulativeBorrowUSD
+      cumulativeLiquidateUSD
+      mintedTokenSupplies
+    }
+
+    ${eventsQuery}
+    market(id:$poolId){
+      id
+      name
+      inputToken {
+        id
+        decimals
+        name
+        symbol
+      }
+      outputToken {
+        id
+        decimals
+        name
+        symbol
+      }
+      rewardTokens {
+        id
+        token {
+          id
+          decimals
+          name
+          symbol
+        }
+      }
+      rates {
+        id
+        side
+        rate
+        type
+      }
+      isActive
+      canUseAsCollateral
+      canBorrowFrom
+      maximumLTV
+      liquidationThreshold
+      liquidationPenalty
+      totalValueLockedUSD
+      cumulativeSupplySideRevenueUSD
+      cumulativeProtocolSideRevenueUSD
+      cumulativeTotalRevenueUSD
       totalDepositBalanceUSD
       cumulativeDepositUSD
       totalBorrowBalanceUSD
