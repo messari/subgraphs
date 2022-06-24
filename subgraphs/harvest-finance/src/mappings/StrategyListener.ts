269fe0696808441a61f3edd5d733e7fcd882fdd1
Andrés Fernández <andresavena19@gmail.com>
2022-06-24 11:58:37 -0500

    wip: handle proofit and buy back

--- subgraphs/harvest-finance/src/mappings/StrategyListener.ts
+++ subgraphs/harvest-finance/src/mappings/StrategyListener.ts
@@ -1,5 +1,26 @@
-import { ProfitAndBuybackLog, ProfitLogInReward } from "../../generated/ControllerListener/StrategyContract";
+import { Address } from "@graphprotocol/graph-ts";
+import { ADDRESS_ZERO } from "@protofire/subgraph-toolkit";
+import { ProfitAndBuybackLog, ProfitLogInReward, StrategyContract } from "../../generated/ControllerListener/StrategyContract";
+import { shared, tokens } from "../modules";
 
 
-export function handleProfitAndBuybackLog(event: ProfitAndBuybackLog): void { }
+export function handleProfitAndBuybackLog(event: ProfitAndBuybackLog): void {
+	let strategyContract = StrategyContract.bind(event.address)
+	let result = strategyContract.try_rewardToken()
+	let rewardTokenAddress = !result.reverted ? result.value : Address.fromHexString(ADDRESS_ZERO) as Address
+	let token = tokens.setValuesForToken(
+		tokens.loadOrCreateToken(rewardTokenAddress),
+		tokens.getValuesForToken(rewardTokenAddress)
+	)
+	token.save()
+
+	// TODO calc profit
+
+	let reward = tokens.loadOrCreateRewardToken(rewardTokenAddress, "")
+	reward.token = token.id
+	reward.save()
+
+
+
+}
 export function handleProfitLogInReward(event: ProfitLogInReward): void { }
