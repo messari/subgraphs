441e65b4b58630e16cc74c109906d392861c1fa5
tnkrxyz <tnkrxyz@gmail.com>
2022-06-24 18:40:24 +0000

    Fixing makerdao subgraph for issues listed in #500
    fix missing value for non-nullable field, e.g. protocol.`cumulativeSupplySideRevenueUSD`
    
    fixing handles of liquidation: liquidators
    
    fixed liquidate amount & liquidateUSD
    
    Fixed deployment failure due to mat=0
    
    Add mapping for DSProxy and UrnHandler to their owner address
    
    Added supply-side revenue
    
    fixed bigIntChangeDecimals
    
    added missing call to updateUsageMetrics for liquidations
    
    fixed wrong id for ActiveAccount entity
    
    edited README and makerdao.template.yaml
    
    upgraded graph-cli to 0.30

--- subgraphs/makerdao/src/common/prices/prices.ts
+++
@@ -1,9 +0,0 @@
-import { Address, BigDecimal, ethereum } from "@graphprotocol/graph-ts";
-import { getOrCreateToken } from "../getters";
-
-export function updateTokenPrice(tokenAddress: string, priceUSD: BigDecimal, event: ethereum.Event): void {
-  let token = getOrCreateToken(Address.fromString(tokenAddress));
-  token.lastPriceUSD = priceUSD;
-  token.lastPriceBlockNumber = event.block.number;
-  token.save();
-}
