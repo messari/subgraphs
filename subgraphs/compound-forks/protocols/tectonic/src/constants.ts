41604afc890283fe3c23bb2abb04743ce44b42e9
pbr48 <84412547+BenReilly19@users.noreply.github.com>
2022-06-25 13:48:35 -0300

    added tectonic protocol code

---
+++ subgraphs/compound-forks/protocols/tectonic/src/constants.ts
@@ -0,0 +1,26 @@
+import { Address } from "@graphprotocol/graph-ts";
+import { cTokenDecimals } from "../../../src/constants";
+import { TokenData } from "../../../src/mapping";
+
+//////////////////////////////
+/////     Addresses      /////
+//////////////////////////////
+
+export let comptrollerAddr = Address.fromString(
+  "0xb3831584acb95ED9cCb0C11f677B5AD01DeaeEc0"
+);
+
+export const nativeToken = new TokenData(
+  Address.fromString("0x0000000000000000000000000000000000000000"),
+  "Cronos",
+  "CRO",
+  18
+);
+
+
+export const nativeCToken = new TokenData(
+  Address.fromString("0xeAdf7c01DA7E93FdB5f16B0aa9ee85f978e89E95"),
+  "Tectonic CRO",
+  "tCRO",
+  cTokenDecimals
+);
