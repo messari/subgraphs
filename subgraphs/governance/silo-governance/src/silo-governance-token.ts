bf572307a57f13c5dd8fce4ba611fc1a1364cd72
mmnavarr <mmnavarr@live.com>
2022-06-28 16:32:53 -0500

    Implemented silo token subgraph

--- subgraphs/governance/silo-governance/src/silo-governance-token.ts
+++ subgraphs/governance/silo-governance/src/silo-governance-token.ts
@@ -1,67 +1,140 @@
 import {
-  Approval as ApprovalEvent,
   DelegateChanged as DelegateChangedEvent,
   DelegateVotesChanged as DelegateVotesChangedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Transfer as TransferEvent
-} from "../generated/SiloGovernanceToken/SiloGovernanceToken"
+  Transfer as TransferEvent,
+} from "../generated/SiloGovernanceToken/SiloGovernanceToken";
+import { log } from "@graphprotocol/graph-ts";
 import {
-  Approval,
-  DelegateChanged,
-  DelegateVotesChanged,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/schema"
+  getOrCreateTokenHolder,
+  getGovernance,
+  ZERO_ADDRESS,
+  toDecimal,
+  BIGINT_ZERO,
+  BIGINT_ONE,
+  getOrCreateDelegate,
+} from "./helpers";
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-  entity.save()
-}
-
+// DelegateChanged(indexed address,indexed address,indexed address)
 export function handleDelegateChanged(event: DelegateChangedEvent): void {
-  let entity = new DelegateChanged(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.delegator = event.params.delegator
-  entity.fromDelegate = event.params.fromDelegate
-  entity.toDelegate = event.params.toDelegate
-  entity.save()
+  let tokenHolder = getOrCreateTokenHolder(
+    event.params.delegator.toHexString()
+  );
+  let previousDelegate = getOrCreateDelegate(
+    event.params.fromDelegate.toHexString()
+  );
+  let newDelegate = getOrCreateDelegate(event.params.toDelegate.toHexString());
+
+  tokenHolder.delegate = newDelegate.id;
+  tokenHolder.save();
+
+  previousDelegate.tokenHoldersRepresentedAmount =
+    previousDelegate.tokenHoldersRepresentedAmount - 1;
+  previousDelegate.save();
+
+  newDelegate.tokenHoldersRepresentedAmount =
+    newDelegate.tokenHoldersRepresentedAmount + 1;
+  newDelegate.save();
 }
 
+// DelegateVotesChanged(indexed address,uint256,uint256)
+// Called in succession to the above DelegateChanged event
 export function handleDelegateVotesChanged(
   event: DelegateVotesChangedEvent
 ): void {
-  let entity = new DelegateVotesChanged(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.delegate = event.params.delegate
-  entity.previousBalance = event.params.previousBalance
-  entity.newBalance = event.params.newBalance
-  entity.save()
+  const delegateAddress = event.params.delegate;
+  const previousBalance = event.params.previousBalance;
+  const newBalance = event.params.newBalance;
+
+  let votesDifference = newBalance.minus(previousBalance);
+
+  let delegate = getOrCreateDelegate(delegateAddress.toHexString());
+  delegate.delegatedVotesRaw = newBalance;
+  delegate.delegatedVotes = toDecimal(newBalance);
+  delegate.save();
+
+  // Update governance delegate count
+  let governance = getGovernance();
+  if (previousBalance == BIGINT_ZERO && newBalance > BIGINT_ZERO) {
+    governance.currentDelegates = governance.currentDelegates.plus(BIGINT_ONE);
+  }
+  if (newBalance == BIGINT_ZERO) {
+    governance.currentDelegates = governance.currentDelegates.minus(BIGINT_ONE);
+  }
+  governance.delegatedVotesRaw = governance.delegatedVotesRaw.plus(
+    votesDifference
+  );
+  governance.delegatedVotes = toDecimal(governance.delegatedVotesRaw);
+  governance.save();
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
-
+// Transfer(indexed address,indexed address,uint256)
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
-  entity.save()
+  const from = event.params.from.toHexString();
+  const to = event.params.to.toHexString();
+  const value = event.params.value;
+
+  let fromHolder = getOrCreateTokenHolder(from);
+  let toHolder = getOrCreateTokenHolder(to);
+  let governance = getGovernance();
+
+  // Deduct from from holder balance + decrement gov token holders
+  // if holder now owns 0 or increment gov token holders if new holder
+  if (from != ZERO_ADDRESS) {
+    let fromHolderPreviousBalance = fromHolder.tokenBalanceRaw;
+    fromHolder.tokenBalanceRaw = fromHolder.tokenBalanceRaw.minus(value);
+    fromHolder.tokenBalance = toDecimal(fromHolder.tokenBalanceRaw);
+
+    if (fromHolder.tokenBalanceRaw < BIGINT_ZERO) {
+      log.error("Negative balance on holder {} with balance {}", [
+        fromHolder.id,
+        fromHolder.tokenBalanceRaw.toString(),
+      ]);
+    }
+
+    if (
+      fromHolder.tokenBalanceRaw == BIGINT_ZERO &&
+      fromHolderPreviousBalance > BIGINT_ZERO
+    ) {
+      governance.currentTokenHolders = governance.currentTokenHolders.minus(
+        BIGINT_ONE
+      );
+      governance.save();
+    } else if (
+      fromHolder.tokenBalanceRaw > BIGINT_ZERO &&
+      fromHolderPreviousBalance == BIGINT_ZERO
+    ) {
+      governance.currentTokenHolders = governance.currentTokenHolders.plus(
+        BIGINT_ONE
+      );
+      governance.save();
+    }
+
+    fromHolder.save();
+  }
+
+  // Increment to holder balance and total tokens ever held
+  let toHolderPreviousBalance = toHolder.tokenBalanceRaw;
+  toHolder.tokenBalanceRaw = toHolder.tokenBalanceRaw.plus(value);
+  toHolder.tokenBalance = toDecimal(toHolder.tokenBalanceRaw);
+  toHolder.totalTokensHeldRaw = toHolder.totalTokensHeldRaw.plus(value);
+  toHolder.totalTokensHeld = toDecimal(toHolder.totalTokensHeldRaw);
+
+  if (
+    toHolder.tokenBalanceRaw == BIGINT_ZERO &&
+    toHolderPreviousBalance > BIGINT_ZERO
+  ) {
+    governance.currentTokenHolders = governance.currentTokenHolders.minus(
+      BIGINT_ONE
+    );
+    governance.save();
+  } else if (
+    toHolder.tokenBalanceRaw > BIGINT_ZERO &&
+    toHolderPreviousBalance == BIGINT_ZERO
+  ) {
+    governance.currentTokenHolders = governance.currentTokenHolders.plus(
+      BIGINT_ONE
+    );
+    governance.save();
+  }
+  toHolder.save();
 }
