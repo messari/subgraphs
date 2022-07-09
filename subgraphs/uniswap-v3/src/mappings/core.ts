1d3f7ae45b1a2858dc29f67f75c7c49c2cc11b7b
Chris Steege <steegecas@gmail.com>
2022-07-09 13:21:59 -0400

    Improve comments and documention for uniswap-v3

--- subgraphs/uniswap-v3/src/mappings/core.ts
+++ subgraphs/uniswap-v3/src/mappings/core.ts
@@ -1,18 +1,37 @@
 // import { log } from '@graphprotocol/graph-ts'
-import { Burn as BurnEvent, Initialize, Mint as MintEvent, Swap as SwapEvent, SetFeeProtocol } from "../../generated/templates/Pool/Pool";
+import {
+  Burn as BurnEvent,
+  Initialize,
+  Mint as MintEvent,
+  Swap as SwapEvent,
+  SetFeeProtocol,
+} from "../../generated/templates/Pool/Pool";
 import { UsageType } from "../common/constants";
-import { createDeposit, createWithdraw, createSwapHandleVolumeAndFees } from "../common/creators";
-import { updatePrices, updatePoolMetrics, updateProtocolFees, updateUsageMetrics, updateFinancials } from "../common/updateMetrics";
+import {
+  createDeposit,
+  createWithdraw,
+  createSwapHandleVolumeAndFees,
+} from "../common/creators";
+import {
+  updatePrices,
+  updatePoolMetrics,
+  updateProtocolFees,
+  updateUsageMetrics,
+  updateFinancials,
+} from "../common/updateMetrics";
 
+// Emitted when a given liquidity pool is first created.
 export function handleInitialize(event: Initialize): void {
   updatePrices(event, event.params.sqrtPriceX96);
   updatePoolMetrics(event);
 }
 
+// Update the fees colected by the protocol.
 export function handleSetFeeProtocol(event: SetFeeProtocol): void {
   updateProtocolFees(event);
 }
 
+// Handle a mint event emitted from a pool contract. Considered a deposit into the given liquidity pool.
 export function handleMint(event: MintEvent): void {
   createDeposit(event, event.params.amount0, event.params.amount1);
   updateUsageMetrics(event, event.params.sender, UsageType.DEPOSIT);
@@ -20,6 +39,7 @@ export function handleMint(event: MintEvent): void {
   updatePoolMetrics(event);
 }
 
+// Handle a burn event emitted from a pool contract. Considered a withdraw into the given liquidity pool.
 export function handleBurn(event: BurnEvent): void {
   createWithdraw(event, event.params.amount0, event.params.amount1);
   updateUsageMetrics(event, event.transaction.from, UsageType.WITHDRAW);
@@ -27,8 +47,16 @@ export function handleBurn(event: BurnEvent): void {
   updatePoolMetrics(event);
 }
 
+// Handle a swap event emitted from a pool contract.
 export function handleSwap(event: SwapEvent): void {
-  createSwapHandleVolumeAndFees(event, event.params.amount0, event.params.amount1, event.params.recipient, event.params.sender, event.params.sqrtPriceX96);
+  createSwapHandleVolumeAndFees(
+    event,
+    event.params.amount0,
+    event.params.amount1,
+    event.params.recipient,
+    event.params.sender,
+    event.params.sqrtPriceX96
+  );
   updateFinancials(event);
   updatePoolMetrics(event);
   updateUsageMetrics(event, event.transaction.from, UsageType.SWAP);
