903d105a6698396e8e4a9e497c1a34507858625e
Tarun Madiraju <tarun@Taruns-MacBook-Pro.local>
2022-07-10 16:55:21 +0530

    update pool metrics first, update Tvl calculation method, validation script/folder updaetd

--- subgraphs/lido/validation/2022-07-07/review.sh
+++ subgraphs/lido/validation/2022-07-07/review.sh
@@ -4,45 +4,20 @@
 # Timestamp
 ZULU_TODAY=`gdate -d @$1 --utc +%FT%T.%3NZ`
 ZULU_TT_TODAY="`printf $ZULU_TODAY | cut -d'T' -f1`T00:00:00.000Z"
+ZULU_TT_TOMORROW=`gdate --date "$ZULU_TT_TODAY + 1 day" --utc +%FT%T.%3NZ`
 EPOCH_TODAY=`gdate --date "$ZULU_TT_TODAY"  +%s`
-EPOCH_TOMORROW=`gdate --date "$ZULU_TT_TODAY + 1 day"  +%s`
+EPOCH_TOMORROW=`gdate --date "$ZULU_TT_TODAY + 1 day" +%s`
 
 printf "\n[+] Timestamps................"
 printf "\n...Input Timestamp: $1"
 printf "\n...Zulu (Input): $ZULU_TODAY"
 printf "\n...Zulu Timestamp for Token Terminal: $ZULU_TT_TODAY"
+printf "\n...Zulu Timestamp for Token Terminal: $ZULU_TT_TOMOROW"
 printf "\n...Epoch Timestamp for Subgraph Query: $EPOCH_TODAY"
 printf "\n...Epoch +1 day Timestamp for Subgraph Query: $EPOCH_TOMORROW"
 
 
-cat  << EOF > data.json
-{
-    "query": "{
-       poolHourlySnapshots (where: { timestamp_gt: $EPOCH_TODAY, timestamp_lt: $EPOCH_TOMORROW }) {
-            id
-            protocol { id }
-            pool { id }
-            blockNumber
-            timestamp
-            totalValueLockedUSD
-            cumulativeSupplySideRevenueUSD
-            hourlySupplySideRevenueUSD
-            cumulativeProtocolSideRevenueUSD
-            hourlyProtocolSideRevenueUSD
-            cumulativeTotalRevenueUSD
-            hourlyTotalRevenueUSD
-            inputTokenBalances
-            outputTokenSupply
-            outputTokenPriceUSD
-            stakedOutputTokenAmount
-            rewardTokenEmissionsAmount
-            rewardTokenEmissionsUSD
-        }
-    }"
-}
-EOF
-
-cat << EOF > data-full.json
+cat << EOF > query.json
 { 
     "query": "{
         protocols {
@@ -159,10 +134,28 @@ EOF
 
 
 # Print fortysevenlabs/lido subgraph metrics
-# Check data.json exists
-printf "\n\n[+] SUBGRAPH: $2 ................."
-curl -s -L -H "Content-Type: application/json" -X POST -d @data-full.json $2 | jq '.'
+printf "\n\n[+] SUBGRAPH: $2 .................\n"
+for each in "${@:3}"; do 
+    printf "\n ......getting... $each\n"
+    curl -s -L -H "Content-Type: application/json" -X POST -d @query.json $2 | \
+        jq --arg param "$each" -r ".[].protocols[] | \
+            .$each, \
+            .financialMetrics[].$each, \
+            .dailyUsageMetrics[].$each, \
+            .hourlyUsageMetrics[].$each, \
+            .pools[].$each, \
+            .pools[].dailySnapshots[].$each, \
+            .pools[].hourlySnapshots[].$each" | \
+        grep -v null
+done
 
 
-printf "\n\n[+] TOKEN TERMINAL............."
-TIMESTAMP="$ZULU_TT_TODAY" jq '.[] | select(.timestamp == env.TIMESTAMP)' metrics.json | grep "timestamp\|revenue\|tvl"
+# Print tokenterminal metrics
+printf "\n\n[+] TOKEN TERMINAL.............\n"
+TIMESTAMP="$ZULU_TT_TOMORROW" jq '.[] | select(.timestamp == env.TIMESTAMP)' metrics-daily.json | grep "timestamp\|revenue\|tvl"
+
+# There is more magic we can do with shell + jq  here to customize comparison per metric further.
+
+
+# Cleanup
+rm query.json
