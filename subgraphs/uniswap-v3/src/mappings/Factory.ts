c1a3a122c7ed49c17c5b79ecb0c0c73defbb29fa
Chris Steege <steegecas@gmail.com>
2022-07-11 15:57:23 -0400

    Stopped tracking aurora/wETH pool on mainnet

--- subgraphs/uniswap-v3/src/mappings/Factory.ts
+++ subgraphs/uniswap-v3/src/mappings/Factory.ts
@@ -2,14 +2,14 @@
 import { PoolCreated } from "../../generated//Factory/Factory";
 import { Address } from "@graphprotocol/graph-ts";
 import { createLiquidityPool } from "../common/creators";
+import { NetworkConfigs } from "../../configurations/configure";
 
 // Liquidity pool is created from the Factory contract.
 // Create a pool entity and start monitoring events from the newly deployed pool contract specified in the subgraph.yaml.
 export function handlePoolCreated(event: PoolCreated): void {
   // temp fix
   if (
-    event.params.pool ==
-    Address.fromHexString("0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248")
+    NetworkConfigs.getUntrackedPairs().includes(event.params.pool.toHexString())
   ) {
     return;
   }
