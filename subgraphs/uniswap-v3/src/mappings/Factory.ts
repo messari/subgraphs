a7c9f3090d95008a80bb1caab571e691aecbf505
steegecs <56660047+steegecs@users.noreply.github.com>
2022-07-10 22:21:19 -0700

    Improve comments and documention for uniswap-v3 (#540)
    * Improve comments and documention for uniswap-v3
    
    * Updated a couple comments

--- subgraphs/uniswap-v3/src/mappings/Factory.ts
+++ subgraphs/uniswap-v3/src/mappings/Factory.ts
@@ -3,10 +3,21 @@ import { PoolCreated } from "../../generated//Factory/Factory";
 import { Address } from "@graphprotocol/graph-ts";
 import { createLiquidityPool } from "../common/creators";
 
+// Liquidity pool is created from the Factory contract.
+// Create a pool entity and start monitoring events from the newly deployed pool contract specified in the subgraph.yaml.
 export function handlePoolCreated(event: PoolCreated): void {
   // temp fix
-  if (event.params.pool == Address.fromHexString("0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248")) {
+  if (
+    event.params.pool ==
+    Address.fromHexString("0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248")
+  ) {
     return;
   }
-  createLiquidityPool(event, event.params.pool.toHexString(), event.params.token0.toHexString(), event.params.token1.toHexString(), event.params.fee);
+  createLiquidityPool(
+    event,
+    event.params.pool.toHexString(),
+    event.params.token0.toHexString(),
+    event.params.token1.toHexString(),
+    event.params.fee
+  );
 }
