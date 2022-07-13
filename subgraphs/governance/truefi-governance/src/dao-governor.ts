2cc84660c2aef2b793ba98c65657fadd9da4e673
daniel <daniel.khoo@messari.io>
2022-07-13 15:12:09 +0800

    Add governanceFramework to proposal entity

--- subgraphs/governance/truefi-governance/src/dao-governor.ts
+++ subgraphs/governance/truefi-governance/src/dao-governor.ts
@@ -77,6 +77,7 @@ export function handleProposalCreated(event: ProposalCreated): void {
     event.block.number >= proposal.startBlock
       ? ProposalState.ACTIVE
       : ProposalState.PENDING;
+  proposal.governanceFramework = event.address.toHexString();
   proposal.save();
 
   // Increment gov proposal count
