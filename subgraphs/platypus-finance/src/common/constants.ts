68672398f1885405dd9952335458f8b1574980aa
Arjun Nemani <nemaniarjun@gmail.com>
2022-07-15 02:40:15 +0530

    add Mai & USX pools

--- subgraphs/platypus-finance/src/common/constants.ts
+++ subgraphs/platypus-finance/src/common/constants.ts
@@ -174,6 +174,8 @@ export class poolDetail {
 
   static getAltPoolAddressArray(): Array<string> {
     return [
+      // We comment out the main pool as we already are "watching" its events
+      // As per the subgraph.yaml manifest definition
       // "0x66357dCaCe80431aee0A7507e2E361B7e2402370",
       "0xe0D166DE15665bC4B7185B2e35E847E51316E126",
       "0xB8E567fc23c39C94a1f6359509D7b43D1Fbed824",
@@ -184,6 +186,8 @@ export class poolDetail {
       "0x233Ba46B01d2FbF1A31bDBc500702E286d6de218",
       "0x91BB10D68C72d64a7cE10482b453153eEa03322C",
       "0x27912AE6Ba9a54219d8287C3540A8969FF35500B",
+      "0x853ea32391AaA14c112C645FD20BA389aB25C5e0",
+      "0x3B55E45fD6bd7d4724F5c47E0d1bCaEdd059263e",
     ];
   }
 
@@ -201,6 +205,8 @@ export class poolDetail {
       ["Factory Pool H2O", "H2O-USDC Pool", "0x233Ba46B01d2FbF1A31bDBc500702E286d6de218", "false"],
       ["Factory Pool TSD", "TSD-USDC Pool", "0x91BB10D68C72d64a7cE10482b453153eEa03322C", "false"],
       ["Factory Pool MONEY", "MONEY-USDC Pool", "0x27912AE6Ba9a54219d8287C3540A8969FF35500B", "false"],
+      ["Factory Pool dForce USX", "USX-USDC Pool", "0x853ea32391AaA14c112C645FD20BA389aB25C5e0", "false"],
+      ["Factory Pool MAI", "MiMatic-USDC Pool", "0x3B55E45fD6bd7d4724F5c47E0d1bCaEdd059263e", "false"],
 
       // Ignore pools while calculating TVL not in above list
       ["Withdraw Pool MIM", "MIM-Ignore", "0x6c84f0580c8ffab0c716c87e66ab474e4bea97d9", "true"],
