cc69ee116cc6167f6b458210504de3a734c8b227
Chris Steege <steegecas@gmail.com>
2022-07-17 18:11:55 -0400

    Updated all network file and folder names for ethereum, gnosis, and polygon. Also set all deploy-on-merge to false

--- subgraphs/uniswap-forks/configurations/configurations/deploy.ts
+++ subgraphs/uniswap-forks/configurations/configurations/deploy.ts
@@ -3,27 +3,27 @@
 // for the protocol/network deployment in the `protocols` folder.
 export namespace Deploy {
   export const APESWAP_BSC = 0;
-  export const APESWAP_MATIC = 1;
+  export const APESWAP_POLYGON = 1;
   export const SUSHISWAP_ARBITRUM = 2;
   export const SUSHISWAP_AVALANCHE = 3;
   export const SUSHISWAP_BSC = 4;
   export const SUSHISWAP_CELO = 5;
   export const SUSHISWAP_FANTOM = 6;
   export const SUSHISWAP_FUSE = 7;
-  export const SUSHISWAP_MAINNET = 8;
-  export const SUSHISWAP_MATIC = 9;
+  export const SUSHISWAP_ETHEREUM = 8;
+  export const SUSHISWAP_POLYGON = 9;
   export const SUSHISWAP_MOONBEAM = 10;
   export const SUSHISWAP_MOONRIVER = 11;
-  export const SUSHISWAP_XDAI = 12;
-  export const UNISWAP_V2_MAINNET = 13;
+  export const SUSHISWAP_GNOSIS = 12;
+  export const UNISWAP_V2_ETHEREUM = 13;
   export const SPOOKYSWAP_FANTOM = 14;
   export const SPIRITSWAP_FANTOM = 15;
   export const TRADER_JOE_AVALANCHE = 16;
-  export const QUICKSWAP_MATIC = 17;
+  export const QUICKSWAP_POLYGON = 17;
   export const TRISOLARIS_AURORA = 18;
   export const SOLARBEAM_MOONRIVER = 19;
   export const VVS_FINANCE_CRONOS = 20;
   export const UBESWAP_CELO = 21;
-  export const HONEYSWAP_XDAI = 22;
-  export const HONEYSWAP_MATIC = 23;
+  export const HONEYSWAP_GNOSIS = 22;
+  export const HONEYSWAP_POLYGON = 23;
 }
