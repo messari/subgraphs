6eb027119e33392cae94a61d7ff6f9e4a60abdc8
daniel <daniel.khoo@messari.io>
2022-07-19 13:57:55 +0800

    Add unlock protocol

---
+++ subgraphs/openzeppelin-governor/protocols/unlock-governance/src/unlock-protocol-governor.ts
@@ -0,0 +1,125 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import {
+  ProposalCanceled,
+  ProposalCreated,
+  ProposalExecuted,
+  ProposalQueued,
+  QuorumUpdated,
+  TimelockChange,
+  VoteCast,
+  VotingDelayUpdated,
+  VotingPeriodUpdated,
+} from "../../../generated/UnlockProtocolGovernor/UnlockProtocolGovernor";
+import {
+  _handleProposalCreated,
+  _handleProposalCanceled,
+  _handleProposalExecuted,
+  _handleProposalQueued,
+  _handleVoteCast,
+} from "../../../src/handlers";
+import { UnlockProtocolGovernor } from "../../../generated/UnlockProtocolGovernor/UnlockProtocolGovernor";
+import { GovernanceFramework } from "../../../generated/schema";
+
+// ProposalCanceled(proposalId)
+export function handleProposalCanceled(event: ProposalCanceled): void {
+  _handleProposalCanceled(event.params.proposalId.toString(), event);
+}
+
+// ProposalCreated(proposalId, proposer, targets, values, signatures, calldatas, startBlock, endBlock, description)
+export function handleProposalCreated(event: ProposalCreated): void {
+  // FIXME: Prefer to use a single object arg for params
+  // e.g.  { proposalId: event.params.proposalId, proposer: event.params.proposer, ...}
+  // but graph wasm compilation breaks for unknown reasons
+
+  _handleProposalCreated(
+    event.params.proposalId.toString(),
+    event.params.proposer.toHexString(),
+    event.params.targets,
+    event.params.values,
+    event.params.signatures,
+    event.params.calldatas,
+    event.params.startBlock,
+    event.params.endBlock,
+    event.params.description,
+    event
+  );
+}
+
+// ProposalExecuted(proposalId)
+export function handleProposalExecuted(event: ProposalExecuted): void {
+  _handleProposalExecuted(event.params.proposalId.toString(), event);
+}
+
+// ProposalQueued(proposalId, eta)
+export function handleProposalQueued(event: ProposalQueued): void {
+  _handleProposalQueued(event.params.proposalId, event.params.eta);
+}
+
+// QuorumUpdated(oldQuorum, newQuorum)
+export function handleQuorumUpdated(event: QuorumUpdated): void {
+  let governanceFramework = getGovernanceFramework(event.address.toHexString());
+  governanceFramework.quorumVotes = event.params.newQuorum;
+  governanceFramework.save();
+}
+
+// TimelockChange (address oldTimelock, address newTimelock)
+export function handleTimelockChange(event: TimelockChange): void {
+  let governanceFramework = getGovernanceFramework(event.address.toHexString());
+  governanceFramework.timelockAddress = event.params.newTimelock.toHexString();
+  governanceFramework.save();
+}
+
+// VoteCast(account, proposalId, support, weight, reason);
+export function handleVoteCast(event: VoteCast): void {
+  _handleVoteCast(
+    event.params.proposalId.toString(),
+    event.params.voter.toHexString(),
+    event.params.weight,
+    event.params.reason,
+    event.params.support,
+    event
+  );
+}
+// VotingDelayUpdated(uint256 oldVotingDelay, uint256 newVotingDelay);
+export function handleVotingDelayUpdated(event: VotingDelayUpdated): void {
+  let governanceFramework = getGovernanceFramework(event.address.toHexString());
+  governanceFramework.votingDelay = event.params.newVotingDelay;
+  governanceFramework.save();
+}
+
+// VotingPeriodUpdated(uint256 oldVotingPeriod, uint256 newVotingPeriod);
+export function handleVotingPeriodUpdated(event: VotingPeriodUpdated): void {
+  let governanceFramework = getGovernanceFramework(event.address.toHexString());
+  governanceFramework.votingPeriod = event.params.newVotingPeriod;
+  governanceFramework.save();
+}
+
+// Helper function that imports and binds the contract
+function getGovernanceFramework(contractAddress: string): GovernanceFramework {
+  let governanceFramework = GovernanceFramework.load(contractAddress);
+
+  if (!governanceFramework) {
+    governanceFramework = new GovernanceFramework(contractAddress);
+    let contract = UnlockProtocolGovernor.bind(
+      Address.fromString(contractAddress)
+    );
+
+    governanceFramework.name = contract.name();
+    governanceFramework.type = "OZGovernor";
+    governanceFramework.version = contract.version();
+
+    governanceFramework.contractAddress = contractAddress;
+    governanceFramework.tokenAddress = contract.timelock().toHexString();
+    governanceFramework.timelockAddress = contract.token().toHexString();
+
+    governanceFramework.votingDelay = contract.votingDelay();
+    governanceFramework.votingPeriod = contract.votingPeriod();
+
+    // Missing from this old version of Governor
+    // governanceFramework.proposalThreshold = contract.proposalThreshold();
+    const startBlock = new BigInt(13148447);
+    governanceFramework.quorumVotes = contract.quorum(startBlock);
+  }
+
+  return governanceFramework;
+}
