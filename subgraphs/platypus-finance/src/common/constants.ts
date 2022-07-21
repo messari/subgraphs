adb75cb31964f18bbd6a760b1fb393866bc609ed
Chris Steege <steegecas@gmail.com>
2022-07-21 14:01:15 -0400

    Prepare platypus finance for deploy on merge

--- subgraphs/platypus-finance/src/common/constants.ts
+++ subgraphs/platypus-finance/src/common/constants.ts
@@ -1,4 +1,4 @@
-import { Address, BigDecimal, BigInt, log } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 ////////////////////////
 ///// Schema Enums /////
