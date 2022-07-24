0412caba203add7a5071230cbba9457807b8832f
dmelotik <dmelotik@wisc.edu>
2022-07-24 19:18:55 +0200

    fix chainlink contract

--- subgraphs/beefy-finance/src/prices/oracles/ChainLinkFeed.ts
+++ subgraphs/beefy-finance/src/prices/oracles/ChainLinkFeed.ts
@@ -1,20 +1,21 @@
 import { Address } from "@graphprotocol/graph-ts";
 import { CustomPriceType } from "../common/types";
-import { ChainlinkOracle } from "../../../generated/aave-aave-eol/ChainlinkOracle";
+import { ChainLinkContract } from "../../../generated/aave-aave-eol/ChainLinkContract";
 import { polygonOracles as oracles } from "./oracles";
 import { ERC20 } from "../../../generated/aave-aave-eol/ERC20";
 import { ZERO_ADDRESS } from "../common/constants";
+import { CHAIN_LINK_USD_ADDRESS } from "../common/constants";
 
 export function getChainLinkContract(
   asset: string,
   network: string
-): ChainlinkOracle {
+): ChainLinkContract {
   for (let i = 0; i < oracles.length; i++) {
     if (oracles[i][0] === asset) {
-      return ChainlinkOracle.bind(Address.fromString(oracles[i][1]));
+      return ChainLinkContract.bind(Address.fromString(oracles[i][1]));
     }
   }
-  return ChainlinkOracle.bind(ZERO_ADDRESS);
+  return ChainLinkContract.bind(ZERO_ADDRESS);
 }
 
 export function getTokenPriceFromChainLink(
@@ -29,7 +30,7 @@ export function getTokenPriceFromChainLink(
     return new CustomPriceType();
   }
 
-  let result = chainLinkContract.try_getLatestRoundData(tokenAddr);
+  let result = chainLinkContract.try_latestRoundData(tokenAddr, CHAIN_LINK_USD_ADDRESS);
 
   if (!result.reverted) {
     const decimals = tokenContract.decimals();
