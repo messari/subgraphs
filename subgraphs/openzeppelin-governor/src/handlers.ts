4db38f6515ca20c611b3f1eb080f033723ab321b
daniel <daniel.khoo@messari.io>
2022-07-28 15:06:30 +0800

    Add txnHash to vote entity

--- subgraphs/openzeppelin-governor/src/handlers.ts
+++ subgraphs/openzeppelin-governor/src/handlers.ts
@@ -279,6 +279,7 @@ export function _handleVoteCast(
   vote.reason = reason;
   vote.block = event.block.number;
   vote.blockTime = event.block.timestamp;
+  vote.txnHash = event.transaction.hash.toHexString();
   // Retrieve enum string key by value (0 = Against, 1 = For, 2 = Abstain)
   vote.choice = getVoteChoiceByValue(support);
   vote.save();
