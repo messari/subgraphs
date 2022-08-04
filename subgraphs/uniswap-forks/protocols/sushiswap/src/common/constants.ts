a4c81ac7f42f90f239f30987e75277575c03308d
Chris Steege <steegecas@gmail.com>
2022-08-04 13:32:51 -0400

    Updated sushiswap version

--- subgraphs/uniswap-forks/protocols/sushiswap/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/sushiswap/src/common/constants.ts
@@ -4,7 +4,7 @@ import { BigInt } from "@graphprotocol/graph-ts";
 //////Versions//////
 ////////////////////
 
-export const PROTOCOL_SUBGRAPH_VERSION = "1.1.4";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.1.6";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 export const PROTOCOL_NAME = "SushiSwap";
 export const PROTOCOL_SLUG = "sushiswap";
