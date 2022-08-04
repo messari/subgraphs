6152500a26959c512d5f7bb32b729116a0b0ddc7
Michael Carroll <69168498+MichaelC1999@users.noreply.github.com>
2022-08-04 14:52:50 -0400

    Dashboard v1.3.1 (#723)
    * Separate poolOverview embedded entity queries
    
    * poolOverview tokens and volume query separation
    
    * Separated pool overview yield query by schema ver
    
    * Added Generic deployments, minor events page fix
    
    * version
    
    * Decentralized Network deployment rows
    
    Co-authored-by: Michael <michaelcarroll1999@gmail.com>

--- dashboard/src/deployments/DeploymentsPage.tsx
+++ dashboard/src/deployments/DeploymentsPage.tsx
@@ -4,8 +4,10 @@ import { SearchInput } from "../common/utilComponents/SearchInput";
 import { DeploymentsContextProvider } from "./DeploymentsContextProvider";
 import { Typography } from "@mui/material";
 import { NewClient } from "../utils";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import DeploymentsTable from "./DeploymentsTable";
+import { useQuery } from "@apollo/client";
+import { decentralizedNetworkSubgraphsQuery } from "../queries/decentralizedNetworkSubgraphsQuery";
 
 const DeploymentsLayout = styled("div")`
   padding: 0;
@@ -47,13 +49,77 @@ function DeploymentsPage() {
           });
       });
   };
+
+  const [decentralizedDeployments, setDecentralizedDeployments] = useState<{
+    [type: string]: { [proto: string]: { [network: string]: string } };
+  }>({})
+
+  const clientDecentralizedEndpoint = useMemo(() => NewClient("https://api.thegraph.com/subgraphs/name/graphprotocol/graph-network-mainnet"), []);
+  const { data: decentralized, error: decentralizedQueryError, loading: decentralizedQueryLoading } = useQuery(decentralizedNetworkSubgraphsQuery, {
+    client: clientDecentralizedEndpoint,
+  });
+
   useEffect(() => {
     getData();
   }, []);
 
+
+  useEffect(() => {
+    if (decentralized && !Object.keys(decentralizedDeployments)?.length) {
+      const decenDepos: { [x: string]: any } = { exchanges: {}, lending: {}, vaults: {}, generic: {} }
+      const subs = decentralized.graphAccount.subgraphs;
+      subs.forEach((sub: any, idx: number) => {
+        let name = sub.currentVersion.subgraphDeployment.originalName.toLowerCase().split(" ");
+        name.pop();
+        name = name.join("-");
+        const network = sub.currentVersion.subgraphDeployment.network.id;
+        const deploymentId = sub.currentVersion.subgraphDeployment.ipfsHash;
+        const subgraphId = sub.id;
+        const schemaVersion = sub.currentVersion.subgraphDeployment.schema.split("\n").join("").split("#").find((x: string[]) => x.includes("Version:")).split("Version:")[1].trim()
+        const protocolTypeRaw = sub.currentVersion.subgraphDeployment.schema.split("\n").join("").split("#").find((x: string[]) => x.includes("Subgraph Schema:")).split("Subgraph Schema:")[1].trim();
+        let protocolType = "generic";
+        if (protocolTypeRaw.toUpperCase().includes("LEND")) {
+          protocolType = "lending";
+        } else if (protocolTypeRaw.toUpperCase().includes("EXCHANGE") || protocolTypeRaw.toUpperCase().includes("DEX")) {
+          protocolType = "exchanges";
+        } else if (protocolTypeRaw.toUpperCase().includes("VAULT") || protocolTypeRaw.toUpperCase().includes("YIELD")) {
+          protocolType = "vaults";
+        }
+        decenDepos[protocolType][name] = { network, schemaVersion, deploymentId, subgraphId }
+      })
+      setDecentralizedDeployments(decenDepos);
+    }
+  }, [decentralized])
+
   const navigate = useNavigate();
-  const clientIndexing = NewClient("https://api.thegraph.com/index-node/graphql");
+  const clientIndexing = useMemo(() => NewClient("https://api.thegraph.com/index-node/graphql"), []);
   window.scrollTo(0, 0);
+
+  let decentralizedSubgraphTable = null;
+  if (Object.keys(decentralizedDeployments)?.length) {
+    decentralizedSubgraphTable = Object.keys(decentralizedDeployments).map(key => (
+      <>
+        <Typography
+          key={"typography-" + key}
+          variant="h4"
+          align="left"
+          fontWeight={500}
+          fontSize={28}
+          sx={{ padding: "6px", my: 2 }}
+        >
+          {key.toUpperCase()}
+        </Typography>
+        <DeploymentsTable
+          key={"depTable-" + key}
+          clientIndexing={clientIndexing}
+          protocolsOnType={decentralizedDeployments[key]}
+          protocolType={key}
+          isDecentralizedNetworkTable={true}
+        />
+      </>
+    ))
+    decentralizedSubgraphTable.unshift(<Typography variant="h4" align="center" sx={{ my: 4 }}>Decentralized Network Subgraphs</Typography>);
+  }
 
   return (
     <DeploymentsContextProvider>
@@ -68,29 +134,41 @@ function DeploymentsPage() {
         >
           Load Subgraph
         </SearchInput>
-        <Typography variant="h4" align="center" sx={{ my: 3 }}>
-          Deployed Subgraphs
+        {decentralizedSubgraphTable}
+        <Typography variant="h4" align="center" sx={{ my: 4 }}>
+          Hosted Service Subgraphs
         </Typography>
-        {Object.keys(ProtocolsToQuery).map((key) => (
-          <>
-            <Typography
-              key={"typography-" + key}
-              variant="h4"
-              align="left"
-              fontWeight={500}
-              fontSize={28}
-              sx={{ padding: "6px", my: 2 }}
-            >
-              {key.toUpperCase()}
-            </Typography>
-            <DeploymentsTable
-              key={"depTable-" + key}
-              clientIndexing={clientIndexing}
-              protocolsOnType={ProtocolsToQuery[key]}
-              protocolType={key}
-            />
-          </>
-        ))}
+        {Object.keys(ProtocolsToQuery).map((key) => {
+          // map through Obj.keys of decentralizedDeployments[key], split them by space and lowercase. Then map through obj.keys of ProtocolsToQuery[key] and see if a similar key is found in both
+          // if (decentralizedDeployments[key]) {
+          //   Object.keys(decentralizedDeployments[key]).forEach(depo => {
+          //     const keyArr = depo.split("-")
+          //     const protocol = Object.keys(ProtocolsToQuery[key]).find(x => x.includes(keyArr[0]))
+          //   })
+          // }
+          // const allProtocolsOnKey = { ...ProtocolsToQuery[key], ...decentralizedDeployments[key] }
+          return (
+            <>
+              <Typography
+                key={"typography-" + key}
+                variant="h4"
+                align="left"
+                fontWeight={500}
+                fontSize={28}
+                sx={{ padding: "6px", my: 2 }}
+              >
+                {key.toUpperCase()}
+              </Typography>
+              <DeploymentsTable
+                key={"depTable-" + key}
+                clientIndexing={clientIndexing}
+                protocolsOnType={ProtocolsToQuery[key]}
+                protocolType={key}
+                isDecentralizedNetworkTable={false}
+              />
+            </>
+          )
+        })}
       </DeploymentsLayout>
     </DeploymentsContextProvider>
   );
