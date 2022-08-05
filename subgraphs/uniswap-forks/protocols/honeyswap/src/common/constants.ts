7ccab65da122d4614f398eb52a18d7a0a5bf7348
steegecs <56660047+steegecs@users.noreply.github.com>
2022-08-04 17:37:38 -0700

    Uniswap Forks: Fixing issue #657 (#727)
    * Removed assignments for emiisions at pool instantiation
    
    * Fixed apeswap status

--- subgraphs/uniswap-forks/protocols/honeyswap/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/honeyswap/src/common/constants.ts
@@ -2,7 +2,7 @@
 //////Versions//////
 ////////////////////
 
-export const PROTOCOL_SUBGRAPH_VERSION = "1.0.1";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.0.2";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 export const PROTOCOL_NAME = "Honeyswap";
 export const PROTOCOL_SLUG = "honeyswap";
