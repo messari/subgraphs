2f942c3e753bff97d9f6f1fcd6cbded1f5c3a066
Chris Steege <steegecas@gmail.com>
2022-08-08 13:19:53 -0400

    Incremented uniswap forks that use masterchef

--- subgraphs/uniswap-forks/protocols/solarbeam/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/solarbeam/src/common/constants.ts
@@ -2,7 +2,7 @@
 //////Versions//////
 ////////////////////
 
-export const PROTOCOL_SUBGRAPH_VERSION = "1.0.2";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.1.3";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 export const PROTOCOL_NAME = "Solarbeam";
 export const PROTOCOL_SLUG = "solarbeam";
--- subgraphs/uniswap-forks/protocols/ubeswap/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/ubeswap/src/common/constants.ts
@@ -4,7 +4,7 @@
 
 import { log } from "@graphprotocol/graph-ts";
 
-export const PROTOCOL_SUBGRAPH_VERSION = "1.1.4";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.1.5";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 export const PROTOCOL_NAME = "Ubeswap";
 export const PROTOCOL_SLUG = "ubeswap";
