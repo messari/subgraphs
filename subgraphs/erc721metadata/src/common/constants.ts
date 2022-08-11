7994d3b6826bfe9f90d4e200b29e7d05f390ca38
Founder <corerouter@163.com>
2022-08-11 22:00:43 +0800

    ERC721 metadata subgraph

---
+++ subgraphs/erc721metadata/src/common/constants.ts
@@ -0,0 +1,7 @@
+export const GENESIS_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+export const IPFS_SLASH = "ipfs/";
+export const IPFS_PREFIX = "ipfs://";
+
+export const IPFS_PREFIX_LEN = IPFS_PREFIX.length;
+export const IPFS_SLASH_LEN = IPFS_SLASH.length;
