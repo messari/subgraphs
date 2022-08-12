5879898b458cd3866f832f1fc7cdf5971033d787
Brian Ho <mr.brianhobo@gmail.com>
2022-08-11 18:26:14 -0700

    add token registry IPFS locations and deployment scripts

--- subgraphs/erc20/src/common/constants.ts
+++ subgraphs/erc20/src/common/constants.ts
@@ -1,7 +1,8 @@
 import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
+import { IPFS_HASH } from "../../configurations/configure";
 
 // Constants
-export const REGISTRY_HASH = "QmZ3Q5FHm9TJJNwPTX9gX949KExn2r6BT2cqddxebDaHMK"; // data/tokens.json
+export const REGISTRY_HASH = IPFS_HASH;
 
 export const DEFAULT_DECIMALS = 18;
 export const GENESIS_ADDRESS = "0x0000000000000000000000000000000000000000";
