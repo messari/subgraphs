25cd47eca2a060458057b853fa5b6233de3f2fa6
loc <l0c4t0r@gmail.com>
2022-08-17 03:06:53 +0000

    Rebase for deployment script changes

---
+++ subgraphs/velodrome-finance/src/common/utils/datetime.ts
@@ -0,0 +1,64 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import { SECONDS_PER_DAY } from "../constants";
+
+export let minute = BigInt.fromI32(60);
+export let hour = BigInt.fromI32(3600);
+export let day = BigInt.fromI32(86400);
+
+export let one = BigInt.fromI32(1);
+
+export function getMinuteOpenTime(timestamp: BigInt): BigInt {
+  let interval = minute;
+  return getOpenTime(timestamp, interval);
+}
+
+export function getMinuteCloseTime(timestamp: BigInt): BigInt {
+  return getMinuteOpenTime(timestamp).plus(minute).minus(one);
+}
+
+export function getTenMinuteOpenTime(timestamp: BigInt): BigInt {
+  let interval = minute.times(BigInt.fromI32(10));
+  return getOpenTime(timestamp, interval);
+}
+
+export function getTenMinuteCloseTime(timestamp: BigInt): BigInt {
+  return getTenMinuteOpenTime(timestamp)
+    .plus(minute.times(BigInt.fromI32(10)))
+    .minus(one);
+}
+
+export function getHourOpenTime(timestamp: BigInt): BigInt {
+  let interval = hour;
+  return getOpenTime(timestamp, interval);
+}
+
+export function getHourCloseTime(timestamp: BigInt): BigInt {
+  return getHourOpenTime(timestamp).plus(hour).minus(one);
+}
+
+export function getDayOpenTime(timestamp: BigInt): BigInt {
+  let interval = day;
+  return getOpenTime(timestamp, interval);
+}
+
+export function getDayCloseTime(timestamp: BigInt): BigInt {
+  return getDayOpenTime(timestamp).plus(day).minus(one);
+}
+
+export function getDaysSinceEpoch(secondsSinceEpoch: number): string {
+  return (<i32>Math.floor(secondsSinceEpoch / SECONDS_PER_DAY)).toString();
+}
+
+// helpers
+
+export function getOpenTime(timestamp: BigInt, interval: BigInt): BigInt {
+  let excess = timestamp.mod(interval);
+  return timestamp.minus(excess);
+}
+
+export function isSameDay(t1: BigInt, t2: BigInt): boolean {
+  let startOfDay1 = getDayOpenTime(t1);
+  let startOfDay2 = getDayOpenTime(t2);
+
+  return startOfDay1.equals(startOfDay2);
+}
---
+++ subgraphs/velodrome-finance/src/mappings/helpers.ts
@@ -0,0 +1,165 @@
+// import { log } from "@graphprotocol/graph-ts"
+import { BigInt, Address, ethereum } from "@graphprotocol/graph-ts";
+import {
+  Deposit,
+  LiquidityPool,
+  LiquidityPoolFee,
+} from "../../generated/schema";
+import { Pair as PairTemplate } from "../../generated/templates";
+// import { PairFactory as FactoryContract } from "../../generated/PairFactory/PairFactory";
+import {
+  BIGDECIMAL_ONE,
+  BIGDECIMAL_TWO,
+  BIGDECIMAL_ZERO,
+  BIGINT_ZERO,
+  FACTORY_ADDRESS,
+  LiquidityPoolFeeType,
+  LP_FEE_TO_ON,
+  PROTOCOL_FEE_TO_ON,
+  TRADING_FEE,
+} from "../common/constants";
+import { getOrCreateDex, getOrCreateToken } from "../common/getters";
+
+// export let factoryContract = FactoryContract.bind(Address.fromString(FACTORY_ADDRESS));
+
+// Create a liquidity pool from PairCreated contract call
+export function createLiquidityPool(
+  event: ethereum.Event,
+  poolAddress: Address,
+  token0Address: Address,
+  token1Address: Address
+): void {
+  let protocol = getOrCreateDex();
+
+  // create the tokens and tokentracker
+  let token0 = getOrCreateToken(token0Address);
+  let token1 = getOrCreateToken(token1Address);
+  let LPtoken = getOrCreateToken(poolAddress);
+
+  let pool = new LiquidityPool(poolAddress.toHexString());
+
+  pool.protocol = protocol.id;
+  pool.name = protocol.name + " " + LPtoken.symbol;
+  pool.symbol = LPtoken.symbol;
+  pool.inputTokens = [token0.id, token1.id];
+  pool.outputToken = LPtoken.id;
+  pool.fees = createPoolFees(poolAddress.toHexString());
+  pool.isSingleSided = false;
+  pool.createdTimestamp = event.block.timestamp;
+  pool.createdBlockNumber = event.block.number;
+  pool.totalValueLockedUSD = BIGDECIMAL_ZERO;
+  pool.cumulativeSupplySideRevenueUSD = BIGDECIMAL_ZERO;
+  pool.cumulativeProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+  pool.cumulativeTotalRevenueUSD = BIGDECIMAL_ZERO;
+  pool.cumulativeVolumeUSD = BIGDECIMAL_ZERO;
+  pool.inputTokenBalances = [BIGINT_ZERO, BIGINT_ZERO];
+  pool.inputTokenWeights = [
+    BIGDECIMAL_ONE.div(BIGDECIMAL_TWO),
+    BIGDECIMAL_ONE.div(BIGDECIMAL_TWO),
+  ];
+  pool.outputTokenSupply = BIGINT_ZERO;
+  pool.outputTokenPriceUSD = BIGDECIMAL_ZERO;
+  pool.stakedOutputTokenAmount = BIGINT_ZERO;
+  pool.rewardTokenEmissionsAmount = [BIGINT_ZERO, BIGINT_ZERO];
+  pool.rewardTokenEmissionsUSD = [BIGDECIMAL_ZERO, BIGDECIMAL_ZERO];
+
+  // create the tracked contract based on the template
+  PairTemplate.create(poolAddress);
+
+  protocol.totalPoolCount += 1;
+
+  pool.save();
+  token0.save();
+  token1.save();
+  LPtoken.save();
+  protocol.save();
+}
+
+function createPoolFees(poolAddress: string): string[] {
+  let poolLpFee = new LiquidityPoolFee(poolAddress.concat("-lp-fee"));
+  let poolProtocolFee = new LiquidityPoolFee(
+    poolAddress.concat("-protocol-fee")
+  );
+  let poolTradingFee = new LiquidityPoolFee(poolAddress.concat("-trading-fee"));
+
+  poolLpFee.feeType = LiquidityPoolFeeType.FIXED_LP_FEE;
+  poolProtocolFee.feeType = LiquidityPoolFeeType.FIXED_PROTOCOL_FEE;
+  poolTradingFee.feeType = LiquidityPoolFeeType.FIXED_TRADING_FEE;
+
+  poolLpFee.feePercentage = LP_FEE_TO_ON;
+  poolProtocolFee.feePercentage = PROTOCOL_FEE_TO_ON;
+  poolTradingFee.feePercentage = TRADING_FEE;
+
+  poolLpFee.save();
+  poolProtocolFee.save();
+  poolTradingFee.save();
+
+  return [poolLpFee.id, poolProtocolFee.id, poolTradingFee.id];
+}
+
+// Generate the deposit entity and update deposit account for the according pool.
+export function createDeposit(
+  event: ethereum.Event,
+  amount0: BigInt,
+  amount1: BigInt,
+  sender: Address
+): void {
+
+  let pool = LiquidityPool.load(event.address.toHex())
+
+  let deposit = new Deposit(
+    "deposit"F
+      .concat("-")
+      .concat(event.transaction.hash.toHex())
+      .concat("-")
+      .concat(event.logIndex.toString())
+  );
+  deposit.hash = event.transaction.hash.toHex();
+  deposit.logIndex = event.logIndex.toI32();
+  deposit.protocol = FACTORY_ADDRESS;
+  deposit.to = event.address.toHex()
+  deposit.from = sender.toHex()
+  deposit.blockNumber = event.block.number
+  deposit.timestamp = event.block.timestamp
+  deposit.inputTokens = [pairContract.token0.toString(), pairContract.token1.toString()]
+  deposit.outputToken =
+
+
+  deposit.save();
+}
+
+// Generate the withdraw entity
+export function createWithdraw(
+  event: ethereum.Event,
+  amount0: BigInt,
+  amount1: BigInt,
+  sender: Address,
+  to: Address
+): void {
+  // let withdrawal = new Withdraw(event.transaction.hash.toHexString().concat("-").concat(event.logIndex.toString()));
+  // withdrawal.hash = event.transaction.hash.toHexString();
+  // withdrawal.logIndex = event.logIndex.toI32();
+  // withdrawal.protocol = FACTORY_ADDRESS;
+  // ..
+  // withdrawal.save();
+}
+
+// Handle swaps data and update entities volumes and fees
+export function createSwapHandleVolumeAndFees(
+  event: ethereum.Event,
+  to: Address,
+  sender: Address,
+  amount0In: BigInt,
+  amount1In: BigInt,
+  amount0Out: BigInt,
+  amount1Out: BigInt
+): void {
+  // let swap = new SwapEvent(event.transaction.hash.toHexString().concat("-").concat(event.logIndex.toString()));
+  // // update swap event
+  // swap.hash = event.transaction.hash.toHexString();
+  // swap.logIndex = event.logIndex.toI32();
+  // swap.protocol = FACTORY_ADDRESS;
+  // ..
+  // swap.save();
+  // updateVolumeAndFees(event, trackedAmountUSD, feeUSD);
+}
