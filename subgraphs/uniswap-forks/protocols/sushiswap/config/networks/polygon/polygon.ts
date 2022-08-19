efd87e7908d311f2821ae360f5e64eef5efa9c92
Chris Steege <steegecas@gmail.com>
2022-08-18 22:25:29 -0500

    Fixed sushi polygon pricing

--- subgraphs/uniswap-forks/protocols/sushiswap/config/networks/polygon/polygon.ts
+++ subgraphs/uniswap-forks/protocols/sushiswap/config/networks/polygon/polygon.ts
@@ -6,6 +6,7 @@ import {
   MINIMUM_LIQUIDITY_FIVE_THOUSAND,
   MINIMUM_LIQUIDITY_ONE_HUNDRED_THOUSAND,
   MINIMUM_LIQUIDITY_TEN_THOUSAND,
+  MINIMUM_LIQUIDITY_TWENTY_FIVE_THOUSAND,
   Network,
   PROTOCOL_SCHEMA_VERSION,
   RewardIntervalType,
@@ -84,11 +85,17 @@ export class SushiswapMaticConfigurations implements Configurations {
   getWhitelistTokens(): string[] {
     return toLowerCaseList([
       "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", // wETH
-      "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", // wMATIC
+      "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", // wMatic
       "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // USDC
       "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", // DAI
       "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", // USDT
       "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6", // wBTC
+      "0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a", // SUSHI
+      "0xd6df932a45c0f255f85145f286ea0b292b21c90b", // AAVE
+      "0x104592a158490a9228070e0a8e5343b499e125d0", // FRAX POS
+      "0x2f800db0fdb5223b3c3f354886d907a671414a7f", // BCT
+      "0x45c32fa6df82ead1e2ef74d17b76547eddfaff89", // FRAX
+      "0x34d4ab47bee066f361fa52d792e69ac7bd05ee23", // AURUM
     ]);
   }
   getStableCoins(): string[] {
@@ -112,9 +119,9 @@ export class SushiswapMaticConfigurations implements Configurations {
     return [];
   }
   getMinimumLiquidityThresholdTrackVolume(): BigDecimal {
-    return MINIMUM_LIQUIDITY_TEN_THOUSAND;
+    return MINIMUM_LIQUIDITY_TWENTY_FIVE_THOUSAND;
   }
   getMinimumLiquidityThresholdTrackPrice(): BigDecimal {
-    return MINIMUM_LIQUIDITY_FIVE_THOUSAND;
+    return MINIMUM_LIQUIDITY_TWENTY_FIVE_THOUSAND;
   }
 }
