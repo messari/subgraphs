eba35b33699aa9ef49e268fd4fb15fcd1730681e
tnkrxyz <97625120+tnkrxyz@users.noreply.github.com>
2022-08-23 17:01:42 -0700

    Fix (#patch); Inverse finance; address QA issues in #571 (#856)

--- subgraphs/inverse-finance/src/rewardtoken.ts
+++ subgraphs/inverse-finance/src/rewardtoken.ts
@@ -1,20 +1,23 @@
 import { Address } from "@graphprotocol/graph-ts";
 import { RewardToken } from "../generated/schema";
 import { INV, OwnerChanged } from "../generated/INV/INV";
-import { ZERO_ADDRESS, RewardTokenType } from "./common/constants";
+import { ZERO_ADDRESS, RewardTokenType, INV_ADDRESS } from "./common/constants";
 import { prefixID } from "./common/utils";
+import { getOrCreateToken } from "./common/getters";
 
 export function handleOwnerChanged(event: OwnerChanged): void {
   // this only happens when the constructor() of the INV contract is called
   if (event.params.owner == Address.fromString(ZERO_ADDRESS)) {
-    let tokenAddr = event.address.toHexString()
+    //let tokenAddr = event.address.toHexString();
+    let tokenAddr = INV_ADDRESS;
+    let invToken = getOrCreateToken(Address.fromString(tokenAddr));
 
     let depositTokenId = prefixID(tokenAddr, RewardTokenType.DEPOSIT);
     let depositRewardToken = RewardToken.load(depositTokenId);
 
     if (depositRewardToken == null) {
       depositRewardToken = new RewardToken(depositTokenId);
-      depositRewardToken.token = tokenAddr;
+      depositRewardToken.token = invToken.id;
       depositRewardToken.type = RewardTokenType.DEPOSIT;
     }
 
@@ -25,7 +28,7 @@ export function handleOwnerChanged(event: OwnerChanged): void {
 
     if (borrowRewardToken == null) {
       borrowRewardToken = new RewardToken(borrowTokenId);
-      borrowRewardToken.token = tokenAddr;
+      borrowRewardToken.token = invToken.id;
       borrowRewardToken.type = RewardTokenType.BORROW;
     }
 
--- subgraphs/inverse-finance/src/stablizer.ts
+++ subgraphs/inverse-finance/src/stablizer.ts
@@ -1,6 +1,9 @@
-import { BigDecimal, ethereum } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, ethereum } from "@graphprotocol/graph-ts";
 import { Buy, Sell } from "../generated/Stablizer/Stablizer";
+import { DOLA_ADDRESS } from "./common/constants";
+import { getOrCreateToken } from "./common/getters";
 import { updateStablizerFees } from "./common/helpers";
+import { decimalsToBigDecimal } from "./common/utils";
 
 // update revenue from Stablizer for
 //    - protocol.cumulativeProtocolSideRevenueUSD
@@ -11,12 +14,20 @@ import { updateStablizerFees } from "./common/helpers";
 //    - FinancialsDailySnapshot.cumulativeTotalRevenueUSD
 export function handleSell(event: Sell): void {
   // assume price of DOLA/DAI equal $1
-  let fees = event.params.sold.minus(event.params.received).toBigDecimal();
+  let token = getOrCreateToken(Address.fromString(DOLA_ADDRESS));
+  let fees = event.params.sold
+    .minus(event.params.received)
+    .toBigDecimal()
+    .div(decimalsToBigDecimal(token.decimals));
   updateStablizerFees(fees, event);
 }
 
 export function handleBuy(event: Buy): void {
   // assume price of DOLA/DAI equal $1
-  let fees = event.params.spent.minus(event.params.purchased).toBigDecimal();
+  let token = getOrCreateToken(Address.fromString(DOLA_ADDRESS));
+  let fees = event.params.spent
+    .minus(event.params.purchased)
+    .toBigDecimal()
+    .div(decimalsToBigDecimal(token.decimals));
   updateStablizerFees(fees, event);
 }
