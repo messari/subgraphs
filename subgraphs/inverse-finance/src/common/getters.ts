ecc0b3ece801b01520e1469f2a0eada53a239cd3
tnkrxyz <tnkrxyz@gmail.com>
2022-08-28 05:56:39 +0000

    Fixed interest rates for snapshots

--- subgraphs/inverse-finance/src/common/getters.ts
+++ subgraphs/inverse-finance/src/common/getters.ts
@@ -450,3 +450,26 @@ export function getOrCreateInterestRate(
   }
   return interestRate;
 }
+
+// create seperate InterestRate Entities for each market snapshot
+// this is needed to prevent snapshot rates from being pointers to the current rate
+export function getSnapshotRates(rates: string[], timeSuffix: string): string[] {
+  let snapshotRates: string[] = [];
+  for (let i = 0; i < rates.length; i++) {
+    let rate = InterestRate.load(rates[i]);
+    if (!rate) {
+      log.warning("[getSnapshotRates] rate {} not found, should not happen", [rates[i]]);
+      continue;
+    }
+
+    // create new snapshot rate
+    let snapshotRateId = rates[i].concat("-").concat(timeSuffix);
+    let snapshotRate = new InterestRate(snapshotRateId);
+    snapshotRate.side = rate.side;
+    snapshotRate.type = rate.type;
+    snapshotRate.rate = rate.rate;
+    snapshotRate.save();
+    snapshotRates.push(snapshotRateId);
+  }
+  return snapshotRates;
+}
