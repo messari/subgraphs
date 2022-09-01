663a46a3ed78a335d2216b677c9ec167bceccd3d
Chris Steege <steegecas@gmail.com>
2022-09-01 18:48:02 -0500

    Converted strings to lowercase

--- subgraphs/uniswap-forks/protocols/vvs-finance/config/networks/cronos/cronos.ts
+++ subgraphs/uniswap-forks/protocols/vvs-finance/config/networks/cronos/cronos.ts
@@ -38,13 +38,11 @@ export class VSSFinanceCronosConfigurations implements Configurations {
     return PROTOCOL_SLUG;
   }
   getFactoryAddress(): string {
-    return "0x3b44b2a187a7b3824131f8db5a74194d0a42fc15".toLowerCase();
+    return "0x3b44b2a187a7b3824131f8db5a74194d0a42fc15";
   }
   getFactoryContract(): Factory {
     return Factory.bind(
-      Address.fromString(
-        "0x3b44b2a187a7b3824131f8db5a74194d0a42fc15".toLowerCase()
-      )
+      Address.fromString("0x3b44b2a187a7b3824131f8db5a74194d0a42fc15")
     );
   }
   getTradeFee(): BigDecimal {
@@ -72,39 +70,39 @@ export class VSSFinanceCronosConfigurations implements Configurations {
     return BIGINT_ZERO;
   }
   getReferenceToken(): string {
-    return "0x5c7f8a570d578ed84e63fdfa7b1ee72deae1ae23".toLowerCase();
+    return "0x5c7f8a570d578ed84e63fdfa7b1ee72deae1ae23";
   }
   getRewardToken(): string {
-    return "0x2D03bECE6747ADC00E1a131BBA1469C15fD11e03".toLowerCase();
+    return "0x2d03bece6747adc00e1a131bba1469c15fd11e03";
   }
   getWhitelistTokens(): string[] {
-    return toLowerCaseList([
+    return [
       "0x5c7f8a570d578ed84e63fdfa7b1ee72deae1ae23", // WCRO
       "0xf2001b145b43032aaf5ee2884e456ccd805f677d", // DAI
       "0xc21223249ca28397b4b6541dffaecc539bff0c59", // USDC
       "0x66e428c3f67a68878562e79a0234c1f83c208770", // USDT
       "0xe44fd7fcb2b1581822d0c862b68222998a0c299a", // wETH
-    ]);
+    ];
   }
   getStableCoins(): string[] {
-    return toLowerCaseList([
+    return [
       "0xf2001b145b43032aaf5ee2884e456ccd805f677d", // DAI
       "0xc21223249ca28397b4b6541dffaecc539bff0c59", // USDC
       "0x66e428c3f67a68878562e79a0234c1f83c208770", // USDT
-    ]);
+    ];
   }
   getStableOraclePools(): string[] {
-    return toLowerCaseList([
-      "0xe61Db569E231B3f5530168Aa2C9D50246525b6d6", // USDC/wCRO
-      "0x3Eb9FF92e19b73235A393000C176c8bb150F1B20", // DAI/wCRO
-      "0x3d2180DB9E1B909f35C398BC39EF36108C0FC8c3", // USDT/wCRO
-    ]);
+    return [
+      "0xe61db569e231b3f5530168aa2c9d50246525b6d6", // USDC/wCRO
+      "0x3eb9ff92e19b73235a393000c176c8bb150f1b20", // DAI/wCRO
+      "0x3d2180db9e1b909f35c398bc39ef36108c0fc8c3", // USDT/wCRO
+    ];
   }
   getUntrackedPairs(): string[] {
-    return toLowerCaseList([]);
+    return [];
   }
   getUntrackedTokens(): string[] {
-    return toLowerCaseList([]);
+    return [];
   }
   getMinimumLiquidityThresholdTrackVolume(): BigDecimal {
     return MINIMUM_LIQUIDITY_TEN_THOUSAND;
