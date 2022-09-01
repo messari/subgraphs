196b4e37d30eaaf5d004ccc834d29841eb3d8896
Chris Steege <steegecas@gmail.com>
2022-09-01 16:13:36 -0500

    Updated VVS Finance version

--- subgraphs/uniswap-forks/configurations/configure.ts
+++ subgraphs/uniswap-forks/configurations/configure.ts
@@ -3,7 +3,7 @@ import { getNetworkConfigurations } from "./configurations/configurations";
 import { Deploy } from "./configurations/deploy";
 
 // Select the deployment protocol and network
-let deployment = Deploy.SUSHISWAP_HARMONY;
+let deployment = Deploy.VVS_FINANCE_CRONOS;
 
 // export const NetworkConfigs = configurationsMap.get(deployment)!
 export const NetworkConfigs = getNetworkConfigurations(deployment);
