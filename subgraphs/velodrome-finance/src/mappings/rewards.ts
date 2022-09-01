0e2ab07fc91f16ceea9b4ea73882e076d4dfd925
loc <l0c4t0r@gmail.com>
2022-09-01 15:02:48 +0000

    WIP: add DistributeReward event

--- subgraphs/velodrome-finance/src/mappings/rewards.ts
+++ subgraphs/velodrome-finance/src/mappings/rewards.ts
@@ -1,7 +1,14 @@
-import { GaugeCreated, GaugeKilled, GaugeRevived } from "../../generated/Voter/Voter"
+import {
+  DistributeReward,
+  GaugeCreated,
+  GaugeKilled,
+  GaugeRevived,
+} from "../../generated/Voter/Voter";
 
 export function handleGaugeCreated(event: GaugeCreated): void {}
 
 export function handleGaugeKilled(event: GaugeKilled): void {}
 
 export function handleGaugeRevived(event: GaugeRevived): void {}
+
+export function handleDistributeReward(event: DistributeReward): void {}
