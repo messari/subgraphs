141315a36c3e66ea15c2f2caf095fd618316c343
daniel <dev.danielkhoo@gmail.com>
2022-09-02 16:08:43 +0800

    Update Hop subgraph

--- subgraphs/openzeppelin-governor/protocols/hop-governance/src/hop-governor.ts
+++ subgraphs/openzeppelin-governor/protocols/hop-governance/src/hop-governor.ts
@@ -14,7 +14,7 @@ import {
   _handleProposalExecuted,
   _handleProposalQueued,
   _handleVoteCast,
-  getOrCreateProposal,
+  getProposal,
   getGovernance,
 } from "../../../src/handlers";
 import { HOPGovernor } from "../../../generated/HOPGovernor/HOPGovernor";
@@ -78,7 +78,7 @@ function getLatestProposalValues(
   proposalId: string,
   contractAddress: Address
 ): Proposal {
-  let proposal = getOrCreateProposal(proposalId);
+  let proposal = getProposal(proposalId);
 
   // On first vote, set state and quorum values
   if (proposal.state == ProposalState.PENDING) {
