7fc3b388f57776bca45334d3f48efb507b1a0a78
Chris Steege <steegecas@gmail.com>
2022-09-09 09:18:39 -0500

    Updated Reward Interval for VVS finance and handled NONE logic in rewards.ts

--- subgraphs/uniswap-forks/protocols/vvs-finance/config/networks/cronos/cronos.ts
+++ subgraphs/uniswap-forks/protocols/vvs-finance/config/networks/cronos/cronos.ts
@@ -64,7 +64,7 @@ export class VSSFinanceCronosConfigurations implements Configurations {
     return FeeSwitch.ON;
   }
   getRewardIntervalType(): string {
-    return RewardIntervalType.BLOCK;
+    return RewardIntervalType.NONE;
   }
   getRewardTokenRate(): BigInt {
     return BIGINT_ZERO;
