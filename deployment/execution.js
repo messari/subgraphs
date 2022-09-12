1973e9f4b912964ef379cc9e7f8c18442f455d9e
Chris Steege <steegecas@gmail.com>
2022-09-12 12:13:38 -0500

    Updated CI/CD for new features and deployment.json

--- deployment/execution.js
+++ deployment/execution.js
@@ -2,93 +2,25 @@ const exec = require("child_process").exec;
 const fs = require("fs");
 
 /**
- * @param {string} protocol - Protocol that is being deployed
- * @param {string} network - Network that the protocol is being deployed to
- * @param {string} template - Template location that will be used to create subgraph.yaml
- * @param {string} location - Location in the subgraph will be deployed to {e.g. messari/uniswap-v2-ethereum}
- */
-function scripts(protocol, network, template, location, constants, type) {
-  let scripts = [];
-  let removeGenerated = "rm -rf generated";
-  let removeBuild = "rm -rf build";
-  let removeResults = "rm -rf results.txt";
-  let removeConfig = "rm -rf configurations/configure.ts";
-  let removeSubgraphYaml = "rm -rf subgraph.yaml";
-  let prepareYaml =
-    "npm run prepare:yaml --PROTOCOL=" +
-    protocol +
-    " --NETWORK=" +
-    network +
-    " --TEMPLATE=" +
-    template;
-  let prepareConstants =
-    "npm run prepare:constants --PROTOCOL=" +
-    protocol +
-    " --NETWORK=" +
-    network;
-  let codegen = "graph codegen";
-  let build = "graph build";
-  let deployment = "npm run deploy:subgraph --LOCATION=" + location;
-
-  scripts.push(removeGenerated);
-  scripts.push(removeBuild);
-  scripts.push(removeResults);
-  scripts.push(removeConfig);
-  scripts.push(removeSubgraphYaml);
-  scripts.push(prepareYaml);
-  if (constants == true) {
-    scripts.push(prepareConstants);
-  }
-  scripts.push(codegen);
-
-  // Null value for type assumes you want to deploy
-  if (["deploy", ""].includes(type.toLowerCase())) {
-    scripts.push(deployment);
-  } else if (type.toLowerCase() == "build") {
-    scripts.push(build);
-  } else {
-    console.log("Error: invalid type - Neither build nor deploy");
-  }
-
-  return scripts;
-}
-
-function getDeploymentNetwork(network) {
-  let deployNetwork = "";
-  switch (network) {
-    case "mainnet":
-      deployNetwork = "ethereum";
-      break;
-    case "xdai":
-      deployNetwork = "gnosis";
-      break;
-    case "matic":
-      deployNetwork = "polygon";
-      break;
-    default:
-      deployNetwork = network;
-  }
-  return deployNetwork;
-}
-
-/**
  * @param {string[]} array - Protocol that is being deployed
  * @param {string} callback
  */
-async function runCommands(allScripts, results, args, callback) {
+async function executeDeployment(deployment, callback) {
   let logs = "";
+  let results = "RESULTS:\n";
   var deploymentIndex = 0;
   var scriptIndex = 0;
-  var httpCounter = 1;
-  let allDeployments = Array.from(allScripts.keys());
+  var httpCounter = 0;
+  let allDeployments = Array.from(deployment.scripts.keys());
 
   function next() {
     if (deploymentIndex < allDeployments.length) {
       exec(
-        allScripts.get(allDeployments[deploymentIndex])[scriptIndex++],
+        deployment.scripts.get(allDeployments[deploymentIndex])[scriptIndex++],
         function (error, stdout, stderr) {
           logs = logs + "stdout: " + stdout;
           logs = logs + "stderr: " + stderr;
+
           if (stderr.includes("HTTP error")) {
             if (httpCounter >= 2) {
               deploymentIndex++;
@@ -96,10 +28,11 @@ async function runCommands(allScripts, results, args, callback) {
             }
             httpCounter++;
           }
+
           if (error !== null) {
             if (
               stderr.includes("HTTP error deploying the subgraph") &&
-              httpCounter <= 3
+              httpCounter < 3
             ) {
               httpCounter++;
               console.log(
@@ -111,8 +44,7 @@ async function runCommands(allScripts, results, args, callback) {
               );
             } else {
               logs = logs + "Exec error: " + error;
-
-              if (args.type == "build") {
+              if (deployment.getDeploy() == false) {
                 results +=
                   "Build Failed: " + allDeployments[deploymentIndex] + "\n";
               } else {
@@ -121,17 +53,16 @@ async function runCommands(allScripts, results, args, callback) {
                   allDeployments[deploymentIndex] +
                   "\n";
               }
-
-              console.log(error);
+              logs = logs + "error: " + error;
               deploymentIndex++;
               scriptIndex = 0;
-              httpCounter = 1;
+              httpCounter = 0;
             }
           } else if (
             scriptIndex ==
-            allScripts.get(allDeployments[deploymentIndex]).length
+            deployment.scripts.get(allDeployments[deploymentIndex]).length
           ) {
-            if (args.type == "build") {
+            if (deployment.getDeploy() == false) {
               results +=
                 "Build Successful: " + allDeployments[deploymentIndex] + "\n";
             } else {
@@ -160,7 +91,7 @@ async function runCommands(allScripts, results, args, callback) {
       );
 
       // Print the logs if printlogs is 't' or 'true'
-      if (["true", "t"].includes(args.printlogs.toLowerCase())) {
+      if (deployment.printlogs) {
         console.log(logs);
       }
       console.log("\n" + results + "END" + "\n\n");
@@ -172,4 +103,4 @@ async function runCommands(allScripts, results, args, callback) {
   next();
 }
 
-module.exports = { scripts, getDeploymentNetwork, runCommands };
+module.exports = { executeDeployment };
