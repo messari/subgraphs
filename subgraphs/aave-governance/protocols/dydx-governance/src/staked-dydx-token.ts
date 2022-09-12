a317a04ff4cd566097cd7d6f158f40f20e7b127f
daniel <dev.danielkhoo@gmail.com>
2022-09-12 18:13:50 +0800

    Add dydx staked token and handler

--- subgraphs/aave-governance/protocols/dydx-governance/src/staked-dydx-token.ts
+++ subgraphs/aave-governance/protocols/dydx-governance/src/staked-dydx-token.ts
@@ -6,7 +6,7 @@ import {
 import {
   _handleDelegateChanged,
   _handleDelegatedPowerChanged,
-  _handleTransfer,
+  _handleStakedTokenTransfer,
 } from "../../../src/tokenHandlers";
 
 // DelegateChanged(indexed address,indexed address,indexed address)
@@ -25,13 +25,13 @@ export function handleDelegatedPowerChanged(
     event.params.delegationType,
     event.params.user.toHexString(),
     event.params.amount,
-    false
+    true
   );
 }
 
 // // Transfer(indexed address,indexed address,uint256)
 export function handleTransfer(event: Transfer): void {
-  _handleTransfer(
+  _handleStakedTokenTransfer(
     event.params.from.toHexString(),
     event.params.to.toHexString(),
     event.params.value,
