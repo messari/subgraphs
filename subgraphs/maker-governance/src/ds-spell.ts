be2853cbb6a1285bc1b8bd9dec3076c64eabf5d6
daniel <dev.danielkhoo@gmail.com>
2022-09-22 21:49:10 +0800

    Fix spell handleCast

--- subgraphs/maker-governance/src/ds-spell.ts
+++ subgraphs/maker-governance/src/ds-spell.ts
@@ -1,13 +1,12 @@
-import { LogNote } from "../generated/DSChief/DSChief";
 import { Spell } from "../generated/schema";
+import { CastCall } from "../generated/DSChief/DSSpell";
 
-export function handleCast(event: LogNote): void {
-  let spellID = event.params.foo.toHexString(); // foo is the spellID
-
+export function handleCast(call: CastCall): void {
+  let spellID = call.to.toHexString(); // spell address is the spellID
   let spell = Spell.load(spellID);
   if (!spell) return;
   spell.cast = true;
-  spell.castBlock = event.block.number;
+  spell.castBlock = call.block.number;
   spell.castWith = spell.totalWeightedVotes;
   spell.save();
 }
