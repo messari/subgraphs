2eaec45366998a85f078d9fed0fb4e09858f709d
tnkrxyz <tnkrxyz@gmail.com>
2022-09-28 03:32:56 +0000

    fixing #1001; bump methodology & subgraph version

--- subgraphs/maple-finance/src/common/mappingHelpers/update/intervalUpdate.ts
+++ subgraphs/maple-finance/src/common/mappingHelpers/update/intervalUpdate.ts
@@ -10,7 +10,7 @@ import { getOrCreateProtocol } from "../getOrCreate/protocol";
 import {
     getOrCreateFinancialsDailySnapshot,
     getOrCreateMarketDailySnapshot,
-    getOrCreateMarketHourlySnapshot
+    getOrCreateMarketHourlySnapshot,
 } from "../getOrCreate/snapshots";
 import { getOrCreateRewardToken, getOrCreateToken } from "../getOrCreate/supporting";
 
@@ -91,9 +91,9 @@ function intervalUpdateMarket(event: ethereum.Event, market: Market): Market {
         .times(market.exchangeRate);
 
     const inputTokenBalanceUSD = getTokenAmountInUSD(event, inputToken, market.inputTokenBalance);
-    market.totalValueLockedUSD = stakeLocker.stakeTokenBalanceUSD.plus(inputTokenBalanceUSD);
 
     market.totalDepositBalanceUSD = getTokenAmountInUSD(event, inputToken, market._totalDepositBalance);
+    market.totalValueLockedUSD = market.totalDepositBalanceUSD;
 
     market.totalBorrowBalanceUSD = getTokenAmountInUSD(event, inputToken, market._totalBorrowBalance);
 
