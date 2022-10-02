6c70edc378147da009d480bad1b6df042bf9d88e
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2022-10-03 00:28:58 +0530

    add Aura Finance subgraph

---
+++ subgraphs/aura-finance/configurations/configurations/configurations.ts
@@ -0,0 +1,19 @@
+import { AuraFinanceMainnetConfigurations } from "../../protocols/aura-finance/config/deployments/aura-finance-mainnet/configurations";
+import { Configurations } from "./interface";
+import { Deploy } from "./deploy";
+import { log } from "@graphprotocol/graph-ts";
+
+export function getNetworkConfigurations(deploy: i32): Configurations {
+  switch (deploy) {
+    case Deploy.AURAFINANCE_MAINNET: {
+      return new AuraFinanceMainnetConfigurations();
+    }
+    default: {
+      log.critical(
+        "No configurations found for deployment protocol/network",
+        []
+      );
+      return new AuraFinanceMainnetConfigurations();
+    }
+  }
+}
---
+++ subgraphs/aura-finance/src/common/utils/pools.ts
@@ -0,0 +1,157 @@
+import { BigInt, BigDecimal, Address, Bytes } from "@graphprotocol/graph-ts";
+
+import {
+  BIGDECIMAL_HUNDRED,
+  BIGINT_ZERO,
+  ZERO_ADDRESS,
+  BIGINT_TEN,
+} from "../constants";
+import { readValue } from "./ethereum";
+import { PoolTokensType } from "../types";
+import { DEFAULT_DECIMALS } from "../../prices/common/constants";
+
+import { _RewardPool } from "../../../generated/schema";
+import { BalancerVault } from "../../../generated/Booster/BalancerVault";
+import { WeightedPool } from "../../../generated/Booster/WeightedPool";
+import { StablePool } from "../../../generated/Booster/StablePool";
+
+export function getPoolTokensInfo(poolAddress: Address): PoolTokensType {
+  const poolContract = WeightedPool.bind(poolAddress);
+
+  const balancerVault = readValue<Address>(
+    poolContract.try_getVault(),
+    Address.fromString(ZERO_ADDRESS)
+  );
+  const balancerPool = readValue<Bytes>(
+    poolContract.try_getPoolId(),
+    new Bytes(0)
+  );
+
+  let supply = BIGINT_ZERO;
+
+  const stablePoolContract = StablePool.bind(poolAddress);
+
+  const actualSupplyCall = stablePoolContract.try_getActualSupply();
+
+  if (actualSupplyCall.reverted) {
+    const virtulSupplyCall = poolContract.try_getVirtualSupply();
+
+    if (virtulSupplyCall.reverted) {
+      supply = readValue<BigInt>(poolContract.try_totalSupply(), BIGINT_ZERO);
+    } else {
+      supply = virtulSupplyCall.value;
+    }
+  } else {
+    supply = actualSupplyCall.value;
+  }
+
+  if (!(balancerVault == Address.fromString(ZERO_ADDRESS))) {
+    let balancerVaultContract = BalancerVault.bind(balancerVault);
+
+    let result = balancerVaultContract.try_getPoolTokens(balancerPool);
+
+    if (result.reverted) return new PoolTokensType(poolAddress, supply);
+
+    return new PoolTokensType(
+      poolAddress,
+      supply,
+      result.value.getTokens(),
+      result.value.getBalances()
+    );
+  }
+
+  return new PoolTokensType(poolAddress, supply);
+}
+
+export function isBPT(tokenAddress: Address): boolean {
+  let poolContract = WeightedPool.bind(tokenAddress);
+
+  let balancerPool = poolContract.try_getPoolId();
+  if (!balancerPool.reverted) {
+    return true;
+  }
+
+  return false;
+}
+
+export function getPoolTokenWeightsForDynamicWeightPools(
+  poolAddress: Address,
+  popIndex: number
+): BigDecimal[] {
+  const poolContract = WeightedPool.bind(poolAddress);
+
+  let scales = readValue<BigInt[]>(poolContract.try_getScalingFactors(), []);
+
+  let inputTokenScales: BigInt[] = [];
+  for (let idx = 0; idx < scales.length; idx++) {
+    if (idx == popIndex) {
+      continue;
+    }
+
+    inputTokenScales.push(scales.at(idx));
+  }
+
+  let totalScale = inputTokenScales
+    .reduce<BigInt>((sum, current) => sum.plus(current), BIGINT_ZERO)
+    .toBigDecimal();
+
+  let inputTokenWeights: BigDecimal[] = [];
+  for (let idx = 0; idx < inputTokenScales.length; idx++) {
+    inputTokenWeights.push(
+      inputTokenScales
+        .at(idx)
+        .divDecimal(totalScale)
+        .times(BIGDECIMAL_HUNDRED)
+    );
+  }
+
+  return inputTokenWeights;
+}
+
+export function getPoolTokenWeightsForNormalizedPools(
+  poolAddress: Address,
+  popIndex: number
+): BigDecimal[] {
+  const poolContract = WeightedPool.bind(poolAddress);
+
+  let weights = readValue<BigInt[]>(
+    poolContract.try_getNormalizedWeights(),
+    []
+  );
+
+  let inputTokenWeights: BigDecimal[] = [];
+  for (let idx = 0; idx < weights.length; idx++) {
+    if (idx == popIndex) {
+      continue;
+    }
+
+    inputTokenWeights.push(
+      weights
+        .at(idx)
+        .divDecimal(
+          BIGINT_TEN.pow(DEFAULT_DECIMALS.toI32() as u8).toBigDecimal()
+        )
+        .times(BIGDECIMAL_HUNDRED)
+    );
+  }
+
+  return inputTokenWeights;
+}
+
+export function getPoolTokenWeights(
+  poolAddress: Address,
+  popIndex: number
+): BigDecimal[] {
+  let inputTokenWeights = getPoolTokenWeightsForNormalizedPools(
+    poolAddress,
+    popIndex
+  );
+  if (inputTokenWeights.length > 0) return inputTokenWeights;
+
+  inputTokenWeights = getPoolTokenWeightsForDynamicWeightPools(
+    poolAddress,
+    popIndex
+  );
+
+  return inputTokenWeights;
+}
