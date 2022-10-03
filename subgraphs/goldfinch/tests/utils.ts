ff2244c3bf144f2b9da787d3b0b466f6c7b2d67c
tnkrxyz <97625120+tnkrxyz@users.noreply.github.com>
2022-10-02 22:08:35 -0700

    feat(#major); goldfinch; Add initial goldfinch Subgraph (#1126)

---
+++ subgraphs/goldfinch/tests/utils.ts
@@ -0,0 +1,4 @@
+import { BigInt } from '@graphprotocol/graph-ts'
+
+export const BEFORE_V2_2_TIMESTAMP = BigInt.fromString("1642943600")
+export const AFTER_V2_2_TIMESTAMP = BigInt.fromString("1662943600")
