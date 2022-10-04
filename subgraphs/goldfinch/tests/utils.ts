069a7ccf9a96d5af01585053d5f5f3d0551b70da
tnkrxyz <tnkrxyz@gmail.com>
2022-10-04 03:52:32 +0000

    re-organized goldfinch directories and files

--- subgraphs/goldfinch/tests/utils.ts
+++
@@ -1,4 +0,0 @@
-import { BigInt } from '@graphprotocol/graph-ts'
-
-export const BEFORE_V2_2_TIMESTAMP = BigInt.fromString("1642943600")
-export const AFTER_V2_2_TIMESTAMP = BigInt.fromString("1662943600")
