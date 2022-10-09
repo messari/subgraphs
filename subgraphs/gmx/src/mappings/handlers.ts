b837883981acba2e8484b2137b4ec79c2092dcaf
loc <l0c4t0r@gmail.com>
2022-10-09 13:23:50 +0000

    Add ABIs and basic manifest

---
+++ subgraphs/gmx/src/mappings/handlers.ts
@@ -0,0 +1,5 @@
+import { IncreasePosition } from "../../generated/vault/Vault";
+
+export function handleIncreasePosition(event: IncreasePosition): void {
+  // Increase position is a deposit to pool
+}
