41892f4dbd9e7795a02c39ea76e5013f2453dae7
Tarun Madiraju <tarun@Taruns-MacBook-Pro.local>
2022-10-10 15:58:31 +0530

    add biswap to uniswap-forks

---
+++ subgraphs/uniswap-forks/protocols/biswap/src/common/constants.ts
@@ -0,0 +1,10 @@
+////////////////////
+//////Versions//////
+////////////////////
+
+import { log } from "@graphprotocol/graph-ts";
+
+export const PROTOCOL_SUBGRAPH_VERSION = "1.0.0";
+export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
+export const PROTOCOL_NAME = "Biswap";
+export const PROTOCOL_SLUG = "Biswap";
