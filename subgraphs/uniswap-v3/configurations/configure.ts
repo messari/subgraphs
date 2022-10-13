8fee0d548a4d5ff3149ea7779334aa6d6a80be12
steegecs <56660047+steegecs@users.noreply.github.com>
2022-10-12 22:20:08 -0500

    (#chore); Uniswap V3; Linted Uniswap V3 directory + update lint ignore (#1188)

--- subgraphs/uniswap-v3/configurations/configure.ts
+++ subgraphs/uniswap-v3/configurations/configure.ts
@@ -3,7 +3,7 @@ import { getNetworkConfigurations } from "./configurations/configurations";
 import { Deploy } from "./configurations/deploy";
 
 // Select the deployment protocol and network
-let deployment = Deploy.UNISWAP_V3_ETHEREUM;
+const deployment = Deploy.UNISWAP_V3_ETHEREUM;
 
 // export const NetworkConfigs = configurationsMap.get(deployment)!
 export const NetworkConfigs = getNetworkConfigurations(deployment);
