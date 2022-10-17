f7709668a78e4bfd5be81eb142237c116c414f9e
tnkrxyz <97625120+tnkrxyz@users.noreply.github.com>
2022-10-17 10:00:16 -0700

    (#chore); euler-finance; prettier/eslint ts scripts in src (#1212)

--- subgraphs/euler-finance/src/mappings/handlers.ts
+++ subgraphs/euler-finance/src/mappings/handlers.ts
@@ -1,4 +1,4 @@
-import { Address, ethereum, BigInt } from "@graphprotocol/graph-ts";
+import { Address, ethereum } from "@graphprotocol/graph-ts";
 import {
   AssetStatus,
   Borrow,
@@ -7,16 +7,14 @@ import {
   Liquidation,
   MarketActivated,
   Repay,
-  Withdraw
-} from "../../generated/euler/Euler"
+  Withdraw,
+} from "../../generated/euler/Euler";
 import {
   EulerGeneralView,
   EulerGeneralView__doQueryInputQStruct,
-  EulerGeneralView__doQueryResultRStruct
+  EulerGeneralView__doQueryResultRStruct,
 } from "../../generated/euler/EulerGeneralView";
-import {
-  getOrCreateProtocolUtility,
-} from "../common/getters";
+import { getOrCreateProtocolUtility } from "../common/getters";
 import {
   EULER_ADDRESS,
   EULER_GENERAL_VIEW_ADDRESS,
@@ -25,7 +23,12 @@ import {
   EULER_GENERAL_VIEW_V2_ADDRESS,
   VIEW_V2_START_BLOCK_NUMBER,
 } from "../common/constants";
-import { updateFinancials, updateMarketDailyMetrics, updateMarketHourlyMetrics, updateUsageMetrics } from "../common/metrics";
+import {
+  updateFinancials,
+  updateMarketDailyMetrics,
+  updateMarketHourlyMetrics,
+  updateUsageMetrics,
+} from "../common/metrics";
 import {
   createBorrow,
   createDeposit,
@@ -43,7 +46,7 @@ export function handleAssetStatus(event: AssetStatus): void {
 }
 
 export function handleBorrow(event: Borrow): void {
-  let borrowUSD = createBorrow(event);
+  const borrowUSD = createBorrow(event);
   const marketId = event.params.underlying.toHexString();
   updateUsageMetrics(event, event.params.account, TransactionType.BORROW);
   updateFinancials(event.block, borrowUSD, TransactionType.BORROW);
@@ -53,7 +56,7 @@ export function handleBorrow(event: Borrow): void {
 }
 
 export function handleDeposit(event: Deposit): void {
-  let depositUSD = createDeposit(event);
+  const depositUSD = createDeposit(event);
   const marketId = event.params.underlying.toHexString();
   updateUsageMetrics(event, event.params.account, TransactionType.DEPOSIT);
   updateFinancials(event.block, depositUSD, TransactionType.DEPOSIT);
@@ -63,7 +66,7 @@ export function handleDeposit(event: Deposit): void {
 }
 
 export function handleRepay(event: Repay): void {
-  let repayUSD = createRepay(event);
+  const repayUSD = createRepay(event);
   const marketId = event.params.underlying.toHexString();
   updateUsageMetrics(event, event.params.account, TransactionType.REPAY);
   updateFinancials(event.block, repayUSD, TransactionType.REPAY);
@@ -73,7 +76,7 @@ export function handleRepay(event: Repay): void {
 }
 
 export function handleWithdraw(event: Withdraw): void {
-  let withdrawUSD = createWithdraw(event);
+  const withdrawUSD = createWithdraw(event);
   const marketId = event.params.underlying.toHexString();
   updateUsageMetrics(event, event.params.account, TransactionType.WITHDRAW);
   updateFinancials(event.block, withdrawUSD, TransactionType.WITHDRAW);
@@ -83,7 +86,7 @@ export function handleWithdraw(event: Withdraw): void {
 }
 
 export function handleLiquidation(event: Liquidation): void {
-  let liquidateUSD = createLiquidation(event);
+  const liquidateUSD = createLiquidation(event);
   const marketId = event.params.underlying.toHexString();
   updateUsageMetrics(event, event.params.liquidator, TransactionType.LIQUIDATE);
   updateFinancials(event.block, liquidateUSD, TransactionType.LIQUIDATE);
@@ -93,7 +96,7 @@ export function handleLiquidation(event: Liquidation): void {
 }
 
 export function handleGovSetAssetConfig(event: GovSetAssetConfig): void {
- updateLendingFactors(event);
+  updateLendingFactors(event);
 }
 
 export function handleMarketActivated(event: MarketActivated): void {
@@ -101,20 +104,23 @@ export function handleMarketActivated(event: MarketActivated): void {
 }
 
 function getEulerViewContract(block: ethereum.Block): EulerGeneralView {
- const viewAddress = block.number.gt(VIEW_V2_START_BLOCK_NUMBER)
-   ? EULER_GENERAL_VIEW_V2_ADDRESS
-   : EULER_GENERAL_VIEW_ADDRESS;
+  const viewAddress = block.number.gt(VIEW_V2_START_BLOCK_NUMBER)
+    ? EULER_GENERAL_VIEW_V2_ADDRESS
+    : EULER_GENERAL_VIEW_ADDRESS;
   return EulerGeneralView.bind(Address.fromString(viewAddress));
 }
 
 /**
  * Query Euler General View contract in order to get markets current status.
- * 
+ *
  * @param marketIds List of markets to query
  * @param block current block
  * @returns query resul or null if nothing could be queried.
  */
-function queryEulerGeneralView(marketIds: string[], block: ethereum.Block): EulerGeneralView__doQueryResultRStruct | null {
+function queryEulerGeneralView(
+  marketIds: string[],
+  block: ethereum.Block,
+): EulerGeneralView__doQueryResultRStruct | null {
   if (marketIds.length === 0) {
     return null; // No market is initialized, nothing to do.
   }
@@ -140,18 +146,18 @@ function queryEulerGeneralView(marketIds: string[], block: ethereum.Block): Eule
 
 // initiates market / protocol updates in syncWithEulerGeneralView()
 function updateProtocolAndMarkets(block: ethereum.Block): void {
-  let blockNumber = block.number.toI32();
-  let protocolUtility = getOrCreateProtocolUtility(blockNumber);
-  let markets = protocolUtility.markets;
+  const blockNumber = block.number.toI32();
+  const protocolUtility = getOrCreateProtocolUtility(blockNumber);
+  const markets = protocolUtility.markets;
 
-  if (protocolUtility.lastBlockNumber >= blockNumber - 120 ) {
+  if (protocolUtility.lastBlockNumber >= blockNumber - 120) {
     // Do this update every 120 blocks
     // this equates to about 30 minutes on ethereum mainnet
     // this is done since the following logic slows down the indexer
     return;
   }
 
-  let eulerViewQueryResult = queryEulerGeneralView(markets, block);
+  const eulerViewQueryResult = queryEulerGeneralView(markets, block);
   if (!eulerViewQueryResult) {
     return;
   }
