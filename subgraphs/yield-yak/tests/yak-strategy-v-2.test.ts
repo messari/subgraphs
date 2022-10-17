25560d47e36bf71173f18d13459726d04eb920a2
Koval1997 <kovalmax06@gmail.com>
2022-10-17 14:15:25 +0300

    chore(#contracts): update generated models

---
+++ subgraphs/yield-yak/tests/yak-strategy-v-2.test.ts
@@ -0,0 +1,48 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { AllowDepositor } from "../generated/YakStrategyV2/YakStrategyV2"
+import { handleAllowDepositor } from "../src/yak-strategy-v-2"
+import { createAllowDepositorEvent } from "./yak-strategy-v-2-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let account = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newAllowDepositorEvent = createAllowDepositorEvent(account)
+    handleAllowDepositor(newAllowDepositorEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "account",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
