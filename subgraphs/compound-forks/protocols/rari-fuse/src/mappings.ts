ee45aa19afb157782ec63deb0a43575edaedcd3b
Dylan Melotik <60583747+dmelotik@users.noreply.github.com>
2022-10-27 10:19:32 -0500

    fix(#patch); rari fuse; fix arbitrum blocks/day (#1262)

--- subgraphs/compound-forks/protocols/rari-fuse/src/mappings.ts
+++ subgraphs/compound-forks/protocols/rari-fuse/src/mappings.ts
@@ -3,6 +3,7 @@ import {
   Address,
   BigDecimal,
   BigInt,
+  dataSource,
   ethereum,
   log,
 } from "@graphprotocol/graph-ts";
@@ -35,6 +36,7 @@ import {
 import { PoolRegistered } from "../../../generated/FusePoolDirectory/FusePoolDirectory";
 import {
   BLOCKLIST_MARKETS,
+  compareNormalizedString,
   ETH_ADDRESS,
   ETH_NAME,
   ETH_SYMBOL,
@@ -97,6 +99,7 @@ import {
   INT_TWO,
   mantissaFactor,
   mantissaFactorBD,
+  Network,
   RewardTokenType,
 } from "../../../src/constants";
 import {
@@ -498,22 +501,31 @@ export function handleAccrueInterest(event: AccrueInterest): void {
   }
   const oracleContract = PriceOracle.bind(Address.fromString(pool.priceOracle));
 
-  // get rolling blocks/day count
-  getRewardsPerDay(
-    event.block.timestamp,
-    event.block.number,
-    BIGDECIMAL_ZERO,
-    RewardIntervalType.BLOCK
-  );
-  const blocksPerDayBD = getOrCreateCircularBuffer().blocksPerDay;
-  const blocksPerDayBI = BigInt.fromString(
-    blocksPerDayBD.truncate(0).toString()
-  );
   let blocksPerYear: i32;
-  if (blocksPerDayBI.isI32()) {
+  let blocksPerDay: BigDecimal;
+  if (compareNormalizedString(dataSource.network(), Network.MAINNET)) {
+    // calculate blocks/yr on ethereum
+    // get rolling blocks/day count
+    getRewardsPerDay(
+      event.block.timestamp,
+      event.block.number,
+      BIGDECIMAL_ZERO,
+      RewardIntervalType.BLOCK
+    );
+    blocksPerDay = getOrCreateCircularBuffer().blocksPerDay;
+    const blocksPerDayBI = BigInt.fromString(
+      blocksPerDay.truncate(0).toString()
+    );
+
     blocksPerYear = blocksPerDayBI.toI32() * DAYS_PER_YEAR;
   } else {
+    // Arbitrum One block speed is the same as ethereum
+    // we do this b/c we cannot calculate the arbitrum block speed accurately
+    // see discussion: https://github.com/messari/subgraphs/issues/939
     blocksPerYear = ETHEREUM_BLOCKS_PER_YEAR;
+    blocksPerDay = BigDecimal.fromString(
+      (ETHEREUM_BLOCKS_PER_YEAR / 365).toString()
+    );
   }
 
   //
@@ -587,7 +599,7 @@ export function handleAccrueInterest(event: AccrueInterest): void {
     event.block.number,
     event.block.timestamp,
     trollerAddr,
-    blocksPerDayBD,
+    blocksPerDay,
     true // update all prices on each transaction for arbitrum / ethereum
   );
   updateProtocol(Address.fromString(FACTORY_CONTRACT));
