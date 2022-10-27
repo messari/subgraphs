88b801a2b7d8724374bc528fb329ce106d7a1dd5
Michael Carroll <69168498+MichaelC1999@users.noreply.github.com>
2022-10-26 21:24:46 -0400

    fix(#minor); Dashboard; v1.6.8 Yellow excl mark (#1276)
    Co-authored-by: Michael <michaelcarroll1999@gmail.com>

--- dashboard/src/common/DashboardVersion.tsx
+++ dashboard/src/common/DashboardVersion.tsx
@@ -11,7 +11,7 @@ const DashboardTag = styled("div")`
   z-index: 2;
 `;
 
-export const dashboardVersion = "v1.6.7";
+export const dashboardVersion = "v1.6.8";
 
 export const DashboardVersion = () => {
   return <DashboardTag>{dashboardVersion}</DashboardTag>;
