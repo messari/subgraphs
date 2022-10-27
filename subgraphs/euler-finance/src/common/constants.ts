886aab33d840ccbaca7f4b2d14956bb4b4835a62
tnkrxyz <tnkrxyz@gmail.com>
2022-10-27 20:50:08 +0000

    bumped subgraph version

--- subgraphs/euler-finance/src/common/constants.ts
+++ subgraphs/euler-finance/src/common/constants.ts
@@ -7,7 +7,7 @@ import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 export const PROTOCOL_NAME = "Euler";
 export const PROTOCOL_SLUG = "euler";
 export const PROTOCOL_SCHEMA_VERSION = "1.3.0";
-export const PROTOCOL_SUBGRAPH_VERSION = "1.2.0";
+export const PROTOCOL_SUBGRAPH_VERSION = "1.2.1";
 export const PROTOCOL_METHODOLOGY_VERSION = "1.1.0";
 
 ////////////////////////
