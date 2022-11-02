a5a02fbea63dad3129666a6022beb6a66ead6b51
Michael <michaelcarroll1999@gmail.com>
2022-11-02 10:33:56 -0300

    Path overlay dropdown errors

--- dashboard/src/common/DashboardVersion.tsx
+++ dashboard/src/common/DashboardVersion.tsx
@@ -11,7 +11,7 @@ const DashboardTag = styled("div")`
   z-index: 2;
 `;
 
-export const dashboardVersion = "v1.7.1";
+export const dashboardVersion = "v1.7.2";
 
 export const DashboardVersion = () => {
   return <DashboardTag>{dashboardVersion}</DashboardTag>;
