ab6a5d06674e21f65b03880192b36f6000749eab
Michael Carroll <69168498+MichaelC1999@users.noreply.github.com>
2022-11-29 22:05:43 -0500

    feat(#major); Dashboard; v1.8.7 Initial depo table protocol type wide csv download (#1471)
    Co-authored-by: Michael <michaelcarroll1999@gmail.com>

--- dashboard/src/deployments/DeploymentsTable.tsx
+++ dashboard/src/deployments/DeploymentsTable.tsx
@@ -1,7 +1,9 @@
 import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import ProtocolSection from "./ProtocolSection";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { NewClient, schemaMapping } from "../utils";
+import FetchEntityCSV from "./FetchEntityCSV";
+import JSZip from "jszip";
 
 interface DeploymentsTable {
     protocolsToQuery: { [x: string]: any };
@@ -17,6 +19,45 @@ interface DeploymentsTable {
 function DeploymentsTable({ protocolsToQuery, issuesMapping, getData, decenDeposToSubgraphIds, indexingStatusLoaded, indexingStatusLoadedPending, indexingStatusError, indexingStatusErrorPending }: DeploymentsTable) {
     const clientIndexing = useMemo(() => NewClient("https://api.thegraph.com/index-node/graphql"), []);
     const [tableExpanded, setTableExpanded] = useState<any>({ lending: false, exchanges: false, vaults: false, generic: false, erc20: false, erc721: false, governance: false, network: false, ["nft-marketplace"]: false });
+    const [generateEntityCSV, triggerGenerateEntityCSV] = useState<string>("");
+    const [resultsObject, setResultsObject] = useState<any>({});
+
+    useEffect(() => {
+        if (generateEntityCSV.length > 0) {
+            let depoCount = 0;
+            Object.entries(protocolsToQuery).forEach(([protocolName, protocol]) => {
+                if (schemaMapping[protocol.schema] !== schemaMapping[generateEntityCSV]) {
+                    return;
+                }
+                Object.keys(protocol.deployments).forEach((depoKey) => {
+                    const deploymentData: any = protocol.deployments[depoKey];
+                    if (deploymentData?.services) {
+                        if (!!deploymentData["services"]["hosted-service"] || !!deploymentData["services"]["decentralized-network"] || !!deploymentData["services"]["cronos-portal"]) {
+                            depoCount += 1;
+                        }
+                    }
+                });
+            });
+            if (resultsObject) {
+                if (Object.keys(resultsObject).length >= depoCount) {
+                    let zip = new JSZip();
+                    Object.values(resultsObject).forEach((depo: any) => {
+                        if (depo?.blob) {
+                            zip.file(depo.filename, depo.blob);
+                        }
+                    });
+                    zip.generateAsync({ type: "base64" }).then(function (content) {
+                        const link = document.createElement('a');
+                        link.download = "charts.zip";
+                        link.href = "data:application/zip;base64," + content;
+                        link.click()
+                        triggerGenerateEntityCSV("");
+                    });
+                }
+            }
+        }
+    }, [resultsObject])
+
     if (Object.keys(protocolsToQuery).length === 0) {
         getData();
         return null;
@@ -101,6 +142,8 @@ function DeploymentsTable({ protocolsToQuery, issuesMapping, getData, decenDepos
         });
     });
 
+
+
     return (
         <>
             {Object.entries(deposToPass).sort().map(([schemaType, subgraph]) => {
@@ -116,7 +159,25 @@ function DeploymentsTable({ protocolsToQuery, issuesMapping, getData, decenDepos
                 const indexQueryErrorPending = indexingStatusErrorPending[schemaType];
                 const tableRows = Object.keys(subgraph).sort().map((subgraphName) => {
                     const protocol = subgraph[subgraphName];
-                    return (
+                    let csvGenerationComponents = null;
+                    if (schemaMapping[generateEntityCSV] === schemaMapping[schemaType] && generateEntityCSV?.length > 0) {
+                        csvGenerationComponents = protocol.networks.map((depo: any) => {
+                            return (
+                                <FetchEntityCSV
+                                    entityName="financialsDailySnapshots"
+                                    deployment={depo.deploymentName}
+                                    protocolType={schemaType.toUpperCase()}
+                                    schemaVersion={depo.versions.schema}
+                                    timestampLt={1000000000000000000}
+                                    timestampGt={0}
+                                    queryURL={`https://api.thegraph.com/subgraphs/name/messari/${depo.hostedServiceId}`}
+                                    resultsObject={resultsObject}
+                                    setResultsObject={setResultsObject}
+                                />)
+                        })
+                    }
+                    return (<>
+                        {csvGenerationComponents}
                         <ProtocolSection
                             key={"ProtocolSection-" + subgraphName.toUpperCase() + '-' + schemaType}
                             issuesMapping={issuesMapping}
@@ -131,7 +192,7 @@ function DeploymentsTable({ protocolsToQuery, issuesMapping, getData, decenDepos
                             isLoadedPending={isLoadedPending}
                             indexQueryError={indexQueryError}
                             indexQueryErrorPending={indexQueryErrorPending}
-                        />);
+                        /></>);
                 });
                 return (
                     <TableContainer sx={{ my: 8 }} key={"TableContainer-" + schemaType.toUpperCase()}>
@@ -159,6 +220,10 @@ function DeploymentsTable({ protocolsToQuery, issuesMapping, getData, decenDepos
                                 </span>
                             </Typography>
                         </div>
+                        {schemaMapping[schemaType] ? (<div style={{ display: "block", paddingLeft: "5px", textAlign: "left", color: "white" }} className="Hover-Underline MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButtonBase-root  css-1huqmjz-MuiButtonBase-root-MuiButton-root" onClick={() => {
+                            triggerGenerateEntityCSV(schemaType.toUpperCase())
+                        }} >Get Bulk FinancialsDailySnapshots CSV</div>) : null}
+
                         <Table stickyHeader>
                             {tableHead}
                             <TableBody>{tableRows}</TableBody>
