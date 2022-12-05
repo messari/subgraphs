cc79c12a1b0ac57d0a059f6e79f46d73ac29c817
jaimehgb <jaimehgb@gmail.com>
2022-12-05 22:59:38 +0000

    Added jsdoc to protocol wrapper

--- subgraphs/synapse/src/sdk/protocols/bridge/protocol.ts
+++ subgraphs/synapse/src/sdk/protocols/bridge/protocol.ts
@@ -18,12 +18,21 @@ interface ProtocolSnapshot {
   // todo
 }
 
+/**
+ * Bridge is a wrapper around the BridgeProtocolSchema entity that takes care of
+ * safely and conveniently updating the entity. Updating the Bridge entity using this
+ * wrapper also takes care of the Financials and Usage snapshots.
+ */
 export class Bridge {
   protocol: BridgeProtocolSchema;
   event: ethereum.Event;
   pricer: TokenPricer;
   snapshoter: ProtocolSnapshot | null;
 
+  /**
+   * Creates a new Bridge instance. This should only be called by the Bridge.load
+   * @private
+   */
   private constructor(
     protocol: BridgeProtocolSchema,
     pricer: TokenPricer,
@@ -35,6 +44,14 @@ export class Bridge {
     this.snapshoter = null; // todo
   }
 
+  /**
+   * This is the main function to instantiate a Bridge entity. Most times it is not called directly, but from the SDK initializer.
+   *
+   * @param conf {BridgeConfigurer} An object that implements the BridgeConfigurer interface, to set some of the protocol's properties
+   * @param pricer {TokenPricer} An object that implements the TokenPricer interface, to allow the wrapper to access pricing data
+   * @param event {ethereum.Event} The event being handled at a time.
+   * @returns Bridge
+   */
   static load(
     conf: BridgeConfigurer,
     pricer: TokenPricer,
@@ -87,6 +104,12 @@ export class Bridge {
     return proto;
   }
 
+  /**
+   * Updates the protocol entity versions. This is called on load to make sure we update the version
+   * if we've grafted the subgraph.
+   *
+   * @param versions {Versions} An object that implements the Versions interface, to get the protocol's versions
+   */
   private setVersions(versions: Versions): void {
     this.protocol.schemaVersion = versions.getSchemaVersion();
     this.protocol.subgraphVersion = versions.getSubgraphVersion();
@@ -94,46 +117,96 @@ export class Bridge {
     this.save();
   }
 
+  /**
+   * This will save the entity to storage and update all its dependent snapshots.
+   * It is meant to be used internally. If you need to save the entity from outside the wrapper
+   * you should probably be using some of the setters instead.
+   * @private
+   */
   private save(): void {
     this.updateSnapshots();
     this.protocol.save();
   }
 
+  /**
+   * Will update all snapshots for the protocol entity: Financials and Usage, both daily and hourly.
+   */
   private updateSnapshots(): void {
     // this.snapshoter.takeSnapshots();
   }
 
+  /**
+   *
+   * @returns {string} The ID of the protocol entity.
+   */
   getID(): string {
     return this.protocol.id.toHexString();
   }
 
+  /**
+   *
+   * @returns {Bytes} The ID of the protocol entity, as Bytes.
+   */
   getBytesID(): Bytes {
     return this.protocol.id;
   }
 
+  /**
+   *
+   * @returns {ethereum.Event} the event currently being handled.
+   */
   getCurrentEvent(): ethereum.Event {
     return this.event;
   }
 
+  /**
+   *
+   * @returns {TokenPricer} The pricer object used by the wrapper.
+   * @see TokenPricer
+   */
   getTokenPricer(): TokenPricer {
     return this.pricer;
   }
 
+  /**
+   *
+   * @returns {i32} The chainID of the network the subgraph is running on.
+   */
   getCurrentChainID(): i32 {
     return 1; // todo
   }
 
+  /**
+   * Sets the TVL in USD for the protocol. Most times this will be called internally by
+   * other members of the library when TVL changes are made to them. But if the library
+   * is not well fitted to a given protocol and you need to set the TVL manually, you can
+   * use this method.
+   * It will also update the protocol's snapshots.
+   * @param tvl {BigDecimal} The new total value locked for the protocol.
+   */
   setTotalValueLocked(tvl: BigDecimal): void {
     this.protocol.totalValueLockedUSD = tvl;
     this.save();
   }
 
+  /**
+   * Adds a given USD value to the protocol's TVL. It can be a positive or negative amount.
+   * Same as for setTotalValueLocked, this is mostly to be called internally by the library.
+   * But you can use it if you need to. It will also update the protocol's snapshots.
+   * @param tvl {BigDecimal} The value to add to the protocol's TVL.
+   */
   addTotalValueLocked(tvl: BigDecimal): void {
     this.protocol.totalValueLockedUSD =
       this.protocol.totalValueLockedUSD.plus(tvl);
     this.save();
   }
 
+  /**
+   * Adds a given USD value to the protocol supplySideRevenue. It can be a positive or negative amount.
+   * Same as for the rest of setters, this is mostly to be called internally by the library.
+   * But you can use it if you need to. It will also update the protocol's snapshots.
+   * @param rev {BigDecimal} The value to add to the protocol's supplySideRevenue.
+   */
   addSupplySideRevenueUSD(rev: BigDecimal): void {
     this.protocol.cumulativeTotalRevenueUSD =
       this.protocol.cumulativeTotalRevenueUSD.plus(rev);
@@ -142,6 +215,12 @@ export class Bridge {
     this.save();
   }
 
+  /**
+   * Adds a given USD value to the protocol protocolSideRevenue. It can be a positive or negative amount.
+   * Same as for the rest of setters, this is mostly to be called internally by the library.
+   * But you can use it if you need to. It will also update the protocol's snapshots.
+   * @param rev {BigDecimal} The value to add to the protocol's protocolSideRevenue.
+   */
   addProtocolSideRevenueUSD(rev: BigDecimal): void {
     this.protocol.cumulativeTotalRevenueUSD =
       this.protocol.cumulativeTotalRevenueUSD.plus(rev);
@@ -150,23 +229,48 @@ export class Bridge {
     this.save();
   }
 
+  /**
+   * Adds a given USD value to the protocol's supplySideRevenue and protocolSideRevenue. It can be a positive or negative amount.
+   * Same as for the rest of setters, this is mostly to be called internally by the library.
+   * But you can use it if you need to. It will also update the protocol's snapshots.
+   * @param protocolSide {BigDecimal} The value to add to the protocol's protocolSideRevenue.
+   * @param supplySide {BigDecimal} The value to add to the protocol's supplySideRevenue.
+   */
   addRevenueUSD(protocolSide: BigDecimal, supplySide: BigDecimal): void {
     this.addSupplySideRevenueUSD(supplySide);
     this.addProtocolSideRevenueUSD(protocolSide);
   }
 
+  /**
+   * Adds a given USD value to the protocol's cumulativeVolumeInUSD. It can be a positive or negative amount.
+   * Same as for the rest of setters, this is mostly to be called internally by the library.
+   * But you can use it if you need to. It will also update the protocol's snapshots.
+   * @param vol {BigDecimal} The value to add to the protocol's cumulativeVolumeInUSD.
+   */
   addVolumeInUSD(vol: BigDecimal): void {
     this.protocol.cumulativeVolumeInUSD =
       this.protocol.cumulativeVolumeInUSD.plus(vol);
     this.updateVolumes();
   }
 
+  /**
+   * Adds a given USD value to the protocol's cumulativeVolumeOutUSD. It can be a positive or negative amount.
+   * Same as for the rest of setters, this is mostly to be called internally by the library.
+   * But you can use it if you need to. It will also update the protocol's snapshots.
+   * @param vol {BigDecimal} The value to add to the protocol's cumulativeVolumeOutUSD.
+   */
   addVolumeOutUSD(vol: BigDecimal): void {
     this.protocol.cumulativeVolumeOutUSD =
       this.protocol.cumulativeVolumeOutUSD.plus(vol);
     this.updateVolumes();
   }
 
+  /**
+   * This method will update the values of cumulativeTotalVolumeUSD and netVolumeUSD, since these can
+   * be computed from the values of cumulativeVolumeInUSD and cumulativeVolumeOutUSD. It is called automatically
+   * when either of those values are changed via the addVolumeInUSD or addVolumeOutUSD methods.
+   * @private
+   */
   private updateVolumes(): void {
     this.protocol.netVolumeUSD = this.protocol.cumulativeVolumeInUSD.minus(
       this.protocol.cumulativeVolumeOutUSD
@@ -178,31 +282,63 @@ export class Bridge {
     this.save();
   }
 
+  /**
+   * Adds some value to the cumulativeUniqueUsers counter. If the value is omitted it will default to 1.
+   * If you are loading accounts with the AccountManager you won't need to use this method.
+   * @param count {u8} The value to add to the counter.
+   */
   addUser(count: u8 = 1): void {
     this.protocol.cumulativeUniqueUsers += count;
     this.save();
   }
 
+  /**
+   * Adds some value to the cumulativeUniqueTransferSenders counter. If the value is omitted it will default to 1.
+   * If you are loading accounts with the AccountManager you won't need to use this method.
+   * @param count {u8} The value to add to the counter.
+   */
   addTransferSender(count: u8 = 1): void {
     this.protocol.cumulativeUniqueTransferSenders += count;
     this.save();
   }
 
+  /**
+   * Adds some value to the cumulativeUniqueTransferReceivers counter. If the value is omitted it will default to 1.
+   * If you are loading accounts with the AccountManager you won't need to use this method.
+   * @param count {u8} The value to add to the counter.
+   */
   addTransferReceiver(count: u8 = 1): void {
     this.protocol.cumulativeUniqueTransferReceivers += count;
     this.save();
   }
 
+  /**
+   * Adds some value to the cumulativeUniqueLiquidityProviders counter. If the value is omitted it will default to 1.
+   * If you are loading accounts with the AccountManager you won't need to use this method.
+   * @param count {u8} The value to add to the counter.
+   */
   addLiquidityProvider(count: u8 = 1): void {
     this.protocol.cumulativeUniqueLiquidityProviders += count;
     this.save();
   }
 
+  /**
+   * Adds some value to the cumulativeUniqueMessageSenders counter. If the value is omitted it will default to 1.
+   * If you are loading accounts with the AccountManager you won't need to use this method.
+   * @param count {u8} The value to add to the counter.
+   */
   addMessageSender(count: u8 = 1): void {
     this.protocol.cumulativeUniqueMessageSenders += count;
     this.save();
   }
 
+  /**
+   * Adds 1 to the cumulativeTransactionCount counter and adds 1 to the counter corresponding the given transaction type.
+   * If you are creating transaction entities from the Account class you won't need to use this method.
+   * @param type {TransactionType} The type of transaction to add.
+   * @see TransactionType
+   * @see Account
+   */
   addTransaction(type: TransactionType): void {
     if (type == TransactionType.TRANSFER_IN) {
       this.protocol.cumulativeTransferInCount += 1;
@@ -222,28 +358,58 @@ export class Bridge {
     this.save();
   }
 
+  /**
+   * Increases the totalPoolRouteCount and totalCanonicalRouteCount counters by the given value.
+   * If you are using the Pool class you won't need to use this method.
+   * @param count {u8} The value to add to the counters.
+   * @see Pool
+   */
   addCanonicalPoolRoute(count: u8 = 1): void {
     this.protocol.totalCanonicalRouteCount += count;
     this.protocol.totalPoolRouteCount += count;
     this.save();
   }
 
+  /**
+   * Increases the totalPoolRouteCount and totalWrappedRouteCount counters by the given value.
+   * If you are using the Pool class you won't need to use this method.
+   * @param count {u8} The value to add to the counters.
+   * @see Pool
+   */
   addWrappedPoolRoute(count: u8 = 1): void {
     this.protocol.totalWrappedRouteCount += count;
     this.protocol.totalPoolRouteCount += count;
     this.save();
   }
 
+  /**
+   * Increases the totalPoolCount counter by the given value.
+   * If you are using the PoolManager class you won't need to use this method.
+   * @param count {u8} The value to add to the counter.
+   * @see PoolManager
+   */
   addPool(count: u8 = 1): void {
     this.protocol.totalPoolCount += count;
     this.save();
   }
 
+  /**
+   * Increases the totalSupportedTokenCount counter by the given value.
+   * If you are using the Pool class you won't need to use this method.
+   * @param count {u8} The value to add to the counter.
+   * @see Pool
+   */
   addSupportedToken(count: u8 = 1): void {
     this.protocol.totalSupportedTokenCount += count;
     this.save();
   }
 
+  /**
+   * Adds the given network to the supportedNetworks array.
+   * If you are using the Pool class you won't need to use this method.
+   * @param chainID {i32} The chainID of the network to add.
+   * @see Pool
+   */
   addSupportedNetwork(chainID: i32): void {
     const network = chainIDToNetwork(chainID);
     if (this.protocol.supportedNetworks.includes(network)) {
@@ -256,12 +422,3 @@ export class Bridge {
     this.save();
   }
 }
-
-namespace UserType {
-  export const TRANSFER_SENDER = "TRANSFER_SENDER";
-  export const TRANSFER_RECEIVER = "TRANSFER_RECEIVER";
-  export const LIQUIDITY_PROVIDER = "LIQUIDITY_PROVIDER";
-  export const MESSAGE_SENDER = "MESSAGE_SENDER";
-  export const OTHER = "OTHER";
-}
-type UserType = string;
