c7b3bd1824407f11c50c9e6db6ddf2ec075009c7
prasad-kumkar <pk111_@live.com>
2022-12-08 14:25:39 +0530

    better structured EventData

--- subgraphs/burrow/src/utils/type.ts
+++ subgraphs/burrow/src/utils/type.ts
@@ -3,26 +3,12 @@ import { JSONValue } from "@graphprotocol/graph-ts";
 import { near } from "@graphprotocol/graph-ts";
 
 export class EventData {
-  eventName: string | null;
-  methodName: string;
-  data: TypedMap<string, JSONValue>;
-  receipt: near.ReceiptWithOutcome;
-  logIndex: number;
-  args: TypedMap<string, JSONValue> | null;
-
   constructor(
-    eventName: string | null,
-    methodName: string,
-    data: TypedMap<string, JSONValue>,
-    receipt: near.ReceiptWithOutcome,
-    logIndex: number,
-    args: TypedMap<string, JSONValue> | null
-  ) {
-    this.eventName = eventName;
-    this.methodName = methodName;
-    this.data = data;
-    this.receipt = receipt;
-    this.logIndex = logIndex;
-    this.args = args;
-  }
+    public readonly eventName: string | null,
+    public readonly methodName: string,
+    public readonly data: TypedMap<string, JSONValue>,
+    public readonly receipt: near.ReceiptWithOutcome,
+    public readonly logIndex: number,
+    public readonly args: TypedMap<string, JSONValue> | null
+  ) {}
 }
