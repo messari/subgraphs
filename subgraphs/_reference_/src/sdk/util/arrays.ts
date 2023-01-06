b68befccca71b624446fd4072e39520e46adc28b
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2023-01-06 20:01:21 +0530

    fix(#patch); bridge sdk; minor fixes (#1568)

--- subgraphs/_reference_/src/sdk/util/arrays.ts
+++ subgraphs/_reference_/src/sdk/util/arrays.ts
@@ -26,3 +26,25 @@ export function sortBytesArray(array: Bytes[]): Bytes[] {
   toSort.sort();
   return toSort.map<Bytes>((item) => Bytes.fromHexString(item));
 }
+
+export function updateArrayAtIndex<T>(x: T[], item: T, index: i32): T[] {
+  if (x.length == 0) {
+    return [item];
+  }
+  if (index == -1 || index > x.length) {
+    index = x.length;
+  }
+  const retval = new Array<T>();
+  let i = 0;
+  while (i < index) {
+    retval.push(x[i]);
+    i += 1;
+  }
+  retval.push(item);
+  i += 1;
+  while (i < x.length) {
+    retval.push(x[i]);
+    i += 1;
+  }
+  return retval;
+}
