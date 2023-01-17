86b6ae17a245a0b00eef2f88b882484b17566a8d
Samuel Pitko <samuel.pitko@gmail.com>
2023-01-17 16:11:08 +0200

    Remove unused event handlers, add readme

---
+++ subgraphs/optimism-gateway/README.md
@@ -0,0 +1,11 @@
+# Optimism Gateway
+
+## Calculation Methodology v1.0.0
+
+## Links
+
+Links to the relevant sources to learn about this protocol.
+
+- Protocol: https://gateway.optimism.io/
+- Docs: https://community.optimism.io/
+- Smart contracts: https://github.com/ethereum-optimism/optimism/tree/develop/packages/contracts/contracts/
--- subgraphs/optimism-gateway/src/mappings/L2CrossDomainMessenger.ts
+++ subgraphs/optimism-gateway/src/mappings/L2CrossDomainMessenger.ts
@@ -1,12 +1,7 @@
-import {
-  RelayedMessage as RelayedMessageEvent,
-  SentMessage as SentMessageEvent,
-} from "../../generated/templates/Bridge/L2CrossDomainMessenger";
+import { SentMessage as SentMessageEvent } from "../../generated/templates/Bridge/L2CrossDomainMessenger";
 import { getSDK } from "../sdk";
 import { networkToChainID } from "../sdk/protocols/bridge/chainIds";
 import { Network } from "../sdk/util/constants";
-
-export function handleRelayedMessage(event: RelayedMessageEvent): void {}
 
 export function handleSentMessage(event: SentMessageEvent): void {
   const sdk = getSDK(event);
