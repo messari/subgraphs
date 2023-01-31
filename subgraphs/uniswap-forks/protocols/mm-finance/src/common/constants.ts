b0edcef40f59fb6db7bfe0ff8b96dee1d447b8b3
tnkrxyz <tnkrxyz@gmail.com>
2023-01-31 01:48:40 +0100

    switched to use versions.ts for versions for uniswap-forks

--- subgraphs/uniswap-forks/protocols/mm-finance/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/mm-finance/src/common/constants.ts
@@ -1,8 +1,9 @@
 ////////////////////
 //////Versions//////
 ////////////////////
+import { Versions } from "../../../../src/versions";
 
-export const PROTOCOL_SUBGRAPH_VERSION = "1.0.2";
-export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
+export const PROTOCOL_SUBGRAPH_VERSION = Versions.getSubgraphVersion();
+export const PROTOCOL_METHODOLOGY_VERSION = Versions.getMethodologyVersion();
 export const PROTOCOL_NAME = "MM Finance";
 export const PROTOCOL_SLUG = "mm-finance";
