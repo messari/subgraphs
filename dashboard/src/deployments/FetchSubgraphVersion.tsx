502e05c8f203ed4f3ec9e0058a39a2c76fd1bd39
Michael <michaelcarroll1999@gmail.com>
2023-02-10 01:56:43 -0300

    Switch sync list to subgraph version

--- dashboard/src/deployments/FetchSubgraphVersion.tsx
+++ dashboard/src/deployments/FetchSubgraphVersion.tsx
@@ -3,13 +3,13 @@ import { useEffect, useMemo } from "react";
 import { gql, useLazyQuery } from "@apollo/client";
 import { ProtocolQuery } from "../queries/protocolQuery";
 
-interface FetchSchemaVersionProps {
+interface FetchSubgraphVersionProps {
     subgraphEndpoint: string;
     slug: string;
-    setSchemaDeployments: any;
+    setDeployments: any;
 }
 
-function FetchSchemaVersion({ subgraphEndpoint, slug, setSchemaDeployments }: FetchSchemaVersionProps) {
+function FetchSubgraphVersion({ subgraphEndpoint, slug, setDeployments }: FetchSubgraphVersionProps) {
     const client = useMemo(() => NewClient(subgraphEndpoint), []);
 
     // Generate query from subgraphEndpoints
@@ -27,14 +27,14 @@ function FetchSchemaVersion({ subgraphEndpoint, slug, setSchemaDeployments }: Fe
     useEffect(() => {
         if (protocolMetaData?.protocols) {
             if (protocolMetaData?.protocols?.length > 0) {
-                setSchemaDeployments((prevState: any) => ({ ...prevState, [slug]: protocolMetaData.protocols[0].schemaVersion }));
+                setDeployments((prevState: any) => ({ ...prevState, [slug]: protocolMetaData.protocols[0].subgraphVersion }));
             }
         }
     }, [protocolMetaData])
 
     useEffect(() => {
         if (!!protocolMetaError) {
-            setSchemaDeployments((prevState: any) => ({ ...prevState, [slug]: null }));
+            setDeployments((prevState: any) => ({ ...prevState, [slug]: null }));
         }
     }, [protocolMetaError])
 
@@ -42,4 +42,4 @@ function FetchSchemaVersion({ subgraphEndpoint, slug, setSchemaDeployments }: Fe
     return null;
 }
 
-export default FetchSchemaVersion;
+export default FetchSubgraphVersion;
