98b9e9f5a6aec05d879225df779fa56e1ce6a541
Founder <corerouter@163.com>
2023-02-10 16:29:49 +0800

    remove some files

--- subgraphs/gmx/src/entityUpdates/usageMetrics.ts
+++ subgraphs/gmx/src/entityUpdates/usageMetrics.ts
@@ -1,110 +1,344 @@
-import { Address, ethereum } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { bigIntToBigDecimal } from "../utils/numbers";
 import { getOrCreateProtocol } from "../entities/protocol";
+import { getOrCreatePool } from "../entities/pool";
 import {
-  Account,
-  ActiveAccount,
-  UsageMetricsDailySnapshot,
-  UsageMetricsHourlySnapshot,
+  FinancialsDailySnapshot,
+  PoolDailySnapshot,
+  PoolHourlySnapshot,
 } from "../../generated/schema";
-import { SECONDS_PER_DAY, SECONDS_PER_HOUR } from "../utils/constants";
+import {
+  BIGDECIMAL_ZERO,
+  BIGINT_ZERO,
+  DEFAULT_DECIMALS,
+  PROTOCOL_ID,
+  SECONDS_PER_DAY,
+  SECONDS_PER_HOUR,
+} from "../utils/constants";
+import { getOrCreateToken } from "../entities/token";
 
-export function updateUsageMetrics(block: ethereum.Block, from: Address): void {
-  const accountId = from.toHexString();
-  let account = Account.load(accountId);
-  if (!account) {
-    account = new Account(accountId);
-    account.save();
-
-    const protocol = getOrCreateProtocol();
-    protocol.cumulativeUniqueUsers += 1;
-    protocol.save();
-  }
-
+export function updateProtocolAndPoolTvl(
+  block: ethereum.Block,
+  aumInUsdg: BigInt,
+  outputTokenSupply: BigInt
+): void {
+  const pool = getOrCreatePool(block.number, block.timestamp);
   const protocol = getOrCreateProtocol();
-  const usageMetricsDailySnapshot = getOrCreateUsageMetricsDailySnapshot(block);
-  const usageMetricsHourlySnapshot = getOrCreateUsageMetricsHourlySnapshot(
-    block
+
+  pool.totalValueLockedUSD = bigIntToBigDecimal(aumInUsdg, DEFAULT_DECIMALS);
+  pool.outputTokenSupply = outputTokenSupply;
+
+  if (outputTokenSupply != BIGINT_ZERO) {
+    const outputToken = getOrCreateToken(
+      Address.fromString(pool.outputToken!),
+      block.number
+    );
+
+    pool.outputTokenPriceUSD = pool.totalValueLockedUSD.div(
+      bigIntToBigDecimal(outputTokenSupply, outputToken.decimals)
+    );
+
+    outputToken.lastPriceUSD = pool.outputTokenPriceUSD;
+    outputToken.lastPriceBlockNumber = block.number;
+    outputToken.save();
+  }
+  pool.save();
+
+  // Protocol
+  protocol.totalValueLockedUSD = pool.totalValueLockedUSD;
+  protocol.save();
+
+  updateSnapshotsTvl(block);
+}
+
+export function updateSnapshotsTvl(block: ethereum.Block): void {
+  const pool = getOrCreatePool(block.number, block.timestamp);
+  const poolMetricsDailySnapshot = getOrCreatePoolsDailySnapshot(block);
+  const poolMetricsHourlySnapshot = getOrCreatePoolsHourlySnapshot(block);
+  const financialMetrics = getOrCreateFinancialDailyMetrics(block);
+
+  // Pool Daily
+  poolMetricsDailySnapshot.totalValueLockedUSD = pool.totalValueLockedUSD;
+  poolMetricsDailySnapshot.inputTokenBalances = pool.inputTokenBalances;
+  poolMetricsDailySnapshot.outputTokenSupply = pool.outputTokenSupply;
+  poolMetricsDailySnapshot.outputTokenPriceUSD = pool.outputTokenPriceUSD;
+  poolMetricsDailySnapshot.cumulativeProtocolSideRevenueUSD =
+    pool.cumulativeProtocolSideRevenueUSD;
+  poolMetricsDailySnapshot.cumulativeSupplySideRevenueUSD =
+    pool.cumulativeSupplySideRevenueUSD;
+  poolMetricsDailySnapshot.cumulativeTotalRevenueUSD =
+    pool.cumulativeTotalRevenueUSD;
+  poolMetricsDailySnapshot.rewardTokenEmissionsAmount =
+    pool.rewardTokenEmissionsAmount;
+  poolMetricsDailySnapshot.rewardTokenEmissionsUSD =
+    pool.rewardTokenEmissionsUSD;
+  poolMetricsDailySnapshot.save();
+
+  // Pool Hourly
+  poolMetricsHourlySnapshot.totalValueLockedUSD = pool.totalValueLockedUSD;
+  poolMetricsHourlySnapshot.inputTokenBalances = pool.inputTokenBalances;
+  poolMetricsHourlySnapshot.outputTokenSupply = pool.outputTokenSupply;
+  poolMetricsHourlySnapshot.outputTokenPriceUSD = pool.outputTokenPriceUSD;
+  poolMetricsHourlySnapshot.cumulativeProtocolSideRevenueUSD =
+    pool.cumulativeProtocolSideRevenueUSD;
+  poolMetricsHourlySnapshot.cumulativeSupplySideRevenueUSD =
+    pool.cumulativeSupplySideRevenueUSD;
+  poolMetricsHourlySnapshot.cumulativeTotalRevenueUSD =
+    pool.cumulativeTotalRevenueUSD;
+  poolMetricsHourlySnapshot.rewardTokenEmissionsAmount =
+    pool.rewardTokenEmissionsAmount;
+  poolMetricsHourlySnapshot.rewardTokenEmissionsUSD =
+    pool.rewardTokenEmissionsUSD;
+  poolMetricsHourlySnapshot.save();
+
+  // Financials Daily
+  financialMetrics.totalValueLockedUSD = pool.totalValueLockedUSD;
+  financialMetrics.cumulativeProtocolSideRevenueUSD =
+    pool.cumulativeProtocolSideRevenueUSD;
+  financialMetrics.cumulativeSupplySideRevenueUSD =
+    pool.cumulativeSupplySideRevenueUSD;
+  financialMetrics.cumulativeTotalRevenueUSD = pool.cumulativeTotalRevenueUSD;
+  financialMetrics.save();
+}
+
+export function updateTotalRevenueMetrics(
+  block: ethereum.Block,
+  amountUSD: BigDecimal
+): void {
+  const pool = getOrCreatePool(block.number, block.timestamp);
+  const protocol = getOrCreateProtocol();
+  const financialMetrics = getOrCreateFinancialDailyMetrics(block);
+  const poolMetricsDailySnapshot = getOrCreatePoolsDailySnapshot(block);
+  const poolMetricsHourlySnapshot = getOrCreatePoolsHourlySnapshot(block);
+
+  // Pool
+  pool.cumulativeTotalRevenueUSD = pool.cumulativeTotalRevenueUSD.plus(
+    amountUSD
   );
+  pool.save();
 
-  usageMetricsDailySnapshot.dailyTransactionCount += 1;
-  usageMetricsHourlySnapshot.hourlyTransactionCount += 1;
+  // Pool Daily
+  poolMetricsDailySnapshot.cumulativeTotalRevenueUSD =
+    pool.cumulativeTotalRevenueUSD;
+  poolMetricsDailySnapshot.dailyTotalRevenueUSD = poolMetricsDailySnapshot.dailyTotalRevenueUSD.plus(
+    amountUSD
+  );
+  poolMetricsDailySnapshot.save();
 
-  usageMetricsDailySnapshot.cumulativeUniqueUsers =
-    protocol.cumulativeUniqueUsers;
-  usageMetricsHourlySnapshot.cumulativeUniqueUsers =
-    protocol.cumulativeUniqueUsers;
+  // Pool Hourly
+  poolMetricsHourlySnapshot.cumulativeTotalRevenueUSD =
+    pool.cumulativeTotalRevenueUSD;
+  poolMetricsHourlySnapshot.hourlyTotalRevenueUSD = poolMetricsHourlySnapshot.hourlyTotalRevenueUSD.plus(
+    amountUSD
+  );
+  poolMetricsHourlySnapshot.save();
 
-  const dailyActiveAccountId = (block.timestamp.toI64() / SECONDS_PER_DAY)
-    .toString()
-    .concat("-")
-    .concat(accountId);
+  // Protocol
+  protocol.cumulativeTotalRevenueUSD = pool.cumulativeTotalRevenueUSD;
+  protocol.save();
 
-  let dailyActiveAccount = ActiveAccount.load(dailyActiveAccountId);
-  if (!dailyActiveAccount) {
-    dailyActiveAccount = new ActiveAccount(dailyActiveAccountId);
-    dailyActiveAccount.save();
-
-    usageMetricsDailySnapshot.dailyActiveUsers += 1;
-    usageMetricsHourlySnapshot.hourlyActiveUsers += 1;
-  }
-
-  usageMetricsDailySnapshot.blockNumber = block.number;
-  usageMetricsHourlySnapshot.blockNumber = block.number;
-
-  usageMetricsDailySnapshot.timestamp = block.timestamp;
-  usageMetricsHourlySnapshot.timestamp = block.timestamp;
-
-  usageMetricsDailySnapshot.save();
-  usageMetricsHourlySnapshot.save();
+  // Financials Daily
+  financialMetrics.cumulativeTotalRevenueUSD = pool.cumulativeTotalRevenueUSD;
+  financialMetrics.dailyTotalRevenueUSD =
+    poolMetricsDailySnapshot.dailyTotalRevenueUSD;
+  financialMetrics.save();
 }
 
-export function getOrCreateUsageMetricsDailySnapshot(
+export function updateProtocolSideRevenueMetrics(
+  block: ethereum.Block,
+  amountUSD: BigDecimal
+): void {
+  const pool = getOrCreatePool(block.number, block.timestamp);
+  const protocol = getOrCreateProtocol();
+  const financialMetrics = getOrCreateFinancialDailyMetrics(block);
+  const poolMetricsDailySnapshot = getOrCreatePoolsDailySnapshot(block);
+  const poolMetricsHourlySnapshot = getOrCreatePoolsHourlySnapshot(block);
+
+  // Pool
+  pool.cumulativeProtocolSideRevenueUSD = pool.cumulativeProtocolSideRevenueUSD.plus(
+    amountUSD
+  );
+  pool.save();
+
+  // Pool Daily
+  poolMetricsDailySnapshot.cumulativeProtocolSideRevenueUSD =
+    pool.cumulativeProtocolSideRevenueUSD;
+  poolMetricsDailySnapshot.dailyProtocolSideRevenueUSD = poolMetricsDailySnapshot.dailyProtocolSideRevenueUSD.plus(
+    amountUSD
+  );
+  poolMetricsDailySnapshot.save();
+
+  // Pool Hourly
+  poolMetricsHourlySnapshot.cumulativeProtocolSideRevenueUSD =
+    pool.cumulativeProtocolSideRevenueUSD;
+  poolMetricsHourlySnapshot.hourlyProtocolSideRevenueUSD = poolMetricsHourlySnapshot.hourlyProtocolSideRevenueUSD.plus(
+    amountUSD
+  );
+  poolMetricsHourlySnapshot.save();
+
+  // Protocol
+  protocol.cumulativeProtocolSideRevenueUSD =
+    pool.cumulativeProtocolSideRevenueUSD;
+  protocol.save();
+
+  // Financial Daily
+  financialMetrics.cumulativeProtocolSideRevenueUSD =
+    pool.cumulativeProtocolSideRevenueUSD;
+  financialMetrics.dailyProtocolSideRevenueUSD = financialMetrics.dailyProtocolSideRevenueUSD.plus(
+    amountUSD
+  );
+  financialMetrics.save();
+}
+
+export function updateSupplySideRevenueMetrics(block: ethereum.Block): void {
+  const pool = getOrCreatePool(block.number, block.timestamp);
+  const protocol = getOrCreateProtocol();
+  const financialMetrics = getOrCreateFinancialDailyMetrics(block);
+  const poolMetricsDailySnapshot = getOrCreatePoolsDailySnapshot(block);
+  const poolMetricsHourlySnapshot = getOrCreatePoolsHourlySnapshot(block);
+
+  // Pool
+  pool.cumulativeSupplySideRevenueUSD =
+    pool.cumulativeTotalRevenueUSD <= pool.cumulativeProtocolSideRevenueUSD
+      ? BIGDECIMAL_ZERO
+      : pool.cumulativeTotalRevenueUSD.minus(
+          pool.cumulativeProtocolSideRevenueUSD
+        );
+  pool.save();
+
+  // Pool Daily
+  poolMetricsDailySnapshot.cumulativeSupplySideRevenueUSD =
+    pool.cumulativeSupplySideRevenueUSD;
+  poolMetricsDailySnapshot.dailySupplySideRevenueUSD =
+    poolMetricsDailySnapshot.dailyTotalRevenueUSD <=
+    poolMetricsDailySnapshot.dailyProtocolSideRevenueUSD
+      ? BIGDECIMAL_ZERO
+      : poolMetricsDailySnapshot.dailyTotalRevenueUSD.minus(
+          poolMetricsDailySnapshot.dailyProtocolSideRevenueUSD
+        );
+  poolMetricsDailySnapshot.save();
+
+  // Pool Hourly
+  poolMetricsHourlySnapshot.cumulativeSupplySideRevenueUSD =
+    pool.cumulativeSupplySideRevenueUSD;
+  poolMetricsHourlySnapshot.hourlySupplySideRevenueUSD =
+    poolMetricsHourlySnapshot.hourlyTotalRevenueUSD <=
+    poolMetricsHourlySnapshot.hourlyProtocolSideRevenueUSD
+      ? BIGDECIMAL_ZERO
+      : poolMetricsHourlySnapshot.hourlyTotalRevenueUSD.minus(
+          poolMetricsHourlySnapshot.hourlyProtocolSideRevenueUSD
+        );
+  poolMetricsHourlySnapshot.save();
+
+  // Protocol
+  protocol.cumulativeSupplySideRevenueUSD = pool.cumulativeSupplySideRevenueUSD;
+  protocol.save();
+
+  // Financial Daily
+  financialMetrics.cumulativeSupplySideRevenueUSD =
+    pool.cumulativeSupplySideRevenueUSD;
+  financialMetrics.dailySupplySideRevenueUSD =
+    financialMetrics.dailyTotalRevenueUSD <=
+    financialMetrics.dailyProtocolSideRevenueUSD
+      ? BIGDECIMAL_ZERO
+      : financialMetrics.dailyTotalRevenueUSD.minus(
+          financialMetrics.dailyProtocolSideRevenueUSD
+        );
+  financialMetrics.save();
+}
+
+export function getOrCreateFinancialDailyMetrics(
   block: ethereum.Block
-): UsageMetricsDailySnapshot {
+): FinancialsDailySnapshot {
   const dayId: string = (block.timestamp.toI64() / SECONDS_PER_DAY).toString();
-  let usageMetrics = UsageMetricsDailySnapshot.load(dayId);
+  let financialMetrics = FinancialsDailySnapshot.load(dayId);
 
-  if (!usageMetrics) {
-    const protocol = getOrCreateProtocol();
-    usageMetrics = new UsageMetricsDailySnapshot(dayId);
+  if (!financialMetrics) {
+    financialMetrics = new FinancialsDailySnapshot(dayId);
+    financialMetrics.protocol = PROTOCOL_ID;
 
-    usageMetrics.protocol = protocol.id;
-    usageMetrics.totalPoolCount = protocol.totalPoolCount;
-
-    usageMetrics.dailyActiveUsers = 0;
-    usageMetrics.cumulativeUniqueUsers = 0;
-    usageMetrics.dailyTransactionCount = 0;
-
-    usageMetrics.blockNumber = block.number;
-    usageMetrics.timestamp = block.timestamp;
+    financialMetrics.totalValueLockedUSD = BIGDECIMAL_ZERO;
+    financialMetrics.dailyTotalRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.cumulativeTotalRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.dailyProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.cumulativeProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.dailySupplySideRevenueUSD = BIGDECIMAL_ZERO;
+    financialMetrics.cumulativeSupplySideRevenueUSD = BIGDECIMAL_ZERO;
   }
 
-  usageMetrics.save();
+  // Set block number and timestamp to the latest for snapshots
+  financialMetrics.blockNumber = block.number;
+  financialMetrics.timestamp = block.timestamp;
 
-  return usageMetrics;
+  financialMetrics.save();
+
+  return financialMetrics;
 }
 
-export function getOrCreateUsageMetricsHourlySnapshot(
+export function getOrCreatePoolsDailySnapshot(
   block: ethereum.Block
-): UsageMetricsHourlySnapshot {
-  const hourId: string = (block.timestamp.toI64() / SECONDS_PER_HOUR).toString();
-  let usageMetrics = UsageMetricsHourlySnapshot.load(hourId);
+): PoolDailySnapshot {
+  const dayId: string = (block.timestamp.toI64() / SECONDS_PER_DAY).toString();
+  let poolMetrics = PoolDailySnapshot.load(dayId);
 
-  if (!usageMetrics) {
-    const protocol = getOrCreateProtocol();
-    usageMetrics = new UsageMetricsHourlySnapshot(hourId);
+  if (!poolMetrics) {
+    poolMetrics = new PoolDailySnapshot(dayId);
+    poolMetrics.protocol = getOrCreateProtocol().id;
+    poolMetrics.pool = getOrCreatePool(block.number, block.timestamp).id;
 
-    usageMetrics.protocol = protocol.id;
+    poolMetrics.totalValueLockedUSD = BIGDECIMAL_ZERO;
+    poolMetrics.cumulativeTotalRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.cumulativeSupplySideRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.cumulativeProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.dailyTotalRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.dailySupplySideRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.dailyProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
 
-    usageMetrics.hourlyActiveUsers = 0;
-    usageMetrics.cumulativeUniqueUsers = 0;
-    usageMetrics.hourlyTransactionCount = 0;
-
-    usageMetrics.blockNumber = block.number;
-    usageMetrics.timestamp = block.timestamp;
+    poolMetrics.inputTokenBalances = [BIGINT_ZERO];
+    poolMetrics.outputTokenSupply = BIGINT_ZERO;
+    poolMetrics.outputTokenPriceUSD = BIGDECIMAL_ZERO;
   }
 
-  usageMetrics.save();
+  // Set block number and timestamp to the latest for snapshots
+  poolMetrics.blockNumber = block.number;
+  poolMetrics.timestamp = block.timestamp;
 
-  return usageMetrics;
+  poolMetrics.save();
+
+  return poolMetrics;
+}
+
+export function getOrCreatePoolsHourlySnapshot(
+  block: ethereum.Block
+): PoolHourlySnapshot {
+  const hourId: string = (
+    block.timestamp.toI64() / SECONDS_PER_HOUR
+  ).toString();
+  let poolMetrics = PoolHourlySnapshot.load(hourId);
+
+  if (!poolMetrics) {
+    poolMetrics = new PoolHourlySnapshot(hourId);
+    poolMetrics.protocol = getOrCreateProtocol().id;
+    poolMetrics.pool = getOrCreatePool(block.number, block.timestamp).id;
+
+    poolMetrics.totalValueLockedUSD = BIGDECIMAL_ZERO;
+    poolMetrics.cumulativeTotalRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.cumulativeSupplySideRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.cumulativeProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.hourlyTotalRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.hourlySupplySideRevenueUSD = BIGDECIMAL_ZERO;
+    poolMetrics.hourlyProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
+
+    poolMetrics.inputTokenBalances = [BIGINT_ZERO];
+    poolMetrics.outputTokenSupply = BIGINT_ZERO;
+    poolMetrics.outputTokenPriceUSD = BIGDECIMAL_ZERO;
+  }
+
+  // Set block number and timestamp to the latest for snapshots
+  poolMetrics.blockNumber = block.number;
+  poolMetrics.timestamp = block.timestamp;
+
+  poolMetrics.save();
+
+  return poolMetrics;
 }
