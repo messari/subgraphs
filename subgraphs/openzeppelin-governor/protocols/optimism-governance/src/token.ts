097f452f996487d44b6fbccd8a5e11a3c3027351
daniel <dev.danielkhoo@gmail.com>
2023-02-21 15:55:51 +0800

    Add block number to delegateVotingPwerChangeId

--- subgraphs/openzeppelin-governor/protocols/optimism-governance/src/token.ts
+++ subgraphs/openzeppelin-governor/protocols/optimism-governance/src/token.ts
@@ -1,11 +1,18 @@
+import { ethereum, BigInt } from "@graphprotocol/graph-ts";
 import {
   DelegateChanged,
   DelegateVotesChanged,
   Transfer,
 } from "../../../generated/GovernanceToken/GovernanceToken";
+import { DelegateVotingPowerChange } from "../../../generated/schema";
+import { BIGINT_ONE, BIGINT_ZERO } from "../../../src/constants";
+import {
+  getGovernance,
+  getOrCreateDelegate,
+  toDecimal,
+} from "../../../src/handlers";
 import {
   _handleDelegateChanged,
-  _handleDelegateVotesChanged,
   _handleTransfer,
 } from "../../../src/tokenHandlers";
 
@@ -22,12 +29,82 @@ export function handleDelegateChanged(event: DelegateChanged): void {
 // DelegateVotesChanged(indexed address,uint256,uint256)
 // Called in succession to the above DelegateChanged event
 export function handleDelegateVotesChanged(event: DelegateVotesChanged): void {
+  // NOTE: We are using a copy/paste of the common _handleDelegateVotesChanged function here
+  // because we want to override the delegateVotingPwerChangeId to include the block number.
+  // This is due to Optimism having multiple blocks with the same block timestamp
   _handleDelegateVotesChanged(
     event.params.delegate.toHexString(),
     event.params.previousBalance,
     event.params.newBalance,
     event
   );
+}
+
+// ================= Extracted from subgraphs/openzeppelin-governor/src/tokenHandlers.ts =================
+function _handleDelegateVotesChanged(
+  delegateAddress: string,
+  previousBalance: BigInt,
+  newBalance: BigInt,
+  event: ethereum.Event
+): void {
+  const votesDifference = newBalance.minus(previousBalance);
+
+  const delegate = getOrCreateDelegate(delegateAddress);
+  delegate.delegatedVotesRaw = newBalance;
+  delegate.delegatedVotes = toDecimal(newBalance);
+  delegate.save();
+
+  // Create DelegateVotingPowerChange
+  const delegateVPChange = createDelegateVotingPowerChange(
+    event,
+    previousBalance,
+    newBalance,
+    delegateAddress
+  );
+  delegateVPChange.save();
+
+  // Update governance delegate count
+  const governance = getGovernance();
+  if (previousBalance == BIGINT_ZERO && newBalance > BIGINT_ZERO) {
+    governance.currentDelegates = governance.currentDelegates.plus(BIGINT_ONE);
+  }
+  if (newBalance == BIGINT_ZERO) {
+    governance.currentDelegates = governance.currentDelegates.minus(BIGINT_ONE);
+  }
+  governance.delegatedVotesRaw =
+    governance.delegatedVotesRaw.plus(votesDifference);
+  governance.delegatedVotes = toDecimal(governance.delegatedVotesRaw);
+  governance.save();
+}
+
+// ================= Extracted from subgraphs/openzeppelin-governor/src/handlers.ts =================
+function createDelegateVotingPowerChange(
+  event: ethereum.Event,
+  previousBalance: BigInt,
+  newBalance: BigInt,
+  delegate: string
+): DelegateVotingPowerChange {
+  // THIS SINGLE LINE IS THE ONLY DIFFERENCE FROM THE ORIGINAL FUNCTION
+  // We are adding the block number to the id to avoid duplicate ids when there are multiple blocks
+  // with the same timestamp
+  const delegateVotingPwerChangeId = `${event.block.timestamp.toI64()}-${
+    event.block.number
+  }-${event.logIndex}`;
+
+  const delegateVPChange = new DelegateVotingPowerChange(
+    delegateVotingPwerChangeId
+  );
+
+  delegateVPChange.previousBalance = previousBalance;
+  delegateVPChange.newBalance = newBalance;
+  delegateVPChange.delegate = delegate;
+  delegateVPChange.tokenAddress = event.address.toHexString();
+  delegateVPChange.txnHash = event.transaction.hash.toHexString();
+  delegateVPChange.blockTimestamp = event.block.timestamp;
+  delegateVPChange.logIndex = event.logIndex;
+  delegateVPChange.blockNumber = event.block.number;
+
+  return delegateVPChange;
 }
 
 // // Transfer(indexed address,indexed address,uint256)
