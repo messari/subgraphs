9b276f0b9386ad37fcd8b21b59d1f7db655be3cf
dmelotik <dylanmelotik@gmail.com>
2023-02-21 19:03:23 -0700

    Add Spark lend georli deployment

---
+++ subgraphs/aave-forks/protocols/spark-lend/src/constants.ts
@@ -0,0 +1,50 @@
+import { Address, dataSource, log } from "@graphprotocol/graph-ts";
+import { Network, ZERO_ADDRESS } from "../../../src/constants";
+
+//////////////////////////////
+///// Ethereum Addresses /////
+//////////////////////////////
+
+export const USDC_TOKEN_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // ETH
+export const USDC_POS_TOKEN_ADDRESS =
+  "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"; // Polygon
+
+/////////////////////////////
+///// Protocol Specific /////
+/////////////////////////////
+
+export namespace Protocol {
+  export const NAME = "Spark Lend";
+  export const SLUG = "spark-lend";
+}
+export const AAVE_DECIMALS = 8;
+
+////////////////////////////
+///// Network Specific /////
+////////////////////////////
+
+export class NetworkSpecificConstant {
+  constructor(
+    public readonly protocolAddress: Address, // aka, LendingPoolAddressesProvider
+    public readonly network: string
+  ) {}
+}
+
+export function getNetworkSpecificConstant(): NetworkSpecificConstant {
+  const network = dataSource.network();
+  if (equalsIgnoreCase(network, "goerli")) {
+    return new NetworkSpecificConstant(
+      Address.fromString("0x5EEE318E5668d578b820d3C80152482E01fcE5cA"),
+      Network.MAINNET // TODO: should be goerli
+    );
+  } else {
+    log.error("[getNetworkSpecificConstant] Unsupported network: {}", [
+      network,
+    ]);
+    return new NetworkSpecificConstant(Address.fromString(ZERO_ADDRESS), "");
+  }
+}
+
+export function equalsIgnoreCase(a: string, b: string): boolean {
+  return a.replace("-", "_").toLowerCase() == b.replace("-", "_").toLowerCase();
+}
