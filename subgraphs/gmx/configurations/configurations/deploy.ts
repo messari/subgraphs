2a32be8b25efe8d9fabf76be541c044456a7eeb6
corerouter <corerouter@163.com>
2023-03-11 08:27:48 +0800

    feat(#major): GMX subgraph; add GMX subgraph (#1640)
    Co-authored-by: Ishraq Alim <47088402+ishraq8@users.noreply.github.com>

---
+++ subgraphs/gmx/configurations/configurations/deploy.ts
@@ -0,0 +1,7 @@
+// Add a numerical value associated with a deployment for each deployment.
+// The text associated with the integer should be included in the .json configuration file
+// for the protocol/network deployment in the `protocols` folder.
+export namespace Deploy {
+  export const GMX_ARBITRUM = 0;
+  export const GMX_AVALANCHE = 1;
+}
