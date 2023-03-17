4d8983421b600e12eb9659860cb5f280f931c080
Chris Steege <steegecas@gmail.com>
2023-03-17 17:29:04 -0500

    Add ticks to grab values from the contract when instantiated

--- subgraphs/uniswap-v3/src/common/entities/tick.ts
+++ subgraphs/uniswap-v3/src/common/entities/tick.ts
@@ -1,12 +1,8 @@
-import { BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { LiquidityPool, Tick } from "../../../generated/schema";
-import {
-  BIGDECIMAL_ONE,
-  BIGDECIMAL_ZERO,
-  BIGINT_ZERO,
-  INT_ZERO,
-} from "../constants";
+import { BIGDECIMAL_ONE, BIGINT_ZERO, INT_ZERO } from "../constants";
 import { bigDecimalExponated, safeDivBigDecimal } from "../utils/utils";
+import { Pool } from "../../../generated/Factory/Pool";
 
 export function getOrCreateTick(
   event: ethereum.Event,
@@ -17,6 +13,28 @@ export function getOrCreateTick(
   let tick = Tick.load(id);
 
   if (!tick) {
+    // Get tick info
+    const poolContract = Pool.bind(Address.fromBytes(pool.id));
+    const tick_info = poolContract.try_ticks(tickIndex.toI32());
+    let liquidityNet = BIGINT_ZERO;
+    let liquidityGross = BIGINT_ZERO;
+
+    if (!tick_info.reverted) {
+      liquidityNet = tick_info.value.value0;
+      liquidityGross = tick_info.value.value1;
+    }
+
+    // Update pool liquidity
+    pool.totalLiquidity = pool.totalLiquidity.plus(liquidityGross);
+    const liquidityPricePerUnit = safeDivBigDecimal(
+      pool.totalLiquidityUSD,
+      pool.totalLiquidity.toBigDecimal()
+    );
+    pool.totalLiquidityUSD = pool.totalLiquidity
+      .toBigDecimal()
+      .times(liquidityPricePerUnit);
+
+    // Create tick
     tick = new Tick(id);
     tick.index = tickIndex;
     tick.pool = pool.id;
@@ -27,11 +45,14 @@ export function getOrCreateTick(
       tick.index
     );
     tick.prices = [price0, safeDivBigDecimal(BIGDECIMAL_ONE, price0)];
-    tick.liquidityGross = BIGINT_ZERO;
-    tick.liquidityGrossUSD = BIGDECIMAL_ZERO;
-    tick.liquidityNet = BIGINT_ZERO;
-    tick.liquidityNetUSD = BIGDECIMAL_ZERO;
-
+    tick.liquidityGross = liquidityGross;
+    tick.liquidityGrossUSD = liquidityGross
+      .toBigDecimal()
+      .times(liquidityPricePerUnit);
+    tick.liquidityNet = liquidityNet;
+    tick.liquidityNetUSD = liquidityNet
+      .toBigDecimal()
+      .times(liquidityPricePerUnit);
     tick.lastSnapshotDayID = INT_ZERO;
     tick.lastSnapshotHourID = INT_ZERO;
     tick.lastUpdateBlockNumber = BIGINT_ZERO;
