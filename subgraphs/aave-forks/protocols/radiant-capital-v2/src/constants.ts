922069ca8e8a9dde05eadfd82465fb22f27cbeea
dmelotik <dylanmelotik@gmail.com>
2023-03-23 00:57:21 -0700

    update RDNT price by watching the uniswap v3 pool swap event

--- subgraphs/aave-forks/protocols/radiant-capital-v2/src/constants.ts
+++ subgraphs/aave-forks/protocols/radiant-capital-v2/src/constants.ts
@@ -1,4 +1,12 @@
-import { Network } from "../../../src/constants";
+import {
+  BIGDECIMAL_ZERO,
+  BIGINT_ONE,
+  BIGINT_TEN,
+  INT_ONE,
+  INT_ZERO,
+  Network,
+} from "../../../src/constants";
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 /////////////////////
 ///// Addresses /////
@@ -7,7 +15,7 @@ import { Network } from "../../../src/constants";
 export const REWARD_TOKEN_ADDRESS =
   "0x3082cc23568ea640225c2467653db90e9250aaa0"; // RDNT token
 export const RDNT_WETH_Uniswap_Pair =
-  "0x24704aff49645d32655a76df6d407e02d146dafc"; // RDNT/WETH TODO
+  "0xa8ba5f3ccfb8d2b7f4225e371cde11871e088933"; // RDNT/WETH TODO
 export const RWETH_ADDRESS = "0x0df5dfd95966753f01cb80e76dc20ea958238c46";
 
 /////////////////////////////
@@ -23,3 +31,24 @@ export namespace Protocol {
 
 // Number of decimals in which rToken oracle prices are returned.
 export const rTOKEN_DECIMALS = 8;
+export const PRECISION = BigInt.fromString("100000000000000000"); // Uniswap V3 precision decimals
+export const Q192 = BigInt.fromString(
+  "6277101735386680763835789423207666416102355444464034512896"
+); // Uniswap V3 Pool constant
+
+export function exponentToBigInt(decimals: i32): BigInt {
+  let bd = BIGINT_ONE;
+  for (let i = INT_ZERO; i < (decimals as i32); i = i + INT_ONE) {
+    bd = bd.times(BIGINT_TEN);
+  }
+  return bd;
+}
+
+// return 0 if denominator is 0 in division
+export function safeDiv(amount0: BigDecimal, amount1: BigDecimal): BigDecimal {
+  if (amount1.equals(BIGDECIMAL_ZERO)) {
+    return BIGDECIMAL_ZERO;
+  } else {
+    return amount0.div(amount1);
+  }
+}
