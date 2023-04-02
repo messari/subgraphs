92b54ea848367f5541074859a3472bbc9511be32
tnkrxyz <tnkrxyz@gmail.com>
2023-04-02 21:29:14 +0200

    no-string-literals

--- subgraphs/_eslint-rules/no-string-literals.js
+++ subgraphs/_eslint-rules/no-string-literals.js
@@ -1,16 +1,74 @@
-const { isImportDeclaration } = require("typescript");
-
 module.exports = {
+  meta: {
+    docs: {
+      description:
+        "string literals is only allowed in log argument or in UPPER_CASE variable in constants.ts or in logs",
+      category: "Stylistic Issues",
+      recommended: true,
+    },
+    fixable: null,
+    schema: [],
+    messages: {
+      forbidden:
+        "String literal must be in log argument or UPPER_CASE variable",
+    },
+  },
+
   create: function (context) {
-    return {
-      Literal(node) {
-        //const regex = /^[^']*$/;
-        //if (!regex.test(node.value)) {
-        if (!isImportDeclaration(node) && typeof node.value === "string") {
+    function checkLogCall(node, ancestors) {
+      const callExpressionAncestor = ancestors.find(
+        (ancestor) => ancestor.type === "CallExpression"
+      );
+
+      if (callExpressionAncestor) {
+        const callee = callExpressionAncestor.callee;
+        if (
+          callee.type === "MemberExpression" &&
+          (callee.object.name === "log" ||
+            ["fromString"].includes(callee.property.name) ||
+            ["concat", "toLowerCase"].includes(callee.property.name))
+        ) {
+          return false;
+        }
+
+        context.report({
+          node: node,
+          message: `'${node.value}' in '${
+            callee.name ? callee.name : callee.object.name
+          }()': String literal argument is only allowed in logs`,
+        });
+        return true;
+      }
+    }
+    function checkVariableDeclarator(node, ancestors) {
+      const variableDeclaratorAncestor = ancestors.find(
+        (ancestor) => ancestor.type === "VariableDeclarator"
+      );
+      if (variableDeclaratorAncestor) {
+        const variableName = variableDeclaratorAncestor.id.name;
+        // check UPPER_CASE variable name
+        const regex = /(\/([_A-Z]+[_]*[A-Z]+)*)*$/;
+        const fileName = context.getFilename().split("/").pop();
+        if (!regex.test(variableName) || fileName !== "constants.ts") {
           context.report({
-            node,
-            message: `${node.value}: String literals are not allowed`,
+            node: node,
+            message: `'${node.value}' in variable '${variableName}': String literal variables need to use UPPER_CASE in constants.ts`,
           });
+        }
+      }
+    }
+    return {
+      Literal: function (node) {
+        const regex = /^[^\w]+$/; // allow non-word string literals (e.g. "-", "_")
+        if (typeof node.value !== "string" || regex.test(node.value)) {
+          return;
+        }
+        const ancestors = context.getAncestors();
+        // if the string literal is a call argument, it must be log.error etc
+        if (!checkLogCall(node, ancestors)) {
+          // if the string literal is used in variable declaration, variable name
+          // must be in UPPER_CASE in constants.ts
+          checkVariableDeclarator(node, ancestors);
         }
       },
     };
