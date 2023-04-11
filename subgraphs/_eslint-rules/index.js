d5bbc62e1dc135f79cc4babc4f0615601d59489d
tnkrxyz <97625120+tnkrxyz@users.noreply.github.com>
2023-04-11 16:47:00 -0700

    chore (); additional eslint rules (#1955)

--- subgraphs/_eslint-rules/index.js
+++ subgraphs/_eslint-rules/index.js
@@ -1,6 +1,7 @@
-
 module.exports = {
-    rules: {
-        "no-checksum-addresses": require("./no-checksum-addresses"),
-    },
+  rules: {
+    "no-checksum-addresses": require("./no-checksum-addresses"),
+    "no-string-literals": require("./no-string-literals"),
+    "no-non-standard-filenames": require("./no-non-standard-filenames"),
+  },
 };
