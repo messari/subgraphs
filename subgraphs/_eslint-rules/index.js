79d065b5aede1f950b3b4c7e8011cc64b4a58967
Dylan Melotik <60583747+dmelotik@users.noreply.github.com>
2023-04-12 02:37:03 -0700

    Revert "chore (); additional eslint rules" (#2001)

--- subgraphs/_eslint-rules/index.js
+++ subgraphs/_eslint-rules/index.js
@@ -1,7 +1,6 @@
+
 module.exports = {
-  rules: {
-    "no-checksum-addresses": require("./no-checksum-addresses"),
-    "no-string-literals": require("./no-string-literals"),
-    "no-non-standard-filenames": require("./no-non-standard-filenames"),
-  },
+    rules: {
+        "no-checksum-addresses": require("./no-checksum-addresses"),
+    },
 };
