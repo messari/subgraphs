920715bd2492071ba5ef4988ad71ac1a1b60452f
Tarun Madiraju <tarunmadiraju@gmail.com>
2023-04-13 17:24:04 +0530

    track pool output token and its supply

--- subgraphs/across-v2/src/handlers/liquidity.ts
+++ subgraphs/across-v2/src/handlers/liquidity.ts
@@ -1,5 +1,6 @@
-import { Bytes } from "@graphprotocol/graph-ts";
+import { Address, Bytes, log } from "@graphprotocol/graph-ts";
 import {
+  HubPool,
   LiquidityAdded,
   LiquidityRemoved,
 } from "../../generated/HubPool/HubPool";
@@ -16,6 +17,7 @@ import {
   Pricer,
   TokenInit,
 } from "../util";
+// import { BIGINT_MINUS_ONE } from "../sdk/util/constants";
 
 const conf = new BridgeConfig(
   ACROSS_HUB_POOL_CONTRACT,
@@ -32,19 +34,39 @@ export function handleLiquidityAdded(event: LiquidityAdded): void {
     new TokenInit(),
     event
   );
+
+  // input token
   const token = sdk.Tokens.getOrCreateToken(event.params.l1Token);
+
+  // pool
   const poolId = event.address
     .toHexString()
     .concat(event.params.l1Token.toHexString());
-
   const pool = sdk.Pools.loadPool<string>(Bytes.fromUTF8(poolId));
   if (!pool.isInitialized) {
     pool.initialize(token.name, token.symbol, BridgePoolType.LIQUIDITY, token);
   }
 
+  // output token
+  const hubPoolContract = HubPool.bind(
+    Address.fromString(ACROSS_HUB_POOL_CONTRACT)
+  );
+  const hubPoolContractCall = hubPoolContract.try_pooledTokens(
+    event.params.l1Token
+  );
+  let outputTokenAddress: Address;
+  if (hubPoolContractCall.reverted) {
+    log.info("[HubPool:pooledTokens()] get LP Token call reverted", []);
+    outputTokenAddress = event.params.l1Token;
+  } else {
+    outputTokenAddress = hubPoolContractCall.value.getLpToken();
+  }
+  // const outputToken = sdk.Tokens.getOrCreateToken(outputTokenAddress);
+  // pool.addOutputTokenSupply(event.params.lpTokensMinted);
+
+  // account
   const amount = event.params.amount;
   const account = sdk.Accounts.loadAccount(event.params.liquidityProvider);
-
   account.liquidityDeposit(pool, amount);
 }
 
@@ -55,18 +77,38 @@ export function handleLiquidityRemoved(event: LiquidityRemoved): void {
     new TokenInit(),
     event
   );
+
+  // input token
   const token = sdk.Tokens.getOrCreateToken(event.params.l1Token);
+
+  // pool
   const poolId = event.address
     .toHexString()
     .concat(event.params.l1Token.toHexString());
-
   const pool = sdk.Pools.loadPool<string>(Bytes.fromUTF8(poolId));
   if (!pool.isInitialized) {
     pool.initialize(token.name, token.symbol, BridgePoolType.LIQUIDITY, token);
   }
 
+  // output token
+  const hubPoolContract = HubPool.bind(
+    Address.fromString(ACROSS_HUB_POOL_CONTRACT)
+  );
+  const hubPoolContractCall = hubPoolContract.try_pooledTokens(
+    event.params.l1Token
+  );
+  let outputTokenAddress: Address;
+  if (hubPoolContractCall.reverted) {
+    log.info("[HubPool:pooledTokens()] get LP Token call reverted", []);
+    outputTokenAddress = event.params.l1Token;
+  } else {
+    outputTokenAddress = hubPoolContractCall.value.getLpToken();
+  }
+  // const outputToken = sdk.Tokens.getOrCreateToken(outputTokenAddress);
+  // pool.addOutputTokenSupply(event.params.lpTokensBurnt.times(BIGINT_MINUS_ONE)); // TODO: verify does plus(minus_amount) work?
+
+  // account
   const amount = event.params.amount;
   const account = sdk.Accounts.loadAccount(event.params.liquidityProvider);
-
   account.liquidityWithdraw(pool, amount);
 }
