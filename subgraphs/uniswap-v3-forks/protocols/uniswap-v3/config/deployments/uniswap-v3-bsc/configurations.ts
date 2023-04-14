75aa0e594b18c307b660cf798b3e51dead2177de
Chris Steege <56660047+steegecs@users.noreply.github.com>
2023-04-14 10:31:08 -0500

    feat(); Uniswap V3 BSC; Add Uniswap V3 BSC (#2010)

---
+++ subgraphs/uniswap-v3-forks/protocols/uniswap-v3/config/deployments/uniswap-v3-bsc/configurations.ts
@@ -0,0 +1,92 @@
+import {
+  Address,
+  BigDecimal,
+  BigInt,
+  Bytes,
+  log,
+} from "@graphprotocol/graph-ts";
+import { Factory } from "../../../../../generated/Factory/Factory";
+import {
+  BIGDECIMAL_ONE,
+  BIGDECIMAL_ZERO,
+  FeeSwitch,
+  Network,
+  RewardIntervalType,
+} from "../../../../../src/common/constants";
+import { Configurations } from "../../../../../configurations/configurations/interface";
+import { PROTOCOL_NAME, PROTOCOL_SLUG } from "../../../src/common/constants";
+import { stringToBytesList } from "../../../../../src/common/utils/utils";
+
+export class UniswapV3BSCConfigurations implements Configurations {
+  getNetwork(): string {
+    return Network.BSC;
+  }
+  getProtocolName(): string {
+    return PROTOCOL_NAME;
+  }
+  getProtocolSlug(): string {
+    return PROTOCOL_SLUG;
+  }
+  getFactoryAddress(): Bytes {
+    return Bytes.fromHexString("0xdb1d10011ad0ff90774d0c6bb92e5c5c8b4461f7");
+  }
+  getFactoryContract(): Factory {
+    return Factory.bind(
+      Address.fromString("0xdb1d10011ad0ff90774d0c6bb92e5c5c8b4461f7")
+    );
+  }
+  getProtocolFeeOnOff(): string {
+    return FeeSwitch.OFF;
+  }
+  getInitialProtocolFeeProportion(fee: i64): BigDecimal {
+    log.warning("getProtocolFeeRatio is not implemented: {}", [fee.toString()]);
+    return BIGDECIMAL_ZERO;
+  }
+  getProtocolFeeProportion(protocolFee: BigInt): BigDecimal {
+    return BIGDECIMAL_ONE.div(protocolFee.toBigDecimal());
+  }
+  getRewardIntervalType(): string {
+    return RewardIntervalType.NONE;
+  }
+  getReferenceToken(): Bytes {
+    return Bytes.fromHexString("0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"); // wbnb
+  }
+  getRewardToken(): Bytes {
+    return Bytes.fromHexString("");
+  }
+  getWhitelistTokens(): Bytes[] {
+    return stringToBytesList([
+      "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", // wBNB
+      "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // wETH
+      "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
+      "0x55d398326f99059ff775485246999027b3197955", // USDT
+      "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+      "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+      "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+    ]);
+  }
+  getStableCoins(): Bytes[] {
+    return stringToBytesList([
+      "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+      "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+      "0x55d398326f99059ff775485246999027b3197955", // USDT
+      "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
+    ]);
+  }
+  getStableOraclePools(): Bytes[] {
+    return stringToBytesList([
+      "0x32776ed4d96ed069a2d812773f0ad8ad9ef83cf8", // wBNB/BUSD - 0.30
+      "0x6fe9e9de56356f7edbfcbb29fab7cd69471a4869", // wBNB/USDT - 0.05
+      "0x7862d9b4be2156b15d54f41ee4ede2d5b0b455e4", // wBNB/USDT - 0.30
+    ]);
+  }
+  getUntrackedPairs(): Bytes[] {
+    return stringToBytesList([]);
+  }
+  getUntrackedTokens(): Bytes[] {
+    return stringToBytesList([]);
+  }
+  getMinimumLiquidityThreshold(): BigDecimal {
+    return BigDecimal.fromString("25000");
+  }
+}
