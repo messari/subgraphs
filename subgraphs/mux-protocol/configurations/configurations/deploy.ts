ede2bda58e82ebbab026184654a66433c5fc5200
Founder <corerouter@163.com>
2023-05-18 19:29:45 +0800

    MUX protocol subgraph

---
+++ subgraphs/mux-protocol/configurations/configurations/deploy.ts
@@ -0,0 +1,10 @@
+// Add a numerical value associated with a deployment for each deployment.
+// The text associated with the integer should be included in the .json configuration file
+// for the protocol/network deployment in the `protocols` folder.
+export namespace Deploy {
+  export const MUX_ARBITRUM = 0;
+  export const MUX_AVALANCHE = 1;
+  export const MUX_BSC = 2;
+  export const MUX_FANTOM = 3;
+  export const MUX_OPTIMISM = 4;
+}
---
+++ subgraphs/mux-protocol/src/prices/common/constants.ts
@@ -0,0 +1,38 @@
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+
+///////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// COMMON ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+export namespace NULL {
+  export const TYPE_STRING = "0x0000000000000000000000000000000000000000";
+  export const TYPE_ADDRESS = Address.fromString(TYPE_STRING);
+}
+
+export namespace OracleType {
+  export const AAVE_ORACLE = "AaveOracle";
+  export const CURVE_ROUTER = "CurveRouter";
+  export const CHAINLINK_FEED = "ChainlinkFeed";
+  export const YEARN_LENS_ORACLE = "YearnLensOracle";
+  export const CURVE_CALCULATIONS = "CurveCalculations";
+  export const UNISWAP_FORKS_ROUTER = "UniswapForksRouter";
+  export const SUSHI_CALCULATIONS = "SushiswapCalculations";
+}
+
+export const CHAIN_LINK_USD_ADDRESS = Address.fromString(
+  "0x0000000000000000000000000000000000000348"
+);
+
+export const PRICE_LIB_VERSION = "1.2.0";
+
+export const BIGINT_ZERO = BigInt.fromI32(0);
+export const BIGINT_ONE = BigInt.fromI32(1);
+export const BIGINT_TEN = BigInt.fromI32(10);
+export const BIGINT_TEN_THOUSAND = BigInt.fromI32(10000);
+
+export const BIGDECIMAL_ZERO = new BigDecimal(BIGINT_ZERO);
+export const BIGDECIMAL_USD_PRICE = BigDecimal.fromString("1000000");
+
+export const AAVE_ORACLE_DECIMALS = 8;
+export const DEFAULT_USDC_DECIMALS = 6;
+export const DEFAULT_DECIMALS = BigInt.fromI32(18);
