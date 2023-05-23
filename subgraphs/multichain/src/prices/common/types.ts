9065ce44ad55e435f06263f65b88d14e505d4df1
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2023-05-24 00:31:30 +0530

    refactor(#major); multichain; add deployment networks, only use eventHandlers (#1705)

--- subgraphs/multichain/src/prices/common/types.ts
+++ subgraphs/multichain/src/prices/common/types.ts
@@ -1,5 +1,5 @@
 import * as constants from "./constants";
-import { BigDecimal } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 export class Wrapped<T> {
   inner: T;
@@ -9,23 +9,48 @@ export class Wrapped<T> {
   }
 }
 
+export class OracleContract {
+  private _contractAddress: string;
+  private _contractStartBlock: i32;
+
+  constructor(
+    contractAddress: string = constants.NULL.TYPE_STRING,
+    startBlock: i32 = -1
+  ) {
+    this._contractAddress = contractAddress;
+    this._contractStartBlock = startBlock;
+  }
+
+  get address(): Address {
+    return Address.fromString(this._contractAddress);
+  }
+
+  get startBlock(): BigInt {
+    return BigInt.fromI32(this._contractStartBlock);
+  }
+}
+
 export class CustomPriceType {
   // `null` indicates a reverted call.
   private _usdPrice: Wrapped<BigDecimal>;
   private _decimals: Wrapped<i32>;
+  private _oracleType: string;
 
   constructor() {
     this._usdPrice = new Wrapped(constants.BIGDECIMAL_ZERO);
     this._decimals = new Wrapped(constants.BIGINT_ZERO.toI32() as u8);
+    this._oracleType = "";
   }
 
   static initialize(
     _usdPrice: BigDecimal,
-    _decimals: i32 = 0
+    _decimals: i32 = 0,
+    _oracleType: string = ""
   ): CustomPriceType {
     const result = new CustomPriceType();
     result._usdPrice = new Wrapped(_usdPrice);
     result._decimals = new Wrapped(_decimals as u8);
+    result._oracleType = _oracleType;
 
     return result;
   }
@@ -35,14 +60,44 @@ export class CustomPriceType {
   }
 
   get usdPrice(): BigDecimal {
-    return changetype<Wrapped<BigDecimal>>(this._usdPrice).inner;
+    return changetype<Wrapped<BigDecimal>>(this._usdPrice).inner.div(
+      constants.BIGINT_TEN.pow(this.decimals as u8).toBigDecimal()
+    );
   }
 
   get decimals(): i32 {
     return changetype<Wrapped<i32>>(this._decimals).inner;
   }
 
-  get decimalsBaseTen(): BigDecimal {
-    return constants.BIGINT_TEN.pow(this.decimals as u8).toBigDecimal();
+  get oracleType(): string {
+    return this._oracleType;
   }
+}
+
+export interface Configurations {
+  network(): string;
+
+  yearnLens(): OracleContract;
+  chainLink(): OracleContract;
+  yearnLensBlacklist(): Address[];
+
+  aaveOracle(): OracleContract;
+  aaveOracleBlacklist(): Address[];
+
+  curveCalculations(): OracleContract;
+  curveCalculationsBlacklist(): Address[];
+
+  sushiCalculations(): OracleContract;
+  sushiCalculationsBlacklist(): Address[];
+
+  uniswapForks(): OracleContract[];
+  curveRegistry(): OracleContract[];
+
+  hardcodedStables(): Address[];
+
+  ethAddress(): Address;
+  wethAddress(): Address;
+  usdcAddress(): Address;
+
+  usdcTokenDecimals(): BigInt;
 }
