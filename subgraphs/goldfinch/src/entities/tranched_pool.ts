4fc1d678066e4a1ade88fcb8d910c4857bd0b72e
Dylan Melotik <60583747+dmelotik@users.noreply.github.com>
2023-06-03 12:37:49 -0700

    fixing goldfinch subgraph error #2115

--- subgraphs/goldfinch/src/entities/tranched_pool.ts
+++ subgraphs/goldfinch/src/entities/tranched_pool.ts
@@ -4,7 +4,7 @@ import {
   JuniorTrancheInfo,
   SeniorTrancheInfo,
   CreditLine,
-  TranchedPoolToken,
+  PoolToken,
 } from "../../generated/schema";
 import {
   TranchedPool as TranchedPoolContract,
@@ -22,7 +22,6 @@ import {
 } from "../common/constants";
 import { getOrInitUser } from "./user";
 import { getOrInitCreditLine, initOrUpdateCreditLine } from "./credit_line";
-import { getOrInitSeniorPoolStatus } from "./senior_pool";
 import {
   getTotalDeposited,
   isV1StyleDeal,
@@ -42,6 +41,7 @@ import {
 } from "../common/utils";
 import { getBackerRewards } from "./backer_rewards";
 import { BackerRewards as BackerRewardsContract } from "../../generated/BackerRewards/BackerRewards";
+import { getOrInitSeniorPoolStatus } from "./senior_pool";
 
 export function updatePoolCreditLine(
   address: Address,
@@ -601,7 +601,7 @@ export function updatePoolRewardsClaimable(
   );
   const poolTokenIds = tranchedPool.tokens;
   for (let i = 0; i < poolTokenIds.length; i++) {
-    const poolToken = assert(TranchedPoolToken.load(poolTokenIds[i]));
+    const poolToken = assert(PoolToken.load(poolTokenIds[i]));
     poolToken.rewardsClaimable =
       backerRewardsContract.poolTokenClaimableRewards(
         BigInt.fromString(poolToken.id)
@@ -623,7 +623,7 @@ export function updatePoolTokensRedeemable(tranchedPool: TranchedPool): void {
   );
   const poolTokenIds = tranchedPool.tokens;
   for (let i = 0; i < poolTokenIds.length; i++) {
-    const poolToken = assert(TranchedPoolToken.load(poolTokenIds[i]));
+    const poolToken = assert(PoolToken.load(poolTokenIds[i]));
     const availableToWithdrawResult =
       tranchedPoolContract.try_availableToWithdraw(
         BigInt.fromString(poolToken.id)
