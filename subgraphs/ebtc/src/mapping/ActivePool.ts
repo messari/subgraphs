2d4f0423a3580ba48b4a625c819021838568f254
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-06-28 15:02:02 +0200

    feat: add `ActivePool` data source

---
+++ subgraphs/ebtc/src/mapping/ActivePool.ts
@@ -0,0 +1,27 @@
+import {
+  ActivePoolCollBalanceUpdated,
+  ActivePoolEBTCDebtUpdated,
+} from "../../generated/ActivePool/ActivePool";
+//   import { setMarketCollBalance, setMarketEBTCDebt } from "../entities/market";
+
+//   /**
+//    * Total stETH collateral was updated
+//    *
+//    * @param event ActivePoolCollBalanceUpdated event
+//    */
+//   export function handleActivePoolCollBalanceUpdated(
+//     event: ActivePoolCollBalanceUpdated
+//   ): void {
+//     setMarketCollBalance(event, event.params._coll);
+//   }
+
+//   /**
+//    * EBTC debt was updated
+//    *
+//    * @param event ActivePoolEBTCDebtUpdated event
+//    */
+//   export function handleActivePoolEBTCDebtUpdated(
+//     event: ActivePoolEBTCDebtUpdated
+//   ): void {
+//     setMarketEBTCDebt(event, event.params._EBTCDebt);
+//   }
