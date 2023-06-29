a3746b356f4704f10567d09fd952dd250ab09b63
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-06-29 14:30:19 +0200

    fix: only comment actual lines in the functions

--- subgraphs/ebtc/src/mapping/ActivePool.ts
+++ subgraphs/ebtc/src/mapping/ActivePool.ts
@@ -2,26 +2,26 @@ import {
   ActivePoolCollBalanceUpdated,
   ActivePoolEBTCDebtUpdated,
 } from "../../generated/ActivePool/ActivePool";
-//   import { setMarketCollBalance, setMarketEBTCDebt } from "../entities/market";
+// import { setMarketCollBalance, setMarketEBTCDebt } from "../entities/market";
 
-//   /**
-//    * Total stETH collateral was updated
-//    *
-//    * @param event ActivePoolCollBalanceUpdated event
-//    */
-//   export function handleActivePoolCollBalanceUpdated(
-//     event: ActivePoolCollBalanceUpdated
-//   ): void {
-//     setMarketCollBalance(event, event.params._coll);
-//   }
+/**
+ * Total stETH collateral was updated
+ *
+ * @param event ActivePoolCollBalanceUpdated event
+ */
+export function handleActivePoolCollBalanceUpdated(
+  event: ActivePoolCollBalanceUpdated
+): void {
+  // setMarketCollBalance(event, event.params._coll);
+}
 
-//   /**
-//    * EBTC debt was updated
-//    *
-//    * @param event ActivePoolEBTCDebtUpdated event
-//    */
-//   export function handleActivePoolEBTCDebtUpdated(
-//     event: ActivePoolEBTCDebtUpdated
-//   ): void {
-//     setMarketEBTCDebt(event, event.params._EBTCDebt);
-//   }
+/**
+ * EBTC debt was updated
+ *
+ * @param event ActivePoolEBTCDebtUpdated event
+ */
+export function handleActivePoolEBTCDebtUpdated(
+  event: ActivePoolEBTCDebtUpdated
+): void {
+  // setMarketEBTCDebt(event, event.params._EBTCDebt);
+}
