a6e35495dc02d1a63e4009340b0dbb7088671ea4
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-07-07 17:50:46 +0200

    chore: include `BorrowerOperations` in template

---
+++ subgraphs/ebtc/src/mapping/BorrowerOperations.ts
@@ -0,0 +1,7 @@
+import { CdpCreated } from "../../generated/BorrowerOperations/BorrowerOperations";
+
+/**
+ * Create a new position when a CDP is created.
+ * @param event The event emitted by BorrowerOperations when a CDP is created.
+ */
+export function handleCdpCreated(event: CdpCreated): void {}
