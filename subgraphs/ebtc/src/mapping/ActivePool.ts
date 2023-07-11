32a75e7d2cf06a40dc46a953d65fadd09b188fcd
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-07-11 13:00:50 +0200

    feat: handle flash loans

--- subgraphs/ebtc/src/mapping/ActivePool.ts
+++ subgraphs/ebtc/src/mapping/ActivePool.ts
@@ -1,9 +1,13 @@
+import { Address } from "@graphprotocol/graph-ts";
 import {
   ActivePoolCollBalanceUpdated,
   ActivePoolEBTCDebtUpdated,
+  FlashLoanSuccess,
 } from "../../generated/ActivePool/ActivePool";
 import { getDataManager, STETH_ADDRESS } from "../constants";
 import { TokenManager } from "../sdk/token";
+import { getUsdPrice } from "../prices";
+import { BIGINT_TEN_TO_EIGHTEENTH } from "../sdk/util/constants";
 
 /**
  * Total stETH collateral was updated
@@ -35,4 +39,24 @@ export function handleActivePoolEBTCDebtUpdated(
     market.inputTokenBalance, // newInputTokenBalance: BigInt
     event.params._EBTCDebt // newVariableBorrowBalance: BigInt | null = null
   );
+}
+
+/**
+ * Create a flashloan object and handle its fee when a flashloan is successful.
+ * @param event FlashLoanSuccess The event emitted by BorrowerOperations when
+ * a flashloan is successful.
+ */
+export function handleFlashLoanSuccess(event: FlashLoanSuccess): void {
+  const dataManager = getDataManager(event);
+  const flashloan = dataManager.createFlashloan(
+    Address.fromBytes(STETH_ADDRESS), // asset: Address
+    event.params._receiver, // account: Address
+    event.params._amount, // amount: BigInt
+    getUsdPrice(
+      Address.fromBytes(STETH_ADDRESS),
+      event.params._amount.div(BIGINT_TEN_TO_EIGHTEENTH).toBigDecimal(),
+      event.block
+    ) // amountUSD: BigDecimal
+  );
+  // TODO: handle fee (event.params._fee)
 }
