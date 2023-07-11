3bccffb4d1a1eeef9815cb53cd7f34e6df8f4825
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-07-11 14:50:16 +0200

    fix: functions need to be exported explicitly

--- subgraphs/ebtc/src/mapping/BorrowerOperations.ts
+++ subgraphs/ebtc/src/mapping/BorrowerOperations.ts
@@ -1,5 +1,6 @@
 import { Address } from "@graphprotocol/graph-ts";
 import { CdpUpdated } from "../../generated/BorrowerOperations/BorrowerOperations";
+import { FlashLoanSuccess } from "../../generated/ActivePool/ActivePool";
 import { getUsdPrice } from "../prices";
 import { BIGINT_TEN_TO_EIGHTEENTH, BIGINT_ZERO } from "../sdk/util/constants";
 import { getDataManager, STETH_ADDRESS } from "../constants";
@@ -78,4 +79,26 @@ export function handleCdpUpdated(event: CdpUpdated): void {
   //     senderNewBalance: BigInt
   //     receiverNewBalance: BigInt
   // )
+}
+
+/**
+ * Create a flashloan object and handle its fee when a flashloan is successful.
+ * @param event FlashLoanSuccess The event emitted by BorrowerOperations when
+ * a flashloan is successful.
+ */
+export function handleFlashLoanSuccess(event: FlashLoanSuccess): void {
+  // TODO: this is a dupe of the same function in ActivePool.ts
+  // TODO: how to export imported functions?
+  const dataManager = getDataManager(event);
+  const flashloan = dataManager.createFlashloan(
+    Address.fromBytes(STETH_ADDRESS), // asset: Address
+    event.params._receiver, // account: Address
+    event.params._amount, // amount: BigInt
+    getUsdPrice(
+      Address.fromBytes(STETH_ADDRESS),
+      event.params._amount.div(BIGINT_TEN_TO_EIGHTEENTH).toBigDecimal(),
+      event.block
+    ) // amountUSD: BigDecimal
+  );
+  // TODO: handle fee (event.params._fee)
 }
