1b9be2f764464035b8f5fc69f1247efdc88fb0e3
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-07-14 20:14:22 +0200

    feat: handle liquidations

--- subgraphs/ebtc/src/mapping/CdpManager.ts
+++ subgraphs/ebtc/src/mapping/CdpManager.ts
@@ -1,46 +1,43 @@
-import { CdpUpdated } from "../../generated/CdpManager/CdpManager";
-import { AccountManager } from "../sdk/account";
-import { PositionManager } from "../sdk/position";
-import { TokenManager } from "../sdk/token";
-import { PositionSide, TransactionType } from "../sdk/util/constants";
-import { getProtocolData, getDataManager, STETH_ADDRESS } from "../constants";
+import { CdpUpdated, Liquidation } from "../../generated/CdpManager/CdpManager";
+import { BIGINT_ZERO } from "../sdk/util/constants";
+import { getDataManager, STETH_ADDRESS } from "../constants";
+import { Address } from "@graphprotocol/graph-ts";
+import { getUsdPrice } from "../prices";
 
 /**
  * Make necessary adjustments to the system when a CDP changes.
  * @param event The event emitted by the CdpManager when a CDP changes.
  */
 export function handleCdpUpdated(event: CdpUpdated): void {
+  // TODO
+}
+
+/**
+ * Register a liquidation event.
+ * @param event Liquidation The event emitted by the CdpManager when a CDP is
+ * liquidated.
+ */
+export function handleLiquidation(event: Liquidation): void {
+  // TODO: event is missing the following data points:
+  // - liquidator
+  // - liquidatee
+  // - newBalance
   const dataManager = getDataManager(event);
-  const accountManager = new AccountManager(event.params._borrower);
-  const positionManager = new PositionManager(
-    accountManager.getAccount(), // account: Account
-    dataManager.getMarket(), // market: Market
-    PositionSide.COLLATERAL //side
-  );
-  const stEthTokenManager = new TokenManager(STETH_ADDRESS, event);
-
-  // TODO: there is also a subtractPosition method
-  positionManager.addPosition(
-    event, // event: ethereum.Event
+  dataManager.createLiquidate(
     STETH_ADDRESS, // asset: Bytes
-    dataManager.getOrCreateLendingProtocol(getProtocolData()), // protocol: LendingProtocol
-    event.params._coll, // newBalance: BigInt
-    // TODO: obivously the transaction type needs to be determined properly
-    TransactionType.BORROW, // transactionType: string
-    stEthTokenManager.getPriceUSD() // priceUSD: BigDecimal
+    new Address(0), // liquidator: Address
+    new Address(0), // liquidatee: Address
+    event.params._liquidatedColl, // amount: BigInt
+    getUsdPrice(
+      Address.fromBytes(STETH_ADDRESS),
+      event.params._liquidatedColl.toBigDecimal(),
+      event.block
+    ), // amountUSD: BigDecimal
+    getUsdPrice(
+      Address.fromBytes(STETH_ADDRESS),
+      event.params._liqReward.toBigDecimal(),
+      event.block
+    ), // profitUSD: BigDecimal
+    BIGINT_ZERO // newBalance: BigInt // repaid token balance for liquidatee
   );
-  if (positionManager.getPositionID()) {
-    positionManager.setCollateral(true);
-    positionManager.setIsolation(true);
-  }
-
-  // createLiquidate(
-  //     asset: Bytes
-  //     liquidator: Address
-  //     liquidatee: Address
-  //     amount: BigInt
-  //     amountUSD: BigDecimal
-  //     profitUSD: BigDecimal
-  //     newBalance: BigInt // repaid token balance for liquidatee
-  // )
 }
