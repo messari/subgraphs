a7c09efc00f4976dc2118f1ef8fcc87784664c5a
Arjun Nemani <nemaniarjun@gmail.com>
2023-07-26 05:00:48 +0530

    try with less sources to speed up syncing

--- subgraphs/polygon-bridge/src/prices/common/types.ts
+++ subgraphs/polygon-bridge/src/prices/common/types.ts
@@ -96,14 +96,14 @@ export class OracleType {
   oracleOrder: string[];
 
   constructor() {
-    this.oracleCount = constants.INT_THREE;
+    this.oracleCount = constants.INT_ONE;
     this.oracleOrder = [
-      constants.OracleType.YEARN_LENS_ORACLE,
       constants.OracleType.CHAINLINK_FEED,
-      constants.OracleType.CURVE_CALCULATIONS,
-      constants.OracleType.SUSHI_CALCULATIONS,
-      constants.OracleType.CURVE_ROUTER,
       constants.OracleType.UNISWAP_FORKS_ROUTER,
+      constants.OracleType.SUSHI_CALCULATIONS,
+      constants.OracleType.YEARN_LENS_ORACLE,
+      constants.OracleType.CURVE_ROUTER,
+      constants.OracleType.CURVE_CALCULATIONS,
     ];
   }
 
