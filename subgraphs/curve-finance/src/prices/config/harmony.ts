7dc63f31d4f3fe350658ba688d90f9f31bdc6951
Harsh Agarwal <hagarwal9200@gmail.com>
2023-08-01 00:09:35 +0530

    fix(#minor); Curve Finance; Address Curve Sidechains QA  (#1664)

--- subgraphs/curve-finance/src/prices/config/harmony.ts
+++ subgraphs/curve-finance/src/prices/config/harmony.ts
@@ -1,6 +1,7 @@
-import * as constants from "../common/constants";
-import { Configurations } from "../common/types";
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
+import { Address, BigInt, TypedMap } from "@graphprotocol/graph-ts";
+import { Configurations, ContractInfo, TokenInfo } from "../common/types";
 
 export const NETWORK_STRING = "harmony";
 
@@ -8,29 +9,31 @@ export const NETWORK_STRING = "harmony";
 ///////////////////// CALCULATIONS/ORACLE CONTRACT ////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
-export const YEARN_LENS_CONTRACT_ADDRESS = constants.NULL.TYPE_ADDRESS;
-export const CHAIN_LINK_CONTRACT_ADDRESS = constants.NULL.TYPE_ADDRESS;
-export const SUSHISWAP_CALCULATIONS_ADDRESS = constants.NULL.TYPE_ADDRESS;
-export const AAVE_ORACLE_CONTRACT_ADDRESS = Address.fromString(
-  "0x3c90887ede8d65ccb2777a5d577beab2548280ad"
+export const AAVE_ORACLE_CONTRACT_ADDRESS = ContractInfo.set(
+  Address.fromString("0x3c90887ede8d65ccb2777a5d577beab2548280ad"),
+  BigInt.fromI32(23930344)
 );
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////// CURVE CONTRACT //////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
-export const CURVE_CALCULATIONS_ADDRESS = constants.NULL.TYPE_ADDRESS;
-
-export const CURVE_REGISTRY_ADDRESSES: Address[] = [
-  Address.fromString("0x0a53FaDa2d943057C47A301D25a4D9b3B8e01e8E"),
+export const CURVE_REGISTRY_ADDRESSES = [
+  ContractInfo.set(
+    Address.fromString("0x0a53fada2d943057c47a301d25a4d9b3b8e01e8e"),
+    BigInt.fromI32(18003250)
+  ),
 ];
 
 ///////////////////////////////////////////////////////////////////////////
 /////////////////////////// UNISWAP FORKS CONTRACT ////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
-export const UNISWAP_FORKS_ROUTER_ADDRESSES: Address[] = [
-  Address.fromString("0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"), // SushiSwap
+export const UNISWAP_FORKS_ROUTER_ADDRESSES = [
+  ContractInfo.set(
+    Address.fromString("0x1b02da8cb0d097eb8d57a175b88c7d8b47997506"), // SushiSwap
+    BigInt.fromI32(11256069)
+  ),
 ];
 
 ///////////////////////////////////////////////////////////////////////////
@@ -38,6 +41,7 @@ export const UNISWAP_FORKS_ROUTER_ADDRESSES: Address[] = [
 ///////////////////////////////////////////////////////////////////////////
 
 export const YEARN_LENS_BLACKLIST: Address[] = [];
+export const INCH_ORACLE_BLACKLIST: Address[] = [];
 export const AAVE_ORACLE_BLACKLIST: Address[] = [];
 export const CURVE_CALCULATIONS_BLACKSLIST: Address[] = [];
 export const SUSHI_CALCULATIONS_BLACKSLIST: Address[] = [];
@@ -52,54 +56,97 @@ export const HARDCODED_STABLES: Address[] = [];
 ///////////////////////////////// HELPERS /////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
-export const USDC_TOKEN_DECIMALS = BigInt.fromI32(6);
+export const WHITELISTED_TOKENS = new TypedMap<string, TokenInfo>();
 
-export const ETH_ADDRESS = Address.fromString(
-  "0x6983D1E6DEf3690C4d616b13597A09e6193EA013"
+WHITELISTED_TOKENS.set(
+  "USDC",
+  TokenInfo.set(
+    "USDC",
+    6,
+    Address.fromString("0x985458e523db3d53125813ed68c274899e9dfab4")
+  )
 );
-export const WETH_ADDRESS = Address.fromString(
-  "0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a"
+
+WHITELISTED_TOKENS.set(
+  "USDT",
+  TokenInfo.set(
+    "USDT",
+    6,
+    Address.fromString("0x3c2b8be99c50593081eaa2a724f0b8285f5aba8f")
+  )
 );
-export const USDC_ADDRESS = Address.fromString(
-  "0x985458E523dB3d53125813eD68c274899e9DfAb4"
+
+WHITELISTED_TOKENS.set(
+  "DAI",
+  TokenInfo.set(
+    "DAI",
+    18,
+    Address.fromString("0xef977d2f931c1978db5f6747666fa1eacb0d0339")
+  )
+);
+
+WHITELISTED_TOKENS.set(
+  "WETH",
+  TokenInfo.set(
+    "WETH",
+    18,
+    Address.fromString("0x6983d1e6def3690c4d616b13597a09e6193ea013")
+  )
+);
+
+WHITELISTED_TOKENS.set(
+  "NATIVE_TOKEN",
+  TokenInfo.set(
+    "WONE",
+    18,
+    Address.fromString("0xcf664087a5bb0237a0bad6742852ec6c8d69a27a")
+  )
 );
 
 export class config implements Configurations {
-  yearnLens(): Address {
-    return YEARN_LENS_CONTRACT_ADDRESS;
-  }
-  chainLink(): Address {
-    return CHAIN_LINK_CONTRACT_ADDRESS;
+  yearnLens(): ContractInfo | null {
+    return null;
   }
   yearnLensBlacklist(): Address[] {
     return YEARN_LENS_BLACKLIST;
   }
 
-  aaveOracle(): Address {
+  inchOracle(): ContractInfo | null {
+    return null;
+  }
+  inchOracleBlacklist(): Address[] {
+    return INCH_ORACLE_BLACKLIST;
+  }
+
+  chainLink(): ContractInfo | null {
+    return null;
+  }
+
+  aaveOracle(): ContractInfo | null {
     return AAVE_ORACLE_CONTRACT_ADDRESS;
   }
   aaveOracleBlacklist(): Address[] {
     return AAVE_ORACLE_BLACKLIST;
   }
 
-  curveCalculations(): Address {
-    return CURVE_CALCULATIONS_ADDRESS;
+  curveCalculations(): ContractInfo | null {
+    return null;
   }
   curveCalculationsBlacklist(): Address[] {
     return CURVE_CALCULATIONS_BLACKSLIST;
   }
 
-  sushiCalculations(): Address {
-    return SUSHISWAP_CALCULATIONS_ADDRESS;
+  sushiCalculations(): ContractInfo | null {
+    return null;
   }
   sushiCalculationsBlacklist(): Address[] {
     return SUSHI_CALCULATIONS_BLACKSLIST;
   }
 
-  uniswapForks(): Address[] {
+  uniswapForks(): ContractInfo[] {
     return UNISWAP_FORKS_ROUTER_ADDRESSES;
   }
-  curveRegistry(): Address[] {
+  curveRegistry(): ContractInfo[] {
     return CURVE_REGISTRY_ADDRESSES;
   }
 
@@ -107,17 +154,7 @@ export class config implements Configurations {
     return HARDCODED_STABLES;
   }
 
-  ethAddress(): Address {
-    return ETH_ADDRESS;
-  }
-  wethAddress(): Address {
-    return WETH_ADDRESS;
-  }
-  usdcAddress(): Address {
-    return USDC_ADDRESS;
-  }
-
-  usdcTokenDecimals(): BigInt {
-    return USDC_TOKEN_DECIMALS;
+  whitelistedTokens(): TypedMap<string, TokenInfo> {
+    return WHITELISTED_TOKENS;
   }
 }
--- subgraphs/curve-finance/src/prices/routers/UniswapForksRouter.ts
+++ subgraphs/curve-finance/src/prices/routers/UniswapForksRouter.ts
@@ -1,7 +1,7 @@
 import * as utils from "../common/utils";
 import * as constants from "../common/constants";
 import { CustomPriceType } from "../common/types";
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt, ethereum, log } from "@graphprotocol/graph-ts";
 import { UniswapPair as UniswapPairContract } from "../../../generated/templates/PoolTemplate/UniswapPair";
 import { UniswapRouter as UniswapRouterContract } from "../../../generated/templates/PoolTemplate/UniswapRouter";
 
@@ -19,49 +19,52 @@ export function isLpToken(tokenAddress: Address, ethAddress: Address): bool {
   return true;
 }
 
-export function getTokenPriceUSDC(tokenAddress: Address): CustomPriceType {
+export function getTokenPriceUSDC(
+  tokenAddress: Address,
+  block: ethereum.Block | null = null
+): CustomPriceType {
   const config = utils.getConfig();
   if (!config) return new CustomPriceType();
 
-  const ethAddress = config.ethAddress();
-  const usdcAddress = config.usdcAddress();
+  const ethAddress = config.whitelistedTokens().mustGet("WETH").address;
+  const usdcAddress = config.whitelistedTokens().mustGet("USDC").address;
 
   if (isLpToken(tokenAddress, ethAddress)) {
-    return getLpTokenPriceUsdc(tokenAddress);
+    return getLpTokenPriceUsdc(tokenAddress, block);
   }
-  return getPriceFromRouterUSDC(tokenAddress, usdcAddress);
+  return getPriceFromRouterUSDC(tokenAddress, usdcAddress, block);
 }
 
 export function getPriceFromRouterUSDC(
   tokenAddress: Address,
-  usdcAddress: Address
+  usdcAddress: Address,
+  block: ethereum.Block | null = null
 ): CustomPriceType {
-  return getPriceFromRouter(tokenAddress, usdcAddress);
+  return getPriceFromRouter(tokenAddress, usdcAddress, block);
 }
 
 export function getPriceFromRouter(
   token0Address: Address,
-  token1Address: Address
+  token1Address: Address,
+  block: ethereum.Block | null = null
 ): CustomPriceType {
   const config = utils.getConfig();
 
-  const ethAddress = config.ethAddress();
-  const wethAddress = config.wethAddress();
+  const wethAddress = config.whitelistedTokens().mustGet("WETH").address;
+  const nativeTokenAddress = config
+    .whitelistedTokens()
+    .mustGet("NATIVE_TOKEN").address;
 
   // Construct swap path
   const path: Address[] = [];
   let numberOfJumps: BigInt;
 
   // Convert ETH address to WETH
-  if (token0Address == ethAddress) {
-    token0Address = wethAddress;
-  }
-  if (token1Address == ethAddress) {
-    token1Address = wethAddress;
-  }
+  if (token0Address == nativeTokenAddress) token0Address = wethAddress;
+  if (token1Address == nativeTokenAddress) token1Address = wethAddress;
 
   const inputTokenIsWeth: bool =
-    token0Address == wethAddress || token1Address == wethAddress;
+    token0Address.equals(wethAddress) || token1Address.equals(wethAddress);
 
   if (inputTokenIsWeth) {
     // Path = [token0, weth] or [weth, token1]
@@ -79,15 +82,16 @@ export function getPriceFromRouter(
   }
 
   const token0Decimals = utils.getTokenDecimals(token0Address);
-  const amountIn = BigInt.fromI32(10).pow(token0Decimals.toI32() as u8);
+  const amountIn = constants.BIGINT_TEN.pow(token0Decimals.toI32() as u8);
 
   const routerAddresses = config.uniswapForks();
 
   let amountOut = constants.BIGINT_ZERO;
   for (let idx = 0; idx < routerAddresses.length; idx++) {
-    const routerAddress = routerAddresses.at(idx);
+    const routerAddress = routerAddresses[idx];
+    if (block && routerAddress.startBlock.gt(block.number)) continue;
 
-    const uniswapForkRouter = UniswapRouterContract.bind(routerAddress);
+    const uniswapForkRouter = UniswapRouterContract.bind(routerAddress.address);
     const amountOutArray = uniswapForkRouter.try_getAmountsOut(amountIn, path);
 
     if (!amountOutArray.reverted) {
@@ -96,6 +100,7 @@ export function getPriceFromRouter(
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   const feeBips = BigInt.fromI32(30);
 
   const amountOutBigDecimal = amountOut
@@ -105,15 +110,21 @@ export function getPriceFromRouter(
 
   return CustomPriceType.initialize(
     amountOutBigDecimal,
-    constants.DEFAULT_USDC_DECIMALS
+    config.whitelistedTokens().mustGet("USDC").decimals as u8,
+    constants.OracleType.UNISWAP_FORKS_ROUTER
   );
 }
 
-export function getLpTokenPriceUsdc(tokenAddress: Address): CustomPriceType {
+export function getLpTokenPriceUsdc(
+  tokenAddress: Address,
+  block: ethereum.Block | null = null
+): CustomPriceType {
   const uniSwapPair = UniswapPairContract.bind(tokenAddress);
 
-  const totalLiquidity: CustomPriceType =
-    getLpTokenTotalLiquidityUsdc(tokenAddress);
+  const totalLiquidity: CustomPriceType = getLpTokenTotalLiquidityUsdc(
+    tokenAddress,
+    block
+  );
   const totalSupply = utils.readValue<BigInt>(
     uniSwapPair.try_totalSupply(),
     constants.BIGINT_ZERO
@@ -126,23 +137,30 @@ export function getLpTokenPriceUsdc(tokenAddress: Address): CustomPriceType {
   const pairDecimalsCall = uniSwapPair.try_decimals();
 
   if (pairDecimalsCall.reverted) {
-    pairDecimals = constants.DEFAULT_DECIMALS.toI32() as u8;
+    log.warning(
+      "[UniswapForksRouter] Failed to fetch pair decimals, tokenAddress: {}",
+      [tokenAddress.toHexString()]
+    );
+
+    return new CustomPriceType();
   } else {
     pairDecimals = pairDecimalsCall.value;
   }
 
   const pricePerLpTokenUsdc = totalLiquidity.usdPrice
-    .times(constants.BIGINT_TEN.pow(pairDecimals as u8).toBigDecimal())
+    .times(utils.exponentToBigDecimal(pairDecimals as u8))
     .div(totalSupply.toBigDecimal());
 
   return CustomPriceType.initialize(
     pricePerLpTokenUsdc,
-    constants.DEFAULT_USDC_DECIMALS
+    constants.DEFAULT_USDC_DECIMALS,
+    constants.OracleType.UNISWAP_FORKS_ROUTER
   );
 }
 
 export function getLpTokenTotalLiquidityUsdc(
-  tokenAddress: Address
+  tokenAddress: Address,
+  block: ethereum.Block | null = null
 ): CustomPriceType {
   const uniSwapPair = UniswapPairContract.bind(tokenAddress);
 
@@ -169,8 +187,8 @@ export function getLpTokenTotalLiquidityUsdc(
 
   if (reservesCall.reverted) return new CustomPriceType();
 
-  const token0Price = getTokenPriceUSDC(token0Address);
-  const token1Price = getTokenPriceUSDC(token1Address);
+  const token0Price = getTokenPriceUSDC(token0Address, block);
+  const token1Price = getTokenPriceUSDC(token1Address, block);
 
   if (token0Price.reverted || token1Price.reverted) {
     return new CustomPriceType();
@@ -185,20 +203,19 @@ export function getLpTokenTotalLiquidityUsdc(
     reserve1.notEqual(constants.BIGINT_ZERO)
   ) {
     const liquidity0 = reserve0
-      .div(constants.BIGINT_TEN.pow(token0Decimals.toI32() as u8))
-      .toBigDecimal()
+      .divDecimal(utils.exponentToBigDecimal(token0Decimals.toI32() as u8))
       .times(token0Price.usdPrice);
 
     const liquidity1 = reserve1
-      .div(constants.BIGINT_TEN.pow(token1Decimals.toI32() as u8))
-      .toBigDecimal()
+      .divDecimal(utils.exponentToBigDecimal(token1Decimals.toI32() as u8))
       .times(token1Price.usdPrice);
 
     const totalLiquidity = liquidity0.plus(liquidity1);
 
     return CustomPriceType.initialize(
       totalLiquidity,
-      constants.DEFAULT_USDC_DECIMALS
+      constants.DEFAULT_USDC_DECIMALS,
+      constants.OracleType.UNISWAP_FORKS_ROUTER
     );
   }
   return new CustomPriceType();
