45d1db2e9153754d8de88e4caaf39b250fd206a8
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2023-08-08 18:49:36 +0530

    cleanup

--- subgraphs/rocket-pool/src/constants/contractConstants.ts
+++ subgraphs/rocket-pool/src/constants/contractConstants.ts
@@ -31,175 +31,39 @@ export namespace RocketContractNames {
 
 // https://docs.rocketpool.net/developers/usage/contracts/contracts.html#interacting-with-rocket-pool
 export const KeyToContractName = new TypedMap<ByteArray, string>();
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_VAULT)
-    )
-  ),
-  RocketContractNames.ROCKET_VAULT
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_TOKEN_RETH)
-    )
-  ),
-  RocketContractNames.ROCKET_TOKEN_RETH
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_NETWORK_BALANCES)
-    )
-  ),
-  RocketContractNames.ROCKET_NETWORK_BALANCES
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_NETWORK_PRICES)
-    )
-  ),
-  RocketContractNames.ROCKET_NETWORK_PRICES
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_NODE_MANAGER)
-    )
-  ),
-  RocketContractNames.ROCKET_NODE_MANAGER
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_NODE_STAKING)
-    )
-  ),
-  RocketContractNames.ROCKET_NODE_STAKING
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_REWARDS_POOL)
-    )
-  ),
-  RocketContractNames.ROCKET_REWARDS_POOL
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_MINIPOOL_MANAGER)
-    )
-  ),
-  RocketContractNames.ROCKET_MINIPOOL_MANAGER
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_MINIPOOL_QUEUE)
-    )
-  ),
-  RocketContractNames.ROCKET_MINIPOOL_QUEUE
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_MINIPOOL_DELEGATE)
-    )
-  ),
-  RocketContractNames.ROCKET_MINIPOOL_DELEGATE
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(
-        RocketContractNames.ROCKET_DAO_NODE_TRUSTED_ACTIONS
-      )
-    )
-  ),
-  RocketContractNames.ROCKET_DAO_NODE_TRUSTED_ACTIONS
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_DEPOSIT_POOL)
-    )
-  ),
-  RocketContractNames.ROCKET_DEPOSIT_POOL
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_NODE_DEPOSIT)
-    )
-  ),
-  RocketContractNames.ROCKET_NODE_DEPOSIT
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_CLAIM_DAO)
-    )
-  ),
-  RocketContractNames.ROCKET_CLAIM_DAO
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_CLAIM_NODE)
-    )
-  ),
-  RocketContractNames.ROCKET_CLAIM_NODE
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_CLAIM_TRUSTED_NODE)
-    )
-  ),
-  RocketContractNames.ROCKET_CLAIM_TRUSTED_NODE
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_DAO_NODE_TRUSTED)
-    )
-  ),
-  RocketContractNames.ROCKET_DAO_NODE_TRUSTED
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_NETWORK_FEES)
-    )
-  ),
-  RocketContractNames.ROCKET_NETWORK_FEES
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(
-        RocketContractNames.ROCKET_DAO_SETTINGS_MINIPOOL
-      )
-    )
-  ),
-  RocketContractNames.ROCKET_DAO_SETTINGS_MINIPOOL
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_DAO_SETTINGS_NODE)
-    )
-  ),
-  RocketContractNames.ROCKET_DAO_SETTINGS_NODE
-);
-KeyToContractName.set(
-  crypto.keccak256(
-    ByteArray.fromUTF8(
-      "contract.address".concat(RocketContractNames.ROCKET_AUCTION_MANAGER)
-    )
-  ),
-  RocketContractNames.ROCKET_AUCTION_MANAGER
-);
+
+function setKeyToContractName(): void {
+  const contractNames = [
+    RocketContractNames.ROCKET_VAULT,
+    RocketContractNames.ROCKET_TOKEN_RETH,
+    RocketContractNames.ROCKET_NETWORK_BALANCES,
+    RocketContractNames.ROCKET_NETWORK_PRICES,
+    RocketContractNames.ROCKET_NODE_MANAGER,
+    RocketContractNames.ROCKET_NODE_STAKING,
+    RocketContractNames.ROCKET_REWARDS_POOL,
+    RocketContractNames.ROCKET_MINIPOOL_MANAGER,
+    RocketContractNames.ROCKET_MINIPOOL_QUEUE,
+    RocketContractNames.ROCKET_MINIPOOL_DELEGATE,
+    RocketContractNames.ROCKET_DAO_NODE_TRUSTED_ACTIONS,
+    RocketContractNames.ROCKET_DEPOSIT_POOL,
+    RocketContractNames.ROCKET_NODE_DEPOSIT,
+    RocketContractNames.ROCKET_CLAIM_DAO,
+    RocketContractNames.ROCKET_CLAIM_NODE,
+    RocketContractNames.ROCKET_CLAIM_TRUSTED_NODE,
+    RocketContractNames.ROCKET_DAO_NODE_TRUSTED,
+    RocketContractNames.ROCKET_NETWORK_FEES,
+    RocketContractNames.ROCKET_DAO_SETTINGS_MINIPOOL,
+    RocketContractNames.ROCKET_DAO_SETTINGS_NODE,
+    RocketContractNames.ROCKET_AUCTION_MANAGER,
+  ];
+
+  for (let i = 0; i < contractNames.length; i++) {
+    KeyToContractName.set(
+      crypto.keccak256(
+        ByteArray.fromUTF8("contract.address".concat(contractNames[i]))
+      ),
+      contractNames[i]
+    );
+  }
+}
+setKeyToContractName();
