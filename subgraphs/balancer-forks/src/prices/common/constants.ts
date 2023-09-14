2bf1e604accfe8098b88bd9bb20673214ad87bce
Harsh <hagarwal9200@gmail.com>
2023-09-14 18:21:58 +0530

    blacklist bancor token

--- subgraphs/balancer-forks/src/prices/common/constants.ts
+++ subgraphs/balancer-forks/src/prices/common/constants.ts
@@ -59,4 +59,5 @@ export const BLACKLISTED_TOKENS: Address[] = [
   Address.fromString("0xf72beacc6fd334e14a7ddac25c3ce1eb8a827e10"), // Curve.fi Factory USD Metapool: Defrost H2O
   Address.fromString("0xae6aab43c4f3e0cea4ab83752c278f8debaba689"), // dForce
   Address.fromString("0x10010078a54396f62c96df8532dc2b4847d47ed3"), // Hundred Finance
+  Address.fromString("0x48fb253446873234f2febbf9bdeaa72d9d387f94"), // Bancor Governance Token (vBNT)
 ];
