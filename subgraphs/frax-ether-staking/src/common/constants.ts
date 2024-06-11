0153de04c9913add49274da07964b51735b80cb4
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2024-06-11 21:09:21 +0530

    feat(#major); frax-ether-staking; add subgraph (#2562)

---
+++ subgraphs/frax-ether-staking/src/common/constants.ts
@@ -0,0 +1,6 @@
+/////////////////////////////
+///// Protocol Specific /////
+/////////////////////////////
+
+export const PROTOCOL_NAME = "Frax Ether Staking";
+export const PROTOCOL_SLUG = "frax-ether-staking";
