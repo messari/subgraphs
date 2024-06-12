47cef858a5db2671baa5e49bf1ad78fe345913c0
harsh9200 <hagarwal9200@gmail.com>
2024-06-13 02:26:46 +0530

    update revenue logic

--- subgraphs/stader/src/mappings/vaultFactoryMappings.ts
+++
@@ -1,8 +0,0 @@
-import { WithdrawVaultCreated } from "../../generated/VaultFactory/VaultFactory";
-import { WithdrawVault as WithdrawVaultTemplate } from "../../generated/templates";
-
-export function handleWithdrawVaultCreated(event: WithdrawVaultCreated): void {
-  const newWithdrawVault = event.params.withdrawVault;
-
-  WithdrawVaultTemplate.create(newWithdrawVault);
-}
