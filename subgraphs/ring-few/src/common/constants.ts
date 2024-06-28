3a7bebae950eb5bbed2d97720d2adfd16ec8ad37
harsh9200 <hagarwal9200@gmail.com>
2024-06-28 22:13:19 +0530

    Blacklist pricing outlier pools

--- subgraphs/ring-few/src/common/constants.ts
+++ subgraphs/ring-few/src/common/constants.ts
@@ -1,4 +1,4 @@
-import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 //////////////////////////////
 /////// Protocol Config //////
@@ -17,6 +17,10 @@ export namespace Protocol {
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const ETH_ADDRESS = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 export const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+
+export const BLACKLISTED_POOLS_LIST: Address[] = [
+  Address.fromString("0x35b1cd11b684c7a9e1aa8d34e6b2a500310ec35c"),
+];
 
 ////////////////////////
 ///// Type Helpers /////
--- subgraphs/ring-few/src/mappings/fewFactoryMappings.ts
+++ subgraphs/ring-few/src/mappings/fewFactoryMappings.ts
@@ -1,8 +1,14 @@
+import * as constants from "../common/constants";
 import { FewWrappedToken } from "../../generated/templates";
 import { WrappedTokenCreated } from "../../generated/FewFactory/FewFactory";
 
 export function handleWrappedTokenCreated(event: WrappedTokenCreated): void {
   const wrappedTokenAddress = event.params.wrappedToken;
+
+  if (constants.BLACKLISTED_POOLS_LIST.includes(wrappedTokenAddress)) {
+    // Skip pools with pricing outliers caused by low liquidity
+    return;
+  }
 
   FewWrappedToken.create(wrappedTokenAddress);
 }
