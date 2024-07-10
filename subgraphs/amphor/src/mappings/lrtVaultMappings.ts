3a4e79ecd9ffcd61e6824e09418a707312bf682e
harsh9200 <hagarwal9200@gmail.com>
2024-07-11 00:46:17 +0530

    Add subgraph

---
+++ subgraphs/amphor/src/mappings/lrtVaultMappings.ts
@@ -0,0 +1,54 @@
+import {
+  updatePoolTvlForLRTVaults,
+  updatePoolOutputTokenSupply,
+} from "../common/utils";
+import {
+  getOrCreatePool,
+  initializeSDKFromEvent,
+} from "../common/initializers";
+import {
+  Deposit,
+  Withdraw,
+  EpochEnd,
+} from "../../generated/LRTVault/AmphorSyntheticVault";
+import * as constants from "../common/constants";
+
+export function handleDeposit(event: Deposit): void {
+  const sdk = initializeSDKFromEvent(event);
+  const pool = getOrCreatePool(event.address, sdk);
+
+  updatePoolOutputTokenSupply(pool);
+  updatePoolTvlForLRTVaults(pool);
+
+  const account = sdk.Accounts.loadAccount(event.transaction.from);
+  account.trackActivity();
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  const sdk = initializeSDKFromEvent(event);
+  const pool = getOrCreatePool(event.address, sdk);
+
+  updatePoolOutputTokenSupply(pool);
+  updatePoolTvlForLRTVaults(pool);
+
+  const account = sdk.Accounts.loadAccount(event.transaction.from);
+  account.trackActivity();
+}
+
+export function handleEpochEnd(event: EpochEnd): void {
+  const fees = event.params.fees;
+  const returnedAssets = event.params.returnedAssets;
+  const lastSavedBalance = event.params.lastSavedBalance;
+
+  const sdk = initializeSDKFromEvent(event);
+  const pool = getOrCreatePool(event.address, sdk);
+
+  const supplySideRevenue = returnedAssets.minus(lastSavedBalance);
+  const protocolSideRevenue = fees;
+
+  pool.addRevenueNative(
+    pool.getInputToken(constants.INT_ZERO),
+    supplySideRevenue,
+    protocolSideRevenue
+  );
+}
