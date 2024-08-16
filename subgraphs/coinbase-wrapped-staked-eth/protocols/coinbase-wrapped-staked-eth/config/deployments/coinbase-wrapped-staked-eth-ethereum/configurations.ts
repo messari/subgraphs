3c032b8e3918dc8176c75e4a40f528fc707d0ad9
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2024-08-16 22:44:33 +0530

    add subgraph

---
+++ subgraphs/coinbase-wrapped-staked-eth/protocols/coinbase-wrapped-staked-eth/config/deployments/coinbase-wrapped-staked-eth-ethereum/configurations.ts
@@ -0,0 +1,23 @@
+import { Address } from "@graphprotocol/graph-ts";
+
+import { Configurations } from "../../../../../configurations/configurations/interface";
+import {
+  PROTOCOL_NAME,
+  PROTOCOL_SLUG,
+} from "../../../../../src/common/constants";
+import { Network } from "../../../../../src/sdk/util/constants";
+
+export class CoinbaseWrappedStakedEthMainnetConfigurations implements Configurations {
+  getNetwork(): string {
+    return Network.MAINNET;
+  }
+  getProtocolName(): string {
+    return PROTOCOL_NAME;
+  }
+  getProtocolSlug(): string {
+    return PROTOCOL_SLUG;
+  }
+  getLSTAddress(): Address {
+    return Address.fromString("0xbe9895146f7af43049ca1c1ae358b0541ea49704");
+  }
+}
