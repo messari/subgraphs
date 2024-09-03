3e683d1aa29d319978367505f9015ae7689c0eed
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2024-09-03 16:04:54 +0530

    feat(#major); stake-link-liquid; add subgraph (#2616)

---
+++ subgraphs/stake-link-liquid/src/common/constants.ts
@@ -0,0 +1,6 @@
+//////////////////////////////
+///// Protocol Constants /////
+//////////////////////////////
+
+export const PROTOCOL_NAME = "Stake Link Liquid";
+export const PROTOCOL_SLUG = "stake-link-liquid";
