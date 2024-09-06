4c1730d13f4643466ffa61b30e5014793c2f8cb6
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2024-09-06 11:32:52 +0530

    add subgraph

---
+++ subgraphs/gaurda-staking/src/common/constants.ts
@@ -0,0 +1,6 @@
+//////////////////////////////
+///// Protocol Constants /////
+//////////////////////////////
+
+export const PROTOCOL_NAME = "Gaurda Staking";
+export const PROTOCOL_SLUG = "gaurda-staking";
---
+++ subgraphs/gaurda-staking/src/mappings/handlers.ts
@@ -0,0 +1,125 @@
+import { Address, BigDecimal, BigInt, log } from "@graphprotocol/graph-ts";
+
+import { Versions } from "../versions";
+import { NetworkConfigs } from "../../configurations/configure";
+import { getUsdPrice, getUsdPricePerToken } from "../prices";
+
+import { SDK } from "../sdk/protocols/generic";
+import { ProtocolConfig, TokenPricer } from "../sdk/protocols/config";
+import { TokenInitializer, TokenParams } from "../sdk/protocols/generic/tokens";
+import { bigIntToBigDecimal } from "../sdk/util/numbers";
+import {
+  BIGDECIMAL_ZERO,
+  ETH_ADDRESS,
+  INT_ZERO,
+  ZERO_ADDRESS,
+} from "../sdk/util/constants";
+
+import { Transfer, GETH } from "../../generated/GETH/GETH";
+import { _ERC20 } from "../../generated/GETH/_ERC20";
+import { Token } from "../../generated/schema";
+import { getUpdatedPricedToken } from "./helpers";
+
+const conf = new ProtocolConfig(
+  NetworkConfigs.getProtocolId(),
+  NetworkConfigs.getProtocolName(),
+  NetworkConfigs.getProtocolSlug(),
+  Versions
+);
+
+class Pricer implements TokenPricer {
+  getTokenPrice(token: Token): BigDecimal {
+    let returnedPrice = BIGDECIMAL_ZERO;
+
+    const pricedToken = getUpdatedPricedToken(Address.fromBytes(token.id));
+    const pricedTokenAddr = pricedToken.addr;
+    const pricedTokenMultiplier = pricedToken.multiplier;
+    const pricedTokenChanged = pricedToken.changed;
+
+    returnedPrice = getUsdPricePerToken(pricedTokenAddr).usdPrice.times(
+      pricedTokenMultiplier
+    );
+    if (pricedTokenChanged) {
+      log.debug(
+        "[getTokenPrice] inputToken: {} pricedToken: {} multiplier: {} returnedPrice: {}",
+        [
+          token.id.toHexString(),
+          pricedTokenAddr.toHexString(),
+          pricedTokenMultiplier.toString(),
+          returnedPrice.toString(),
+        ]
+      );
+    }
+    return returnedPrice;
+  }
+
+  getAmountValueUSD(token: Token, amount: BigInt): BigDecimal {
+    let returnedPrice = BIGDECIMAL_ZERO;
+    const _amount = bigIntToBigDecimal(amount, token.decimals);
+
+    const pricedToken = getUpdatedPricedToken(Address.fromBytes(token.id));
+    const pricedTokenAddr = pricedToken.addr;
+    const pricedTokenMultiplier = pricedToken.multiplier;
+    const pricedTokenChanged = pricedToken.changed;
+
+    returnedPrice = getUsdPrice(pricedTokenAddr, _amount).times(
+      pricedTokenMultiplier
+    );
+    if (pricedTokenChanged) {
+      log.debug(
+        "[getAmountValueUSD] inputToken: {} pricedToken: {} multiplier: {} amount: {} returnedPrice: {}",
+        [
+          token.id.toHexString(),
+          pricedTokenAddr.toHexString(),
+          pricedTokenMultiplier.toString(),
+          _amount.toString(),
+          returnedPrice.toString(),
+        ]
+      );
+    }
+    return returnedPrice;
+  }
+}
+
+class TokenInit implements TokenInitializer {
+  getTokenParams(address: Address): TokenParams {
+    let name = "unknown";
+    let symbol = "UNKNOWN";
+    let decimals = INT_ZERO as i32;
+
+    if (address == Address.fromString(ETH_ADDRESS)) {
+      name = "eth";
+      symbol = "ETH";
+      decimals = 18 as i32;
+    } else {
+      const erc20 = _ERC20.bind(address);
+      name = erc20.name();
+      symbol = erc20.symbol();
+      decimals = erc20.decimals().toI32();
+    }
+    return new TokenParams(name, symbol, decimals);
+  }
+}
+
+export function handleTransfer(event: Transfer): void {
+  const sdk = SDK.initializeFromEvent(
+    conf,
+    new Pricer(),
+    new TokenInit(),
+    event
+  );
+  const token = sdk.Tokens.getOrCreateToken(Address.fromString(ETH_ADDRESS));
+
+  const pool = sdk.Pools.loadPool(event.address);
+  if (!pool.isInitialized) {
+    pool.initialize("Guarded Ether", "gETH", [token.id], null);
+  }
+
+  const gETH = GETH.bind(event.address);
+  const supply = gETH.totalSupply();
+  pool.setInputTokenBalances([supply], true);
+
+  const user = event.transaction.from;
+  const account = sdk.Accounts.loadAccount(user);
+  account.trackActivity();
+}
