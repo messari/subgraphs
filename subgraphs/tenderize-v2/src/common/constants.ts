5807346c138389cdac549137b4b0aa337716eabd
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2024-09-12 19:15:25 +0530

    feat(#major); tenderize-v2; add subgraph (#2621)

---
+++ subgraphs/tenderize-v2/src/common/constants.ts
@@ -0,0 +1,6 @@
+//////////////////////////////
+///// Protocol Constants /////
+//////////////////////////////
+
+export const PROTOCOL_NAME = "Tenderize V2";
+export const PROTOCOL_SLUG = "tenderize-v2";
