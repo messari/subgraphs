bd9d636b430b26d1be9ec15eff81bd6c07cc6973
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2024-10-29 19:31:50 +0530

    add subgraph

---
+++ subgraphs/mev-protocol/src/common/constants.ts
@@ -0,0 +1,6 @@
+//////////////////////////////
+///// Protocol Constants /////
+//////////////////////////////
+
+export const PROTOCOL_NAME = "MEV Protocol";
+export const PROTOCOL_SLUG = "mev-protocol";
