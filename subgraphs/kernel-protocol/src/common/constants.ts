657880d298b31eb6b4bb76441e914a0970350b87
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2024-12-01 23:49:34 +0530

    add subgraph

---
+++ subgraphs/kernel-protocol/src/common/constants.ts
@@ -0,0 +1,6 @@
+//////////////////////////////
+///// Protocol Constants /////
+//////////////////////////////
+
+export const PROTOCOL_NAME = "Kernel Protocol";
+export const PROTOCOL_SLUG = "kernel-protocol";
