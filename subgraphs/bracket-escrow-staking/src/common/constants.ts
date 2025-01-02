d5f6e0d522da34f074ebcece1b5a79ba4891aedf
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2025-01-02 22:29:08 +0530

    feat(#major); bracket escrow staking; add subgraph (#2667)

---
+++ subgraphs/bracket-escrow-staking/src/common/constants.ts
@@ -0,0 +1,6 @@
+//////////////////////////////
+///// Protocol Constants /////
+//////////////////////////////
+
+export const PROTOCOL_NAME = "Bracket Escrow Staking";
+export const PROTOCOL_SLUG = "bracket-escrow-staking";
