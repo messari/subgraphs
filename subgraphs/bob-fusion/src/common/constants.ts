6a0e6241964a96c729b4045927f1d6b31aeb5eaa
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2025-02-07 02:03:37 +0530

    feat(#major); bob-fusion; add subgraph (#2673)
    Co-authored-by: Chris Steege <56660047+steegecs@users.noreply.github.com>

---
+++ subgraphs/bob-fusion/src/common/constants.ts
@@ -0,0 +1,6 @@
+//////////////////////////////
+///// Protocol Constants /////
+//////////////////////////////
+
+export const PROTOCOL_NAME = "Bob Fusion";
+export const PROTOCOL_SLUG = "bob-fusion";
